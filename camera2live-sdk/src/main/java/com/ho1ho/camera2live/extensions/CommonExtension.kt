d4366db61f430d4359bb909cd415358919521f7e
Michael Leo <yhzemail61010@aliyun.com>
2020-06-16 19:32:18 +0800

    1. Add Camera2Live module 2. Add demonstration in separate Activity

---
+++ camera2live-sdk/src/main/java/com/ho1ho/camera2live/extensions/CommonExtension.kt
@@ -0,0 +1,9 @@
+package com.ho1ho.camera2live.extensions
+
+/**
+ * Author: Michael Leo
+ * Date: 20-4-1 上午11:36
+ */
+fun fail(message: String): Nothing {
+    throw IllegalStateException(message)
+}

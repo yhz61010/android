0c071d7fe9193f77a10a7397a815d90865ba2aa7
MikeoLeo <yhzemail61010@aliyun.com>
2020-06-16 14:31:13 +0800

    init

---
+++ androidbase/src/main/java/com/ho1ho/androidbase/http/okhttp/HttpLoggingInterceptor.kt
@@ -0,0 +1,261 @@
+package com.ho1ho.androidbase.http.okhttp
+
+
+import com.ho1ho.androidbase.utils.CLog
+import okhttp3.Headers
+import okhttp3.Interceptor
+import okhttp3.Protocol
+import okhttp3.Response
+import okhttp3.internal.http.promisesBody
+import okio.Buffer
+import java.io.EOFException
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
+/**
+ * Author: Michael Leo
+ * Date: 20-5-27 下午8:41
+ */
+class HttpLoggingInterceptor constructor(private val logger: Logger = Logger.DEFAULT) : Interceptor {
+    enum class Level {
+        /**
+         * No logs.
+         */
+        NONE,
+
+        /**
+         * Logs request and response lines.
+         *
+         *
+         * Example:
+         * <pre>`--> POST /greeting http/1.1 (3-byte body)
+         *
+         * <-- 200 OK (22ms, 6-byte body)
+        `</pre> *
+         */
+        @Suppress("unused")
+        BASIC,
+
+        /**
+         * Logs request and response lines and their respective headers.
+         *
+         *
+         * Example:
+         * <pre>`--> POST /greeting http/1.1
+         * Host: example.com
+         * Content-Type: plain/text
+         * Content-Length: 3
+         * --> END POST
+         *
+         * <-- 200 OK (22ms)
+         * Content-Type: plain/text
+         * Content-Length: 6
+         * <-- END HTTP
+        `</pre> *
+         */
+        HEADERS,
+
+        /**
+         * Logs request and response lines and their respective headers and bodies (if present).
+         *
+         *
+         * Example:
+         * <pre>`--> POST /greeting http/1.1
+         * Host: example.com
+         * Content-Type: plain/text
+         * Content-Length: 3
+         *
+         * Hi?
+         * --> END POST
+         *
+         * <-- 200 OK (22ms)
+         * Content-Type: plain/text
+         * Content-Length: 6
+         *
+         * Hello!
+         * <-- END HTTP
+        `</pre> *
+         */
+        BODY
+    }
+
+    interface Logger {
+        fun log(message: String?)
+
+        companion object {
+            /**
+             * A [Logger] defaults output appropriate for the current platform.
+             */
+            val DEFAULT: Logger = object : Logger {
+                override fun log(message: String?) {
+                    CLog.w(TAG, message)
+                }
+            }
+        }
+    }
+
+    @Volatile
+    var level = Level.NONE
+
+    /**
+     * Change the level at which this interceptor logs.
+     */
+    @Suppress("unused")
+    fun setLevel(level: Level?): HttpLoggingInterceptor {
+        if (level == null) throw NullPointerException("level == null. Use Level.NONE instead.")
+        this.level = level
+        return this
+    }
+
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val level = level
+        val request = chain.request()
+        if (level == Level.NONE) {
+            return chain.proceed(request)
+        }
+        val logBody = level == Level.BODY
+        val logHeaders = logBody || level == Level.HEADERS
+        val requestBody = request.body
+        val hasRequestBody = requestBody != null
+        val connection = chain.connection()
+        val protocol = connection?.protocol() ?: Protocol.HTTP_1_1
+        logger.log("─────────────────────────────────────────────────────────────────────────────────────────────────────────────────")
+        var requestStartMessage = "--> ${request.method} ${request.url} $protocol"
+        if (!logHeaders && hasRequestBody) requestStartMessage =
+            "$requestStartMessage (${requestBody?.contentLength()}-byte body)"
+        logger.log(requestStartMessage)
+        if (logHeaders) {
+            if (hasRequestBody) {
+                // Request body headers are only present when installed as a network interceptor. Force
+                // them to be included (when available) so there values are known.
+                requestBody?.contentType()?.let { logger.log("Content-Type: $it") }
+                if (requestBody?.contentLength() ?: -1 != -1L) {
+                    logger.log("Content-Length: ${requestBody?.contentLength()}")
+                }
+            }
+            val headers = request.headers
+            var i = 0
+            val count = headers.size
+            while (i < count) {
+                val name = headers.name(i)
+                // Skip headers from the request body as they are explicitly logged above.
+                if (!"Content-Type".equals(name, ignoreCase = true) && !"Content-Length".equals(
+                        name,
+                        ignoreCase = true
+                    )
+                ) {
+                    logger.log("$name: ${headers.value(i)}")
+                }
+                i++
+            }
+            if (!logBody || !hasRequestBody) {
+                logger.log("--> END ${request.method}")
+            } else if (bodyEncoded(request.headers)) {
+                logger.log("--> END ${request.method} (encoded body omitted)")
+            } else {
+                val buffer = Buffer()
+                requestBody?.writeTo(buffer)
+                var charset = DEFAULT_CHARSET
+                requestBody?.contentType()?.also {
+                    charset = it.charset(DEFAULT_CHARSET)!!
+                }
+                logger.log("")
+                if (isPlaintext(buffer)) {
+                    val content = buffer.readString(charset)
+                    logger.log(content)
+                    logger.log("--> END ${request.method} (${requestBody?.contentLength()}-byte body)")
+                } else {
+                    logger.log("--> END ${request.method} (binary ${requestBody?.contentLength()}-byte body omitted)")
+                }
+            }
+            logger.log("─────────────────────────────────────────────────────────────────────────────────────────────────────────────────")
+        }
+        val startNs = System.nanoTime()
+        val response: Response
+        response = try {
+            chain.proceed(request)
+        } catch (e: Exception) {
+            logger.log("─────────────────────────────────────────────────────────────────────────────────────────────────────────────────")
+            logger.log("<-- HTTP FAILED: $e")
+            logger.log("─────────────────────────────────────────────────────────────────────────────────────────────────────────────────")
+            throw e
+        }
+        val tookMs = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNs)
+        logger.log("─────────────────────────────────────────────────────────────────────────────────────────────────────────────────")
+        val responseBody = response.body
+        val contentLength = responseBody?.contentLength() ?: -1
+        val bodySize = if (contentLength != -1L) "$contentLength-byte" else "unknown-length"
+        logger.log(
+            "<-- ${response.code} ${response.message} ${response.request.url} (${tookMs}ms ${if (!logHeaders) ", $bodySize body" else ""})"
+        )
+        if (logHeaders) {
+            val headers = response.headers
+            var i = 0
+            val count = headers.size
+            while (i < count) {
+                logger.log("${headers.name(i)}: ${headers.value(i)}")
+                i++
+            }
+            if (!logBody || !response.promisesBody()) {
+                logger.log("<-- END HTTP")
+            } else if (bodyEncoded(response.headers)) {
+                logger.log("<-- END HTTP (encoded body omitted)")
+            } else {
+                val source = responseBody?.source()
+                source?.request(Long.MAX_VALUE)
+                // Buffer the entire body.
+                val buffer = source?.buffer
+                var charset = DEFAULT_CHARSET
+                val contentType = responseBody?.contentType()
+                if (contentType != null) {
+                    charset = contentType.charset(DEFAULT_CHARSET)!!
+                }
+                if (buffer != null && !isPlaintext(buffer)) {
+                    logger.log(" \n<-- END HTTP (binary ${buffer.size}-byte body omitted)")
+                    return response
+                }
+                if (contentLength != 0L) {
+                    logger.log(" \n${buffer?.clone()?.readString(charset)}")
+                }
+                logger.log("<-- END HTTP (${buffer?.size}-byte body)")
+            }
+        }
+        logger.log("─────────────────────────────────────────────────────────────────────────────────────────────────────────────────")
+        return response
+    }
+
+    private fun bodyEncoded(headers: Headers): Boolean {
+        val contentEncoding = headers["Content-Encoding"]
+        return contentEncoding != null && !contentEncoding.equals("identity", ignoreCase = true)
+    }
+
+    companion object {
+        private val DEFAULT_CHARSET = Charsets.UTF_8
+        private const val TAG = "HTTP"
+
+        /**
+         * Returns true if the body in question probably contains human readable text. Uses a small sample
+         * of code points to detect unicode control characters commonly used in binary file signatures.
+         */
+        fun isPlaintext(buffer: Buffer): Boolean {
+            return try {
+                val prefix = Buffer()
+                val byteCount = if (buffer.size < 64) buffer.size else 64
+                buffer.copyTo(prefix, 0, byteCount)
+                for (i in 0..15) {
+                    if (prefix.exhausted()) {
+                        break
+                    }
+                    val codePoint = prefix.readUtf8CodePoint()
+                    if (Character.isISOControl(codePoint) && !Character.isWhitespace(codePoint)) {
+                        return false
+                    }
+                }
+                true
+            } catch (e: EOFException) {
+                false // Truncated UTF-8 sequence.
+            }
+        }
+    }
+}
---
+++ screencapture-sdk/src/main/java/com/ho1ho/screenshot/base/ScreenshotStrategy.kt
@@ -0,0 +1,133 @@
+package com.ho1ho.screenshot.base
+
+import android.graphics.Bitmap
+import android.graphics.PixelFormat
+import android.hardware.display.DisplayManager
+import android.hardware.display.VirtualDisplay
+import android.media.ImageReader
+import android.media.projection.MediaProjection
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.SystemClock
+import com.ho1ho.androidbase.utils.CLog
+import com.ho1ho.androidbase.utils.media.ImageUtil
+import java.io.IOException
+
+/**
+ * Author: Michael Leo
+ * Date: 20-5-15 下午1:53
+ */
+class ScreenshotStrategy private constructor(private val builder: Builder) : ScreenProcessor {
+
+    companion object {
+        private const val TAG = "ScrSt"
+    }
+
+    private var virtualDisplay: VirtualDisplay? = null
+    private var imageReader: ImageReader? = null
+    private var screenshotThread: HandlerThread? = null
+    private var screenshotHandler: Handler? = null
+    private var previousDisplayTime = 0L
+
+    class Builder(
+        val width: Int,
+        val height: Int,
+        val dpi: Int,
+        val mediaProjection: MediaProjection?,
+        val screenDataListener: ScreenDataListener
+    ) {
+        @Suppress("WeakerAccess")
+        var fps = 20F
+            private set
+        var sampleSize = 1
+            private set
+
+        var displayIntervalInMs: Int = 0
+            private set
+
+        fun setFps(fps: Float) = apply { this.fps = fps }
+        fun setSampleSize(sample: Int) = apply { this.sampleSize = sample }
+
+        fun build(): ScreenshotStrategy {
+            displayIntervalInMs = (1000 / (fps + 1)).toInt()
+            CLog.w(TAG, "width=$width height=$height dpi=$dpi fps=$fps sampleSize=$sampleSize")
+            return ScreenshotStrategy(this)
+        }
+
+    }
+
+    @Throws(IOException::class)
+    override fun onInit() {
+        imageReader = ImageReader.newInstance(builder.width, builder.height, PixelFormat.RGBA_8888, 2)
+        virtualDisplay = builder.mediaProjection!!.createVirtualDisplay(
+            "screenshot",
+            builder.width, builder.height, builder.dpi,
+            DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC, // VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR
+            imageReader!!.surface, null, null
+        )
+    }
+
+    override fun onStart() {
+        previousDisplayTime = SystemClock.elapsedRealtime()
+        try {
+            screenshotThread = HandlerThread("screenshot-thread")
+            screenshotThread!!.start()
+            screenshotHandler = Handler(screenshotThread!!.looper)
+            imageReader?.setOnImageAvailableListener(mOnImageAvailableListener, screenshotHandler)
+        } catch (e: IOException) {
+            CLog.e(TAG, "onStart error", e)
+        }
+    }
+
+    override fun onRelease() {
+        onStop()
+        virtualDisplay?.release()
+        imageReader?.close()
+    }
+
+    override fun onStop() {
+        screenshotHandler?.removeCallbacksAndMessages(null)
+        screenshotThread?.quitSafely()
+    }
+
+    private val mOnImageAvailableListener = ImageReader.OnImageAvailableListener { reader ->
+        val image = reader.acquireLatestImage()
+        if (image == null || screenshotHandler == null) return@OnImageAvailableListener
+
+        if ((SystemClock.elapsedRealtime() - previousDisplayTime) < builder.displayIntervalInMs) {
+            image.close()
+            CLog.d(TAG, "Ignore image due to fps")
+            return@OnImageAvailableListener
+        }
+
+        previousDisplayTime = SystemClock.elapsedRealtime()
+
+        screenshotHandler!!.post {
+            try {
+                val width = image.width
+                val height = image.height
+                CLog.v(TAG, "Image width=$width height=$height")
+
+//                var st = SystemClock.elapsedRealtimeNanos()
+                val bitmap = ImageUtil.createBitmapFromImage(image)
+//                CLog.i(TAG, "createBitmapFromImage cost=${(SystemClock.elapsedRealtimeNanos() - st) / 1000}us")
+//                st = SystemClock.elapsedRealtimeNanos()
+                val targetBitmap: Bitmap
+                if (builder.sampleSize != 1) {
+                    targetBitmap = ImageUtil.compressBitmap(bitmap, builder.sampleSize)
+                    bitmap.recycle()
+                } else {
+                    targetBitmap = bitmap
+                }
+//                CLog.i(TAG, "compressBitmap cost=${(SystemClock.elapsedRealtimeNanos() - st) / 1000}us")
+                builder.screenDataListener.onDataUpdate(targetBitmap)
+                targetBitmap.recycle()
+//                FileUtil.writeBitmapToFile(bitmap, 1)
+            } catch (e: Exception) {
+                CLog.e(TAG, "screenshotHandler error=${e.message}")
+            } finally {
+                image.close()
+            }
+        }
+    }
+}

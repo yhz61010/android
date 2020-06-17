3736bc22f9273a9e2ea56215932395ec04e5961d
MikeoLeo <yhzemail61010@aliyun.com>
2020-06-17 11:17:46 +0800

    1. Add more examples. 2. Refactor examples.

---
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/ui/base/BaseDemonstrationActivity.kt
@@ -0,0 +1,15 @@
+package com.ho1ho.leoandroidbaseutil.ui.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Author: Michael Leo
+ * Date: 20-6-17 上午11:14
+ */
+open class BaseDemonstrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        title = intent.getStringExtra("title")
+    }
+}

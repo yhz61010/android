7c8c193088bf721bbd2d77833c0e1b76fa62bba5
MikeoLeo <yhzemail61010@aliyun.com>
2020-06-24 14:36:36 +0800

    Refactor Camera2Live

--- camera2live-sdk/src/main/java/com/ho1ho/camera2live/view/CameraViewFragment.kt
+++ camera2live-sdk/src/main/java/com/ho1ho/camera2live/view/CameraViewFragment.kt
@@ -43,15 +43,14 @@ class CameraViewFragment : Fragment() {
         switchBtn.setOnCheckedChangeListener { _: CompoundButton?, _: Boolean -> camera2Component.switchCamera() }
         switchFlashBtn = v.findViewById(R.id.switchFlashBtn)
         switchFlashBtn.setOnCheckedChangeListener { _: CompoundButton?, _: Boolean -> camera2Component.switchFlash() }
-        val desiredSize = CAMERA_SIZE_HIGH
 
         // CAMERA_SIZE_NORMAL & BITRATE_NORMAL & CAMERA_FPS_NORMAL & VIDEO_FPS_FREQUENCY_HIGH & KEY_I_FRAME_INTERVAL=5
         // BITRATE_MODE_CQ: 348.399kB/s
         // BITRATE_MODE_CBR: 85.875kB/s
         // BITRATE_MODE_VBR: 84.929kB/s
-
         // CAMERA_SIZE_HIGH & BITRATE_NORMAL & CAMERA_FPS_NORMAL & VIDEO_FPS_FREQUENCY_HIGH & KEY_I_FRAME_INTERVAL=3
         // BITRATE_MODE_CBR: 113.630kB/s
+        val desiredSize = CAMERA_SIZE_HIGH
         val camera2ComponentBuilder = Camera2Component(this).Builder(desiredSize[0], desiredSize[1])
         camera2ComponentBuilder.cameraSurfaceView = v.findViewById(R.id.cameraSurfaceView)
 //        camera2ComponentBuilder.previewInFullscreen = true
@@ -63,7 +62,6 @@ class CameraViewFragment : Fragment() {
         camera2ComponentBuilder.bitrateMode = MediaCodecInfo.EncoderCapabilities.BITRATE_MODE_CBR
         camera2Component = camera2ComponentBuilder.build()
         camera2Component.setDebugOutputH264(true)
-        camera2Component.initializeCamera(previousLensFacing)
         camera2Component.setEncodeListener(object : EncodeDataUpdateListener {
             override fun onUpdate(h264Data: ByteArray) {
                 Log.d(TAG, "Get encoded video data length=" + h264Data.size)

7db0a322ab538295a881e7e192511650df2a6ae5
Michael Leo <yhzemail61010@aliyun.com>
2020-06-28 15:00:56 +0800

    Improve take photo feature

--- camera2live-sdk/src/main/java/com/ho1ho/camera2live/CameraPhotoFragmentHelper.kt
+++ camera2live-sdk/src/main/java/com/ho1ho/camera2live/CameraPhotoFragmentHelper.kt
@@ -7,6 +7,7 @@ import android.graphics.ImageFormat
 import android.hardware.camera2.*
 import android.media.Image
 import android.media.ImageReader
+import android.media.MediaActionSound
 import android.os.Build
 import android.os.Environment
 import android.os.Handler
@@ -19,8 +20,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.ho1ho.androidbase.exts.computeExifOrientation
 import com.ho1ho.androidbase.exts.fail
-import com.ho1ho.camera2live.view.CameraPhotoFragment
-import com.ho1ho.camera2live.view.base.CameraSurfaceView
+import com.ho1ho.camera2live.view.CameraSurfaceView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
@@ -40,13 +40,15 @@ import kotlin.coroutines.suspendCoroutine
  * Author: Michael Leo
  * Date: 20-6-24 下午5:05
  */
-class Camera2Helper(
+class CameraPhotoFragmentHelper(
     private val context: Fragment,
     private val lensFacing: Int,
     private val cameraSurfaceView: CameraSurfaceView,
     private val overlay: View
 ) {
     private val cameraId: String by lazy { if (CameraMetadata.LENS_FACING_BACK == lensFacing) "0" else "1" }
+
+    private lateinit var captureRequestBuilder: CaptureRequest.Builder
 
     /** Detects, characterizes, and connects to a CameraDevice (used for all camera operations) */
     private val cameraManager: CameraManager by lazy {
@@ -105,12 +107,9 @@ class Camera2Helper(
         camera = openCamera(cameraManager, cameraId, cameraHandler)
 
         // Initialize an image reader which will be used to capture still photos
-        val size = characteristics.get(
-            CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP
-        )!!.getOutputSizes(ImageFormat.JPEG).maxBy { it.height * it.width }!!
-        imageReader = ImageReader.newInstance(
-            size.width, size.height, ImageFormat.JPEG, IMAGE_BUFFER_SIZE
-        )
+        val size = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!.getOutputSizes(ImageFormat.JPEG)
+            .maxBy { it.height * it.width }!!
+        imageReader = ImageReader.newInstance(size.width, size.height, ImageFormat.JPEG, IMAGE_BUFFER_SIZE)
 
         // Creates list of Surfaces where the camera will output frames
         val targets = listOf(cameraSurfaceView.holder.surface, imageReader.surface)
@@ -118,13 +117,13 @@ class Camera2Helper(
         // Start a capture session using our open camera and list of Surfaces where frames will go
         session = createCaptureSession(camera, targets, cameraHandler)
 
-        val captureRequest = camera.createCaptureRequest(
+        captureRequestBuilder = camera.createCaptureRequest(
             CameraDevice.TEMPLATE_STILL_CAPTURE
         ).apply { addTarget(cameraSurfaceView.holder.surface) }
 
         // This will keep sending the capture request as frequently as possible until the
         // session is torn down or session.stopRepeating() is called
-        session.setRepeatingRequest(captureRequest.build(), null, cameraHandler)
+        session.setRepeatingRequest(captureRequestBuilder.build(), null, cameraHandler)
     }
 
     /** Opens the camera and returns the opened device (as the result of the suspend coroutine) */
@@ -222,6 +221,7 @@ class Camera2Helper(
                 result: TotalCaptureResult
             ) {
                 super.onCaptureCompleted(session, request, result)
+                MediaActionSound().play(MediaActionSound.SHUTTER_CLICK)
                 val resultTimestamp = result.get(CaptureResult.SENSOR_TIMESTAMP)
                 Log.d(TAG, "Capture result received: $resultTimestamp")
 
@@ -261,11 +261,7 @@ class Camera2Helper(
                         val exifOrientation = computeExifOrientation(rotation, mirrored)
 
                         // Build the result and resume progress
-                        cont.resume(
-                            CombinedCaptureResult(
-                                image, result, exifOrientation, imageReader.imageFormat
-                            )
-                        )
+                        cont.resume(CombinedCaptureResult(image, result, exifOrientation, imageReader.imageFormat))
 
                         // There is no need to break out of the loop, this coroutine will suspend
                     }
@@ -274,7 +270,43 @@ class Camera2Helper(
         }, cameraHandler)
     }
 
-    /** Helper function used to save a [CombinedCaptureResult] into a [File] */
+    // ===========================================================
+    fun turnOnFlash() {
+        // On Samsung, you must also set CONTROL_AE_MODE to CONTROL_AE_MODE_ON.
+        // Otherwise the flash will not be on.
+        captureRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON)
+        captureRequestBuilder.set(CaptureRequest.FLASH_MODE, CaptureRequest.FLASH_MODE_TORCH)
+        //                    mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AWB_MODE, CaptureRequest.CONTROL_AWB_MODE_FLUORESCENT);
+        val captureRequest = captureRequestBuilder.build()
+        session.setRepeatingRequest(captureRequest, null, cameraHandler)
+
+//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                        mCameraManager.setTorchMode(mCameraId, true);
+//                    }
+        Log.w(TAG, "Flash ON")
+    }
+
+    fun turnOffFlash() {
+        // On Samsung, you must also set CONTROL_AE_MODE to CONTROL_AE_MODE_ON.
+        // Otherwise the flash will not be off.
+        captureRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON)
+        captureRequestBuilder.set(CaptureRequest.FLASH_MODE, CaptureRequest.FLASH_MODE_OFF)
+        //                    mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AWB_MODE, CaptureRequest.CONTROL_AWB_MODE_DAYLIGHT);
+        val captureRequest = captureRequestBuilder.build()
+        session.setRepeatingRequest(captureRequest, null, cameraHandler)
+
+//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                        mCameraManager.setTorchMode(mCameraId, false);
+//                    }
+        Log.w(TAG, "Flash OFF")
+    }
+    // ===========================================================
+
+    /**
+     * **Attention:** This method is Debug ONLY
+     *
+     * Helper function used to save a [CombinedCaptureResult] into a [File]
+     */
     suspend fun saveResult(result: CombinedCaptureResult): File = suspendCoroutine { cont ->
         when (result.format) {
 
@@ -328,7 +360,7 @@ class Camera2Helper(
     }
 
     companion object {
-        private val TAG = Camera2Helper::class.java.simpleName
+        private val TAG = CameraPhotoFragmentHelper::class.java.simpleName
 
         private const val ANIMATION_FAST_MILLIS = 50L
 
@@ -349,6 +381,8 @@ class Camera2Helper(
         }
 
         /**
+         * **Attention:** This method is Debug ONLY
+         *
          * Create a [File] named a using formatted timestamp with the current date and time.
          *
          * @return [File] created.

5e3ec97ffad84e0d268b4420db08e624f8746d1a
Michael Leo <yhzemail61010@aliyun.com>
2020-06-28 15:00:56 +0800

    Improve take photo feature

--- camera2live-sdk/src/main/java/com/ho1ho/camera2live/CameraPhotoFragment.kt
+++ camera2live-sdk/src/main/java/com/ho1ho/camera2live/CameraPhotoFragment.kt
@@ -1,23 +1,23 @@
-package com.ho1ho.camera2live.view
+package com.ho1ho.camera2live
 
 import android.annotation.SuppressLint
 import android.hardware.camera2.CameraMetadata
-import android.media.MediaActionSound
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.SurfaceHolder
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import android.widget.ImageView
+import android.widget.ToggleButton
 import androidx.exifinterface.media.ExifInterface
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import com.ho1ho.androidbase.exts.getPreviewOutputSize
-import com.ho1ho.camera2live.Camera2Helper
-import com.ho1ho.camera2live.R
 import com.ho1ho.camera2live.utils.OrientationLiveData
-import com.ho1ho.camera2live.view.base.CameraSurfaceView
+import com.ho1ho.camera2live.view.CameraSurfaceView
 import kotlinx.android.synthetic.main.fragment_camera_view.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -29,13 +29,17 @@ import kotlinx.coroutines.launch
  */
 class CameraPhotoFragment : Fragment() {
 
-    private lateinit var camera2Helper: Camera2Helper
+    private lateinit var camera2Helper: CameraPhotoFragmentHelper
+
+    private lateinit var switchFlashBtn: ToggleButton
+
+    var backPressListener: BackPressedListener? = null
 
     /** Overlay on top of the camera preview */
     private lateinit var overlay: View
 
     /** Where the camera preview is displayed */
-    private lateinit var viewFinder: CameraSurfaceView
+    private lateinit var cameraView: CameraSurfaceView
 
     /** Live data listener for changes in the device orientation relative to the camera */
     lateinit var relativeOrientation: OrientationLiveData
@@ -49,22 +53,27 @@ class CameraPhotoFragment : Fragment() {
     @SuppressLint("MissingPermission")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        view.findViewById<ImageView>(R.id.ivBack).setOnClickListener {
+            activity?.supportFragmentManager?.popBackStackImmediate()
+            backPressListener?.onBackPressed()
+        }
         overlay = view.findViewById(R.id.overlay)
-        viewFinder = view.findViewById(R.id.cameraSurfaceView)
-        camera2Helper = Camera2Helper(this, CameraMetadata.LENS_FACING_BACK, viewFinder, overlay)
+        cameraView = view.findViewById(R.id.cameraSurfaceView)
+        camera2Helper = CameraPhotoFragmentHelper(this, CameraMetadata.LENS_FACING_BACK, cameraView, overlay)
+        switchFlashBtn = view.findViewById(R.id.switchFlashBtn)
+        switchFlashBtn.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            if (isChecked) camera2Helper.turnOnFlash() else camera2Helper.turnOffFlash()
+        }
 
-        viewFinder.holder.addCallback(object : SurfaceHolder.Callback {
+        cameraView.holder.addCallback(object : SurfaceHolder.Callback {
             override fun surfaceDestroyed(holder: SurfaceHolder) = Unit
             override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) = Unit
             override fun surfaceCreated(holder: SurfaceHolder) {
-
                 // Selects appropriate preview size and configures view finder
-                val previewSize = getPreviewOutputSize(
-                    viewFinder.display, camera2Helper.characteristics, SurfaceHolder::class.java
-                )
-                Log.d(TAG, "View finder size: ${viewFinder.width} x ${viewFinder.height}")
+                val previewSize = getPreviewOutputSize(cameraView.display, camera2Helper.characteristics, SurfaceHolder::class.java)
+                Log.d(TAG, "View finder size: ${cameraView.width} x ${cameraView.height}")
                 Log.d(TAG, "Selected preview size: $previewSize")
-                viewFinder.setDimension(previewSize.width, previewSize.height)
+                cameraView.setDimension(previewSize.width, previewSize.height)
 
                 // To ensure that size is set, initialize camera in the view's thread
                 view.post { camera2Helper.initializeCamera() }
@@ -73,8 +82,6 @@ class CameraPhotoFragment : Fragment() {
 
         // Listen to the capture button
         ivShot.setOnClickListener {
-            MediaActionSound().play(MediaActionSound.SHUTTER_CLICK)
-
             // Disable click listener to prevent multiple requests simultaneously in flight
             it.isEnabled = false
 
@@ -90,9 +97,7 @@ class CameraPhotoFragment : Fragment() {
                     // If the result is a JPEG file, update EXIF metadata with orientation info
                     if (output.extension == "jpg") {
                         val exif = ExifInterface(output.absolutePath)
-                        exif.setAttribute(
-                            ExifInterface.TAG_ORIENTATION, result.orientation.toString()
-                        )
+                        exif.setAttribute(ExifInterface.TAG_ORIENTATION, result.orientation.toString())
                         exif.saveAttributes()
                         Log.d(TAG, "EXIF metadata saved: ${output.absolutePath}")
                     }

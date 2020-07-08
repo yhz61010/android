eaa9760de828fecf96f026ae74a086e3f8d6ee5d
Michael Leo <yhzemail61010@aliyun.com>
2020-07-08 17:22:17 +0800

    Add BytesConversion unit test

--- androidbase-sdk/src/main/java/com/ho1ho/androidbase/exts/ByteArrayExt.kt
+++ androidbase-sdk/src/main/java/com/ho1ho/androidbase/exts/ByteArrayExt.kt
@@ -6,46 +6,6 @@ package com.ho1ho.androidbase.exts
  */
 private val HEX_CHARS = "0123456789ABCDEF".toCharArray()
 
-/**
- * Attention.
- * This method is little bit slow if you want to use in it loop.
- */
-//fun ByteArray.toHexString(delimiter: CharSequence = " ") = joinToString(delimiter) { "%02X".format(it) }
-
-fun ByteArray.toHexString(delimiter: CharSequence = " "): String {
-    val result = StringBuilder()
-    forEach {
-        val octet = it.toInt()
-        val highBit = octet and 0x0F
-        val lowBit = (octet and 0xF0).ushr(4)
-        result.append(HEX_CHARS[highBit])
-        result.append(HEX_CHARS[lowBit])
-        if (delimiter.isNotEmpty()) result.append(delimiter)
-    }
-    if (delimiter.isNotEmpty()) result.deleteCharAt(result.length - 1)
-    return result.toString()
-}
-
-fun ByteArray.toHexStringLE(delimiter: CharSequence = " "): String {
-    val result = StringBuilder()
-    forEach {
-        val octet = it.toInt()
-        val highBit = (octet and 0xF0).ushr(4)
-        val lowBit = octet and 0x0F
-        result.append(HEX_CHARS[highBit])
-        result.append(HEX_CHARS[lowBit])
-        if (delimiter.isNotEmpty()) result.append(delimiter)
-    }
-    if (delimiter.isNotEmpty()) result.deleteCharAt(result.length - 1)
-    return result.toString()
-}
-
-fun Byte.toHexString() = let { "%02X".format(it) }
-
-fun ByteArray.toAsciiString(delimiter: CharSequence = "") = map { it.toChar() }.joinToString(delimiter)
-
-// =========================================================================
-
 fun Byte.toBytes(): ByteArray = byteArrayOf(this)
 
 /**
@@ -92,3 +52,42 @@ fun Long.toBytes(): ByteArray =
     ByteArray(Long.SIZE_BYTES).also { for (i in it.indices) it[i] = (this ushr ((Long.SIZE_BYTES - 1 - i) * 8) and 0xFF).toByte() }
 
 fun Long.toBytesLE(): ByteArray = ByteArray(Long.SIZE_BYTES).also { for (i in it.indices) it[i] = ((this ushr (i * 8)) and 0xFF).toByte() }
+
+// ==============================================
+
+fun Byte.toHexString() = let { "%02X".format(it) }
+fun ByteArray.toAsciiString(delimiter: CharSequence = "") = map { it.toChar() }.joinToString(delimiter)
+
+/**
+ * Attention.
+ * This method is little bit slow if you want to use in it loop.
+ */
+//fun ByteArray.toHexString(delimiter: CharSequence = " ") = joinToString(delimiter) { "%02X".format(it) }
+
+fun ByteArray.toHexString(delimiter: CharSequence = " "): String {
+    val result = StringBuilder()
+    forEach {
+        val octet = it.toInt()
+        val highBit = octet and 0x0F
+        val lowBit = (octet and 0xF0).ushr(4)
+        result.append(HEX_CHARS[highBit])
+        result.append(HEX_CHARS[lowBit])
+        if (delimiter.isNotEmpty()) result.append(delimiter)
+    }
+    if (delimiter.isNotEmpty()) result.deleteCharAt(result.length - 1)
+    return result.toString()
+}
+
+fun ByteArray.toHexStringLE(delimiter: CharSequence = " "): String {
+    val result = StringBuilder()
+    forEach {
+        val octet = it.toInt()
+        val highBit = (octet and 0xF0).ushr(4)
+        val lowBit = octet and 0x0F
+        result.append(HEX_CHARS[highBit])
+        result.append(HEX_CHARS[lowBit])
+        if (delimiter.isNotEmpty()) result.append(delimiter)
+    }
+    if (delimiter.isNotEmpty()) result.deleteCharAt(result.length - 1)
+    return result.toString()
+}

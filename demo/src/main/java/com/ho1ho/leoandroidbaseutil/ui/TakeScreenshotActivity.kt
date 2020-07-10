f445c33f3caa95ec75deb92ac777ee642e8999ea
MikeoLeo <yhzemail61010@aliyun.com>
2020-07-10 14:09:13 +0800

    Add more demo

--- demo/src/main/java/com/ho1ho/leoandroidbaseutil/ui/TakeScreenshotActivity.kt
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/ui/TakeScreenshotActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import com.ho1ho.androidbase.utils.device.ScreenUtil
 import com.ho1ho.androidbase.utils.media.ImageUtil
+import com.ho1ho.androidbase.utils.ui.ToastUtil
 import com.ho1ho.leoandroidbaseutil.R
 import com.ho1ho.leoandroidbaseutil.ui.base.BaseDemonstrationActivity
 import java.io.File
@@ -17,7 +18,9 @@ class TakeScreenshotActivity : BaseDemonstrationActivity() {
     fun onScreenshot(view: View) {
         val bitmap = ScreenUtil.takeScreenshot(window)
         bitmap?.let {
-            ImageUtil.writeBitmapToFile(File(getExternalFilesDir(null), "screenshot.jpg"), it, 100)
+            val screenshotFile = File(getExternalFilesDir(null), "screenshot.jpg")
+            ImageUtil.writeBitmapToFile(screenshotFile, it, 100)
+            ToastUtil.showToast("Screenshot is saved in ${screenshotFile.absolutePath}")
         }
     }
 }

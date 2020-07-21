94f2223f042694876720502581d22f3be26fcf0c
Michael Leo <yhzemail61010@aliyun.com>
2020-07-21 17:01:08 +0800

    Add clipboard demo

---
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/ui/ClipboardActivity.kt
@@ -0,0 +1,41 @@
+package com.ho1ho.leoandroidbaseutil.ui
+
+import android.os.Bundle
+import android.view.View
+import com.ho1ho.androidbase.exts.ITAG
+import com.ho1ho.androidbase.utils.ClipboardUtil
+import com.ho1ho.androidbase.utils.LLog
+import com.ho1ho.leoandroidbaseutil.R
+import com.ho1ho.leoandroidbaseutil.ui.base.BaseDemonstrationActivity
+import kotlinx.android.synthetic.main.activity_clipboard.*
+
+class ClipboardActivity : BaseDemonstrationActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clipboard)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        ClipboardUtil.getClipboardText(this) {
+            edTxt.setText(it)
+            LLog.i(ITAG, "ClipboardText=$it")
+        }
+    }
+
+    fun onClearClipboardClick(view: View) {
+        ClipboardUtil.clear(this)
+        ClipboardUtil.getClipboardText(this) {
+            edTxt.setText(it)
+            LLog.i(ITAG, "ClipboardText=$it")
+        }
+    }
+
+    fun onSetTextToClipboardClick(view: View) {
+        ClipboardUtil.setTextToClipboard(this, "Welcome Leo")
+        ClipboardUtil.getClipboardText(this) {
+            edTxt.setText(it)
+            LLog.i(ITAG, "ClipboardText=$it")
+        }
+    }
+}

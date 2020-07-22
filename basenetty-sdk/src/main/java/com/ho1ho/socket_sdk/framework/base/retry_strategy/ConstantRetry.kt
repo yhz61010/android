6286c823a4106e2f5da609aa30fd6ae46f37999d
Michael Leo <yhzemail61010@aliyun.com>
2020-07-22 18:34:16 +0800

    Implement RetryStrategy

---
+++ basenetty-sdk/src/main/java/com/ho1ho/socket_sdk/framework/base/retry_strategy/ConstantRetry.kt
@@ -0,0 +1,17 @@
+package com.ho1ho.socket_sdk.framework.base.retry_strategy
+
+import com.ho1ho.socket_sdk.framework.base.retry_strategy.base.RetryStrategy
+
+/**
+ * Author: Michael Leo
+ * Date: 20-7-22 下午6:14
+ */
+class ConstantRetry(private val maxTimes: Int = 10, private val delayInMillSec: Long = 2000L) : RetryStrategy {
+    override fun getMaxTimes(): Int {
+        return maxTimes
+    }
+
+    override fun getDelayInMillSec(): Long {
+        return delayInMillSec
+    }
+}

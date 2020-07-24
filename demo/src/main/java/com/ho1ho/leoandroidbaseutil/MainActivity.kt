fb8c1bc941e31c94f909d99535278d44e799b984
Michael Leo <yhzemail61010@aliyun.com>
2020-07-24 18:05:03 +0800

    Update ScreenshotRecordH264 demo

--- demo/src/main/java/com/ho1ho/leoandroidbaseutil/MainActivity.kt
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/MainActivity.kt
@@ -80,13 +80,13 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private val featureList = arrayOf(
+            Pair("ScreenShare\nMaster side", ScreenShareMasterActivity::class.java),
+            Pair("ScreenShare\nClient side", ScreenShareClientActivity::class.java),
             Pair("Device Info", DeviceInfoActivity::class.java),
             Pair("Log", LogActivity::class.java),
             Pair("Clipboard", ClipboardActivity::class.java),
             Pair("Network Monitor", NetworkMonitorActivity::class.java),
             Pair("Camera2Live", Camera2LiveActivity::class.java),
-            Pair("ScreenShare\nMaster side", ScreenShareMasterActivity::class.java),
-            Pair("ScreenShare\nClient side", ScreenShareClientActivity::class.java),
             Pair("SaveInstanceState", SaveInstanceStateActivity::class.java),
             Pair("Audio", AudioActivity::class.java),
             Pair("Coroutine", CoroutineActivity::class.java),

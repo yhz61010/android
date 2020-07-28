664ce8935a85b01036e0b86020f2fe602e1fa8d4
Michael Leo <yhzemail61010@aliyun.com>
2020-07-28 11:38:14 +0800

    Update NetworkMonitor

--- androidbase-sdk/src/main/java/com/ho1ho/androidbase/utils/network/NetworkMonitor.kt
+++ androidbase-sdk/src/main/java/com/ho1ho/androidbase/utils/network/NetworkMonitor.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
-import com.ho1ho.androidbase.exts.humanReadableByteCount
 import com.ho1ho.androidbase.utils.LLog
 import java.util.concurrent.TimeUnit
 
@@ -32,9 +31,6 @@ class NetworkMonitor(private val ctx: Context, private val ip: String) {
         override fun handleMessage(msg: Message) {
             if (TrafficStatHelper.MESSAGE_TRAFFIC_UPDATE == msg.what) {
                 val speedArray = (msg.obj) as Array<*>
-                val downloadSpeed = (speedArray[0] as Long).humanReadableByteCount()
-                val uploadSpeed = (speedArray[1] as Long).humanReadableByteCount()
-                LLog.i(TAG, "S:$downloadSpeed/$uploadSpeed")
                 monitorCallback?.onSpeedChanged(speedArray[0] as Long, speedArray[1] as Long)
             }
         }
@@ -43,31 +39,29 @@ class NetworkMonitor(private val ctx: Context, private val ip: String) {
         private var pingCountdown: Long = 0
         private var strengthCountdown: Long = 0
         private val MAX_COUNT: Long = 10
+        private var showPingLatencyStatus: Int = -1
+        private var showWifiSignalStatus: Int = -1
         override fun run() {
             try {
+                showPingLatencyStatus = -1
+                showWifiSignalStatus = -1
+
                 if (--pingCountdown < 0) {
                     pingCountdown = 0
                 }
                 if (--strengthCountdown < 0) {
                     strengthCountdown = 0
                 }
-                val ping = NetworkUtil.getLatency(ctx, ip, 1).toInt()
+                var ping = NetworkUtil.getLatency(ctx, ip, 1).toInt()
                 val networkStats: IntArray? = NetworkUtil.getWifiNetworkStats(ctx)
-                if (networkStats == null) {
-                    LLog.i(TAG, "P:$ping")
-                } else {
+                if (networkStats != null) {
                     val strengthIn100Score = networkStats[3]
-                    // Ping Link Rssi
-                    LLog.i(
-                        TAG,
-                        "P:$ping\tL:${networkStats[0]}Mbps\tR:${networkStats[1]} ${networkStats[2]} $strengthIn100Score/100"
-                    )
                     if (strengthIn100Score != Int.MIN_VALUE) {
                         if (strengthIn100Score <= NetworkUtil.NETWORK_SIGNAL_STRENGTH_VERY_BAD && strengthCountdown < 1) {
-                            LLog.w(TAG, "Strength: [${networkStats[1]}][${networkStats[2]}][$strengthIn100Score/100] Very bad")
+                            showWifiSignalStatus = NetworkUtil.NETWORK_SIGNAL_STRENGTH_VERY_BAD
                             strengthCountdown = MAX_COUNT
                         } else if (strengthIn100Score <= NetworkUtil.NETWORK_SIGNAL_STRENGTH_BAD && strengthCountdown < 1) {
-                            LLog.w(TAG, "Strength: [${networkStats[1]}][${networkStats[2]}][$strengthIn100Score/100] Bad")
+                            showWifiSignalStatus = NetworkUtil.NETWORK_SIGNAL_STRENGTH_BAD
                             strengthCountdown = MAX_COUNT
                         }
                     }
@@ -75,20 +69,23 @@ class NetworkMonitor(private val ctx: Context, private val ip: String) {
                 // Sometimes, the ping value will be extremely high(like, 319041664, 385195024). For this abnormal case, we just ignore it.
                 if (ping < 60000) {
                     if (ping > NetworkUtil.NETWORK_PING_DELAY_VERY_HIGH && pingCountdown < 1) {
-                        LLog.w(TAG, "P:$ping Very High")
+                        showPingLatencyStatus = NetworkUtil.NETWORK_PING_DELAY_VERY_HIGH
                         pingCountdown = MAX_COUNT
                     } else if (ping > NetworkUtil.NETWORK_PING_DELAY_HIGH && pingCountdown < 1) {
-                        LLog.w(TAG, "P:$ping High")
+                        showPingLatencyStatus = NetworkUtil.NETWORK_PING_DELAY_HIGH
                         pingCountdown = MAX_COUNT
                     }
                 } else {
-                    LLog.w(TAG, "DO NOT show abnormal ping value [$ping]")
+                    // DO NOT show abnormal ping value when its value is greater equal than 60000
+                    ping = -3
                 }
 
                 monitorCallback?.onPingWifiSignalChanged(
                     ping,
+                    showPingLatencyStatus,
                     networkStats?.get(0) ?: -1, networkStats?.get(1) ?: -1,
-                    networkStats?.get(2) ?: -1, networkStats?.get(3) ?: -1
+                    networkStats?.get(2) ?: -1, networkStats?.get(3) ?: -1,
+                    showWifiSignalStatus
                 )
                 monitorHandler.postDelayed(this, TimeUnit.SECONDS.toMillis(freq.toLong()))
             } catch (e: Exception) {
@@ -126,6 +123,14 @@ class NetworkMonitor(private val ctx: Context, private val ip: String) {
 
     interface Callback {
         fun onSpeedChanged(downloadSpeed: Long, uploadSpeed: Long)
-        fun onPingWifiSignalChanged(ping: Int, linkSpeed: Int, rssi: Int, wifiScoreIn5: Int, wifiScore: Int)
+        fun onPingWifiSignalChanged(
+            ping: Int,
+            showPingLatencyToast: Int,
+            linkSpeed: Int,
+            rssi: Int,
+            wifiScoreIn5: Int,
+            wifiScore: Int,
+            showWifiSignalStatus: Int
+        )
     }
 }

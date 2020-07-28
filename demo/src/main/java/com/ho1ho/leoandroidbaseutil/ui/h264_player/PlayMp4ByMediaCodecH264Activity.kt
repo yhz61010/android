a7a2928f16a704bbc73b21907c5f038d88e9b23d
Michael Leo <yhzemail61010@aliyun.com>
2020-07-28 18:32:34 +0800

    Update PlayMp4ByMediaCodecH264Activity demo

--- demo/src/main/java/com/ho1ho/leoandroidbaseutil/ui/h264_player/PlayMp4ByMediaCodecH264Activity.kt
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/ui/h264_player/PlayMp4ByMediaCodecH264Activity.kt
@@ -8,7 +8,6 @@ import com.ho1ho.androidbase.exts.ITAG
 import com.ho1ho.androidbase.utils.LLog
 import com.ho1ho.leoandroidbaseutil.R
 import com.ho1ho.leoandroidbaseutil.ui.base.BaseDemonstrationActivity
-import com.ho1ho.leoandroidbaseutil.ui.h264_player.base.AudioPlayManager
 import com.ho1ho.leoandroidbaseutil.ui.h264_player.base.DecoderManager
 import kotlinx.android.synthetic.main.activity_play_mp4_encode_by_h264.*
 import java.io.File
@@ -33,16 +32,15 @@ class PlayMp4ByMediaCodecH264Activity : BaseDemonstrationActivity() {
         surface = surfaceView.holder.surface
         surfaceView.holder.addCallback(object : SurfaceHolder.Callback {
             override fun surfaceCreated(holder: SurfaceHolder) {
-                val intent = intent
                 isPlayH264 = intent.getBooleanExtra("isPlayH264", false)
                 LLog.w(ITAG, "isPlayH264=$isPlayH264")
                 if (isPlayH264) {
-                    DecoderManager.getInstance().startH264Decode()
-                    return
+                    DecoderManager.startH264Decode()
+                } else {
+                    DecoderManager.startMP4Decode()
+//                    AudioPlayManager.getInstance().setContext(applicationContext)
+//                    AudioPlayManager.getInstance().startThread()
                 }
-                DecoderManager.getInstance().startMP4Decode()
-                AudioPlayManager.getInstance().setContext(applicationContext)
-                AudioPlayManager.getInstance().startThread()
             }
 
             override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) = Unit

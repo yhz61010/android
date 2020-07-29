7e29b480f0ab5d1ca66dcf3f5d6953d7b46275cd
MikeoLeo <yhzemail61010@aliyun.com>
2020-07-29 10:08:04 +0800

    Update PlayMp4ByMediaCodecH264Activity demo

--- demo/src/main/java/com/ho1ho/leoandroidbaseutil/ui/h264_player/PlayMp4ByMediaCodecH264Activity.kt
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/ui/h264_player/PlayMp4ByMediaCodecH264Activity.kt
@@ -8,6 +8,7 @@ import com.ho1ho.androidbase.exts.ITAG
 import com.ho1ho.androidbase.utils.LLog
 import com.ho1ho.leoandroidbaseutil.R
 import com.ho1ho.leoandroidbaseutil.ui.base.BaseDemonstrationActivity
+import com.ho1ho.leoandroidbaseutil.ui.h264_player.base.AudioPlayManager
 import com.ho1ho.leoandroidbaseutil.ui.h264_player.base.DecoderManager
 import kotlinx.android.synthetic.main.activity_play_mp4_encode_by_h264.*
 import java.io.File
@@ -35,11 +36,11 @@ class PlayMp4ByMediaCodecH264Activity : BaseDemonstrationActivity() {
                 isPlayH264 = intent.getBooleanExtra("isPlayH264", false)
                 LLog.w(ITAG, "isPlayH264=$isPlayH264")
                 if (isPlayH264) {
-                    DecoderManager.startH264Decode()
+//                    DecoderManager.startH264Decode()
                 } else {
                     DecoderManager.startMP4Decode()
-//                    AudioPlayManager.getInstance().setContext(applicationContext)
-//                    AudioPlayManager.getInstance().startThread()
+                    AudioPlayManager.getInstance().setContext(applicationContext)
+                    AudioPlayManager.getInstance().startThread()
                 }
             }
 

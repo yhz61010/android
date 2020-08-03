aa2a00230175eed0dca5fa77a32ea28f04f13b8e
yuanhongzhi <yuanhongzhi@zhizhangyi.com>
2020-08-03 13:43:55 +0800

    Add more utils

--- androidbase-sdk/src/main/java/com/ho1ho/androidbase/utils/cipher/MD5Util.kt
+++ androidbase-sdk/src/main/java/com/ho1ho/androidbase/utils/cipher/MD5Util.kt
@@ -1,5 +1,9 @@
 package com.ho1ho.androidbase.utils.cipher
 
+import android.text.TextUtils
+import java.io.File
+import java.io.FileInputStream
+import java.math.BigInteger
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 import java.util.*
@@ -8,7 +12,6 @@ import java.util.*
  * Author: Michael Leo
  * Date: 19-10-15 下午1:49
  */
-@Suppress("unused")
 object MD5Util {
     /**
      * Encodes a string to MD5
@@ -16,7 +19,6 @@ object MD5Util {
      * @param str String to encode
      * @return Encoded String
      */
-    @Suppress("unused")
     fun encrypt(str: String): String {
         if (str.isBlank()) {
             return ""
@@ -38,5 +40,29 @@ object MD5Util {
             e.printStackTrace()
         }
         return ""
+    }
+
+    fun checkMd5(md5: String, updateFile: File) = calculateFileMd5(updateFile).let {
+        if (TextUtils.isEmpty(it)) false else it.equals(md5, ignoreCase = true)
+    }
+
+    fun calculateFileMd5(file: File): String {
+        if (!file.isFile) {
+            return ""
+        }
+        val buffer = ByteArray(8 shl 10)
+        var len: Int
+        return kotlin.runCatching {
+            FileInputStream(file).use { fis ->
+                val digest = MessageDigest.getInstance("MD5")
+                while (fis.read(buffer, 0, buffer.size).also { len = it } != -1) {
+                    digest.update(buffer, 0, len)
+                }
+                val md5sum = digest.digest()
+                val bigInt = BigInteger(1, md5sum)
+                val output = bigInt.toString(16)
+                String.format("%32s", output).replace(' ', '0')
+            }
+        }.getOrDefault("")
     }
 }

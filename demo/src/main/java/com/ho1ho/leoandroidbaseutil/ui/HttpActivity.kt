9b9c16b48e780e448839c8c8a2a01780e9aee165
MikeoLeo <yhzemail61010@aliyun.com>
2020-08-04 18:10:50 +0800

    Add HTTP demo

--- demo/src/main/java/com/ho1ho/leoandroidbaseutil/ui/HttpActivity.kt
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/ui/HttpActivity.kt
@@ -14,8 +14,7 @@ import com.ho1ho.leoandroidbaseutil.R
 import com.ho1ho.leoandroidbaseutil.ui.base.BaseDemonstrationActivity
 import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_http.*
-import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.*
 
 class HttpActivity : BaseDemonstrationActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +25,11 @@ class HttpActivity : BaseDemonstrationActivity() {
     interface CommonService {
         @GET("/data/sk/{id}.html")
         fun getData(@Path("id") param: String): Observable<WeatherInfoResult>
+
+        @FormUrlEncoded
+        @Headers("Authorization:APPCODE Add-you-app-code-here")
+        @POST("/ai_market/ai_face_position")
+        fun postData(@Field("VIDEO") video: String): Observable<String>
     }
 
     fun onGetClick(@Suppress("UNUSED_PARAMETER") view: View) {
@@ -37,6 +41,21 @@ class HttpActivity : BaseDemonstrationActivity() {
         }
         val service = ApiService.getService("http://www.weather.com.cn", CommonService::class.java)
         ApiSubscribe.subscribe(service.getData("101070201"), NoProgressObserver(observer))
+    }
+
+    fun onPostClick(@Suppress("UNUSED_PARAMETER") view: View) {
+        txtResult.text = "It will cost serveral seconds. Please be patient..."
+        val observer: ObserverOnNextListener<String> = object : ObserverOnNextListener<String> {
+            override fun onNext(t: String) {
+                LLog.w(ITAG, "Response=$t")
+                txtResult.text = t
+            }
+        }
+        val service = ApiService.getService("https://iface.market.alicloudapi.com", CommonService::class.java)
+        ApiSubscribe.subscribe(
+            service.postData("https://icredit-api-market.oss-cn-hangzhou.aliyuncs.com/%E8%89%BE%E7%A7%91%E7%91%9E%E7%89%B9_%E6%99%BA%E8%83%BD%E5%9B%BE%E5%83%8F%E8%AF%86%E5%88%AB_%E6%99%BA%E8%83%BD%E4%BA%BA%E8%84%B8%E5%A7%BF%E6%80%81%E6%A3%80%E6%B5%8B/%E4%BA%BA%E8%84%B8%E5%A7%BF%E6%80%81.mp4"),
+            NoProgressObserver(observer)
+        )
     }
 
     @Keep

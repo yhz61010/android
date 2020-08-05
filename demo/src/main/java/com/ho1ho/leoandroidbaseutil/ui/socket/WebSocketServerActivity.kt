6befe1e6b0893dd76212aaec3b5cc43f755dfbe5
MikeoLeo <yhzemail61010@aliyun.com>
2020-08-05 19:43:34 +0800

    Refactor Netty

--- demo/src/main/java/com/ho1ho/leoandroidbaseutil/ui/socket/WebSocketServerActivity.kt
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/ui/socket/WebSocketServerActivity.kt
@@ -10,36 +10,34 @@ import com.ho1ho.leoandroidbaseutil.R
 import com.ho1ho.leoandroidbaseutil.ui.base.BaseDemonstrationActivity
 import com.ho1ho.socket_sdk.framework.BaseChannelInboundHandler
 import com.ho1ho.socket_sdk.framework.BaseNettyClient
+import com.ho1ho.socket_sdk.framework.BaseNettyServer
 import com.ho1ho.socket_sdk.framework.inter.NettyConnectionListener
-import com.ho1ho.socket_sdk.framework.retry_strategy.ConstantRetry
 import com.ho1ho.socket_sdk.framework.retry_strategy.base.RetryStrategy
 import io.netty.channel.ChannelHandler
 import io.netty.channel.ChannelHandlerContext
-import io.netty.channel.ChannelPipeline
-import io.netty.handler.codec.DelimiterBasedFrameDecoder
-import io.netty.handler.codec.Delimiters
 import io.netty.handler.codec.http.websocketx.PongWebSocketFrame
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame
 import io.netty.handler.codec.http.websocketx.WebSocketFrame
-import io.netty.handler.codec.string.StringDecoder
-import io.netty.handler.codec.string.StringEncoder
 import kotlinx.android.synthetic.main.activity_socket_client.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.net.URI
 import java.nio.charset.Charset
 
-class WebSocketClientActivity : BaseDemonstrationActivity() {
+class WebSocketServerActivity : BaseDemonstrationActivity() {
+
+    companion object {
+        const val TAG = "WebSocketServerActivity"
+    }
+
     private val cs = CoroutineScope(Dispatchers.IO)
 
-    private lateinit var webSocketClient: WebSocketClient
-    private lateinit var webSocketClientHandler: WebSocketClientHandler
+    private lateinit var webSocketServer: WebSocketServer
+    private lateinit var webSocketServerHandler: WebSocketServerHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_websocket_client)
+        setContentView(R.layout.activity_web_socket_server)
 
         val connectionListener = object : NettyConnectionListener {
             override fun onConnected(netty: BaseNettyClient) {
@@ -69,66 +67,41 @@ class WebSocketClientActivity : BaseDemonstrationActivity() {
             }
         }
 
-        webSocketClient = WebSocketClient(URI("ws://50d.win:9090/ws"), connectionListener, ConstantRetry(10, 2000))
-        webSocketClientHandler = WebSocketClientHandler(webSocketClient)
-        webSocketClient.initHandler(webSocketClientHandler)
+//        webSocketServer = WebSocketServer(10086, connectionListener, ConstantRetry(10, 2000))
+//        webSocketServerHandler = WebSocketServerHandler(webSocketServer)
+//        webSocketServer.initHandler(webSocketServerHandler)
+    }
+
+    fun onStartServerClick(@Suppress("UNUSED_PARAMETER") view: View) {
+        cs.launch {
+            repeat(1) {
+//                webSocketServer.connect()
+            }
+        }
     }
 
     override fun onDestroy() {
         cs.launch {
-            webSocketClient.release()
+//            webSocketServer.release()
         }
         super.onDestroy()
     }
 
-    fun onConnectClick(@Suppress("UNUSED_PARAMETER") view: View) {
-        cs.launch {
-            repeat(1) {
-                webSocketClient.connect()
-
-                // You can also create multiple sockets at the same time like this(It's thread safe so you can create them freely):
-                // val socketClient = SocketClient("50d.win", 8080, connectionListener)
-                // val socketClientHandler = SocketClientHandler(socketClient)
-                // socketClient.initHandler(socketClientHandler)
-                // socketClient.connect()
-            }
-        }
-    }
-
-    fun sendMsg(@Suppress("UNUSED_PARAMETER") view: View) {
-        cs.launch {
-            val result = webSocketClientHandler.sendMsgToServer(editText.text.toString())
-            withContext(Dispatchers.Main) { editText.text.clear();if (!result) ToastUtil.showDebugErrorToast("Send command error") }
-        }
-    }
-
-    fun onDisconnectClick(@Suppress("UNUSED_PARAMETER") view: View) {
-        cs.launch {
-            webSocketClient.disconnectManually()
-        }
-    }
-
-    fun onConnectRelease(@Suppress("UNUSED_PARAMETER") view: View) {
-        cs.launch {
-            webSocketClient.release()
-        }
-    }
-
     // =====================================================
 
-    class WebSocketClient(webSocketUri: URI, connectionListener: NettyConnectionListener, retryStrategy: RetryStrategy) :
-        BaseNettyClient(webSocketUri, connectionListener, retryStrategy) {
-        override fun addLastToPipeline(pipeline: ChannelPipeline) {
-            with(pipeline) {
-                addLast(DelimiterBasedFrameDecoder(65535, *Delimiters.lineDelimiter()))
-                addLast(StringDecoder())
-                addLast(StringEncoder())
-            }
-        }
+    class WebSocketServer(port: Int, connectionListener: NettyConnectionListener, retryStrategy: RetryStrategy) :
+        BaseNettyServer(port, connectionListener, retryStrategy) {
+//        override fun addLastToPipeline(pipeline: ChannelPipeline) {
+//            with(pipeline) {
+//                addLast(DelimiterBasedFrameDecoder(65535, *Delimiters.lineDelimiter()))
+//                addLast(StringDecoder())
+//                addLast(StringEncoder())
+//            }
+//        }
     }
 
     @ChannelHandler.Sharable
-    class WebSocketClientHandler(private val netty: BaseNettyClient) : BaseChannelInboundHandler<Any>(netty) {
+    class WebSocketServerHandler(private val netty: BaseNettyClient) : BaseChannelInboundHandler<Any>(netty) {
         override fun onReceivedData(ctx: ChannelHandlerContext, msg: Any) {
             val receivedString: String?
             val frame = msg as WebSocketFrame
@@ -152,9 +125,5 @@ class WebSocketClientActivity : BaseDemonstrationActivity() {
 
         override fun release() {
         }
-    }
-
-    companion object {
-        const val TAG = "WebSocketClient"
     }
 }

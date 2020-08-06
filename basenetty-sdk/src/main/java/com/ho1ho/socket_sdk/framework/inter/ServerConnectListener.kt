1724c7eacfc7325ecbc829b3a9f1eddae6e19a0f
MikeoLeo <yhzemail61010@aliyun.com>
2020-08-06 19:21:12 +0800

    Refactor Netty[Server/Client]

--- basenetty-sdk/src/main/java/com/ho1ho/socket_sdk/framework/inter/ServerConnectListener.kt
+++ basenetty-sdk/src/main/java/com/ho1ho/socket_sdk/framework/inter/ServerConnectListener.kt
@@ -1,6 +1,7 @@
 package com.ho1ho.socket_sdk.framework.inter
 
 import com.ho1ho.socket_sdk.framework.BaseNetty
+import io.netty.channel.Channel
 
 /**
  * Author: Michael Leo
@@ -8,10 +9,11 @@ import com.ho1ho.socket_sdk.framework.BaseNetty
  */
 interface ServerConnectListener {
 
-    fun onConnected(netty: BaseNetty)
-    fun onReceivedData(netty: BaseNetty, data: Any?)
-    fun onDisconnected(netty: BaseNetty)
+    fun onStarted(netty: BaseNetty)
     fun onFailed(netty: BaseNetty, code: Int, msg: String?)
+    fun onReceivedData(netty: BaseNetty, data: Any?)
+    fun onClientConnected(netty: BaseNetty, clientChannel: Channel)
+    fun onClientDisconnected(netty: BaseNetty, clientChannel: Channel)
     fun onException(netty: BaseNetty, cause: Throwable)
 
     //    fun onConnectionTimeout()
@@ -26,14 +28,14 @@ interface ServerConnectListener {
 //    fun onAllIdle(ctx: ChannelHandlerContext?) {}
 
     companion object {
-        const val CONNECTION_ERROR_ALREADY_RELEASED = 0x1000
+        const val CONNECTION_ERROR_ALREADY_RELEASED = 0x8000
 
         @Suppress("unused")
-        const val CONNECTION_ERROR_CAN_NOT_CONNECT_TO_SERVER = 0x1001
-        const val CONNECTION_ERROR_CONNECT_EXCEPTION = 0x1002
-        const val CONNECTION_ERROR_UNEXPECTED_EXCEPTION = 0x1003
-        const val CONNECTION_ERROR_EXCEED_MAX_RETRY_TIMES = 0x1004
-        const val CONNECTION_ERROR_SERVER_DOWN = 0x1005
-        const val CONNECTION_ERROR_NETWORK_LOST = 0x1006
+        const val CONNECTION_ERROR_SERVER_START_ERROR = 0x8001
+        const val CONNECTION_ERROR_CONNECT_EXCEPTION = 0x8002
+        const val CONNECTION_ERROR_UNEXPECTED_EXCEPTION = 0x8003
+        const val CONNECTION_ERROR_EXCEED_MAX_RETRY_TIMES = 0x8004
+        const val CONNECTION_ERROR_SERVER_DOWN = 0x8005
+        const val CONNECTION_ERROR_NETWORK_LOST = 0x8006
     }
 }

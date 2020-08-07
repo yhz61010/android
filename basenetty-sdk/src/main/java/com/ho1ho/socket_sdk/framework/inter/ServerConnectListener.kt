c900003590cc89175f8bf46ea4680fe6a2b51d03
yuanhongzhi <yuanhongzhi@zhizhangyi.com>
2020-08-07 14:38:05 +0800

    Refactor Netty[Server/Client]

--- basenetty-sdk/src/main/java/com/ho1ho/socket_sdk/framework/inter/ServerConnectListener.kt
+++ basenetty-sdk/src/main/java/com/ho1ho/socket_sdk/framework/inter/ServerConnectListener.kt
@@ -19,8 +19,8 @@ interface ServerConnectListener<in T : BaseNetty> {
     companion object {
         const val CONNECTION_ERROR_ALREADY_RELEASED = 0x8000
         const val CONNECTION_ERROR_SERVER_START_ERROR = 0x8001
-        const val CONNECTION_ERROR_CONNECT_EXCEPTION = 0x8002
         const val CONNECTION_ERROR_UNEXPECTED_EXCEPTION = 0x8003
+        const val CONNECTION_ERROR_CONNECT_EXCEPTION = 0x8002
         const val CONNECTION_ERROR_EXCEED_MAX_RETRY_TIMES = 0x8004
         const val CONNECTION_ERROR_SERVER_DOWN = 0x8005
     }

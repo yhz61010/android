3bd1fb248412854813d882f3ef50587ea8d657d5
yuanhongzhi <yuanhongzhi@zhizhangyi.com>
2020-08-07 10:15:44 +0800

    Refactor Netty[Server/Client]

--- basenetty-sdk/src/main/java/com/ho1ho/socket_sdk/framework/inter/ServerConnectListener.kt
+++ basenetty-sdk/src/main/java/com/ho1ho/socket_sdk/framework/inter/ServerConnectListener.kt
@@ -16,26 +16,12 @@ interface ServerConnectListener {
     fun onClientConnected(netty: BaseNetty, clientChannel: Channel)
     fun onClientDisconnected(netty: BaseNetty, clientChannel: Channel)
 
-    //    fun onConnectionTimeout()
-
-    // =========================================
-//    fun sendHeartbeat() {}
-//    fun onReceiveHeartbeat(msg: Any?) {}
-
-    // =========================================
-//    fun onReaderIdle(ctx: ChannelHandlerContext?) {}
-//    fun onWriterIdle(ctx: ChannelHandlerContext?) {}
-//    fun onAllIdle(ctx: ChannelHandlerContext?) {}
-
     companion object {
         const val CONNECTION_ERROR_ALREADY_RELEASED = 0x8000
-
-        @Suppress("unused")
         const val CONNECTION_ERROR_SERVER_START_ERROR = 0x8001
         const val CONNECTION_ERROR_CONNECT_EXCEPTION = 0x8002
         const val CONNECTION_ERROR_UNEXPECTED_EXCEPTION = 0x8003
         const val CONNECTION_ERROR_EXCEED_MAX_RETRY_TIMES = 0x8004
         const val CONNECTION_ERROR_SERVER_DOWN = 0x8005
-        const val CONNECTION_ERROR_NETWORK_LOST = 0x8006
     }
 }

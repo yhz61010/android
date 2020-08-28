c3dc152a383058cb0d9c4d0a34cb59b50209d92b
yuanhongzhi <yuanhongzhi@zhizhangyi.com>
2020-08-28 19:58:33 +0800

    improve KeepAlive

--- androidbase-sdk/src/main/java/com/ho1ho/androidbase/utils/system/KeepAlive.kt
+++ androidbase-sdk/src/main/java/com/ho1ho/androidbase/utils/system/KeepAlive.kt
@@ -7,7 +7,6 @@ import android.media.AudioManager
 import android.media.AudioTrack
 import com.ho1ho.androidbase.BuildConfig
 import com.ho1ho.androidbase.R
-import com.ho1ho.androidbase.exts.ITAG
 import com.ho1ho.androidbase.utils.LLog
 import kotlinx.coroutines.*
 import java.io.InputStream
@@ -25,12 +24,17 @@ class KeepAlive(private val context: Context) {
     }
 
     private val ioScope = CoroutineScope(Dispatchers.IO)
-    private lateinit var audioTrack: AudioTrack
     private lateinit var inputStream: InputStream
     private lateinit var musicBytes: ByteArray
 
+    private lateinit var audioAttributes: AudioAttributes
+    private var audioTrack: AudioTrack? = null
+    private lateinit var audioFormat: AudioFormat
+    private var bufferSize = 0
+    private var sessionId = 0
+
     fun keepAlive() {
-        LLog.w(ITAG, "Start keepAlive()")
+        LLog.w(TAG, "Start keepAlive()")
         ioScope.launch {
             runCatching {
                 inputStream = context.resources.openRawResource(R.raw.magic)
@@ -48,42 +52,48 @@ class KeepAlive(private val context: Context) {
     }
 
     fun release() {
-        LLog.w(ITAG, "Release keepAlive()")
+        LLog.w(TAG, "Release keepAlive()")
+        ioScope.launch { stop() }
         ioScope.cancel()
-        stop()
     }
 
     private fun init() {
         val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        val bufferSize = AudioTrack.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT) * 2
-        val sessionId = audioManager.generateAudioSessionId()
-        val audioAttributesBuilder = AudioAttributes.Builder().apply {
+        bufferSize = AudioTrack.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT)
+        sessionId = audioManager.generateAudioSessionId()
+        audioAttributes = AudioAttributes.Builder().apply {
             setUsage(AudioAttributes.USAGE_MEDIA)              // AudioAttributes.USAGE_MEDIA         AudioAttributes.USAGE_VOICE_COMMUNICATION
             setContentType(AudioAttributes.CONTENT_TYPE_MUSIC) // AudioAttributes.CONTENT_TYPE_MUSIC   AudioAttributes.CONTENT_TYPE_SPEECH
             setLegacyStreamType(AudioManager.STREAM_MUSIC)
-        }
-        val audioFormat = AudioFormat.Builder().setSampleRate(SAMPLE_RATE)
+        }.build()
+        audioFormat = AudioFormat.Builder().setSampleRate(SAMPLE_RATE)
             .setEncoding(AUDIO_FORMAT)
             .setChannelMask(CHANNEL_CONFIG)
             .build()
-        audioTrack = AudioTrack(audioAttributesBuilder.build(), audioFormat, bufferSize * 2, AudioTrack.MODE_STREAM, sessionId)
-        runCatching { audioTrack.play() }.onFailure { it.printStackTrace() }
+        audioTrack = AudioTrack(audioAttributes, audioFormat, bufferSize, AudioTrack.MODE_STREAM, sessionId)
+        audioTrack?.setVolume(0F)
+        audioTrack?.play()
     }
 
     private fun play() {
-        runCatching { audioTrack.write(musicBytes, 0, musicBytes.size) }.onFailure { it.printStackTrace() }
+        runCatching { audioTrack?.write(musicBytes, 0, musicBytes.size) }.onFailure { it.printStackTrace() }
+    }
+
+    private fun pause() {
+        runCatching {
+            audioTrack?.pause()
+            audioTrack?.flush()
+        }.onFailure { it.printStackTrace() }
     }
 
     private fun stop() {
+        pause()
         runCatching {
-            audioTrack.pause()
-            audioTrack.flush()
+            audioTrack?.stop()
         }.onFailure { it.printStackTrace() }
         runCatching {
-            audioTrack.stop()
+            audioTrack?.release()
         }.onFailure { it.printStackTrace() }
-        runCatching {
-            audioTrack.release()
-        }.onFailure { it.printStackTrace() }
+        audioTrack = null
     }
 }

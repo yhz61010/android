175244d99a7005009f7d5d346c1c3c375834cedb
MikeoLeo <yhzemail61010@aliyun.com>
2020-09-03 20:28:36 +0800

    developing

--- demo/src/main/java/com/ho1ho/leoandroidbaseutil/jetpack_components/JetpackFragment.kt
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/jetpack_components/JetpackFragment.kt
@@ -6,25 +6,65 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ho1ho.leoandroidbaseutil.R
 
 class JetpackFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: JetpackViewModel
+    companion object {
+        val mNicolasCageMovies = listOf(
+            Movie("Raising Arizona", 1987),
+            Movie("Vampire's Kiss", 1988),
+            Movie("Con Air", 1997),
+            Movie("Gone in 60 Seconds", 1997),
+            Movie("National Treasure", 2004),
+            Movie("The Wicker Man", 2006),
+            Movie("Ghost Rider", 2007),
+            Movie("Knowing", 2009)
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(JetpackViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_jetpack, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, {
-            textView.text = it
-        })
-        return root
+    ): View? = inflater.inflate(R.layout.fragment_jetpack, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<RecyclerView>(R.id.recyclerView).run {
+            layoutManager = LinearLayoutManager(requireActivity())
+            adapter = ListAdapter(JetpackFragment.mNicolasCageMovies)
+        }
     }
 }
+
+class MovieViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+    RecyclerView.ViewHolder(inflater.inflate(R.layout.grid_item, parent, false)) {
+    private var mTitleView: TextView? = null
+
+    init {
+        mTitleView = itemView.findViewById(R.id.name)
+    }
+
+    fun bind(movie: Movie) {
+        mTitleView?.text = movie.title
+    }
+}
+
+class ListAdapter(private val list: List<Movie>) : RecyclerView.Adapter<MovieViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return MovieViewHolder(inflater, parent)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val movie: Movie = list[position]
+        holder.bind(movie)
+    }
+
+    override fun getItemCount(): Int = list.size
+}
+
+data class Movie(val title: String, val year: Int)

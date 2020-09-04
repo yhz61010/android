01b7fa7239ca8e60e1d0c2c7602a395a894d8ba6
MikeoLeo <yhzemail61010@aliyun.com>
2020-09-04 14:32:03 +0800

    add room demo

---
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/jetpack_components/examples/room/RoomActivity.kt
@@ -0,0 +1,58 @@
+package com.ho1ho.leoandroidbaseutil.jetpack_components.examples.room
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.ho1ho.leoandroidbaseutil.R
+import com.ho1ho.leoandroidbaseutil.base.BaseDemonstrationActivity
+import com.ho1ho.leoandroidbaseutil.jetpack_components.examples.room.entity.Word
+
+class RoomActivity : BaseDemonstrationActivity() {
+
+    private val newWordActivityRequestCode = 1
+    private lateinit var wordViewModel: WordViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        wordViewModel = ViewModelProvider(this).get(WordViewModel::class.java)
+        wordViewModel.allWords.observe(this, { words ->
+            // Update the cached copy of the words in the adapter.
+            words?.let { adapter.setWords(it) }
+        })
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@RoomActivity, NewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let {
+                val word = Word(it)
+                wordViewModel.insert(word)
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                R.string.empty_not_saved,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+}

c9b22c6b8ac806c2a4b1da6abdbe3074012da0df
MikeoLeo <yhzemail61010@aliyun.com>
2020-09-04 11:19:33 +0800

    add more utils

---
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/jetpack_components/examples/RoomActivity.kt
@@ -0,0 +1,12 @@
+package com.ho1ho.leoandroidbaseutil.jetpack_components.examples
+
+import android.os.Bundle
+import com.ho1ho.leoandroidbaseutil.R
+import com.ho1ho.leoandroidbaseutil.base.BaseDemonstrationActivity
+
+class RoomActivity : BaseDemonstrationActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room)
+    }
+}

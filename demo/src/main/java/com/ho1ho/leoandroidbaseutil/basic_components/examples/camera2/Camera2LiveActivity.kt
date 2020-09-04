a2619c4054320493115337ffccaf20a8eec58ba6
MikeoLeo <yhzemail61010@aliyun.com>
2020-09-04 11:12:03 +0800

    developing

---
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/basic_components/examples/camera2/Camera2LiveActivity.kt
@@ -0,0 +1,107 @@
+package com.ho1ho.leoandroidbaseutil.basic_components.examples.camera2
+
+import android.app.Activity
+import android.content.Intent
+import android.media.MediaFormat
+import android.os.Bundle
+import android.os.Environment
+import android.view.WindowManager
+import android.widget.Toast
+import com.ho1ho.androidbase.utils.AppUtil
+import com.ho1ho.androidbase.utils.LLog
+import com.ho1ho.androidbase.utils.media.CameraUtil
+import com.ho1ho.androidbase.utils.media.CodecUtil
+import com.ho1ho.camera2live.view.BackPressedListener
+import com.ho1ho.leoandroidbaseutil.R
+import com.ho1ho.leoandroidbaseutil.base.BaseDemonstrationActivity
+import com.yanzhenjie.permission.AndPermission
+import com.yanzhenjie.permission.runtime.Permission
+import java.io.File
+import java.io.FileOutputStream
+
+class Camera2LiveActivity : BaseDemonstrationActivity() {
+    private val cameraViewFragment = Camera2LiveFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AppUtil.requestFullScreen(this)
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        setContentView(R.layout.activity_camera2_live)
+
+        CodecUtil.getEncoderListByMimeType(MediaFormat.MIMETYPE_VIDEO_AVC)
+            .forEach { LLog.e(TAG, "H264 Encoder: ${it.name}") }
+        val hasTopazEncoder =
+            CodecUtil.hasEncoderByCodecName(MediaFormat.MIMETYPE_VIDEO_AVC, "OMX.IMG.TOPAZ.VIDEO.Encoder")
+        LLog.e(TAG, "hasTopazEncoder=$hasTopazEncoder")
+
+        cameraViewFragment.backPressListener = object :
+            BackPressedListener {
+            override fun onBackPressed() {
+                this@Camera2LiveActivity.onBackPressed()
+            }
+        }
+
+        if (AndPermission.hasPermissions(this, Permission.CAMERA)) {
+            addFragment()
+        } else {
+            AndPermission.with(this)
+                .runtime()
+                .permission(Permission.CAMERA)
+                .onGranted {
+                    Toast.makeText(this, "Grant camera permission", Toast.LENGTH_SHORT).show()
+                    addFragment()
+                }
+                .onDenied { Toast.makeText(this, "Deny camera permission", Toast.LENGTH_SHORT).show();finish() }
+                .start()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (CameraUtil.REQUEST_CODE_OPEN_GALLERY == requestCode && resultCode == Activity.RESULT_OK) {
+            LLog.i(TAG, "OPEN_GALLERY onActivityResult")
+//            CameraUtil.handleImageAboveKitKat(this, data).forEach { CLog.i(TAG, "Selected image=$it") }
+            // The following code is just for demo. The exception is not considered.
+            data?.data?.let {
+                // In Android 10+, I really do not know how to get the file real path.
+                // According to the post [https://stackoverflow.com/a/2790688],
+                // there is no need for us to know the real path. I just need to get the InputStream directly. That's enough.
+                // Set uri for ImageView
+//                ImageView(this).setImageURI(uri)
+                // Or get file input stream.
+                val inputStream = contentResolver.openInputStream(it)!!
+                val outputStream = FileOutputStream(File(getExternalFilesDir(Environment.DIRECTORY_PICTURES)?.absolutePath!!, "os.jpg"))
+                inputStream.copyTo(outputStream)
+                LLog.e(TAG, "File output")
+            }
+        }
+    }
+
+    private fun addFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.cameraFragment, cameraViewFragment, "cameraview")
+            .addToBackStack(null)
+            .commitAllowingStateLoss()
+    }
+
+    override fun onResume() {
+        AppUtil.hideNavigationBar(this)
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        supportFragmentManager.beginTransaction()
+            .remove(cameraViewFragment)
+            .commitAllowingStateLoss()
+        super.onDestroy()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+
+    companion object {
+        private const val TAG = "Camera2LiveActivity"
+    }
+}
---
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/basic_components/examples/sharescreen/master/MediaProjectionService.kt
@@ -0,0 +1,255 @@
+package com.ho1ho.leoandroidbaseutil.basic_components.examples.sharescreen.master
+
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ServiceInfo
+import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
+import android.os.*
+import androidx.core.app.NotificationCompat
+import com.ho1ho.androidbase.exts.ITAG
+import com.ho1ho.androidbase.exts.toJsonString
+import com.ho1ho.androidbase.utils.LLog
+import com.ho1ho.androidbase.utils.media.H264Util
+import com.ho1ho.androidbase.utils.notification.NotificationUtil
+import com.ho1ho.leoandroidbaseutil.R
+import com.ho1ho.leoandroidbaseutil.basic_components.BasicFragment
+import com.ho1ho.screenshot.ScreenCapture
+import com.ho1ho.screenshot.base.ScreenDataListener
+import com.ho1ho.screenshot.base.ScreenProcessor
+import com.ho1ho.screenshot.base.ScreenRecordMediaCodecStrategy
+import okhttp3.internal.closeQuietly
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileOutputStream
+
+/**
+ * Android Q+(Android 10+) MediaProjection must be used in Service and with android:foregroundServiceType="mediaProjection" permission.
+ * Example:
+ * <pre>
+ * <uses-permission android:name="android.permission.FOREGROUND_SERVICE" />
+ *
+ * <service
+ *  android:name=".MediaProjectionService"
+ *  tools:targetApi="q"
+ *  android:enabled="true"
+ *  android:exported="false"
+ *  android:foregroundServiceType="mediaProjection" />
+ * </pre>
+ */
+
+class MediaProjectionService : Service() {
+
+    var outputH264File = false
+    private lateinit var videoH264File: File
+    private lateinit var videoH264Os: BufferedOutputStream
+
+    private var resultCode = -1
+    private lateinit var data: Intent
+
+    private lateinit var serviceThread: HandlerThread
+    private lateinit var serviceHandler: Handler
+    private val binder = CustomBinder()
+    private lateinit var mediaProjectionManager: MediaProjectionManager
+    private lateinit var mediaProjection: MediaProjection
+    private var screenProcessor: ScreenProcessor? = null
+
+    val spsPps get() = run { (screenProcessor as? ScreenRecordMediaCodecStrategy)?.spsPpsBuf }
+
+    var screenDataUpdateListener: ScreenDataUpdateListener? = null
+
+    private val screenDataListener = object : ScreenDataListener {
+        override fun onDataUpdate(buffer: Any, flags: Int) {
+            val buf = buffer as ByteArray
+            if (outputH264File) {
+                try {
+                    videoH264Os.write(buf)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+            screenDataUpdateListener?.onUpdate(buffer)
+            // Bitmap for screenshot
+//            val bitmap = buffer as Bitmap
+//            FileUtil.writeBitmapToFile(bitmap, 0)
+        }
+    }
+
+    override fun onCreate() {
+        LLog.i(ITAG, "=====> onCreate <=====")
+        super.onCreate()
+        serviceThread = HandlerThread("service-thread")
+        serviceThread.start()
+        serviceHandler = Handler(serviceThread.looper)
+        startForeground()
+    }
+
+    private fun setDebugInfo() {
+        if (outputH264File) {
+            try {
+                val baseFolder = File(getExternalFilesDir(null)?.absolutePath + File.separator + "leo-media")
+                baseFolder.mkdirs()
+                videoH264File = File(baseFolder, "screen.h264")
+                videoH264Os = BufferedOutputStream(FileOutputStream(videoH264File))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        LLog.i(ITAG, "=====> onBind <=====")
+        return binder
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        LLog.i(ITAG, "=====> onStartCommand <=====")
+        return START_STICKY
+    }
+
+    override fun onRebind(intent: Intent?) {
+        LLog.i(ITAG, "=====> onRebind <=====")
+        super.onRebind(intent)
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        LLog.w(ITAG, "=====> onUnbind <=====")
+        stopScreenShare()
+        return false
+    }
+
+    override fun onDestroy() {
+        LLog.w(ITAG, "=====> onDestroy <=====")
+        super.onDestroy()
+    }
+
+    private fun startForeground() {
+        var channelId: String? = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channelId = NotificationUtil.createNotificationChannel(
+                this,
+                false,
+                "default-foreground-notification",
+                "Foreground Service"
+            )
+        }
+
+        val notifyIntent = Intent(this, BasicFragment::class.java)
+        val mainPendingIntent = PendingIntent.getActivity(
+            this,
+            SystemClock.elapsedRealtime().toInt(),
+            notifyIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val notification = NotificationUtil.generateBigTextStyleNotification(
+            this,
+            channelId,
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher,
+            getString(R.string.app_name),
+            "App is running...",
+            null,
+            true,
+            NotificationCompat.BADGE_ICON_SMALL,
+            NotificationCompat.PRIORITY_DEFAULT,
+            NotificationCompat.VISIBILITY_PUBLIC,
+            null,
+            mainPendingIntent
+        )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            startForeground(10086, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PROJECTION)
+        } else {
+            startForeground(10086, notification)
+        }
+    }
+
+    /**
+     * This method must be called before `startScreenShare()`
+     */
+    fun setData(resultCode: Int, data: Intent) {
+        this.resultCode = resultCode
+        this.data = data
+    }
+
+    // ==========================================
+
+    inner class CustomBinder : Binder() {
+        val service: MediaProjectionService get() = this@MediaProjectionService
+    }
+
+    /**
+     * This method must be following `setData()` method
+     */
+    fun startScreenShare(setting: ScreenShareSetting) {
+        LLog.i(ITAG, "startScreenShare: ${setting.toJsonString()}")
+        setDebugInfo()
+        mediaProjectionManager = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, data)
+
+        screenProcessor = ScreenCapture.Builder(
+            setting.width, // 600 768 720     [1280, 960][1280, 720][960, 720][720, 480]
+            setting.height, // 800 1024 1280
+            setting.dpi,
+            mediaProjection,
+            // SCREEN_CAPTURE_TYPE_IMAGE
+            // SCREEN_CAPTURE_TYPE_MEDIACODEC
+            // SCREEN_CAPTURE_TYPE_X264
+            ScreenCapture.BY_MEDIA_CODEC,
+            screenDataListener
+        )
+            .setFps(setting.fps)
+            // For H264 and x264
+            .setBitrate(setting.bitrate)
+            // For H264
+            .setBitrateMode(setting.bitrateMode)
+            .setKeyFrameRate(setting.keyFrameRate)
+            .setIFrameInterval(setting.iFrameInterval) // 20
+            // For bitmap
+//          .setSampleSize(2)
+            .build().apply {
+                onInit()
+                onStart()
+            }
+    }
+
+    fun stopScreenShare() {
+        LLog.w(ITAG, "stopScreenShare")
+        if (outputH264File) {
+            try {
+                videoH264Os.flush()
+                videoH264Os.closeQuietly()
+            } catch (e: java.lang.Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        serviceHandler.post {
+            LLog.w(ITAG, "screenProcessor onStop()")
+            screenProcessor?.onStop()
+        }
+    }
+
+    fun onReleaseScreenShare() {
+        LLog.w(ITAG, "onReleaseScreenShare()")
+        stopScreenShare()
+        serviceHandler.post {
+            LLog.w(ITAG, "onReleaseScreenShare onRelease()")
+            screenProcessor?.onRelease()
+        }
+    }
+
+    @Suppress("WeakerAccess")
+    fun triggerIFrame() {
+        LLog.w(ITAG, "triggerIFrame()")
+        val encoder = (screenProcessor as? ScreenRecordMediaCodecStrategy)?.h264Encoder
+        encoder?.let { H264Util.sendIdrFrameByManual(it) }
+    }
+}
+
+interface ScreenDataUpdateListener {
+    fun onUpdate(data: Any)
+}

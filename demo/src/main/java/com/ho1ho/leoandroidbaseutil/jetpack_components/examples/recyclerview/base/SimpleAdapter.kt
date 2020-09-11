3ec36f984c1938b67e645e5a062a283a497a827d
MikeoLeo <yhzemail61010@aliyun.com>
2020-09-11 15:25:07 +0800

    add more examples for RecyclerView

--- demo/src/main/java/com/ho1ho/leoandroidbaseutil/jetpack_components/examples/recyclerview/base/SimpleAdapter.kt
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/jetpack_components/examples/recyclerview/base/SimpleAdapter.kt
@@ -1,6 +1,8 @@
 package com.ho1ho.leoandroidbaseutil.jetpack_components.examples.recyclerview.base
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -12,20 +14,22 @@ import com.google.android.material.shape.CornerFamily
 import com.ho1ho.leoandroidbaseutil.R
 import com.ho1ho.leoandroidbaseutil.jetpack_components.examples.recyclerview.ItemBean
 
+
 /**
  * Author: Michael Leo
  * Date: 2020/9/11 上午11:24
  */
-class SimpleAdapter(private val dataArray: MutableList<ItemBean>) :
-    RecyclerView.Adapter<SimpleAdapter.ItemViewHolder>() {
+class SimpleAdapter(private val dataArray: MutableList<ItemBean>) : RecyclerView.Adapter<SimpleAdapter.ItemViewHolder>() {
     var onItemClickListener: OnItemClickListener? = null
     private var lastDeletedItem: Pair<Int, ItemBean>? = null
+    var startDragListener: OnStartDragListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_demo_item, parent, false)
         return ItemViewHolder(view)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         holder.bind(dataArray[position])
         holder.itemView.setOnClickListener {
@@ -36,6 +40,12 @@ class SimpleAdapter(private val dataArray: MutableList<ItemBean>) :
             onItemClickListener?.onItemLongClick(holder.itemView, holder.layoutPosition)
             true
         }
+        holder.ivDrag.setOnTouchListener { _, motionEvent ->
+            if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                startDragListener?.onStartDrag(holder)
+            }
+            false
+        }
     }
 
     override fun getItemCount(): Int = dataArray.size
@@ -45,6 +55,12 @@ class SimpleAdapter(private val dataArray: MutableList<ItemBean>) :
     fun insertAdd(position: Int, item: ItemBean) {
         dataArray.add(position, item)
         notifyItemRangeInserted(position, 1)
+    }
+
+    fun itemMove(srcPos: Int, targetPos: Int) {
+        val sourceItem = dataArray.removeAt(srcPos)
+        dataArray.add(targetPos, sourceItem)
+        notifyItemMoved(srcPos, targetPos)
     }
 
     fun removeAt(position: Int) {
@@ -65,12 +81,16 @@ class SimpleAdapter(private val dataArray: MutableList<ItemBean>) :
         fun onItemClick(view: View, position: Int) {}
         fun onItemLongClick(view: View, position: Int) {}
     }
+
+    interface OnStartDragListener {
+        fun onStartDrag(viewHolder: RecyclerView.ViewHolder)
+    }
     // =============================================
 
     class ItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         private val txtView: TextView = itemView.findViewById(R.id.name)
         private val ivAlbum: ShapeableImageView = itemView.findViewById(R.id.ivAlbum)
-        private val ivDrag: ImageView = itemView.findViewById(R.id.ivDrag)
+        val ivDrag: ImageView = itemView.findViewById(R.id.ivDrag)
 
         fun bind(item: ItemBean) {
             txtView.text = item.title

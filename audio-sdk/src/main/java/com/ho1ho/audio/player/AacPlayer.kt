12d58526618ec5c636e249735e939a103a46a432
Michael Leo <yhzemail61010@aliyun.com>
2020-09-16 19:00:04 +0800

    add more audio examples

--- audio-sdk/src/main/java/com/ho1ho/audio/player/AacPlayer.kt
+++ audio-sdk/src/main/java/com/ho1ho/audio/player/AacPlayer.kt
@@ -6,7 +6,7 @@ import android.os.SystemClock
 import com.ho1ho.androidbase.exts.ITAG
 import com.ho1ho.androidbase.utils.JsonUtil
 import com.ho1ho.androidbase.utils.LLog
-import com.ho1ho.audio.recorder.bean.AudioCodecInfo
+import com.ho1ho.audio.base.AudioCodecInfo
 import kotlinx.coroutines.*
 import java.nio.ByteBuffer
 import java.util.concurrent.ArrayBlockingQueue

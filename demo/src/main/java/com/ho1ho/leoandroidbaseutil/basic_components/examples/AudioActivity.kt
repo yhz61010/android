202488bf426bf270beaedfad27ee0667d9d2a8a9
MikeoLeo <yhzemail61010@aliyun.com>
2020-09-17 10:42:12 +0800

    add more audio examples

--- demo/src/main/java/com/ho1ho/leoandroidbaseutil/basic_components/examples/AudioActivity.kt
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/basic_components/examples/AudioActivity.kt
@@ -9,6 +9,7 @@ import com.ho1ho.androidbase.utils.file.FileUtil
 import com.ho1ho.audio.base.AudioCodecInfo
 import com.ho1ho.audio.player.PcmPlayer
 import com.ho1ho.audio.recorder.MicRecorder
+import com.ho1ho.audio.recorder.aac.AacEncoder
 import com.ho1ho.leoandroidbaseutil.R
 import com.ho1ho.leoandroidbaseutil.base.BaseDemonstrationActivity
 import com.yanzhenjie.permission.AndPermission
@@ -20,14 +21,22 @@ import java.io.FileInputStream
 import java.io.FileOutputStream
 
 class AudioActivity : BaseDemonstrationActivity() {
+    companion object {
+        private const val RECORD_TYPE_PCM = 1
+        private const val RECORD_TYPE_AAC = 2
+    }
+
     private lateinit var micRecorder: MicRecorder
+    private var aacEncoder: AacEncoder? = null
     private lateinit var pcmPlayer: PcmPlayer
 
     private val audioEncoderCodec = AudioCodecInfo(16000, 32000, AudioFormat.CHANNEL_IN_MONO, 1, AudioFormat.ENCODING_PCM_16BIT)
     private val audioPlayCodec = AudioCodecInfo(16000, 32000, AudioFormat.CHANNEL_OUT_MONO, 1, AudioFormat.ENCODING_PCM_16BIT)
 
-    private val pcmFile by lazy { FileUtil.createFile(this, "pcm.pcm") }
+    private val pcmFile by lazy { FileUtil.createFile(this, "audio.pcm") }
+    private val aacFile by lazy { FileUtil.createFile(this, "audio.aac") }
     private var pcmOs: BufferedOutputStream? = null
+    private var aacOs: BufferedOutputStream? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,29 +44,26 @@ class AudioActivity : BaseDemonstrationActivity() {
 
         btnRecordPcm.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-                AndPermission.with(this)
-                    .runtime()
-                    .permission(Permission.RECORD_AUDIO)
-                    .onGranted {
-                        micRecorder = MicRecorder(audioEncoderCodec, object : MicRecorder.RecordCallback {
-                            override fun onRecording(pcmData: ByteArray) {
-                                LLog.d(ITAG, "PCM data[${pcmData.size}]")
-                                runCatching { pcmOs?.write(pcmData) }.onFailure { it.printStackTrace() }
-                            }
+                record(RECORD_TYPE_PCM)
+            } else {
+                micRecorder.stopRecord()
+            }
+        }
 
-                            override fun onStop(stopResult: Boolean) {
-                                runCatching {
-                                    pcmOs?.flush()
-                                    pcmOs?.close()
-                                }.onFailure { it.printStackTrace() }
-                            }
-                        })
-
-                        pcmOs = BufferedOutputStream(FileOutputStream(pcmFile))
-                        micRecorder.startRecord()
-                    }
-                    .onDenied { Toast.makeText(this, "Deny record permission", Toast.LENGTH_SHORT).show();finish() }
-                    .start()
+        btnRecordAac.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                aacEncoder = AacEncoder(
+                    audioEncoderCodec.sampleRate,
+                    audioEncoderCodec.bitrate,
+                    audioEncoderCodec.channelCount,
+                    object : AacEncoder.AacEncodeCallback {
+                        override fun onEncoded(aacData: ByteArray) {
+                            LLog.i(ITAG, "AAC Data[${aacData.size}]")
+                            runCatching { aacOs?.write(aacData) }.onFailure { it.printStackTrace() }
+                        }
+                    })
+                aacEncoder?.start()
+                record(RECORD_TYPE_AAC)
             } else {
                 micRecorder.stopRecord()
             }
@@ -86,11 +92,52 @@ class AudioActivity : BaseDemonstrationActivity() {
                 if (::pcmPlayer.isInitialized) pcmPlayer.release()
             }
         }
+
+        btnPlayAac.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+            } else {
+            }
+        }
+    }
+
+    private fun record(type: Int) {
+        AndPermission.with(this)
+            .runtime()
+            .permission(Permission.RECORD_AUDIO)
+            .onGranted {
+                when (type) {
+                    RECORD_TYPE_PCM -> pcmOs = BufferedOutputStream(FileOutputStream(pcmFile))
+                    RECORD_TYPE_AAC -> aacOs = BufferedOutputStream(FileOutputStream(aacFile))
+                }
+                micRecorder = MicRecorder(audioEncoderCodec, object : MicRecorder.RecordCallback {
+                    override fun onRecording(pcmData: ByteArray) {
+                        LLog.d(ITAG, "PCM data[${pcmData.size}]")
+                        when (type) {
+                            RECORD_TYPE_PCM -> runCatching { pcmOs?.write(pcmData) }.onFailure { it.printStackTrace() }
+                            RECORD_TYPE_AAC -> aacEncoder?.queue?.offer(pcmData)
+                        }
+                    }
+
+                    override fun onStop(stopResult: Boolean) {
+                        runCatching {
+                            pcmOs?.flush()
+                            pcmOs?.close()
+                            aacOs?.flush()
+                            aacOs?.close()
+                        }.onFailure { it.printStackTrace() }
+                        aacEncoder?.release()
+                    }
+                })
+                micRecorder.startRecord()
+            }
+            .onDenied { Toast.makeText(this, "Deny record permission", Toast.LENGTH_SHORT).show();finish() }
+            .start()
     }
 
     override fun onStop() {
         if (::micRecorder.isInitialized) micRecorder.stopRecord()
         if (::pcmPlayer.isInitialized) pcmPlayer.release()
+        aacEncoder?.release()
         super.onStop()
     }
 }

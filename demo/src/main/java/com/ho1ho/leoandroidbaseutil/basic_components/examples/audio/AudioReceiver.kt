8c365733e20811f254554b4c1c80e7bce8a9797f
Michael Leo <yhzemail61010@aliyun.com>
2020-09-21 09:54:33 +0800

    add more audio examples

--- demo/src/main/java/com/ho1ho/leoandroidbaseutil/basic_components/examples/audio/AudioReceiver.kt
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/basic_components/examples/audio/AudioReceiver.kt
@@ -100,7 +100,7 @@ class AudioReceiver {
 
     fun stopServer() {
         ioScope.cancel()
-        if (::pcmPlayer.isInitialized) pcmPlayer.release()
         if (::webSocketServer.isInitialized) webSocketServer.stopServer()
+        if (::pcmPlayer.isInitialized) pcmPlayer.release()
     }
 }

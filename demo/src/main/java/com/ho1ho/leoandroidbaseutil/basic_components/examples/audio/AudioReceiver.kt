7610d91b0a1d1add72873181ed408e2e0f856f57
yuanhongzhi <yuanhongzhi@zhizhangyi.com>
2020-09-21 10:31:48 +0800

    add more audio examples

--- demo/src/main/java/com/ho1ho/leoandroidbaseutil/basic_components/examples/audio/AudioReceiver.kt
+++ demo/src/main/java/com/ho1ho/leoandroidbaseutil/basic_components/examples/audio/AudioReceiver.kt
@@ -100,7 +100,11 @@ class AudioReceiver {
 
     fun stopServer() {
         ioScope.cancel()
-        if (::webSocketServer.isInitialized) webSocketServer.stopServer()
+        // You must release AudioTrack first, otherwise, you will crash due to following exception:
+        // AudioTrackShared: Assertion failed: !(stepCount <= mUnreleased && mUnreleased <= mFrameCount)
+        // Fatal signal 6 (SIGABRT), code -6 in tid 26866 (DefaultDispatch)
+        // releaseBuffer() track 0xde4c9100 disabled due to previous underrun, restarting
         if (::pcmPlayer.isInitialized) pcmPlayer.release()
+        if (::webSocketServer.isInitialized) webSocketServer.stopServer()
     }
 }

b06e92bde9e540ede8f6c05a1a94a814562b39bd
MikeoLeo <yhzemail61010@aliyun.com>
2020-09-22 17:32:17 +0800

    rotate front lens according to the cameraSensorOrientation

--- camera2live-sdk/src/main/java/com/ho1ho/camera2live/base/DefaultEncoderStrategy.kt
+++ camera2live-sdk/src/main/java/com/ho1ho/camera2live/base/DefaultEncoderStrategy.kt
@@ -3,6 +3,7 @@ package com.ho1ho.camera2live.base
 import android.hardware.camera2.CameraMetadata
 import android.media.Image
 import com.ho1ho.androidbase.utils.media.YuvUtil
+import com.ho1ho.camera2live.base.iters.CameraSensorOrientation
 import com.ho1ho.camera2live.base.iters.IDataProcessStrategy
 
 /**
@@ -10,14 +11,23 @@ import com.ho1ho.camera2live.base.iters.IDataProcessStrategy
  * Date: 20-4-1 上午11:12
  */
 class DefaultEncoderStrategy : IDataProcessStrategy {
-    override fun doProcess(image: Image, lensFacing: Int): ByteArray {
+    override fun doProcess(image: Image, lensFacing: Int, @CameraSensorOrientation cameraSensorOrientation: Int): ByteArray {
         val width = image.width
         val height = image.height
         val imageBytes = YuvUtil.getBytesFromImage(image)
         return if (lensFacing == CameraMetadata.LENS_FACING_BACK) {
             YuvUtil.rotateYUV420Degree90(imageBytes, width, height)
         } else {
-            YuvUtil.rotateYUVDegree270AndMirror(imageBytes, width, height)
+            // Front lens
+            return when (cameraSensorOrientation) {
+                90 -> {
+                    // Nexus phone(like Nexus 6 and Nexus 6P), the front lens cameraSensorOrientation is 90 not 270 degrees
+                    // Tested on Nexus 6 and Nexus 6P
+                    YuvUtil.mirrorNv21(imageBytes, width, height)
+                    YuvUtil.rotateYUV420Degree270(imageBytes, width, height)
+                }
+                else /* 270 */ -> YuvUtil.rotateYUVDegree270AndMirror(imageBytes, width, height)
+            }
         }
     }
 }

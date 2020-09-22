95f91267e7ac3dea17be38eb61b00715aa51a3d7
MikeoLeo <yhzemail61010@aliyun.com>
2020-09-22 16:41:41 +0800

    remove redundant file

--- camera2live-sdk/src/main/java/com/ho1ho/camera2live/extensions/CommonExtension.kt
+++
@@ -1,9 +0,0 @@
-package com.ho1ho.camera2live.extensions
-
-/**
- * Author: Michael Leo
- * Date: 20-4-1 上午11:36
- */
-fun fail(message: String): Nothing {
-    throw IllegalStateException(message)
-}

a200d9ff0b4e5072a5cc2a49d28e9b1c022f931d
Michael Leo <yhzemail61010@aliyun.com>
2020-09-22 18:04:49 +0800

    remove cameraSensorOrientation annotation

--- camera2live-sdk/src/main/java/com/ho1ho/camera2live/base/iters/IDataProcessStrategy.kt
+++ camera2live-sdk/src/main/java/com/ho1ho/camera2live/base/iters/IDataProcessStrategy.kt
@@ -1,17 +1,11 @@
 package com.ho1ho.camera2live.base.iters
 
 import android.media.Image
-import androidx.annotation.IntDef
 
 /**
  * Author: Michael Leo
  * Date: 20-4-1 上午11:05
  */
 interface IDataProcessStrategy {
-    fun doProcess(image: Image, lensFacing: Int, @CameraSensorOrientation cameraSensorOrientation: Int): ByteArray
+    fun doProcess(image: Image, lensFacing: Int, cameraSensorOrientation: Int): ByteArray
 }
-
-@IntDef(flag = true, value = [0, 90, 180, 270])
-@Retention(AnnotationRetention.SOURCE)
-@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FIELD)
-annotation class CameraSensorOrientation

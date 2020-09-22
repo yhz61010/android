4d0492bbeee0b056b2477733cffa7e6827ef5ee5
Michael Leo <yhzemail61010@aliyun.com>
2020-09-22 17:32:17 +0800

    rotate front lens according to the cameraSensorOrientation

--- camera2live-sdk/src/main/java/com/ho1ho/camera2live/base/DataProcessFactory.kt
+++ camera2live-sdk/src/main/java/com/ho1ho/camera2live/base/DataProcessFactory.kt
@@ -8,13 +8,11 @@ object DataProcessFactory {
 
     const val ENCODER_TYPE_NORMAL = 1
     const val ENCODER_TYPE_YUV_ORIGINAL = 2
-    const val ENCODER_TYPE_NEXUS = 3
 
     fun getConcreteObject(type: Int): DataProcessContext? {
         return when (type) {
             ENCODER_TYPE_NORMAL -> DataProcessContext(DefaultEncoderStrategy())
             ENCODER_TYPE_YUV_ORIGINAL -> DataProcessContext(YuvEncoderStrategy())
-            ENCODER_TYPE_NEXUS -> DataProcessContext(NexusEncoderStrategy())
             else -> null
         }
     }
--- camera2live-sdk/src/main/java/com/ho1ho/camera2live/base/iters/IDataProcessStrategy.kt
+++ camera2live-sdk/src/main/java/com/ho1ho/camera2live/base/iters/IDataProcessStrategy.kt
@@ -1,11 +1,17 @@
 package com.ho1ho.camera2live.base.iters
 
 import android.media.Image
+import androidx.annotation.IntDef
 
 /**
  * Author: Michael Leo
  * Date: 20-4-1 上午11:05
  */
 interface IDataProcessStrategy {
-    fun doProcess(image: Image, lensFacing: Int): ByteArray
+    fun doProcess(image: Image, lensFacing: Int, @CameraSensorOrientation cameraSensorOrientation: Int): ByteArray
 }
+
+@IntDef(flag = true, value = [0, 90, 180, 270])
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FIELD)
+annotation class CameraSensorOrientation

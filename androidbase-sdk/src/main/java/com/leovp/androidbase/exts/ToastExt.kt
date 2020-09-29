798a244c2bba5b5a72da87be3c47f94db30bd07a
yuanhongzhi <yuanhongzhi@zhizhangyi.com>
2020-09-29 13:51:02 +0800

    add toast ext

---
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/exts/ToastExt.kt
@@ -0,0 +1,17 @@
+package com.leovp.androidbase.exts
+
+import android.content.Context
+import android.widget.Toast
+import androidx.annotation.StringRes
+
+/**
+ * Author: Michael Leo
+ * Date: 2020/9/29 上午11:52
+ */
+fun Context.toast(@StringRes resId: Int, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, getString(resId), length).show()
+}
+
+fun Context.toast(msg: String, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, msg, length).show()
+}

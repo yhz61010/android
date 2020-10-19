095d34f86baca61e64355c78309a29cc0622c47f
MikeoLeo <yhzemail61010@aliyun.com>
2020-10-19 14:17:23 +0800

    move CLog to demo module

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/log/LogActivity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/log/LogActivity.kt
@@ -7,33 +7,50 @@ import com.leovp.androidbase.utils.log.LogContext
 import com.leovp.leoandroidbaseutil.R
 import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
 
+/**
+ * CLog is the wrapper of XlogCLog is the wrapper of Xlog
+ * Before using it, you must initialize it in `Application` by calling:
+ * ```kotlin
+ * LogContext.setLogImp(CLog().apply { init(this@CustomApplication) })
+ * ```
+ *
+ * After CLog initializing, the usage of CLog is just the same as LogContext.log.
+ * ```kotlin
+ * LogContext.log.w(ITAG, "Device Info:\n${DeviceUtil.getDeviceInfo(this)}")
+ * ```
+ *
+ * It's better to call
+ * ```kotlin
+ * (LogContext.log as CLog).flushLog()
+ * ```
+ * in `onStop()` to make sure flush all memory logs into file.
+ *
+ * When you no need to use CLog, please close it.
+ * ```kotlin
+ * (LogContext.log as CLog).closeLog()
+ * ```
+ * Generally speaking, this method should be called before you exit app.
+ *
+ *
+ * LLog is the wrapper of Android default log
+ * No need to do any initializing before using it.
+ */
 class LogActivity : BaseDemonstrationActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
 
-        // CLog is the wrapper of Xlog
-        // Before using it, you must initialize it by calling CLog.init(ctx) in Application
-        // After CLog initializing, the usage of CLog is just the same as LogContext.log.
-//        CLog.w(ITAG, "1Device Info:\n${DeviceUtil.getDeviceInfo(this)}")
-
-        // LLog is the wrapper of Android default log
-        // No need to do any initializing before using it
         LogContext.log.w(ITAG, "2Device Info:\n${DeviceUtil.getDeviceInfo(this)}")
     }
 
     override fun onStop() {
-        // It's better to call LogContext.log.flushLog(boolean) in onStop() to make sure flush all memory logs into file.
-//        CLog.flushLog(false)
+        (LogContext.log as CLog).flushLog()
         super.onStop()
     }
 
     override fun onDestroy() {
         LogContext.log.w(ITAG, "=====> onDestroy <=====")
-        // When you no need to use CLog, please close it.
-        // Generally speaking, this method should be called before you exit app.
-        // As demonstration, we do not close it here. Instead, we call it in MainActivity.
-//        CLog.closeLog()
+//        (LogContext.log as CLog).closeLog()
         super.onDestroy()
     }
 }
--- log-sdk/consumer-rules.pro
+++
@@ -1,2 +0,0 @@
-#mars
--keep class com.tencent.mars.** { *; }

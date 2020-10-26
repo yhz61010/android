1e1625f0fb0e3d2a28e95dc747fc3163a51aea95
yuanhongzhi <yuanhongzhi@zhizhangyi.com>
2020-10-26 18:53:31 +0800

    Refactor Socket utils

--- basenetty-sdk/src/main/java/com/leovp/socket_sdk/framework/client/ClientConnectListener.kt
+++ basenetty-sdk/src/main/java/com/leovp/socket_sdk/framework/client/ClientConnectListener.kt
@@ -9,18 +9,18 @@ import com.leovp.socket_sdk.framework.base.BaseNetty
 interface ClientConnectListener<in T : BaseNetty> {
 
     fun onConnected(netty: T)
-    fun onReceivedData(netty: T, data: Any?)
+    fun onReceivedData(netty: T, data: Any?, action: Int = -1)
     fun onDisconnected(netty: T)
     fun onFailed(netty: T, code: Int, msg: String?)
 
     companion object {
-        const val CONNECTION_ERROR_ALREADY_RELEASED = 0x1000 // 4096
+        private const val CONNECTION_ERROR_BASE_CODE = 0x1000 // 4096
 
-        //        const val CONNECTION_ERROR_CAN_NOT_CONNECT_TO_SERVER = 0x1001
-        const val CONNECTION_ERROR_CONNECT_EXCEPTION = 0x1002 // 4098
-        const val CONNECTION_ERROR_UNEXPECTED_EXCEPTION = 0x1003 // 4099
-        const val CONNECTION_ERROR_EXCEED_MAX_RETRY_TIMES = 0x1004 // 4100
-        const val CONNECTION_ERROR_SERVER_DOWN = 0x1005 // 4101
-        const val CONNECTION_ERROR_NETWORK_LOST = 0x1006 // 4102
+        const val CONNECTION_ERROR_ALREADY_RELEASED = CONNECTION_ERROR_BASE_CODE + 1
+        const val CONNECTION_ERROR_CONNECT_EXCEPTION = CONNECTION_ERROR_BASE_CODE + 2
+        const val CONNECTION_ERROR_UNEXPECTED_EXCEPTION = CONNECTION_ERROR_BASE_CODE + 3
+        const val CONNECTION_ERROR_EXCEED_MAX_RETRY_TIMES = CONNECTION_ERROR_BASE_CODE + 4
+        const val CONNECTION_ERROR_CONNECTION_DISCONNECT = CONNECTION_ERROR_BASE_CODE + 5
+        const val CONNECTION_ERROR_NETWORK_LOST = CONNECTION_ERROR_BASE_CODE + 6
     }
 }

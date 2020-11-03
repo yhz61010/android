a778d8e86e7b0bb7e486c7d09c49c12b24157491
MikeoLeo <yhzemail61010@aliyun.com>
2020-11-03 18:19:55 +0800

    add finger paint demo and related module

---
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/FingerPaintActivity.kt
@@ -0,0 +1,80 @@
+package com.leovp.leoandroidbaseutil.basic_components.examples
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.SeekBar
+import com.leovp.leoandroidbaseutil.R
+import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
+import kotlinx.android.synthetic.main.activity_finger_paint.*
+
+class FingerPaintActivity : BaseDemonstrationActivity(), SeekBar.OnSeekBarChangeListener, View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finger_paint)
+
+        close.setOnClickListener(this)
+        save.setOnClickListener(this)
+        undo.setOnClickListener(this)
+        clear.setOnClickListener(this)
+        red.setOnSeekBarChangeListener(this)
+        green.setOnSeekBarChangeListener(this)
+        blue.setOnSeekBarChangeListener(this)
+        tolerance.setOnSeekBarChangeListener(this)
+        width.setOnSeekBarChangeListener(this)
+        normal.setOnClickListener(this)
+//        emboss.setOnClickListener(this)
+        blur.setOnClickListener(this)
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        if (seekBar?.id == red.id || seekBar?.id == green.id || seekBar?.id == blue.id) {
+            val r = red.progress
+            val g = green.progress
+            val b = blue.progress
+            val color = Color.argb(255, r, g, b)
+            finger.strokeColor = color
+            colorPreview.setBackgroundColor(color)
+        } else if (seekBar?.id == tolerance.id) {
+            finger.touchTolerance = progress.toFloat()
+        } else if (seekBar?.id == width.id) {
+            finger.strokeWidth = progress.toFloat()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            undo -> finger.undo()
+            clear -> finger.clear()
+            close -> hidePreview()
+            save -> showPreview()
+//            emboss -> finger.emboss()
+            blur -> finger.blur()
+            normal -> finger.normal()
+        }
+    }
+
+    private fun showPreview() {
+        previewContainer.visibility = View.VISIBLE
+        preview.setImageDrawable(finger.drawable)
+    }
+
+    private fun hidePreview() {
+        previewContainer.visibility = View.INVISIBLE
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+    }
+
+    override fun onBackPressed() {
+        if (previewContainer.visibility == View.VISIBLE) {
+            hidePreview()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}

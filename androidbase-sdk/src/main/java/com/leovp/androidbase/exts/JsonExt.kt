0b3223bb7c73e6598e349ac05f05cd677b148aae
MikeoLeo <yhzemail61010@aliyun.com>
2020-11-06 09:58:52 +0800

    1. add more utils 2. add more feature to screen share

--- androidbase-sdk/src/main/java/com/leovp/androidbase/exts/JsonExt.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/exts/JsonExt.kt
@@ -2,6 +2,7 @@ package com.leovp.androidbase.exts
 
 import com.google.gson.Gson
 import com.leovp.androidbase.utils.JsonUtil
+import java.lang.reflect.Type
 
 /**
  * Author: Michael Leo
@@ -17,10 +18,24 @@ fun ByteArray.toHexadecimalString(): String = JsonUtil.toHexadecimalString(this)
 /**
  * Convert json string to object
  *
- * @param json  the string from which the object is to be deserialized
  * @param clazz the class of T
- * @param <T>   the type of the desired object
+ * @param <T> the type of the desired object
  * @return an object of type T from the string. Returns `null` if `json` is `null`
  * or if `json` is empty.
-</T> */
-fun <T> String.toObject(json: String?, clazz: Class<T>): T? = JsonUtil.toObject(json, clazz)
+ */
+fun <T> String.toObject(clazz: Class<T>): T? = JsonUtil.toObject(this, clazz)
+
+/**
+ * Convert json string to object
+ *
+ * Example:
+ * ```kotlin
+ * val listType  = object : TypeToken<MutableList<Pair<Path, Paint>>>() {}.type
+ * val paths: MutableList<Pair<Path, Paint>> = jsonString.toObject(listType)!!
+ * ```
+ *
+ * @param type the type of the desired object
+ * @return an object of type T from the string. Returns `null` if `json` is `null`
+ * or if `json` is empty.
+ */
+fun <T> String.toObject(type: Type): T? = JsonUtil.toObject(this, type)

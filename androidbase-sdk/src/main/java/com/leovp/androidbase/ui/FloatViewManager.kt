03f3ca722b2eec6c49a67c8008517dc6b9e571e4
Michael Leo <yhzemail61010@aliyun.com>
2020-11-06 14:03:35 +0800

    add FloatViewManager.kt

---
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/ui/FloatViewManager.kt
@@ -0,0 +1,115 @@
+package com.leovp.androidbase.ui
+
+import android.content.Context
+import android.content.Context.WINDOW_SERVICE
+import android.graphics.PixelFormat
+import android.os.Build
+import android.view.*
+import androidx.annotation.LayoutRes
+import com.leovp.androidbase.exts.canDrawOverlays
+import kotlin.math.abs
+
+/**
+ * Author: Michael Leo
+ * Date: 20-3-3 下午3:40
+ *
+ * @see [Float View](https://stackoverflow.com/a/53092436)
+ * @see [Float View Github](https://github.com/aminography/FloatingWindowApp)
+ */
+class FloatViewManager constructor(private val context: Context, @LayoutRes private val layoutId: Int, val enableDrag: Boolean = true) {
+
+    private var windowManager: WindowManager? = null
+        get() {
+            if (field == null) field = (context.getSystemService(WINDOW_SERVICE) as WindowManager)
+            return field
+        }
+
+    @Suppress("WeakerAccess")
+    var floatView: View = LayoutInflater.from(context).inflate(layoutId, null)
+        private set
+
+    private lateinit var layoutParams: WindowManager.LayoutParams
+
+    private var lastX: Int = 0
+    private var lastY: Int = 0
+    private var firstX: Int = 0
+    private var firstY: Int = 0
+
+    private var isShowing = false
+    private var touchConsumedByMove = false
+
+    private val onTouchListener = View.OnTouchListener { view, event ->
+        if (!enableDrag) return@OnTouchListener false
+        val totalDeltaX = lastX - firstX
+        val totalDeltaY = lastY - firstY
+
+        when (event.actionMasked) {
+            MotionEvent.ACTION_DOWN -> {
+                lastX = event.rawX.toInt()
+                lastY = event.rawY.toInt()
+                firstX = lastX
+                firstY = lastY
+            }
+            MotionEvent.ACTION_UP -> {
+                view.performClick()
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val deltaX = event.rawX.toInt() - lastX
+                val deltaY = event.rawY.toInt() - lastY
+                lastX = event.rawX.toInt()
+                lastY = event.rawY.toInt()
+                if (abs(totalDeltaX) >= 5 || abs(totalDeltaY) >= 5) {
+                    if (event.pointerCount == 1) {
+                        layoutParams.x += deltaX
+                        layoutParams.y += deltaY
+                        touchConsumedByMove = true
+                        windowManager?.apply {
+                            updateViewLayout(floatView, layoutParams)
+                        }
+                    } else {
+                        touchConsumedByMove = false
+                    }
+                } else {
+                    touchConsumedByMove = false
+                }
+            }
+            else -> {
+            }
+        }
+        touchConsumedByMove
+    }
+
+    init {
+        floatView.setOnTouchListener(onTouchListener)
+
+        layoutParams = WindowManager.LayoutParams().apply {
+            format = PixelFormat.TRANSLUCENT
+            flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+            @Suppress("DEPRECATION")
+            type = when {
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+                else -> WindowManager.LayoutParams.TYPE_TOAST
+            }
+
+            gravity = Gravity.CENTER
+            width = WindowManager.LayoutParams.WRAP_CONTENT
+            height = WindowManager.LayoutParams.WRAP_CONTENT
+        }
+    }
+
+    fun show() {
+        if (context.canDrawOverlays) {
+            dismiss()
+            isShowing = true
+            windowManager?.addView(floatView, layoutParams)
+        }
+    }
+
+    @Suppress("unused")
+    fun dismiss() {
+        if (isShowing) {
+            windowManager?.removeView(floatView)
+            isShowing = false
+        }
+    }
+}

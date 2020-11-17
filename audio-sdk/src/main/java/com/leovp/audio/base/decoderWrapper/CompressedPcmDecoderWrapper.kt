38af74bd4c235a1dd05f07185c648c40da1fe890
MikeoLeo <yhzemail61010@aliyun.com>
2020-11-17 09:48:54 +0800

    refactor all Audio demos

---
+++ audio-sdk/src/main/java/com/leovp/audio/base/decoderWrapper/CompressedPcmDecoderWrapper.kt
@@ -0,0 +1,19 @@
+package com.leovp.audio.base.decoderWrapper
+
+import com.leovp.androidbase.exts.decompress
+import com.leovp.audio.base.bean.AudioCodecInfo
+import com.leovp.audio.base.iters.AudioDecoderWrapper
+import com.leovp.audio.base.iters.OutputCallback
+
+/**
+ * Author: Michael Leo
+ * Date: 20-11-14 上午11:03
+ */
+class CompressedPcmDecoderWrapper(encoderInfo: AudioCodecInfo, private val outputCallback: OutputCallback) : AudioDecoderWrapper {
+    override fun decode(input: ByteArray) {
+        outputCallback.output(input.decompress())
+    }
+
+    override fun release() {
+    }
+}
---
+++ audio-sdk/src/main/java/com/leovp/audio/base/encoderWrapper/AacEncoderWrapper.kt
@@ -0,0 +1,34 @@
+package com.leovp.audio.base.encoderWrapper
+
+import com.leovp.audio.aac.AacEncoder
+import com.leovp.audio.base.bean.AudioCodecInfo
+import com.leovp.audio.base.iters.AudioEncoderWrapper
+import com.leovp.audio.base.iters.OutputCallback
+
+/**
+ * Author: Michael Leo
+ * Date: 20-11-14 上午11:03
+ */
+class AacEncoderWrapper(encoderInfo: AudioCodecInfo, private val outputCallback: OutputCallback) : AudioEncoderWrapper {
+    private var aacEncoder: AacEncoder
+
+    init {
+        aacEncoder = AacEncoder(
+            encoderInfo.sampleRate,
+            encoderInfo.bitrate,
+            encoderInfo.channelCount,
+            object : AacEncoder.AacEncodeCallback {
+                override fun onEncoded(aacData: ByteArray) {
+                    outputCallback.output(aacData)
+                }
+            }).apply { start() }
+    }
+
+    override fun encode(input: ByteArray) {
+        aacEncoder.queue.offer(input)
+    }
+
+    override fun release() {
+        runCatching { aacEncoder.release() }.onFailure { it.printStackTrace() }
+    }
+}

8dfdc3dd92c928c8ceb7936cbbe749d4ddb6bba4
MikeoLeo <yhzemail61010@aliyun.com>
2020-11-17 10:38:31 +0800

    refactor audio-sdk module

---
+++ audio-sdk/src/main/java/com/leovp/audio/base/bean/AudioEncoderInfo.kt
@@ -0,0 +1,32 @@
+package com.leovp.audio.base.bean
+
+import androidx.annotation.Keep
+
+/**
+ * Author: Michael Leo
+ * Date: 20-8-13 下午7:21
+ *
+ * Example:
+ * ```
+ * sampleRate: 44100Hz
+ * bitrate: 32000bps
+ * channelConfig:
+ *              AudioFormat.CHANNEL_IN_MONO(16=0x10)
+ *              AudioFormat.CHANNEL_IN_STEREO(12=0xc)
+ *              AudioFormat.CHANNEL_OUT_STEREO(12=0xc)
+ *              AudioFormat.CHANNEL_OUT_MONO (0x4)
+ * channelCount: 1
+ * audioFormat(bit depth):
+ *              AudioFormat.ENCODING_PCM_16BIT(0x2)
+ *              AudioFormat.ENCODING_PCM_8BIT(0x3)
+ *              AudioFormat.ENCODING_PCM_FLOAT(0x4)
+ * ```
+ */
+@Keep
+data class AudioEncoderInfo(val sampleRate: Int, val bitrate: Int, val channelConfig: Int, val audioFormat: Int) {
+    val channelCount: Int = when (channelConfig) {
+        android.media.AudioFormat.CHANNEL_IN_MONO -> 1
+        android.media.AudioFormat.CHANNEL_IN_STEREO -> 2
+        else -> throw kotlin.IllegalArgumentException("Illegal channelConfig value=${channelConfig}")
+    }
+}
--- audio-sdk/src/main/java/com/leovp/audio/base/decoderWrapper/CompressedPcmDecoderWrapper.kt
+++ audio-sdk/src/main/java/com/leovp/audio/base/decoderWrapper/CompressedPcmDecoderWrapper.kt
@@ -1,7 +1,7 @@
 package com.leovp.audio.base.decoderWrapper
 
 import com.leovp.androidbase.exts.decompress
-import com.leovp.audio.base.bean.AudioCodecInfo
+import com.leovp.audio.base.bean.AudioDecoderInfo
 import com.leovp.audio.base.iters.AudioDecoderWrapper
 import com.leovp.audio.base.iters.OutputCallback
 
@@ -9,7 +9,7 @@ import com.leovp.audio.base.iters.OutputCallback
  * Author: Michael Leo
  * Date: 20-11-14 上午11:03
  */
-class CompressedPcmDecoderWrapper(encoderInfo: AudioCodecInfo, private val outputCallback: OutputCallback) : AudioDecoderWrapper {
+class CompressedPcmDecoderWrapper(decoderInfo: AudioDecoderInfo, private val outputCallback: OutputCallback) : AudioDecoderWrapper {
     override fun decode(input: ByteArray) {
         outputCallback.output(input.decompress())
     }

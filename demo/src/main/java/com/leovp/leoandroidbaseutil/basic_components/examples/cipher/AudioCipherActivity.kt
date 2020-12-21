92d19d81ff3308fd4840ec6b95089167dbc56ae2
MikeoLeo <yhzemail61010@aliyun.com>
2020-12-21 18:23:15 +0800

    refactor AudioCipherActivity.kt demo

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/cipher/AudioCipherActivity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/cipher/AudioCipherActivity.kt
@@ -2,66 +2,42 @@ package com.leovp.leoandroidbaseutil.basic_components.examples.cipher
 
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.os.Environment
 import android.view.View
 import com.leovp.androidbase.exts.android.toast
 import com.leovp.androidbase.utils.cipher.AesUtil
 import com.leovp.leoandroidbaseutil.R
 import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
-import java.io.*
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 import javax.crypto.SecretKey
 
 class AudioCipherActivity : BaseDemonstrationActivity() {
     companion object {
         private const val ENCRYPTED_MP3_FILE_NAME = "encrypted_audio.mp3"
-
     }
 
-    private var secretKey: SecretKey? = null
+    private lateinit var secretKey: SecretKey
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_audio_cipher)
+        secretKey = AesUtil.generateKey()
     }
 
-    private fun saveFile(stringToSave: ByteArray) {
+    private fun saveFile(dataToEncode: ByteArray) {
         runCatching {
-            val file = File(Environment.getExternalStorageDirectory().absolutePath + File.separator, ENCRYPTED_MP3_FILE_NAME)
-            val bos = BufferedOutputStream(FileOutputStream(file))
-            secretKey = AesUtil.generateKey()
-            val filesBytes = AesUtil.encode(secretKey!!, stringToSave)
-            bos.write(filesBytes)
-            bos.flush()
-            bos.close()
+            val encodedData = AesUtil.encode(secretKey, dataToEncode)
+            val encodedOutFile = File(getExternalFilesDir(null), ENCRYPTED_MP3_FILE_NAME)
+            BufferedOutputStream(FileOutputStream(encodedOutFile)).use { it.write(encodedData) }
         }.onFailure { it.printStackTrace() }
     }
 
-    private fun decode(mp3File: File) {
-        runCatching { playMP3(AesUtil.decode(secretKey!!, FileInputStream(mp3File).use { it.readBytes() })) }.onFailure { it.printStackTrace() }
-    }
-
-    private fun getAudioFile(): ByteArray? {
-        return try {
-            // use recorded file instead of getting file from assets folder.
-            val ins: InputStream = resources.openRawResource(R.raw.music)
-            val length: Int = ins.available()
-            val audioData = ByteArray(length)
-            var bytesRead: Int
-            val output = ByteArrayOutputStream()
-            while (ins.read(audioData).also { bytesRead = it } != -1) {
-                output.write(audioData, 0, bytesRead)
-            }
-            output.toByteArray()
-        } catch (e: IOException) {
-            e.printStackTrace()
-            null
-        }
-    }
-
-    private fun playMP3(mp3SoundByteArray: ByteArray?) {
+    private fun playMP3(mp3SoundByteArray: ByteArray) {
         runCatching {
             // Create temp file that will hold byte array
-            val tempMp3: File = File.createTempFile("temp", "mp3", cacheDir)
+            val tempMp3: File = File.createTempFile("decrypted_temp_music_file", "mp3", cacheDir)
             tempMp3.deleteOnExit()
             val fos = FileOutputStream(tempMp3)
             fos.write(mp3SoundByteArray)
@@ -78,13 +54,13 @@ class AudioCipherActivity : BaseDemonstrationActivity() {
 
     fun onEncryptAudioClick(@Suppress("UNUSED_PARAMETER") view: View) {
         // saveFile("Hello World")
-        saveFile(getAudioFile()!!)
+        saveFile(resources.openRawResource(R.raw.music).readBytes())
         toast("onEncryptAudioClick")
     }
 
     fun onDecryptAudioClick(@Suppress("UNUSED_PARAMETER") view: View) {
-        val file = File(Environment.getExternalStorageDirectory().absolutePath + File.separator, ENCRYPTED_MP3_FILE_NAME)
-        decode(file)
+        val mp3File = File(getExternalFilesDir(null), ENCRYPTED_MP3_FILE_NAME)
+        runCatching { playMP3(AesUtil.decode(secretKey, FileInputStream(mp3File).use { it.readBytes() })) }.onFailure { it.printStackTrace() }
         toast("onDecryptAudioClick")
     }
 }

09791fe52ec960a478cf251dfcf2fdd70f7b368c
yuanhongzhi <yuanhongzhi@zhizhangyi.com>
2020-12-21 19:53:24 +0800

    update AesUtil.kt demo

--- androidbase-sdk/src/main/java/com/leovp/androidbase/utils/cipher/AesUtil.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/utils/cipher/AesUtil.kt
@@ -17,10 +17,16 @@ import javax.crypto.spec.SecretKeySpec
 object AesUtil {
     private const val ALGORITHM_AES = "AES/CBC/PKCS7Padding"
 
+    // PBKDF2WithHmacSHA1: will produce a hash length of 160 bits
+    // PBKDF2WithHmacSHA512: will produce a hash length of 512 bits
+    private const val ALGORITHM_SHA = "PBKDF2WithHmacSHA512"
+
     fun generateKey(): SecretKey {
         return runCatching {
             // Generate a 256-bit key
-            val outputKeyLength = 256
+            // AES allows 128, 192 and 256 bit of key length. In other words 16, 24 or 32 byte.
+            // 32 shl 3 = 256
+            val outputKeyLength = 32 shl 3
             val secureRandom = SecureRandom()
             // Do NOT seed secureRandom! Automatically seeded from system entropy.
             val keyGenerator = KeyGenerator.getInstance("AES")
@@ -42,8 +48,12 @@ object AesUtil {
             val iterations = 1000
 
             // Generate a 256-bit key
-            val outputKeyLength = 256
-            val secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
+            // AES allows 128, 192 and 256 bit of key length. In other words 16, 24 or 32 byte.
+            // 32 shl 3 = 256
+            val outputKeyLength = 32 shl 3
+            // PBKDF2WithHmacSHA1
+            // PBKDF2WithHmacSHA512
+            val secretKeyFactory = SecretKeyFactory.getInstance(ALGORITHM_SHA)
             val keySpec = PBEKeySpec(passphraseOrPin, salt, iterations, outputKeyLength)
             secretKeyFactory.generateSecret(keySpec)
         }.getOrThrow()

4f3f582ae9e276972006dfd6c05963a81ad6ef4a
MikeoLeo <yhzemail61010@aliyun.com>
2020-12-21 11:37:57 +0800

    update DeviceUtil.kt and DeviceExt.kt

--- androidbase-sdk/src/main/java/com/leovp/androidbase/exts/android/DeviceExt.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/exts/android/DeviceExt.kt
@@ -10,6 +10,7 @@ import android.provider.Settings
 import android.telephony.TelephonyManager
 import android.text.TextUtils
 import android.util.DisplayMetrics
+import com.leovp.androidbase.utils.device.DeviceProp
 import com.leovp.androidbase.utils.device.DeviceUtil
 import java.lang.reflect.Method
 import java.util.*
@@ -224,4 +225,22 @@ fun getImei(ctx: Context, slotId: Int): String? {
 fun getDimenInPixel(name: String): Int {
     val resourceId = app.resources.getIdentifier(name, "dimen", "android")
     return if (resourceId > 0) app.resources.getDimensionPixelSize(resourceId) else -1
+}
+
+fun isProbablyAnEmulator(): Boolean {
+    return (Build.FINGERPRINT.startsWith("google/sdk_gphone_")
+            && Build.FINGERPRINT.endsWith(":user/release-keys")
+            && Build.MANUFACTURER == "Google" && Build.PRODUCT.startsWith("sdk_gphone_") && Build.BRAND == "google"
+            && Build.MODEL.startsWith("sdk_gphone_")) // Android SDK emulator
+            || Build.FINGERPRINT.startsWith("generic")
+            || Build.FINGERPRINT.startsWith("unknown")
+            || Build.MODEL.contains("google_sdk")
+            || Build.MODEL.contains("Emulator")
+            || Build.MODEL.contains("Android SDK built for x86")
+            || "QC_Reference_Phone" == Build.BOARD  //bluestacks
+            || Build.MANUFACTURER.contains("Genymotion")
+            || Build.HOST.startsWith("Build") //MSI App Player
+            || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
+            || Build.PRODUCT == "google_sdk"
+            || DeviceProp.getSystemProperty("ro.kernel.qemu") == "1"// another Android SDK emulator check
 }

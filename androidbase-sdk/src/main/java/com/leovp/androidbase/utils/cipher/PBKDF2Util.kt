2333e384e41a541ab91b13e36030c9bdd52288b7
MikeoLeo <yhzemail61010@aliyun.com>
2020-12-22 16:28:54 +0800

    add more extension

--- androidbase-sdk/src/main/java/com/leovp/androidbase/utils/cipher/PBKDF2Util.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/utils/cipher/PBKDF2Util.kt
@@ -1,9 +1,9 @@
 package com.leovp.androidbase.utils.cipher
 
 import androidx.annotation.IntRange
+import com.leovp.androidbase.exts.kotlin.hexToByteArray
 import com.leovp.androidbase.exts.kotlin.toHexStringLE
 import java.security.SecureRandom
-import javax.crypto.SecretKey
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
 
@@ -16,6 +16,10 @@ object PBKDF2Util {
     // PBKDF2WithHmacSHA1: will produce a hash length of 160 bits
     // PBKDF2WithHmacSHA512: will produce a hash length of 512 bits
     const val ALGORITHM_SHA = "PBKDF2WithHmacSHA512"
+
+    private const val DEFAULT_PRE_SALT_LENGTH = 4
+    private const val DEFAULT_SUFFIX_SALT_LENGTH = 2
+    private const val DEFAULT_ITERATIONS = 1000
 
     /**
      * @param iterations Default value 1000.
@@ -31,31 +35,94 @@ object PBKDF2Util {
      * random.nextBytes(salt)
      */
     fun generateKey(
-        plainPassphraseOrPin: CharArray,
+        plainPassphrase: CharArray,
         salt: ByteArray,
-        iterations: Int = 1000,
+        iterations: Int = DEFAULT_ITERATIONS,
         @IntRange(from = 128, to = 256) outputKeyLengthInBits: Int = 32 shl 3
-    ): SecretKey {
+    ): ByteArray {
         return runCatching {
             // PBKDF2WithHmacSHA1
             // PBKDF2WithHmacSHA512
             val secretKeyFactory = SecretKeyFactory.getInstance(ALGORITHM_SHA)
-            val keySpec = PBEKeySpec(plainPassphraseOrPin, salt, iterations, outputKeyLengthInBits)
-            secretKeyFactory.generateSecret(keySpec)
+            val keySpec = PBEKeySpec(plainPassphrase, salt, iterations, outputKeyLengthInBits)
+            secretKeyFactory.generateSecret(keySpec).encoded
         }.getOrThrow()
     }
 
     fun generateKey(
-        plainPassphraseOrPin: CharArray,
-        saltLength: Int = 32,
-        iterations: Int = 1000,
+        plainPassphrase: CharArray,
+        salt: String,
+        iterations: Int = DEFAULT_ITERATIONS,
         @IntRange(from = 128, to = 256) outputKeyLengthInBits: Int = 32 shl 3
-    ): SecretKey {
-        val random = SecureRandom()
+    ): ByteArray = generateKey(plainPassphrase, salt.toByteArray(), iterations, outputKeyLengthInBits)
+
+    fun generateKey(
+        plainPassphrase: String,
+        salt: String,
+        iterations: Int = DEFAULT_ITERATIONS,
+        @IntRange(from = 128, to = 256) outputKeyLengthInBits: Int = 32 shl 3
+    ): ByteArray = generateKey(plainPassphrase.toCharArray(), salt.toByteArray(), iterations, outputKeyLengthInBits)
+
+    fun generateKey(
+        plainPassphrase: String,
+        salt: ByteArray,
+        iterations: Int = DEFAULT_ITERATIONS,
+        @IntRange(from = 128, to = 256) outputKeyLengthInBits: Int = 32 shl 3
+    ): ByteArray = generateKey(plainPassphrase.toCharArray(), salt, iterations, outputKeyLengthInBits)
+
+    // =====================================
+
+    fun generateKey(
+        plainPassphrase: CharArray,
+        saltLength: Int = 32,
+        iterations: Int = DEFAULT_ITERATIONS,
+        @IntRange(from = 128, to = 256) outputKeyLengthInBits: Int = 32 shl 3
+    ): ByteArray {
         val salt = ByteArray(saltLength)
-        random.nextBytes(salt)
-        return generateKey(plainPassphraseOrPin, salt, iterations, outputKeyLengthInBits)
+        SecureRandom().nextBytes(salt)
+        return generateKey(plainPassphrase, salt, iterations, outputKeyLengthInBits)
     }
 
-    fun hash(plainText: String): String = generateKey(plainText.toCharArray()).encoded.toHexStringLE(true, "")
+    fun generateKey(
+        plainPassphrase: String,
+        saltLength: Int = 32,
+        iterations: Int = DEFAULT_ITERATIONS,
+        @IntRange(from = 128, to = 256) outputKeyLengthInBits: Int = 32 shl 3
+    ): ByteArray = generateKey(plainPassphrase, saltLength, iterations, outputKeyLengthInBits)
+
+    // =====================================
+
+    fun hash(plainText: String): String {
+        val preSalt = ByteArray(DEFAULT_PRE_SALT_LENGTH)
+        SecureRandom().nextBytes(preSalt)
+        val preSaltHex = preSalt.toHexStringLE(true, "")
+
+        val suffixSalt = ByteArray(DEFAULT_SUFFIX_SALT_LENGTH)
+        SecureRandom().nextBytes(suffixSalt)
+        val suffixSaltHex = suffixSalt.toHexStringLE(true, "")
+
+//        if (BuildConfig.DEBUG) LogContext.log.w(ITAG, "preSaltHex=$preSaltHex suffixSaltHex=$suffixSaltHex")
+
+        val onlyHash = generateKey(plainText.toCharArray(), preSalt, DEFAULT_ITERATIONS).toHexStringLE(true, "")
+//        if (BuildConfig.DEBUG) LogContext.log.v(ITAG, "onlyHash=$onlyHash")
+
+        return "$preSaltHex$onlyHash$suffixSaltHex"
+    }
+
+    /**
+     * Usage:
+     * ```kotlin
+     * PBKDF2Util.validate("1", "724C135B1AD210C216EF40E9C5230D7BF30FC2FEBCAAB75986EAE1356464DF292486B158ADD7")}
+     * ```
+     */
+    fun validate(plainText: String, correctHash: String): Boolean {
+//        if (BuildConfig.DEBUG) LogContext.log.v(ITAG, "correctHash=$correctHash")
+        val preSalt = correctHash.substring(0, DEFAULT_PRE_SALT_LENGTH * 2)
+//        if (BuildConfig.DEBUG) LogContext.log.v(ITAG, "preSalt=$preSalt")
+        val onlyHash = correctHash.substring(DEFAULT_PRE_SALT_LENGTH * 2, correctHash.length - DEFAULT_SUFFIX_SALT_LENGTH * 2)
+//        if (BuildConfig.DEBUG) LogContext.log.v(ITAG, "onlyHash=$onlyHash")
+        val testHash = generateKey(plainText.toCharArray(), preSalt.hexToByteArray(), DEFAULT_ITERATIONS).toHexStringLE(true, "")
+//        if (BuildConfig.DEBUG) LogContext.log.v(ITAG, "testHash=$testHash")
+        return onlyHash == testHash
+    }
 }

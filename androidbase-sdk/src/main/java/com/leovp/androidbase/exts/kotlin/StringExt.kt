0cb16c401122eb6aa57d0ddb25eacccf32a85d6a
MikeoLeo <yhzemail61010@aliyun.com>
2020-12-22 16:28:54 +0800

    add more extension

--- androidbase-sdk/src/main/java/com/leovp/androidbase/exts/kotlin/StringExt.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/exts/kotlin/StringExt.kt
@@ -6,3 +6,14 @@ package com.leovp.androidbase.exts.kotlin
  */
 
 fun String.toAsciiByteArray() = this.toByteArray(Charsets.US_ASCII)
+
+/**
+ * Transform each 2 hex chars to one byte
+ */
+fun String.hexToByteArray(): ByteArray {
+    val binary = ByteArray(this.length / 2)
+    for (i in binary.indices) {
+        binary[i] = this.substring(2 * i, 2 * i + 2).toInt(16).toByte()
+    }
+    return binary
+}

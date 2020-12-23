b40e64e1014d47ec4cb73f35ad70926d53263b32
yuanhongzhi <yuanhongzhi@zhizhangyi.com>
2020-12-23 18:46:03 +0800

    improve sleep method

--- androidbase-sdk/src/main/java/com/leovp/androidbase/exts/kotlin/ThreadExt.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/exts/kotlin/ThreadExt.kt
@@ -1,9 +1,9 @@
 package com.leovp.androidbase.exts.kotlin
 
+import android.os.SystemClock
+
 /**
  * Author: Michael Leo
  * Date: 20-12-12 下午2:49
  */
-fun sleep(millis: Long) {
-    runCatching { Thread.sleep(millis) }.onFailure { it.printStackTrace() }
-}
+fun sleep(millis: Long) = SystemClock.sleep(millis)

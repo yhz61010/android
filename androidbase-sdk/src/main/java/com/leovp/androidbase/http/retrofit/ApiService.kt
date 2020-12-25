2bd5425db032fbcb79682780dd1d459962f80f3e
MikeoLeo <yhzemail61010@aliyun.com>
2020-12-25 09:42:43 +0800

    change the way to do server certificate verification

--- androidbase-sdk/src/main/java/com/leovp/androidbase/http/retrofit/ApiService.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/http/retrofit/ApiService.kt
@@ -1,19 +1,18 @@
 package com.leovp.androidbase.http.retrofit
 
 import okhttp3.HttpUrl
-import java.io.InputStream
 
 /**
  * Author: Michael Leo
  * Date: 19-7-24 下午5:08
  */
+@Suppress("unused")
 object ApiService {
-    fun <T> getService(baseUrl: String, clazz: Class<T>, certificateInputStream: InputStream? = null, trustHostNames: Array<String> = emptyArray()): T {
-        return HttpRequest.getInstance(certificateInputStream, trustHostNames).getRetrofit(baseUrl).create(clazz)
+    fun <T> getService(baseUrl: String, clazz: Class<T>): T {
+        return HttpRequest.getRetrofit(baseUrl).create(clazz)
     }
 
-    @Suppress("unused")
-    fun <T> getService(baseUrl: HttpUrl, clazz: Class<T>, certificateInputStream: InputStream? = null, trustHostNames: Array<String> = emptyArray()): T {
-        return getService(baseUrl.toString(), clazz, certificateInputStream, trustHostNames)
+    fun <T> getService(baseUrl: HttpUrl, clazz: Class<T>): T {
+        return getService(baseUrl.toString(), clazz)
     }
 }

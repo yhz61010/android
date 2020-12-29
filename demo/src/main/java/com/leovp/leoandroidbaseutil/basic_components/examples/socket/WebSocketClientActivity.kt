fb394b951debf19f39b05321f9ca8ee4baa392f2
yuanhongzhi <yuanhongzhi@zhizhangyi.com>
2020-12-29 15:42:54 +0800

    update WebSocketClientActivity.kt demo

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/socket/WebSocketClientActivity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/socket/WebSocketClientActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.os.SystemClock
 import android.view.View
 import com.leovp.androidbase.exts.kotlin.toJsonString
-import com.leovp.androidbase.http.SslUtils
 import com.leovp.androidbase.utils.log.LogContext
 import com.leovp.androidbase.utils.ui.ToastUtil
 import com.leovp.leoandroidbaseutil.R
@@ -25,6 +24,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.InputStream
 import java.net.URI
 import java.nio.charset.Charset
 
@@ -34,34 +34,40 @@ class WebSocketClientActivity : BaseDemonstrationActivity() {
     private lateinit var webSocketClient: WebSocketClient
     private lateinit var webSocketClientHandler: WebSocketClientHandler
 
+    private val connectionListener = object : ClientConnectListener<BaseNettyClient> {
+        override fun onConnected(netty: BaseNettyClient) {
+            LogContext.log.i(TAG, "onConnected")
+            ToastUtil.showDebugToast("onConnected")
+        }
+
+        @SuppressLint("SetTextI18n")
+        override fun onReceivedData(netty: BaseNettyClient, data: Any?, action: Int) {
+            LogContext.log.i(TAG, "onReceivedData: ${data?.toJsonString()}")
+            runOnUiThread { txtView.text = txtView.text.toString() + data?.toJsonString() + "\n" }
+        }
+
+        override fun onDisconnected(netty: BaseNettyClient) {
+            LogContext.log.w(TAG, "onDisconnect")
+            ToastUtil.showDebugToast("onDisconnect")
+        }
+
+        override fun onFailed(netty: BaseNettyClient, code: Int, msg: String?, e: Throwable?) {
+            LogContext.log.w(TAG, "onFailed code: $code message: $msg")
+            ToastUtil.showDebugToast("onFailed code: $code message: $msg")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_websocket_client)
 
-        val connectionListener = object : ClientConnectListener<BaseNettyClient> {
-            override fun onConnected(netty: BaseNettyClient) {
-                LogContext.log.i(TAG, "onConnected")
-                ToastUtil.showDebugToast("onConnected")
-            }
-
-            @SuppressLint("SetTextI18n")
-            override fun onReceivedData(netty: BaseNettyClient, data: Any?, action: Int) {
-                LogContext.log.i(TAG, "onReceivedData: ${data?.toJsonString()}")
-                runOnUiThread { txtView.text = txtView.text.toString() + data?.toJsonString() + "\n" }
-            }
-
-            override fun onDisconnected(netty: BaseNettyClient) {
-                LogContext.log.w(TAG, "onDisconnect")
-                ToastUtil.showDebugToast("onDisconnect")
-            }
-
-            override fun onFailed(netty: BaseNettyClient, code: Int, msg: String?, e: Throwable?) {
-                LogContext.log.w(TAG, "onFailed code: $code message: $msg")
-                ToastUtil.showDebugToast("onFailed code: $code message: $msg")
-            }
-        }
-
-        webSocketClient = WebSocketClient(URI("wss://www.qvdv.com:443/Websocket"), connectionListener, ConstantRetry(10, 2000))
+        webSocketClient = WebSocketClient(
+//            URI("wss://www.qvdv.com:443/Websocket"),
+            URI("ws://123.207.136.134:9010/ajaxchattest"),
+            connectionListener,
+            ConstantRetry(10, 2000),
+            null /*assets.open("cert/certificate.crt")*/
+        )
         webSocketClientHandler = WebSocketClientHandler(webSocketClient)
         webSocketClient.initHandler(webSocketClientHandler)
     }
@@ -73,14 +79,49 @@ class WebSocketClientActivity : BaseDemonstrationActivity() {
         super.onDestroy()
     }
 
+    // =====================================================
+
+    class WebSocketClient(webSocketUri: URI, connectionListener: ClientConnectListener<BaseNettyClient>, retryStrategy: RetryStrategy, certInputStream: InputStream? = null) :
+        BaseNettyClient(webSocketUri, connectionListener, retryStrategy, certInputStream) {
+        override fun getTagName() = "WebSocketClient"
+    }
+
+    @ChannelHandler.Sharable
+    class WebSocketClientHandler(private val netty: BaseNettyClient) : BaseClientChannelInboundHandler<Any>(netty) {
+        override fun onReceivedData(ctx: ChannelHandlerContext, msg: Any) {
+            val receivedString: String?
+            val frame = msg as WebSocketFrame
+            receivedString = when (frame) {
+                is TextWebSocketFrame -> {
+                    frame.text()
+                }
+                is PongWebSocketFrame -> {
+                    frame.content().toString(Charset.forName("UTF-8"))
+                }
+                else -> {
+                    null
+                }
+            }
+            netty.connectionListener.onReceivedData(netty, receivedString)
+        }
+
+        fun sendMsgToServer(msg: String): Boolean {
+            return netty.executeCommand("WebSocketCmd", "Send msg to server", msg)
+        }
+
+        override fun release() {
+        }
+    }
+
+    // =================================================
+
     fun onConnectClick(@Suppress("UNUSED_PARAMETER") view: View) {
         LogContext.log.i(TAG, "onConnectClick at ${SystemClock.elapsedRealtime()}")
+
         cs.launch {
             repeat(1) {
                 if (::webSocketClient.isInitialized) {
                     LogContext.log.i(TAG, "do connect at ${SystemClock.elapsedRealtime()}")
-                    SslUtils.hostnames = arrayOf("www.qvdv.com")
-                    SslUtils.certificateInputStream = assets.open("cert/www.qvdv.com.crt")
                     webSocketClient.connect()
                 }
 
@@ -111,40 +152,6 @@ class WebSocketClientActivity : BaseDemonstrationActivity() {
     fun onConnectRelease(@Suppress("UNUSED_PARAMETER") view: View) {
         cs.launch {
             if (::webSocketClient.isInitialized) webSocketClient.release()
-        }
-    }
-
-    // =====================================================
-
-    class WebSocketClient(webSocketUri: URI, connectionListener: ClientConnectListener<BaseNettyClient>, retryStrategy: RetryStrategy) :
-        BaseNettyClient(webSocketUri, connectionListener, retryStrategy) {
-        override fun getTagName() = "WebSocketClient"
-    }
-
-    @ChannelHandler.Sharable
-    class WebSocketClientHandler(private val netty: BaseNettyClient) : BaseClientChannelInboundHandler<Any>(netty) {
-        override fun onReceivedData(ctx: ChannelHandlerContext, msg: Any) {
-            val receivedString: String?
-            val frame = msg as WebSocketFrame
-            receivedString = when (frame) {
-                is TextWebSocketFrame -> {
-                    frame.text()
-                }
-                is PongWebSocketFrame -> {
-                    frame.content().toString(Charset.forName("UTF-8"))
-                }
-                else -> {
-                    null
-                }
-            }
-            netty.connectionListener.onReceivedData(netty, receivedString)
-        }
-
-        fun sendMsgToServer(msg: String): Boolean {
-            return netty.executeCommand("WebSocketCmd", "Send msg to server", msg)
-        }
-
-        override fun release() {
         }
     }
 

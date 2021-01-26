894454e532a7c576039ed421883c9b2ec55de258
Michael Leo <yhzemail61010@aliyun.com>
2021-01-26 15:27:34 +0800

    remove unused java files and transform other java to kotlin

--- androidbase-sdk/src/main/java/com/leovp/androidbase/ui/MovableFloatingActionButton.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/ui/MovableFloatingActionButton.kt
@@ -1,96 +1,69 @@
-package com.leovp.androidbase.ui;
+package com.leovp.androidbase.ui
 
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnTouchListener
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
-import android.content.Context;
-import android.util.AttributeSet;
-import android.view.MotionEvent;
-import android.view.View;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-public class MovableFloatingActionButton extends FloatingActionButton implements View.OnTouchListener {
-
-    // Often, there will be a slight, unintentional, drag when the user taps the FAB, so we need to account for this.
-    private final static float CLICK_DRAG_TOLERANCE = 10;
-
-    private float downRawX, downRawY;
-    private float dX, dY;
-
-    public MovableFloatingActionButton(Context context) {
-        super(context);
-        init();
+@Suppress("unused")
+class MovableFloatingActionButton(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : FloatingActionButton(context, attrs, defStyleAttr), OnTouchListener {
+    init {
+        setOnTouchListener(this)
     }
 
-    public MovableFloatingActionButton(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init();
-    }
+    private var downRawX = 0f
+    private var downRawY = 0f
+    private var dX = 0f
+    private var dY = 0f
 
-    public MovableFloatingActionButton(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
-    }
-
-    private void init() {
-        setOnTouchListener(this);
-    }
-
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-
-        int action = motionEvent.getAction();
-        if (action == MotionEvent.ACTION_DOWN) {
-
-            downRawX = motionEvent.getRawX();
-            downRawY = motionEvent.getRawY();
-            dX = view.getX() - downRawX;
-            dY = view.getY() - downRawY;
-
-            return true; // Consumed
-
+    override fun onTouch(view: View, motionEvent: MotionEvent): Boolean {
+        val action = motionEvent.action
+        return if (action == MotionEvent.ACTION_DOWN) {
+            downRawX = motionEvent.rawX
+            downRawY = motionEvent.rawY
+            dX = view.x - downRawX
+            dY = view.y - downRawY
+            true // Consumed
         } else if (action == MotionEvent.ACTION_MOVE) {
-
-            int viewWidth = view.getWidth();
-            int viewHeight = view.getHeight();
-
-            View viewParent = (View) view.getParent();
-            int parentWidth = viewParent.getWidth();
-            int parentHeight = viewParent.getHeight();
-
-            float newX = motionEvent.getRawX() + dX;
-            newX = Math.max(0, newX); // Don't allow the FAB past the left hand side of the parent
-            newX = Math.min(parentWidth - viewWidth, newX); // Don't allow the FAB past the right hand side of the parent
-
-            float newY = motionEvent.getRawY() + dY;
-            newY = Math.max(0, newY); // Don't allow the FAB past the top of the parent
-            newY = Math.min(parentHeight - viewHeight, newY); // Don't allow the FAB past the bottom of the parent
-
+            val viewWidth = view.width
+            val viewHeight = view.height
+            val viewParent = view.parent as View
+            val parentWidth = viewParent.width
+            val parentHeight = viewParent.height
+            var newX = motionEvent.rawX + dX
+            newX = max(0f, newX) // Don't allow the FAB past the left hand side of the parent
+            newX = min((parentWidth - viewWidth).toFloat(), newX) // Don't allow the FAB past the right hand side of the parent
+            var newY = motionEvent.rawY + dY
+            newY = max(0f, newY) // Don't allow the FAB past the top of the parent
+            newY = min((parentHeight - viewHeight).toFloat(), newY) // Don't allow the FAB past the bottom of the parent
             view.animate()
-                    .x(newX)
-                    .y(newY)
-                    .setDuration(0)
-                    .start();
-
-            return true; // Consumed
-
+                .x(newX)
+                .y(newY)
+                .setDuration(0)
+                .start()
+            true // Consumed
         } else if (action == MotionEvent.ACTION_UP) {
-
-            float upRawX = motionEvent.getRawX();
-            float upRawY = motionEvent.getRawY();
-
-            float upDX = upRawX - downRawX;
-            float upDY = upRawY - downRawY;
-
-            if (Math.abs(upDX) < CLICK_DRAG_TOLERANCE && Math.abs(upDY) < CLICK_DRAG_TOLERANCE) { // A click
-                return performClick();
+            val upRawX = motionEvent.rawX
+            val upRawY = motionEvent.rawY
+            val upDX = upRawX - downRawX
+            val upDY = upRawY - downRawY
+            if (abs(upDX) < CLICK_DRAG_TOLERANCE && abs(upDY) < CLICK_DRAG_TOLERANCE) { // A click
+                performClick()
             } else { // A drag
-                return true; // Consumed
+                true // Consumed
             }
-
         } else {
-            return super.onTouchEvent(motionEvent);
+            super.onTouchEvent(motionEvent)
         }
-
     }
 
+    companion object {
+        // Often, there will be a slight, unintentional, drag when the user taps the FAB, so we need to account for this.
+        private const val CLICK_DRAG_TOLERANCE = 10f
+    }
 }

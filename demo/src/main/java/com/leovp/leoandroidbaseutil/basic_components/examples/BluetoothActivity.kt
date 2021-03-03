11c5a269b4033fae812f3f935bdf7496cc70454b
yuanhongzhi <yuanhongzhi@zhizhangyi.com>
2021-03-03 17:57:38 +0800

    add bluetooth example

---
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/BluetoothActivity.kt
@@ -0,0 +1,28 @@
+package com.leovp.leoandroidbaseutil.basic_components.examples
+
+import android.os.Bundle
+import android.view.View
+import com.leovp.androidbase.exts.android.toast
+import com.leovp.androidbase.utils.device.BluetoothUtil
+import com.leovp.androidbase.utils.log.LogContext
+import com.leovp.leoandroidbaseutil.R
+import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
+
+class BluetoothActivity : BaseDemonstrationActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bluetooth)
+    }
+
+    fun onEnableBluetooth(@Suppress("UNUSED_PARAMETER") view: View) {
+        val enable = BluetoothUtil.enable()
+        LogContext.log.i("Enable=$enable")
+        toast("Enable=$enable")
+    }
+
+    fun onDisableBluetooth(@Suppress("UNUSED_PARAMETER") view: View) {
+        val disable = BluetoothUtil.disable()
+        LogContext.log.i("Disable=$disable")
+        toast("Disable=$disable")
+    }
+}

d406109ae72a9b87daedfe4ccf8c2e6de6eb502e
Michael Leo <yhzemail61010@aliyun.com>
2021-03-04 15:17:36 +0800

    improve bluetooth example

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/bluetooth/base/DeviceModel.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/bluetooth/base/DeviceModel.kt
@@ -7,7 +7,7 @@ import android.bluetooth.BluetoothDevice
  * Date: 21-3-3 下午6:14
  */
 data class DeviceModel(
-    var device: BluetoothDevice?, var name: String?, var macAddress: String?, var rssi: String?
+    var device: BluetoothDevice, var name: String?, var macAddress: String, var rssi: String?
 ) {
     var index: Int = 0
 }

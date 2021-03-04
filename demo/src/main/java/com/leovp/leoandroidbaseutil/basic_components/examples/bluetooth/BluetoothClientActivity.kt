7540cfa568d939ad5f61ea33d2627b7cadcce371
Michael Leo <yhzemail61010@aliyun.com>
2021-03-04 19:46:49 +0800

    improve bluetooth client & server demo

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/bluetooth/BluetoothClientActivity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/bluetooth/BluetoothClientActivity.kt
@@ -9,6 +9,14 @@ import com.leovp.androidbase.utils.log.LogContext
 import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
 import com.leovp.leoandroidbaseutil.databinding.ActivityBluetoothClientBinding
 
+/**
+ *  Need following permissions:
+ *
+ * <uses-permission android:name="android.permission.BLUETOOTH" />
+ * <uses-permission android:name="android.permission.BLUETOOTH_ADMIN" />
+ * <!--  For get nearby devices, need location permission when above android M  -->
+ * <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />
+ */
 class BluetoothClientActivity : BaseDemonstrationActivity() {
 
     private var _binding: ActivityBluetoothClientBinding? = null

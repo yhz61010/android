ed25f2ff1f367ead96ae299bdb6f10e2f18907d9
Michael Leo <yhzemail61010@aliyun.com>
2021-03-04 11:46:21 +0800

    modify bluetooth example

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/bluetooth/ScanBluetoothActivity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/bluetooth/ScanBluetoothActivity.kt
@@ -1,10 +1,16 @@
 package com.leovp.leoandroidbaseutil.basic_components.examples.bluetooth
 
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.leovp.androidbase.exts.android.toast
+import com.leovp.androidbase.exts.kotlin.toJsonString
 import com.leovp.androidbase.utils.device.BluetoothUtil
 import com.leovp.androidbase.utils.log.LogContext
 import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
@@ -30,6 +36,16 @@ class ScanBluetoothActivity : BaseDemonstrationActivity() {
 
         initView()
         initBluetooth()
+        initReceiver()
+    }
+
+    private fun initReceiver() {
+        val filter = IntentFilter()
+        filter.addAction(BluetoothDevice.ACTION_FOUND)
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED)
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+
+        registerReceiver(bluetoothReceiver, filter)
     }
 
     override fun onDestroy() {
@@ -57,24 +73,49 @@ class ScanBluetoothActivity : BaseDemonstrationActivity() {
     fun onScanClick(@Suppress("UNUSED_PARAMETER") view: View) {
         bluetoothDeviceMap.clear()
         binding.btnDoScan.text = "Scan"
-        BluetoothUtil.scan { device: BluetoothDevice, rssi: Int ->
-            // Remove redundant data
-            if (bluetoothDeviceMap.containsKey(device.address)) {
-                return@scan
-            }
-            val model = DeviceModel(bluetoothDeviceMap.size + 1, device, device.name, device.address, rssi.toString())
-            bluetoothDeviceMap[device.address] = model
-            binding.btnDoScan.text = "Scan(${bluetoothDeviceMap.size})"
-            val list = bluetoothDeviceMap.values.toMutableList()
-            list.sortBy { it.name }
-            adapter?.clearAndAddList(list)
-
-            LogContext.log.d("All Keys=${bluetoothDeviceMap.keys}")
-        }
+        BluetoothUtil.scan()
     }
 
     fun onStopScan(@Suppress("UNUSED_PARAMETER") view: View) {
         bluetoothDeviceMap.clear()
         binding.btnDoScan.text = "Scan"
+    }
+
+    private fun oldScan() {
+        bluetoothDeviceMap.clear()
+        binding.btnDoScan.text = "Scan"
+        BluetoothUtil.scan { device: BluetoothDevice, rssi: Int ->
+            // Remove redundant data
+            if (bluetoothDeviceMap.containsKey(device.address)) {
+                return@scan
+            }
+            val model = DeviceModel(device, device.name, device.address, rssi.toString())
+            bluetoothDeviceMap[device.address] = model
+            binding.btnDoScan.text = "Scan(${bluetoothDeviceMap.size})"
+            val list = bluetoothDeviceMap.values.toMutableList()
+            list.sortBy { it.name }
+            adapter?.clearAndAddList(list)
+        }
+    }
+
+    private val bluetoothReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            when (intent.action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    val device: BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE) ?: return
+                    val model = DeviceModel(device, device.name, device.address, null)
+                    LogContext.log.w("Found device: ${model.toJsonString()}")
+                    bluetoothDeviceMap[device.address] = model
+                    binding.btnDoScan.text = "Scan(${bluetoothDeviceMap.size})"
+                    val list = bluetoothDeviceMap.values.toMutableList()
+                    list.sortByDescending { it.name }
+                    adapter?.clearAndAddList(list)
+                }
+                BluetoothAdapter.ACTION_DISCOVERY_STARTED -> {
+                }
+                BluetoothAdapter.ACTION_DISCOVERY_FINISHED -> {
+                }
+            }
+        }
     }
 }

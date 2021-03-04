5a278d5de999edbaf8d26cbb4f6ca7beca9b2fbc
MikeoLeo <yhzemail61010@aliyun.com>
2021-03-04 10:53:58 +0800

    modify bluetooth example

--- androidbase-sdk/src/main/java/com/leovp/androidbase/utils/device/BluetoothUtil.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/utils/device/BluetoothUtil.kt
@@ -2,9 +2,12 @@ package com.leovp.androidbase.utils.device
 
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.content.pm.PackageManager
-import android.media.midi.MidiManager
+import android.os.Build
 import com.leovp.androidbase.exts.android.app
 import com.leovp.androidbase.exts.android.bluetoothManager
 
@@ -21,7 +24,6 @@ import com.leovp.androidbase.exts.android.bluetoothManager
  * Date: 21-3-3 下午5:38
  */
 object BluetoothUtil {
-    private val callback: MidiManager.DeviceCallback? = null
     private val bluetoothManager: BluetoothManager by lazy { app.bluetoothManager }
     private val bluetoothAdapter: BluetoothAdapter by lazy { bluetoothManager.adapter }
 
@@ -48,5 +50,25 @@ object BluetoothUtil {
     @SuppressLint("MissingPermission")
     fun disable(): Boolean {
         return if (bluetoothAdapter.isEnabled) bluetoothAdapter.disable() else true
+    }
+
+    /**
+     * Requires
+     * <uses-permission android:name="android.permission.BLUETOOTH_ADMIN" />
+     * permission
+     */
+    @SuppressLint("MissingPermission")
+    fun scan(callback: (device: BluetoothDevice, rssi: Int) -> Unit) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            bluetoothAdapter.bluetoothLeScanner?.startScan(object : ScanCallback() {
+                override fun onScanResult(callbackType: Int, result: ScanResult) {
+                    super.onScanResult(callbackType, result)
+                    val device = result.device
+                    callback(device, result.rssi)
+                }
+            })
+        } else {
+            bluetoothAdapter.startLeScan { device, rssi, _ -> callback(device, rssi) }
+        }
     }
 }

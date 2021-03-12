76c11752ef1aea529809f82bf7a20de7c5409d1d
MikeoLeo <yhzemail61010@aliyun.com>
2021-03-12 10:32:01 +0800

    improve AESUtil.kt

--- androidbase-sdk/src/main/java/com/leovp/androidbase/utils/cipher/AESUtil.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/utils/cipher/AESUtil.kt
@@ -1,86 +1,67 @@
 package com.leovp.androidbase.utils.cipher
 
-import android.os.SystemClock
-import androidx.annotation.IntRange
 import com.leovp.androidbase.exts.kotlin.hexToByteArray
 import com.leovp.androidbase.exts.kotlin.toHexStringLE
+import java.security.SecureRandom
 import javax.crypto.Cipher
 import javax.crypto.SecretKey
 import javax.crypto.spec.IvParameterSpec
-import javax.crypto.spec.SecretKeySpec
 
 
 /**
  * Author: Michael Leo
  * Date: 20-12-21 下午5:39
+ *
+ * https://stackoverflow.com/questions/13433529/android-4-2-broke-my-encrypt-decrypt-code-and-the-provided-solutions-dont-work/39002997#39002997
  */
 @Suppress("unused", "WeakerAccess")
 object AESUtil {
     private const val ALGORITHM_AES = "AES"
     private const val CIPHER_AES = "AES/CBC/PKCS7Padding"
-
-    /**
-     * @param outputKeyLengthInBits Default value 32 shl 3 = 256
-     * AES allows 128, 192 and 256 bit of key length. In other words 16, 24 or 32 byte.
-     */
-    fun generateKey(
-        secKey: String = SystemClock.elapsedRealtimeNanos().toString(),
-        salt: String,
-        @IntRange(from = 128, to = 256) outputKeyLengthInBits: Int = 32 shl 3
-    ): SecretKey {
-        val secKeyBytes = PBKDF2Util.generateKeyWithSHA1(plainPassphrase = secKey, salt = salt, outputKeyLengthInBits = outputKeyLengthInBits)
-        return SecretKeySpec(secKeyBytes, ALGORITHM_AES)
-    }
-
-    /**
-     * @param plainData Bytes to be encrypted
-     */
-    fun encrypt(secretKey: SecretKey, plainData: ByteArray): ByteArray = doEncrypt(secretKey.encoded, plainData)
-
-    /**
-     * @param encryptedData Bytes to be decoded
-     */
-    fun decrypt(secretKey: SecretKey, encryptedData: ByteArray): ByteArray = doDecrypt(secretKey.encoded, encryptedData)
-
-    private fun doEncrypt(keyBytes: ByteArray, plainData: ByteArray): ByteArray {
-        return runCatching {
-            val secKeySpec = SecretKeySpec(keyBytes, ALGORITHM_AES)
-            Cipher.getInstance(CIPHER_AES).run {
-                init(Cipher.ENCRYPT_MODE, secKeySpec, IvParameterSpec(ByteArray(blockSize)))
-                doFinal(plainData)
-            }
-        }.getOrThrow()
-    }
-
-    private fun doDecrypt(keyBytes: ByteArray, encryptedData: ByteArray): ByteArray {
-        return runCatching {
-            val secKeySpec = SecretKeySpec(keyBytes, ALGORITHM_AES)
-            Cipher.getInstance(CIPHER_AES).run {
-                init(Cipher.DECRYPT_MODE, secKeySpec, IvParameterSpec(ByteArray(blockSize)))
-                doFinal(encryptedData)
-            }
-        }.getOrThrow()
-    }
-
-    // ===================================================
+    private const val DEFAULT_PRE_SALT_LENGTH = 4
 
     /**
      * Encrypt data with specified secure key.
      *
-     * @param secKey AES allows 128, 192 and 256 bit of key length. In other words 16, 24 or 32 byte.
+     * AES allows 128(16*8), 192(24*8) and 256(32*8) bit of key length.
+     * In other words 16, 24 or 32 byte.
      */
-    fun encrypt(secKey: String, salt: String, plainText: String): String {
-        val rawKey: SecretKey = generateKey(secKey, salt)
-        return encrypt(rawKey, plainText.toByteArray()).toHexStringLE(true, "")
+    fun encrypt(plainText: String, secKey: String): String {
+        return encrypt(plainText.toByteArray(), secKey).toHexStringLE(true, "")
     }
 
-    /**
-     * Decrypt data with specified secure key.
-     *
-     * @param secKey AES allows 128, 192 and 256 bit of key length. In other words 16, 24 or 32 byte.
-     */
-    fun decrypt(secKey: String, salt: String, encryptedString: String): String {
-        val rawKey: SecretKey = generateKey(secKey, salt)
-        return decrypt(rawKey, encryptedString.hexToByteArray()).decodeToString()
+    fun decrypt(cipherText: String, secKey: String): String {
+        return decrypt(cipherText.hexToByteArray(), secKey).decodeToString()
+    }
+
+    fun encrypt(plainData: ByteArray, secKey: String): ByteArray {
+        val cipher = Cipher.getInstance(CIPHER_AES)
+        val salt: ByteArray = PBKDF2Util.generateSalt(DEFAULT_PRE_SALT_LENGTH)
+//        val iv: ByteArray = generateIv(cipher.blockSize)
+        val rawKey: SecretKey = PBKDF2Util.generateKeyWithSHA512(secKey, salt)
+//        val ivParams = IvParameterSpec(iv)
+        val cipherBytes: ByteArray = cipher.run {
+            init(Cipher.ENCRYPT_MODE, rawKey, IvParameterSpec(ByteArray(blockSize)))
+            doFinal(plainData)
+        }
+        return salt + cipherBytes
+    }
+
+    fun decrypt(cipherData: ByteArray, secKey: String): ByteArray {
+        val salt: ByteArray = cipherData.copyOfRange(0, DEFAULT_PRE_SALT_LENGTH)
+        val cipherBytes: ByteArray = cipherData.copyOfRange(DEFAULT_PRE_SALT_LENGTH, cipherData.size)
+        val rawKey: SecretKey = PBKDF2Util.generateKeyWithSHA512(secKey, salt)
+
+        return Cipher.getInstance(CIPHER_AES).run {
+            init(Cipher.DECRYPT_MODE, rawKey, IvParameterSpec(ByteArray(blockSize)))
+            doFinal(cipherBytes)
+        }
+    }
+
+    private fun generateIv(length: Int): ByteArray {
+        val iv = ByteArray(length)
+        // Do NOT seed secureRandom! Automatically seeded from system entropy.
+        SecureRandom().nextBytes(iv)
+        return iv
     }
 }

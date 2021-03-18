a63a3e433b19e05c87a0c1cfacff9b58fe39b72c
Michael Leo <yhzemail61010@aliyun.com>
2021-03-18 17:43:01 +0800

    add x264 module

---
+++ x264/src/main/cpp/build_x264_all.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+NDK_PATH=/home/yhz61010/Android/Sdk/ndk/22.0.7026061
+
+HOST_TAG=linux-x86_64
+TOOLCHAINS=$NDK_PATH/toolchains/llvm/prebuilt/$HOST_TAG
+SYSROOT=$TOOLCHAINS/sysroot
+
+MIN_SDK_VER=21
+
+function build_one
+{
+if [ $ARCH == "arm" ]
+then
+    CROSS_PREFIX=$TOOLCHAINS/bin/arm-linux-androideabi-
+    HOST=arm-linux
+elif [ $ARCH == "aarch64" ]
+then
+    CROSS_PREFIX=$TOOLCHAINS/bin/aarch64-linux-android-
+    HOST=aarch64-linux
+elif [ $ARCH == "i686" ]
+then
+    CROSS_PREFIX=$TOOLCHAINS/bin/i686-linux-android-
+    HOST=i686-linux
+else
+    CROSS_PREFIX=$TOOLCHAINS/bin/x86_64-linux-android-
+    HOST=x86_64-linux
+fi
+
+pushd libx264
+./configure \
+    --prefix=$PREFIX \
+    --extra-cflags="$OPTIMIZE_CFLAGS" \
+    --cross-prefix=$CROSS_PREFIX \
+    --sysroot=$SYSROOT \
+    --host=$HOST \
+    --extra-ldflags="-nostdlib" \
+    --enable-pic \
+    --enable-static \
+    --enable-strip \
+    --disable-cli \
+    --disable-win32thread \
+    --disable-avs \
+    --disable-swscale \
+    --disable-lavf \
+    --disable-ffms \
+    --disable-gpac \
+    --disable-lsmash
+
+make clean
+#make -j4 install V=1
+make -j4
+make install
+popd
+}
+
+#armeabi-v7a
+ARCH=arm
+OPTIMIZE_CFLAGS="-g -DANDROID -fdata-sections -ffunction-sections -funwind-tables -fstack-protector-strong -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -march=armv7-a -mthumb -Wformat -Werror=format-security   -Oz -DNDEBUG  -fPIC --target=armv7-none-linux-androideabi$MIN_SDK_VER --gcc-toolchain=$TOOLCHAINS"
+PREFIX=`pwd`/prebuilt/armeabi-v7a
+export CC=$TOOLCHAINS/bin/armv7a-linux-androideabi$MIN_SDK_VER-clang
+export CXX=$TOOLCHAINS/bin/armv7a-linux-androideabi$MIN_SDK_VER-clang++
+build_one
+
+#arm64-v8a
+ARCH=aarch64
+OPTIMIZE_CFLAGS="-g -DANDROID -fdata-sections -ffunction-sections -funwind-tables -fstack-protector-strong -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -Wformat -Werror=format-security   -O2 -DNDEBUG  -fPIC --target=aarch64-none-linux-android$MIN_SDK_VER --gcc-toolchain=$TOOLCHAINS"
+PREFIX=`pwd`/prebuilt/arm64-v8a
+export CC=$TOOLCHAINS/bin/aarch64-linux-android$MIN_SDK_VER-clang
+export CXX=$TOOLCHAINS/bin/aarch64-linux-android$MIN_SDK_VER-clang++
+build_one
+
+#x86
+ARCH=i686
+OPTIMIZE_CFLAGS="-g -DANDROID -fdata-sections -ffunction-sections -funwind-tables -fstack-protector-strong -no-canonical-prefixes -mstackrealign -D_FORTIFY_SOURCE=2 -Wformat -Werror=format-security   -O2 -DNDEBUG  -fPIC --target=i686-none-linux-android$MIN_SDK_VER --gcc-toolchain=$TOOLCHAINS"
+PREFIX=`pwd`/prebuilt/x86
+export CC=$TOOLCHAINS/bin/i686-linux-android$MIN_SDK_VER-clang
+export CXX=$TOOLCHAINS/bin/i686-linux-android$MIN_SDK_VER-clang++
+build_one
+
+#x86_64
+ARCH=x86_64
+OPTIMIZE_CFLAGS="-g -DANDROID -fdata-sections -ffunction-sections -funwind-tables -fstack-protector-strong -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -Wformat -Werror=format-security   -O2 -DNDEBUG  -fPIC --target=x86_64-none-linux-android$MIN_SDK_VER --gcc-toolchain=$TOOLCHAINS"
+PREFIX=`pwd`/prebuilt/x86_64
+export CC=$TOOLCHAINS/bin/x86_64-linux-android$MIN_SDK_VER-clang
+export CXX=$TOOLCHAINS/bin/x86_64-linux-android$MIN_SDK_VER-clang++
+build_one

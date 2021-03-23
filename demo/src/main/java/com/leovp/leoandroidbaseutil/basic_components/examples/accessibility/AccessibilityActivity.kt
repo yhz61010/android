a3d60de14fa2a2271371605ddb544aa127dcf91d
Michael Leo <yhzemail61010@aliyun.com>
2021-03-23 16:30:57 +0800

    update AccessibilityActivity.kt demo

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/accessibility/AccessibilityActivity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/accessibility/AccessibilityActivity.kt
@@ -1,13 +1,18 @@
 package com.leovp.leoandroidbaseutil.basic_components.examples.accessibility
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.os.SystemClock
 import android.view.View
+import android.widget.ArrayAdapter
 import com.leovp.androidbase.exts.android.id
 import com.leovp.androidbase.utils.system.AccessibilityUtil
 import com.leovp.androidbase.utils.ui.ToastUtil
 import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
+import com.leovp.leoandroidbaseutil.basic_components.examples.sharescreen.client.ScreenShareClientActivity
 import com.leovp.leoandroidbaseutil.databinding.ActivityAccessibilityBinding
+import org.greenrobot.eventbus.EventBus
 
 class AccessibilityActivity : BaseDemonstrationActivity() {
     private lateinit var binding: ActivityAccessibilityBinding
@@ -15,6 +20,12 @@ class AccessibilityActivity : BaseDemonstrationActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAccessibilityBinding.inflate(layoutInflater).apply { setContentView(root) }
+
+        val items = mutableListOf<String>()
+        for (i in 0 until 50) {
+            items.add("Item$i")
+        }
+        binding.list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
 
         if (!AccessibilityUtil.isAccessibilityEnabled()) {
             AccessibilityUtil.jumpToSettingPage(this)
@@ -26,6 +37,12 @@ class AccessibilityActivity : BaseDemonstrationActivity() {
         AccessibilityUtil.clickById("$id:id/btnDemo1")
         AccessibilityUtil.clickById("$id:id/cb1")
         AccessibilityUtil.clickById("$id:id/sw1")
+        AccessibilityUtil.scrollForwardById("$id:id/list")
+        Handler(Looper.getMainLooper()).postDelayed({
+            val touchBean = ScreenShareClientActivity.TouchBean(ScreenShareClientActivity.TouchType.DRAG, 500F, 1400F, 500F, 800F, 500L)
+            EventBus.getDefault().post(touchBean)
+        }, 1000)
+        Handler(Looper.getMainLooper()).postDelayed({ AccessibilityUtil.clickHomeKey() }, 5000)
     }
 
     fun onButtonClick(@Suppress("UNUSED_PARAMETER") view: View) {

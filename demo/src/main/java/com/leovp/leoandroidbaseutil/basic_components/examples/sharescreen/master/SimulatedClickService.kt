80951fc7a68b7ae3c906fed02a8e9856aa9555a3
MikeoLeo <yhzemail61010@aliyun.com>
2021-03-23 14:55:50 +0800

    add AccessibilityActivity.kt demo

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/sharescreen/master/SimulatedClickService.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/sharescreen/master/SimulatedClickService.kt
@@ -2,30 +2,25 @@ package com.leovp.leoandroidbaseutil.basic_components.examples.sharescreen.maste
 
 import android.accessibilityservice.AccessibilityService
 import android.accessibilityservice.GestureDescription
+import android.content.Intent
 import android.graphics.Path
 import android.os.Build
 import android.view.accessibility.AccessibilityEvent
 import androidx.annotation.RequiresApi
 import com.leovp.androidbase.exts.kotlin.toJsonString
 import com.leovp.androidbase.utils.log.LogContext
+import com.leovp.androidbase.utils.system.AccessibilityUtil
 import com.leovp.leoandroidbaseutil.basic_components.examples.sharescreen.client.ScreenShareClientActivity
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 
+/**
+ * https://github.com/PopFisher/AccessibilitySample
+ * https://www.jianshu.com/p/27df6983321f
+ * https://www.jianshu.com/p/cd1cd53909d7
+ */
 class SimulatedClickService : AccessibilityService() {
-
-    override fun onCreate() {
-        LogContext.log.i(TAG, "onCreate()")
-        super.onCreate()
-        EventBus.getDefault().register(this)
-    }
-
-    override fun onDestroy() {
-        LogContext.log.i(TAG, "onDestroy()")
-        EventBus.getDefault().unregister(this)
-        super.onDestroy()
-    }
 
     @Suppress("unused")
     @Subscribe(threadMode = ThreadMode.POSTING)
@@ -41,9 +36,14 @@ class SimulatedClickService : AccessibilityService() {
     // Attention: this callback is calling from main thread.
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
+        // The package names that you want to monitor which have been set in accessibility_service_config.
+        // If you does not set it, it means monitor all application.
+//        val packageName: String = event.packageName.toString()
         val eventType: Int = event.eventType
-        LogContext.log.i(TAG, "onAccessibilityEvent: $eventType")
+        val className: String? = event.className?.toString()
+        LogContext.log.i(TAG, "onAccessibilityEvent [$className]:$eventType")
         // eventType: AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED
+        AccessibilityUtil.init(this)
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
@@ -59,11 +59,18 @@ class SimulatedClickService : AccessibilityService() {
 
     override fun onInterrupt() {
         LogContext.log.i(TAG, "onInterrupt()")
+        EventBus.getDefault().unregister(this)
     }
 
     override fun onServiceConnected() {
         super.onServiceConnected()
         LogContext.log.i(TAG, "onServiceConnected()")
+        EventBus.getDefault().register(this)
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        LogContext.log.i(TAG, "onUnbind()")
+        return super.onUnbind(intent)
     }
 
     companion object {

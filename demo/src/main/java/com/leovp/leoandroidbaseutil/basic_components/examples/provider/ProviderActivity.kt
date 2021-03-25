ce14291ecb878557c7892e63459a7a5aefbd4017
Michael Leo <yhzemail61010@aliyun.com>
2021-03-25 14:08:30 +0800

    add provider demo

---
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/provider/ProviderActivity.kt
@@ -0,0 +1,12 @@
+package com.leovp.leoandroidbaseutil.basic_components.examples.provider
+
+import android.os.Bundle
+import com.leovp.leoandroidbaseutil.R
+import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
+
+class ProviderActivity : BaseDemonstrationActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_provider)
+    }
+}

e3d4a44c56d1e9af3bf05af976b2ec066b57c0ae
MikeoLeo <yhzemail61010@aliyun.com>
2021-03-30 16:59:48 +0800

    1. Update toast extension 2. Deprecate ToastUtil.kt

--- androidbase-sdk/src/main/java/com/leovp/androidbase/exts/android/ToastExt.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/exts/android/ToastExt.kt
@@ -1,6 +1,5 @@
 package com.leovp.androidbase.exts.android
 
-import android.content.Context
 import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
@@ -10,15 +9,15 @@ import androidx.annotation.StringRes
  * Author: Michael Leo
  * Date: 2020/9/29 上午11:52
  */
-fun Context.toast(@StringRes resId: Int, length: Int = Toast.LENGTH_SHORT) {
-    toast(getString(resId), length)
+fun toast(@StringRes resId: Int, length: Int = Toast.LENGTH_SHORT) {
+    toast(app.getString(resId), length)
 }
 
-fun Context.toast(msg: String, length: Int = Toast.LENGTH_SHORT) {
+fun toast(msg: String, length: Int = Toast.LENGTH_SHORT) {
     if (Looper.myLooper() == Looper.getMainLooper()) {
-        Toast.makeText(this, msg, length).show()
+        Toast.makeText(app, msg, length).show()
     } else {
         // Be sure toast can be shown in thread
-        Handler(Looper.getMainLooper()).post { Toast.makeText(this, msg, length).show() }
+        Handler(Looper.getMainLooper()).post { Toast.makeText(app, msg, length).show() }
     }
 }

e9d38e56a878fdf2a76f77c47bc8c5befa2b5a16
MikeoLeo <yhzemail61010@aliyun.com>
2021-04-06 18:09:28 +0800

    update animation demo

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/animation/AnimationActivity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/animation/AnimationActivity.kt
@@ -1,6 +1,8 @@
 package com.leovp.leoandroidbaseutil.basic_components.examples.animation
 
 import android.os.Bundle
+import com.leovp.androidbase.exts.android.setOnSingleClickListener
+import com.leovp.androidbase.exts.android.startActivity
 import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
 import com.leovp.leoandroidbaseutil.databinding.ActivityAnimationBinding
 
@@ -11,5 +13,9 @@ class AnimationActivity : BaseDemonstrationActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAnimationBinding.inflate(layoutInflater).apply { setContentView(root) }
+
+        binding.btnDrawableAnim.setOnSingleClickListener { startActivity(DrawableAnimActivity::class) }
+        binding.btnViewAnim.setOnSingleClickListener { startActivity(ViewAnimActivity::class) }
+        binding.btnPropAnim.setOnSingleClickListener { startActivity(PropertyAnimActivity::class) }
     }
 }

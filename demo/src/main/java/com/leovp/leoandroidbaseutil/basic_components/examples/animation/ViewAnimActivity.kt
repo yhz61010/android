30427e819a53f183502b577ecb6d928978db52e2
MikeoLeo <yhzemail61010@aliyun.com>
2021-04-07 14:23:05 +0800

    update view animation demo

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/animation/ViewAnimActivity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/animation/ViewAnimActivity.kt
@@ -1,8 +1,7 @@
 package com.leovp.leoandroidbaseutil.basic_components.examples.animation
 
 import android.os.Bundle
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
+import android.view.animation.*
 import com.leovp.androidbase.exts.android.setOnSingleClickListener
 import com.leovp.leoandroidbaseutil.R
 import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
@@ -27,19 +26,72 @@ class ViewAnimActivity : BaseDemonstrationActivity() {
             binding.ivBeauty.startAnimation(loadAnimation)
         }
 
+        binding.btnAlpha2.setOnSingleClickListener {
+            val alphaAnim = AlphaAnimation(0.1f, 1.0f).apply { duration = 1000 }
+            binding.ivBeauty.startAnimation(alphaAnim)
+        }
+
+        // ----------------------
+
         binding.btnRotate.setOnSingleClickListener {
             val loadAnimation = AnimationUtils.loadAnimation(this, R.anim.view_anim_rotate)
             binding.ivBeauty.startAnimation(loadAnimation)
         }
+
+        binding.btnRotate2.setOnSingleClickListener {
+            val rotateAnim = RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f).apply { duration = 1000 }
+            binding.ivBeauty.startAnimation(rotateAnim)
+        }
+
+        // ----------------------
 
         binding.btnScale.setOnSingleClickListener {
             val loadAnimation = AnimationUtils.loadAnimation(this, R.anim.view_anim_scale)
             binding.ivBeauty.startAnimation(loadAnimation)
         }
 
+        binding.btnScale2.setOnSingleClickListener {
+            val scaleAnim = ScaleAnimation(0f, 1f, 0f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f).apply { duration = 2000 }
+            binding.ivBeauty.startAnimation(scaleAnim)
+        }
+
+        // ----------------------
+
         binding.btnTranslate.setOnSingleClickListener {
             val loadAnimation = AnimationUtils.loadAnimation(this, R.anim.view_anim_translate)
             binding.ivBeauty.startAnimation(loadAnimation)
         }
+
+        binding.btnTranslate2.setOnSingleClickListener {
+            val transAnim = TranslateAnimation(10f, 100f, 10f, 100f).apply { duration = 1000 }
+            binding.ivBeauty.startAnimation(transAnim)
+        }
+
+        // ----------------------
+
+        binding.btnSequence.setOnSingleClickListener {
+            val loadAnimation = AnimationUtils.loadAnimation(this, R.anim.view_anim_sequence)
+            binding.ivBeauty.startAnimation(loadAnimation)
+        }
+
+        binding.btnSequence2.setOnSingleClickListener {
+            val scaleAnim = ScaleAnimation(0f, 1f, 0f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f).apply { duration = 2000 }
+            val alphaAnim = AlphaAnimation(1.0f, 0.1f).apply { duration = 1000 }
+            binding.ivBeauty.startAnimation(scaleAnim)
+            scaleAnim.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation) {
+                }
+
+                override fun onAnimationEnd(animation: Animation) {
+                    binding.ivBeauty.startAnimation(alphaAnim)
+
+                }
+
+                override fun onAnimationRepeat(animation: Animation) {
+                }
+
+            })
+        }
+
     }
 }

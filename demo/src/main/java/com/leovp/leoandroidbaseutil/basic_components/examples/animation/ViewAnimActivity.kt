2adb6faf95c983ed8f6e9c9b3207674fb329f622
MikeoLeo <yhzemail61010@aliyun.com>
2021-04-07 13:39:35 +0800

    update view animation demo

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/animation/ViewAnimActivity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/animation/ViewAnimActivity.kt
@@ -1,14 +1,45 @@
 package com.leovp.leoandroidbaseutil.basic_components.examples.animation
 
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.leovp.androidbase.exts.android.setOnSingleClickListener
+import com.leovp.leoandroidbaseutil.R
 import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
 import com.leovp.leoandroidbaseutil.databinding.ActivityViewAnimBinding
 
+
 class ViewAnimActivity : BaseDemonstrationActivity() {
+    private lateinit var binding: ActivityViewAnimBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityViewAnimBinding.inflate(layoutInflater).apply { setContentView(root) }
-
+        binding = ActivityViewAnimBinding.inflate(layoutInflater).apply { setContentView(root) }
         title = "View Animation"
+
+
+        initView()
+    }
+
+    private fun initView() {
+        binding.btnAlpha.setOnSingleClickListener {
+            val loadAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.view_anim_alpha)
+            binding.ivBeauty.startAnimation(loadAnimation)
+        }
+
+        binding.btnRotate.setOnSingleClickListener {
+            val loadAnimation = AnimationUtils.loadAnimation(this, R.anim.view_anim_rotate)
+            binding.ivBeauty.startAnimation(loadAnimation)
+        }
+
+        binding.btnScale.setOnSingleClickListener {
+            val loadAnimation = AnimationUtils.loadAnimation(this, R.anim.view_anim_scale)
+            binding.ivBeauty.startAnimation(loadAnimation)
+        }
+
+        binding.btnTranslate.setOnSingleClickListener {
+            val loadAnimation = AnimationUtils.loadAnimation(this, R.anim.view_anim_translate)
+            binding.ivBeauty.startAnimation(loadAnimation)
+        }
     }
 }

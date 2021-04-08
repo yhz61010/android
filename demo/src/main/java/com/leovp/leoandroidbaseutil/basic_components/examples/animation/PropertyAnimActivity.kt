5433cb37e9106d01b2cf41f89ffd2bab79364502
Michael Leo <yhzemail61010@aliyun.com>
2021-04-08 14:29:15 +0800

    update property animation demo

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/animation/PropertyAnimActivity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/animation/PropertyAnimActivity.kt
@@ -11,6 +11,16 @@ import kotlin.random.Random
 
 /**
  * https://developer.android.com/guide/topics/graphics/prop-animation?hl=zh-cn
+ * https://juejin.cn/post/6846687601118691341#heading-6
+ *
+ * LinearInterpolator 线性（匀速）
+ * AccelerateInterpolator 持续加速
+ * DecelerateInterpolator 持续减速
+ * AccelerateDecelerateInterpolator 先加速后减速
+ * OvershootInterpolator 结束时回弹一下
+ * AnticipateInterpolator 开始回拉一下
+ * BounceInterpolator 结束时Q弹一下
+ * CycleInterpolator 来回循环
  */
 class PropertyAnimActivity : BaseDemonstrationActivity() {
     private lateinit var binding: ActivityPropertyAnimBinding
@@ -21,7 +31,12 @@ class PropertyAnimActivity : BaseDemonstrationActivity() {
         title = "Property Animation"
 
         binding.btnAlpha.setOnSingleClickListener {
-            binding.ivBeauty.animate().alpha(Random.nextFloat()).duration = 1000
+//            binding.ivBeauty.animate().alpha(Random.nextFloat()).duration = 1000
+
+            ObjectAnimator.ofFloat(binding.ivBeauty, "alpha", 1.0f, Random.nextFloat(), 1.0f).apply {
+                duration = 2000
+                start()
+            }
         }
         binding.btnScale.setOnSingleClickListener {
             val scaleValue = Random.nextInt(150, 250) * 1.0F / 100
@@ -41,6 +56,11 @@ class PropertyAnimActivity : BaseDemonstrationActivity() {
         binding.btnRotate.setOnSingleClickListener {
             binding.ivBeauty.animate().rotation(Random.nextInt(0, 360).toFloat()).duration = 1000
         }
-        binding.btnTranslate.setOnSingleClickListener { }
+        binding.btnTranslate.setOnSingleClickListener {
+            ObjectAnimator.ofFloat(binding.ivBeauty, "translationX", 0f, 20f, -20f).apply {
+                duration = 1000
+                start()
+            }
+        }
     }
 }

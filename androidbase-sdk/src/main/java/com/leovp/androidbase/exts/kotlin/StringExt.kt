ff6c31ca9699c0df63449f06a567b7d55745cbc8
Michael Leo <yhzemail61010@aliyun.com>
2021-04-25 10:38:42 +0800

    add more utils in StringExt.kt

--- androidbase-sdk/src/main/java/com/leovp/androidbase/exts/kotlin/StringExt.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/exts/kotlin/StringExt.kt
@@ -1,5 +1,9 @@
 package com.leovp.androidbase.exts.kotlin
 
+import android.util.Patterns
+import android.webkit.URLUtil
+import java.util.regex.Pattern
+
 /**
  * Author: Michael Leo
  * Date: 20-3-13 下午4:44
@@ -16,4 +20,19 @@ fun String.hexToByteArray(): ByteArray {
         binary[i] = this.substring(2 * i, 2 * i + 2).toInt(16).toByte()
     }
     return binary
+}
+
+fun String.isValidUrl(url: String?): Boolean {
+    if (url.isNullOrBlank()) {
+        return false
+    }
+
+    // On 华为畅享5S Android 5.1, if the url ending with slash, although the url is a valid url,
+    // the Patterns.WEB_URL.matcher will return false. So we need to remove the ending slash.
+    val tmpUrl = if (url.endsWith("/")) url.substring(0, url.length - 1) else url
+    return if (Pattern.matches("^[\\x00-\\xff]+$", tmpUrl) && Patterns.WEB_URL.matcher(tmpUrl).matches()) {
+        URLUtil.isValidUrl(tmpUrl)
+    } else {
+        false
+    }
 }

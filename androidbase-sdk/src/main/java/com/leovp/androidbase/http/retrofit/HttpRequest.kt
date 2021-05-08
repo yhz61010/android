baffefa01d1f4ea47a9f8d4414b6c8c69b651be6
MikeoLeo <yhzemail61010@aliyun.com>
2021-05-08 13:10:22 +0800

    refactor the way to set default headers for http request

--- androidbase-sdk/src/main/java/com/leovp/androidbase/http/retrofit/HttpRequest.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/http/retrofit/HttpRequest.kt
@@ -1,6 +1,5 @@
 package com.leovp.androidbase.http.retrofit
 
-import com.leovp.androidbase.SingletonHolder
 import com.leovp.androidbase.http.retrofit.base.BaseHttpRequest
 import okhttp3.HttpUrl
 import retrofit2.Retrofit
@@ -13,13 +12,14 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
  * Date: 20-5-27 下午8:41
  */
 @Suppress("unused", "WeakerAccess")
-class HttpRequest private constructor(headerMap: Map<String, String>?) : BaseHttpRequest(headerMap) {
-    companion object : SingletonHolder<HttpRequest, Map<String, String>?>(::HttpRequest)
-
+object HttpRequest : BaseHttpRequest() {
     private var builder: Retrofit.Builder = getRetrofitBuilder()
 
-    fun reInit() {
-        builder = getRetrofitBuilder()
+    /**
+     * If you want to re-init header, you must call this method everytime.
+     */
+    fun initWithHeader(headerMap: Map<String, String>) {
+        builder = getRetrofitBuilder(headerMap)
     }
 
     fun getRetrofit(baseUrl: String): Retrofit {
@@ -31,9 +31,9 @@ class HttpRequest private constructor(headerMap: Map<String, String>?) : BaseHtt
         return getRetrofit(baseUrl.toString())
     }
 
-    private fun getRetrofitBuilder(): Retrofit.Builder {
+    private fun getRetrofitBuilder(headerMap: Map<String, String>? = null): Retrofit.Builder {
         return Retrofit.Builder()
-            .client(getOkHttpClient())
+            .client(getOkHttpClient(headerMap))
             .addConverterFactory(ScalarsConverterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
 //        .addConverterFactory(MoshiConverterFactory.create())

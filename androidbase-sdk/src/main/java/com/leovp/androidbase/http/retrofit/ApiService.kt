5e802b2324dafe7c5b6ad2aabc174e812256cd1b
Michael Leo <yhzemail61010@aliyun.com>
2021-05-08 13:10:22 +0800

    refactor the way to set default headers for http request

--- androidbase-sdk/src/main/java/com/leovp/androidbase/http/retrofit/ApiService.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/http/retrofit/ApiService.kt
@@ -8,11 +8,11 @@ import okhttp3.HttpUrl
  */
 @Suppress("unused")
 object ApiService {
-    fun <T> getService(baseUrl: String, clazz: Class<T>, headers: Map<String, String>? = null): T {
-        return HttpRequest.getInstance(headers ?: emptyMap()).getRetrofit(baseUrl).create(clazz)
+    fun <T> getService(baseUrl: String, clazz: Class<T>): T {
+        return HttpRequest.getRetrofit(baseUrl).create(clazz)
     }
 
-    fun <T> getService(baseUrl: HttpUrl, clazz: Class<T>, headers: Map<String, String>? = null): T {
-        return getService(baseUrl.toString(), clazz, headers)
+    fun <T> getService(baseUrl: HttpUrl, clazz: Class<T>): T {
+        return getService(baseUrl.toString(), clazz)
     }
 }

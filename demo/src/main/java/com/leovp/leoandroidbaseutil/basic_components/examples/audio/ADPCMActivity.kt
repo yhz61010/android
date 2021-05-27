47702ee8ce86360437919ed387c0d91ca2eaf836
MikeoLeo <yhzemail61010@aliyun.com>
2021-05-27 17:11:34 +0800

    1. add AdpcmImaQTDecoder.kt and related demo 2. upgrade abiFilters to "arm64-v8a"

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/audio/ADPCMActivity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/audio/ADPCMActivity.kt
@@ -4,22 +4,42 @@ import android.media.AudioFormat
 import android.os.Bundle
 import android.view.View
 import com.leovp.androidbase.exts.android.toast
+import com.leovp.androidbase.exts.kotlin.fail
 import com.leovp.androidbase.exts.kotlin.toByteArrayLE
 import com.leovp.androidbase.exts.kotlin.toShortArrayLE
 import com.leovp.androidbase.utils.file.FileUtil
+import com.leovp.androidbase.utils.log.LogContext
 import com.leovp.audio.AudioPlayer
 import com.leovp.audio.adpcm.ADPCMCodec
+import com.leovp.audio.adpcm.AdpcmImaQTDecoder
 import com.leovp.audio.base.AudioType
 import com.leovp.audio.base.bean.AudioDecoderInfo
 import com.leovp.leoandroidbaseutil.R
 import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
+import org.bytedeco.ffmpeg.avutil.AVFrame
+import org.bytedeco.ffmpeg.global.avutil.av_get_bytes_per_sample
+import org.bytedeco.javacpp.BytePointer
 import kotlin.concurrent.thread
 
 class ADPCMActivity : BaseDemonstrationActivity() {
+    companion object {
+        private const val OUTPUT_IMA_FILE_NAME = "adpcm_ima_22050_2ch_s16le.ima"
+    }
+
     private val adpcm = ADPCMCodec()
+    private val adpcmQT = AdpcmImaQTDecoder(2, 44100)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_a_d_p_c_m)
+    }
+
+    fun onEncodeToADPCMClick(@Suppress("UNUSED_PARAMETER") view: View) {
+        val inputStream = resources.openRawResource(R.raw.raw_22050_2ch_s16le)
+        val pcmData = inputStream.readBytes()
+        val adpcmArray = adpcm.encode(pcmData.toShortArrayLE())
+        FileUtil.createFile(this, OUTPUT_IMA_FILE_NAME).writeBytes(adpcmArray)
+        toast("Encode done!")
     }
 
     fun onPlayADPCMClick(@Suppress("UNUSED_PARAMETER") view: View) {
@@ -35,15 +55,48 @@ class ADPCMActivity : BaseDemonstrationActivity() {
         thread { player.play(shortPcmArray.toByteArrayLE()) }
     }
 
-    fun onEncodeToADPCMClick(@Suppress("UNUSED_PARAMETER") view: View) {
-        val inputStream = resources.openRawResource(R.raw.raw_22050_2ch_s16le)
-        val pcmData = inputStream.readBytes()
-        val adpcmArray = adpcm.encode(pcmData.toShortArrayLE())
-        FileUtil.createFile(this, OUTPUT_IMA_FILE_NAME).writeBytes(adpcmArray)
-        toast("Encode done!")
-    }
+    fun onPlayRawAdpcmClick(@Suppress("UNUSED_PARAMETER") view: View) {
+        val player = AudioPlayer(this, AudioDecoderInfo(44100, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT), AudioType.PCM)
+//        val adpcmIMAQtPcmFile = FileUtil.createFile(this, "adpcm_ima_qt_44100_2channels_s16le.pcm")
+        thread {
+//            val os = BufferedOutputStream(FileOutputStream(adpcmIMAQtPcmFile))
+            val inputStream = resources.openRawResource(R.raw.adpcm_ima_qt_44100_2channels_s16le)
+            val musicBytes = inputStream.readBytes()
+            val chunkSize: Int = adpcmQT.chunkSize()
+            for (i in musicBytes.indices step chunkSize) {
+                val chunk = musicBytes.copyOfRange(i, i + chunkSize)
+                val avFrame: AVFrame = adpcmQT.decode(chunk) ?: fail("ADPCM decode error")
 
-    companion object {
-        private const val OUTPUT_IMA_FILE_NAME = "adpcm_ima_22050_2ch_s16le.ima"
+//                val bp: BytePointer = avFrame.data(0)
+//                val pcmChunkBytes = ByteArray(avFrame.linesize().get())
+//                bp.get(pcmChunkBytes)
+
+                val bpLeft: BytePointer = avFrame.extended_data(0)
+                val leftChunkBytes = ByteArray(avFrame.linesize(0))
+                bpLeft.get(leftChunkBytes)
+
+//                val bpRight: BytePointer = avFrame.extended_data(1)
+//                val rightChunkBytes = ByteArray(avFrame.linesize(1))
+//                bpRight.get(rightChunkBytes)
+//
+                val stereoPcmBytes = leftChunkBytes // + rightChunkBytes
+
+//                os.write(pcmChunkBytes)
+
+                if (LogContext.enableLog) LogContext.log.i(
+                    "$i: bytes per sample=${av_get_bytes_per_sample(avFrame.format())} chunk[${stereoPcmBytes.size}] ch:${avFrame.channels()} sampleRate:${avFrame.sample_rate()} np_samples:${avFrame.nb_samples()} " +
+                            " linesize[0]=${avFrame.linesize(0)} fmt[${avFrame.format()}]:${
+                                AdpcmImaQTDecoder.getSampleFormatName(
+                                    avFrame.format()
+                                )
+                            }"
+                )
+
+                player.play(stereoPcmBytes)
+            }
+//            os.flush()
+//            os.close()
+            if (LogContext.enableLog) LogContext.log.i("PCM wrote.")
+        }
     }
 }

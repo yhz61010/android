46a475ca66be37188775f2ccdde05e819f1d43b0
Michael Leo <yhzemail61010@aliyun.com>
2021-06-04 17:40:06 +0800

    improve LangUtil.kt and demo

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/ChangeAppLanguageActivity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/ChangeAppLanguageActivity.kt
@@ -3,12 +3,18 @@ package com.leovp.leoandroidbaseutil.basic_components.examples
 import android.os.Bundle
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.leovp.androidbase.exts.android.setOnSingleClickListener
+import com.leovp.androidbase.utils.log.LogContext
 import com.leovp.androidbase.utils.system.LangUtil
 import com.leovp.leoandroidbaseutil.R
 import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
 import com.leovp.leoandroidbaseutil.databinding.ActivityChangeAppLanguageBinding
 
 /**
+ * Attention:
+ * For Chinese language(Simplified Chinese, Traditional Chinese),
+ * - If you set language in `zh_CN`, you should create `values-zh-rCN` folder in `values` folder.
+ * - If you set language in `zh`, you should create `values-zh` folder in `values` folder.
+ *
  * Add the following codes into your base activity:
  *
  * ```kotlin
@@ -36,6 +42,10 @@ class ChangeAppLanguageActivity : BaseDemonstrationActivity() {
         val binding = ActivityChangeAppLanguageBinding.inflate(layoutInflater).apply { setContentView(root) }
         val itemList = resources.getStringArray(R.array.lang_list)
         val itemCodeList = resources.getStringArray(R.array.lang_code_list)
+
+        LogContext.log.i("Default language: ${LangUtil.getDefaultDisplayLanguage()}[${LangUtil.getDefaultLanguageCountryCode()}][${LangUtil.getDefaultLanguageFullCode()}]")
+        LogContext.log.i("Device locale: ${LangUtil.getDeviceLocale()}[${LangUtil.getDeviceLanguageCountryCode()}]")
+        LogContext.log.i("========================================================")
 
         binding.btnSelectLang.setOnSingleClickListener {
             MaterialAlertDialogBuilder(this)

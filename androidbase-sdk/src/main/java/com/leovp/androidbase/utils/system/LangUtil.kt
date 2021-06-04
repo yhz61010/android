cf2e1b2909351c30632b6c089228a0856e68ee9d
Michael Leo <yhzemail61010@aliyun.com>
2021-06-04 15:06:31 +0800

    update LangUtil.kt

--- androidbase-sdk/src/main/java/com/leovp/androidbase/utils/system/LangUtil.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/utils/system/LangUtil.kt
@@ -9,7 +9,6 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.leovp.androidbase.exts.android.app
 import com.leovp.androidbase.exts.android.sharedPrefs
 import com.leovp.androidbase.exts.kotlin.fail
-import com.leovp.androidbase.utils.log.LogContext
 import java.util.*
 
 /**
@@ -20,24 +19,38 @@ object LangUtil {
     const val INTENT_APP_LANG_CHANGE = "app-lang-change-broadcast"
     private const val PREF_KEY_LANGUAGE = "language"
 
-    private var currentAppLang: String? = null
+    private var currentAppLang: Locale? = null
 
+    fun getLocale(languageAndCountry: String): Locale? {
+        return runCatching {
+            if (languageAndCountry.contains("_")) {
+                val langCountry = languageAndCountry.split("_".toRegex())
+                Locale(langCountry[0], langCountry[1])
+            } else {
+                Locale(languageAndCountry)
+            }
+        }.getOrNull()
+    }
+
+    /**
+     * Most of time, you should call the handy method [saveLanguageAndRefreshUI].
+     */
     @Synchronized
-    fun saveLanguage(language: String) {
-        app.sharedPrefs.edit().run { putString(PREF_KEY_LANGUAGE, language); apply() }
+    fun saveLanguage(language: Locale) {
+        app.sharedPrefs.edit().run { putString(PREF_KEY_LANGUAGE, language.toString()); apply() }
         currentAppLang = language
     }
 
     @Synchronized
-    fun saveLanguageAndRefreshUI(language: String) {
+    fun saveLanguageAndRefreshUI(language: Locale) {
         saveLanguage(language)
         LocalBroadcastManager.getInstance(app).sendBroadcast(Intent(INTENT_APP_LANG_CHANGE))
     }
 
     @Synchronized
-    fun getAppLanguage(): String {
+    fun getAppLanguage(): Locale {
         if (currentAppLang == null) {
-            currentAppLang = app.sharedPrefs.getString(PREF_KEY_LANGUAGE, "en") ?: "en"
+            currentAppLang = getLocale(app.sharedPrefs.getString(PREF_KEY_LANGUAGE, "en") ?: "en")
         }
         return currentAppLang ?: fail("Unexpected exception on getLanguageFromPreference()")
     }
@@ -46,24 +59,14 @@ object LangUtil {
      * @param ctx Most of time, this context should be `Activity` context.
      */
     fun changeAppLanguage(ctx: Context) {
-        val savedLang: String = getAppLanguage()
-        if (savedLang.isBlank()) {
-            if (LogContext.enableLog) LogContext.log.w("Can not get language from preference.")
-            return
-        }
-        val locale: Locale = if (savedLang.contains("_")) {
-            val langCountry = savedLang.split("_".toRegex())
-            Locale(langCountry[0], langCountry[1])
-        } else {
-            Locale(savedLang)
-        }
+        val savedLang: Locale = getAppLanguage()
         val res: Resources = ctx.resources
         val conf = res.configuration
-        conf.locale = locale
+        conf.locale = savedLang
         res.updateConfiguration(conf, res.displayMetrics)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            Locale.setDefault(locale)
-            val localeList = LocaleList(locale)
+            Locale.setDefault(savedLang)
+            val localeList = LocaleList(savedLang)
             conf.setLocales(localeList)
             ctx.createConfigurationContext(conf)
         }

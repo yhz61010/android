4b85d05174c3a0f2a808275982703892aa24d383
Michael Leo <yhzemail61010@aliyun.com>
2021-06-17 17:12:06 +0800

    minor changes

--- adpcm-ima-qt-sdk/src/main/jni/Android.mk
+++ adpcm-ima-qt-sdk/src/main/jni/Android.mk
@@ -32,6 +32,11 @@ LOCAL_MODULE := libswresample
 LOCAL_SRC_FILES := $(MY_PREBUILT)/lib/libswresample.so
 include $(PREBUILT_SHARED_LIBRARY)
 
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libswscale
+#LOCAL_SRC_FILES := $(MY_PREBUILT)/lib/libswscale.so
+#include $(PREBUILT_SHARED_LIBRARY)
+
 # ==================================
 
 include $(CLEAR_VARS)
@@ -40,6 +45,7 @@ LOCAL_SRC_FILES := libadpcm_ima_qt_jni.cpp
 LOCAL_CFLAGS    :=
 LOCAL_LDLIBS    := -llog -ljnigraphics -lz -landroid -lm -pthread -L$(SYSROOT)/usr/lib
 LOCAL_C_INCLUDES := $(LOCAL_C_INCLUDES) $(MY_PREBUILT)/include
+# libswscale
 LOCAL_SHARED_LIBRARIES := libavdevice libavcodec libavfilter libavformat libavutil libswresample
 LOCAL_DISABLE_FORMAT_STRING_CHECKS := true
 LOCAL_DISABLE_FATAL_LINKER_WARNINGS := true

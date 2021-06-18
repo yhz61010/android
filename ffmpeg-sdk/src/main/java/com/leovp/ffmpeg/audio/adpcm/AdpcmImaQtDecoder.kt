fdec41fe3493787091c6df69f66cf45b190c1345
Michael Leo <yhzemail61010@aliyun.com>
2021-06-18 14:39:22 +0800

    improve adpcm demo

--- ffmpeg-sdk/src/main/java/com/leovp/ffmpeg/audio/adpcm/AdpcmImaQtDecoder.kt
+++ ffmpeg-sdk/src/main/java/com/leovp/ffmpeg/audio/adpcm/AdpcmImaQtDecoder.kt
@@ -1,39 +1,35 @@
-package com.leovp.ffmpeg.audio.adpcm;
+package com.leovp.ffmpeg.audio.adpcm
 
-import com.leovp.ffmpeg.base.DecodedAudioResult;
+import com.leovp.ffmpeg.base.DecodedAudioResult
 
 /**
  * Author: Michael Leo
  * Date: 2021/6/11 09:57
  */
-public class AdpcmImaQtDecoder {
-
-    private AdpcmImaQtDecoder() {
+class AdpcmImaQtDecoder private constructor() {
+    companion object {
+        init {
+            System.loadLibrary("adpcm-ima-qt")
+            System.loadLibrary("avcodec")
+            System.loadLibrary("avutil")
+        }
     }
 
-    public AdpcmImaQtDecoder(int sampleRate, int channels) {
-        init(sampleRate, channels);
+    constructor(sampleRate: Int, channels: Int) : this() {
+        init(sampleRate, channels)
     }
 
-    private native int init(int sampleRate, int channels);
-
-    public native void release();
-
-    public native DecodedAudioResult decode(byte[] adpcmBytes);
-
-    public native String getVersion();
+    private external fun init(sampleRate: Int, channels: Int): Int
+    external fun release()
+    external fun decode(adpcmBytes: ByteArray): DecodedAudioResult
+    val version: String
+        external get
 
     /**
      * In QuickTime, IMA is encoded by chunks of 34 bytes (=64 samples).
      * Channel data is interleaved per-chunk.
-     * <p>
+     *
      * The return result is 34 bytes * channels
      */
-    public native int chunkSize();
-
-    static {
-        System.loadLibrary("adpcm-ima-qt");
-        System.loadLibrary("avcodec");
-        System.loadLibrary("avutil");
-    }
+    external fun chunkSize(): Int
 }

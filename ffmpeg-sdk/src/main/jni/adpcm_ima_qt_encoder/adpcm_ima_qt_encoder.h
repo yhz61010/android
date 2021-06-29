d80287f9e1884e05e0686aa29ad8029f04a09031
Michael Leo <yhzemail61010@aliyun.com>
2021-06-29 17:31:24 +0800

    refactor adpcm-ima-qt encoder

---
+++ ffmpeg-sdk/src/main/jni/adpcm_ima_qt_encoder/adpcm_ima_qt_encoder.h
@@ -0,0 +1,42 @@
+#ifndef LEOANDROIDBASEUTIL_ADPCM_IMA_QT_ENCODER_H
+#define LEOANDROIDBASEUTIL_ADPCM_IMA_QT_ENCODER_H
+
+#include <jni.h>
+#include <string>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <libavcodec/avcodec.h>
+
+#include <libavutil/channel_layout.h>
+#include <libavutil/common.h>
+#include <libavutil/frame.h>
+#include <libavutil/samplefmt.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+typedef void(*pCallbackFunc)(uint8_t *encodedAudioData, int decodedAudioLength);
+
+class AdpcmImaQtEncoder {
+private:
+    AVFrame *frame = nullptr;
+    AVPacket *pkt = nullptr;
+
+    static void encode_n(AVCodecContext *pCtx, AVFrame *pFrame, AVPacket *pPkt, pCallbackFunc callback);
+
+public:
+    AVCodecContext *c = nullptr;
+
+    int init(int sampleRate, int channels, int bitRate);
+
+    void release();
+
+    void encode(const uint8_t *pcmByteArray, int pcmLen, pCallbackFunc callback);
+
+};
+
+#endif //LEOANDROIDBASEUTIL_ADPCM_IMA_QT_ENCODER_H

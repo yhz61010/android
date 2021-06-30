03270ae51d9c69221b2b1209d0f1769c40571581
MikeoLeo <yhzemail61010@aliyun.com>
2021-06-30 18:00:10 +0800

    add comments

--- ffmpeg-sdk/src/main/java/com/leovp/ffmpeg/audio/adpcm/AdpcmImaQtDecoder.kt
+++ ffmpeg-sdk/src/main/java/com/leovp/ffmpeg/audio/adpcm/AdpcmImaQtDecoder.kt
@@ -24,16 +24,23 @@ class AdpcmImaQtDecoder private constructor() {
      * In QuickTime, IMA is encoded by chunks of 34 bytes (=64 samples) for each channel.
      * Channel data is interleaved per-chunk.
      *
+     * That means for mono audio, it will decode 34 bytes each time,
+     * for stereo audio, it will decode 68 bytes each time,
+     *
      * @param adpcmBytes The length of this parameter is 34 bytes * channels
+     * @return The returned pcm data is interleaved.
+     * Like this:
+     * ```
+     * | L0        | L0         | R0        | R0         | L0        | L1         | L1        |R1          |
+     * | --------- | ---------- | --------- | ---------- | --------- | ---------- | --------- | ---------- |
+     * | Low 8bits | High 8bits | Low 8bits | High 8bits | Low 8bits | High 8bits | Low 8bits | High 8bits |
+     * ```
      */
     external fun decode(adpcmBytes: ByteArray): ByteArray
     external fun getVersion(): String
 
     /**
-     * In QuickTime, IMA is encoded by chunks of 34 bytes (=64 samples) for each channel.
-     * Channel data is interleaved per-chunk.
-     *
-     * The return result is 34 bytes * channels
+     * @return The result is 34 bytes * channels
      */
     external fun chunkSize(): Int
 }

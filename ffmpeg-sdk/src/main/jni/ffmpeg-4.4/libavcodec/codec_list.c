2499e93b0b3624d1da76152d12ab770eb7c260db
MikeoLeo <yhzemail61010@aliyun.com>
2021-07-01 11:17:26 +0800

    remove ffmpeg-4.4 source codes from project and add its zip source file instead.

--- ffmpeg-sdk/src/main/jni/ffmpeg-4.4/libavcodec/codec_list.c
+++
@@ -1,4 +0,0 @@
-static const AVCodec * const codec_list[] = {
-    &ff_adpcm_ima_qt_encoder,
-    &ff_adpcm_ima_qt_decoder,
-    NULL };
--- ffmpeg-sdk/src/main/jni/ffmpeg-4.4/libavcodec/parser_list.c
+++
@@ -1,2 +0,0 @@
-static const AVCodecParser * const parser_list[] = {
-    NULL };

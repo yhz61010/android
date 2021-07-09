4e66b44eb950c8c525822fd81ed6ea327d73087e
Michael Leo <yhzemail61010@aliyun.com>
2021-07-09 16:38:48 +0800

    replace java to kotlin file for x264 module although it hasn't been used yet

---
+++ x264/src/main/java/com/leovp/x264/X264Encoder.kt
@@ -0,0 +1,19 @@
+package com.leovp.ffmpeg.audio.x264
+
+/**
+ * Author: Michael Leo
+ * Date: 21-3-18 下午4:21
+ */
+class X264Encoder {
+    external fun initEncoder(params: X264Params?): X264InitResult?
+    external fun releaseEncoder()
+    external fun encodeFrame(frame: ByteArray?, colorFormat: Int, pts: Long): X264EncodeResult?
+    external fun getVersion(): String
+    private val ctx: Long = 0
+
+    companion object {
+        init {
+            System.loadLibrary("x264a")
+        }
+    }
+}

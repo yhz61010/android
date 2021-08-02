d7accfb51ca7ab3b2022995da2e7be859f1def38
Michael Leo <yhzemail61010@aliyun.com>
2021-08-02 11:24:24 +0800

    add more utils and test cases

--- androidbase-sdk/src/test/java/com/leovp/leoandroidbaseutil/H264UtilTest.kt
+++ androidbase-sdk/src/test/java/com/leovp/leoandroidbaseutil/H264UtilTest.kt
@@ -2,9 +2,7 @@ package com.leovp.leoandroidbaseutil
 
 import android.util.Log
 import com.leovp.androidbase.exts.kotlin.toJsonString
-import com.leovp.androidbase.utils.media.H264SPSParser
 import com.leovp.androidbase.utils.media.H264Util
-import com.leovp.androidbase.utils.media.SpsFrame
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -27,12 +25,5 @@ class H264UtilTest {
         Assert.assertEquals("[0,0,0,1,103,66,-128,31,-23,3,-64,-41,64,54,-123,9,-88]", sps.toJsonString())
         val pps = H264Util.getPps(byteArray)!!
         Assert.assertEquals("[0,0,0,1,104,-50,6,-30]", pps.toJsonString())
-
-        val spsByteArray = byteArrayOf(103, 66, -128, 31, -23, 3, -64, -41, 64, 54, -123, 9, -88)
-
-        val h264Parser = H264SPSParser()
-        val h264Frame = SpsFrame().getSpsFrame(spsByteArray)
-        println("width=${h264Parser.getWidth(h264Frame)}")
-        println("height=${h264Parser.getHeight(h264Frame)}")
     }
 }

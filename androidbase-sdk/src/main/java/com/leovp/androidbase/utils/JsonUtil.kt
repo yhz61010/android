daf12fc7fcc39c98b8a12407a3f486a2967bd126
MikeoLeo <yhzemail61010@aliyun.com>
2021-08-04 13:05:53 +0800

    1. refactor json util 2. add JsonUnitTest.kt

--- androidbase-sdk/src/main/java/com/leovp/androidbase/utils/JsonUtil.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/utils/JsonUtil.kt
@@ -1,10 +1,7 @@
 package com.leovp.androidbase.utils
 
-import com.google.gson.ExclusionStrategy
-import com.google.gson.FieldAttributes
-import com.google.gson.GsonBuilder
-import com.leovp.androidbase.exts.kotlin.Exclude
-import com.leovp.androidbase.utils.log.LogContext
+import com.leovp.androidbase.exts.kotlin.toJsonString
+import com.leovp.androidbase.exts.kotlin.toObject
 import java.lang.reflect.Type
 
 /**
@@ -12,14 +9,6 @@ import java.lang.reflect.Type
  * Date: 20-5-13 下午1:56
  */
 object JsonUtil {
-    private const val TAG = "JsonUtil"
-    private val gson
-        get() = GsonBuilder().addSerializationExclusionStrategy(object : ExclusionStrategy {
-            override fun shouldSkipField(f: FieldAttributes) = f.getAnnotation(Exclude::class.java) != null
-
-            override fun shouldSkipClass(clazz: Class<*>?) = false
-        }).create()
-
     /**
      * Convert json string to object
      *
@@ -29,12 +18,7 @@ object JsonUtil {
      * @return an object of type T from the string. Returns `null` if `json` is `null`
      * or if `json` is empty.
      */
-    fun <T> toObject(json: String?, clazz: Class<T>): T? {
-        return runCatching { gson.fromJson(json, clazz) }.getOrElse {
-            LogContext.log.e(TAG, "Can not to object. Exception: ${it.message}")
-            null
-        }
-    }
+    fun <T> toObject(json: String?, clazz: Class<T>): T? = json?.toObject(clazz)
 
     /**
      * Convert json string to object
@@ -49,10 +33,7 @@ object JsonUtil {
      * @return an object of type T from the string. Returns `null` if `json` is `null`
      * or if `json` is empty.
      */
-    fun <T> toObject(json: String?, type: Type): T? {
-        runCatching { return gson.fromJson(json, type) }.onFailure { LogContext.log.e(TAG, "Can not to object. Exception: ${it.message}") }
-        return null
-    }
+    fun <T> toObject(json: String?, type: Type): T? = json?.toObject(type)
 
     /**
      * Serializing the specified object into string.
@@ -60,10 +41,5 @@ object JsonUtil {
      * @param obj the object for which JSON representation is to be created
      * @return Returns `json string` or `""` if serializing failed.
      */
-    fun toJsonString(obj: Any?): String {
-        return runCatching { gson.toJson(obj) }.getOrElse {
-            LogContext.log.e(TAG, "Can not to json string. Exception: ${it.message}")
-            ""
-        }
-    }
+    fun toJsonString(obj: Any?): String = obj?.toJsonString() ?: ""
 }

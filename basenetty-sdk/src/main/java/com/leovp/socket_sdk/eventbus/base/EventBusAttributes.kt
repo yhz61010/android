ecc8685976e3e4d5b3035fa6c8836df2f30b7413
MikeoLeo <yhzemail61010@aliyun.com>
2021-08-09 16:56:10 +0800

    add EventBusBridge demo

---
+++ basenetty-sdk/src/main/java/com/leovp/socket_sdk/eventbus/base/EventBusAttributes.kt
@@ -0,0 +1,18 @@
+package com.leovp.socket_sdk.eventbus.base
+
+/**
+ * Author: Michael Leo
+ * Date: 2021/7/22 17:16
+ */
+object EventBusAttributes {
+    const val ADDRESS = "address"
+    const val REPLY_ADDRESS = "replyAddress"
+    const val HEADERS = "headers"
+    const val BODY = "body"
+    const val TYPE = "type"
+
+    const val TYPE_SEND = "send"
+    const val TYPE_PUBLISH = "publish"
+    const val TYPE_REGISTER = "register"
+    const val TYPE_UNREGISTER = "unregister"
+}

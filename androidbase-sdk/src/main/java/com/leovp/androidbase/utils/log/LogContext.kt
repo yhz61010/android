a329b22ea3aee8544bdde12fa1ef3fd8c8af1e9d
Michael Leo <yhzemail61010@aliyun.com>
2021-09-26 09:28:46 +0800

    minor changes

--- androidbase-sdk/src/main/java/com/leovp/androidbase/utils/log/LogContext.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/utils/log/LogContext.kt
@@ -3,6 +3,19 @@ package com.leovp.androidbase.utils.log
 import com.leovp.androidbase.utils.log.base.ILog
 
 /**
+ *  You can implement your log wrapper by implement `ILog` or else the default log wrapper `LLog` will be used.
+ * `LLog` is the wrapper of Android default log.
+ *
+ * After implementing your log wrapper, you must initialize it in `Application` by calling:
+ * Assume that `CLog` is your custom log wrapper.
+ * ```kotlin
+ * LogContext.setLogImp(CLog().apply { init(this@CustomApplication) })
+ * ```
+ * The default implementation is like this below:
+ * ```kotlin
+ * LogContext.setLogImp(LLog("LEO"))
+ * ```
+ *
  * Author: Michael Leo
  * Date: 2020/10/16 下午6:15
  */

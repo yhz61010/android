632150bf9d8731cff5c1aef500cf64481f2c659d
Michael Leo <yhzemail61010@aliyun.com>
2021-09-30 13:51:04 +0800

    Fix vulnerabilities.

--- androidbase-sdk/src/main/java/com/leovp/androidbase/utils/cipher/RSAUtil.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/utils/cipher/RSAUtil.kt
@@ -14,6 +14,20 @@ import javax.crypto.Cipher
  */
 @Suppress("unused")
 object RSAUtil {
+    private const val CIPHER_ALGORITHM = "RSA"
+
+    /**
+     * Recommended for RSA:
+     * - RSA/None/OAEPWITHSHA-256ANDMGF1PADDING
+     * - RSA/ECB/OAEPWITHSHA-256ANDMGF1PADDING
+     *
+     * The ECB mode can be used for RSA when "None" is not available with the security provider used.
+     * In that case, ECB will be treated as "None" for RSA.
+     *
+     * It's better not to use **RSA/ECB/PKCS1Padding**.
+     */
+    private const val CIPHER_TRANSFORMATION = "RSA/ECB/OAEPWITHSHA-256ANDMGF1PADDING"
+
     private const val KEY_SIZE = 2048
     private const val MAX_ENCRYPT_LEN = KEY_SIZE / 8 - 11
 
@@ -24,9 +38,9 @@ object RSAUtil {
         return try {
             val keyBytes = Base64.decode(pubKey.toByteArray(), Base64.NO_WRAP)
             val spec = X509EncodedKeySpec(keyBytes)
-            val keyFactory = KeyFactory.getInstance("RSA")
+            val keyFactory = KeyFactory.getInstance(CIPHER_ALGORITHM)
             val key = keyFactory.generatePublic(spec)
-            val cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding")
+            val cipher = Cipher.getInstance(CIPHER_TRANSFORMATION)
             cipher.init(Cipher.DECRYPT_MODE, key)
             String(cipher.doFinal(encryptedData))
         } catch (e: Exception) {
@@ -40,9 +54,9 @@ object RSAUtil {
         return try {
             val data =
                 PKCS8EncodedKeySpec(Base64.decode(priKey.toByteArray(), Base64.NO_WRAP))
-            val factory = KeyFactory.getInstance("RSA")
+            val factory = KeyFactory.getInstance(CIPHER_ALGORITHM)
             val key = factory.generatePrivate(data)
-            val cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding")
+            val cipher = Cipher.getInstance(CIPHER_TRANSFORMATION)
             cipher.init(Cipher.ENCRYPT_MODE, key)
             Base64.encodeToString(cipher.doFinal(plainText.toByteArray()), Base64.NO_WRAP)
         } catch (e: Exception) {
@@ -68,7 +82,7 @@ object RSAUtil {
     // The RSA key MUST BE 2048 bits or higher.
     fun decryptStringByFragment(pubKey: String, wholeText: String): String {
         val result = StringBuilder()
-        val configParts = wholeText.split("\n").toTypedArray()
+        val configParts = wholeText.split('\n').toTypedArray()
         for (part in configParts) {
             result.append(decrypt(pubKey, Base64.decode(part, Base64.NO_WRAP)))
         }

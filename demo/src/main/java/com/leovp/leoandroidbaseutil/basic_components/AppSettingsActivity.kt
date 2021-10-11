b7ce60419dcf84d848e1db87032e4ad02431a1eb
Michael Leo <yhzemail61010@aliyun.com>
2021-10-11 15:09:10 +0800

    Add more utils.

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/AppSettingsActivity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/AppSettingsActivity.kt
@@ -1,10 +1,13 @@
 package com.leovp.leoandroidbaseutil.basic_components
 
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import com.leovp.androidbase.exts.android.startAppDetailSetting
 import com.leovp.androidbase.exts.android.startAppStorageSettings
 import com.leovp.androidbase.exts.android.startManageDrawOverlaysPermission
+import com.leovp.androidbase.exts.android.startUnitySettingPage
 import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
 import com.leovp.leoandroidbaseutil.databinding.ActivityAppSettingsBinding
 
@@ -26,5 +29,13 @@ class AppSettingsActivity : BaseDemonstrationActivity() {
 
     fun onOverlayPermissionClick(@Suppress("UNUSED_PARAMETER") view: View) {
         runCatching { startManageDrawOverlaysPermission() }
+    }
+
+    fun onUnitySettingClick(@Suppress("UNUSED_PARAMETER") view: View) {
+        runCatching {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                startUnitySettingPage(Settings.ACTION_MANAGE_WRITE_SETTINGS)
+            }
+        }
     }
 }

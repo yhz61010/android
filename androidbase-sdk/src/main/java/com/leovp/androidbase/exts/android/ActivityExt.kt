2508b99f28d4dcbcf530dab52cdecd97c09c2e32
Michael Leo <yhzemail61010@aliyun.com>
2021-10-13 10:18:38 +0800

    Enhance ActivityExt.kt

--- androidbase-sdk/src/main/java/com/leovp/androidbase/exts/android/ActivityExt.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/exts/android/ActivityExt.kt
@@ -19,29 +19,60 @@ fun Activity.ignoreDuplicateStartSplash(): Boolean {
 // ============================================================================
 
 /** Launch a Activity */
-fun Context.startActivity(cls: KClass<*>, flags: Int? = null) = this.startActivity(Intent(this, cls.java).apply { flags?.let { addFlags(it) } })
+fun Context.startActivity(kcls: KClass<*>, flags: Int? = null, options: Bundle? = null) =
+    this.startActivity(Intent(this, kcls.java).apply { flags?.let { addFlags(it) } }, options)
 
 /** Launch a Activity */
-fun Fragment.startActivity(cls: KClass<*>, flags: Int? = null) = this.startActivity(Intent(context, cls.java).apply { flags?.let { addFlags(it) } })
+fun Context.startActivity(cls: Class<*>, flags: Int? = null, options: Bundle? = null) =
+    this.startActivity(Intent(this, cls).apply { flags?.let { addFlags(it) } }, options)
 
 /** Launch a Activity */
-fun Context.startActivity(cls: KClass<*>, flags: Int? = null, options: Bundle? = null) = this.startActivity(Intent(this, cls.java).apply { flags?.let { addFlags(it) } }, options)
-
-/** Launch a Activity */
-fun Fragment.startActivity(cls: KClass<*>, flags: Int? = null, options: Bundle? = null) =
-    this.startActivity(Intent(context, cls.java).apply { flags?.let { addFlags(it) } }, options)
-
-/** Launch a Activity */
-@JvmOverloads
-fun Activity.startActivityForResult(cls: KClass<*>, requestCode: Int, flags: Int? = null, options: Bundle? = null) {
-    startActivityForResult(Intent(this, cls.java).apply { flags?.let { addFlags(it) } }, requestCode, options)
-}
+fun Context.startActivity(clsStr: String, flags: Int? = null, options: Bundle? = null) =
+    this.startActivity(Intent(this, Class.forName(clsStr)).apply { flags?.let { addFlags(it) } }, options)
 
 /** Launch a Activity */
 @JvmOverloads
-fun Fragment.startActivityForResult(cls: KClass<*>, requestCode: Int, options: Bundle? = null) {
-    startActivityForResult(Intent(context, cls.java), requestCode, options)
-}
+fun Activity.startActivityForResult(kcls: KClass<*>, requestCode: Int, flags: Int? = null, options: Bundle? = null) =
+    startActivityForResult(Intent(this, kcls.java).apply { flags?.let { addFlags(it) } }, requestCode, options)
+
+/** Launch a Activity */
+@JvmOverloads
+fun Activity.startActivityForResult(cls: Class<*>, requestCode: Int, flags: Int? = null, options: Bundle? = null) =
+    startActivityForResult(Intent(this, cls).apply { flags?.let { addFlags(it) } }, requestCode, options)
+
+/** Launch a Activity */
+@JvmOverloads
+fun Activity.startActivityForResult(clsStr: String, requestCode: Int, flags: Int? = null, options: Bundle? = null) =
+    startActivityForResult(Intent(this, Class.forName(clsStr)).apply { flags?.let { addFlags(it) } }, requestCode, options)
+
+// ----------
+
+/** Launch a Activity in Fragment */
+fun Fragment.startActivity(kcls: KClass<*>, flags: Int? = null, options: Bundle? = null) =
+    this.startActivity(Intent(requireContext(), kcls.java).apply { flags?.let { addFlags(it) } }, options)
+
+/** Launch a Activity in Fragment */
+fun Fragment.startActivity(cls: Class<*>, flags: Int? = null, options: Bundle? = null) =
+    this.startActivity(Intent(requireContext(), cls).apply { flags?.let { addFlags(it) } }, options)
+
+/** Launch a Activity in Fragment */
+fun Fragment.startActivity(clsStr: String, flags: Int? = null, options: Bundle? = null) =
+    this.startActivity(Intent(requireContext(), Class.forName(clsStr)).apply { flags?.let { addFlags(it) } }, options)
+
+/** Launch a Activity in Fragment */
+@JvmOverloads
+fun Fragment.startActivityForResult(kcls: KClass<*>, requestCode: Int, options: Bundle? = null) =
+    startActivityForResult(Intent(requireContext(), kcls.java), requestCode, options)
+
+/** Launch a Activity in Fragment */
+@JvmOverloads
+fun Fragment.startActivityForResult(cls: Class<*>, requestCode: Int, options: Bundle? = null) =
+    startActivityForResult(Intent(requireContext(), cls), requestCode, options)
+
+/** Launch a Activity in Fragment */
+@JvmOverloads
+fun Fragment.startActivityForResult(clsStr: String, requestCode: Int, options: Bundle? = null) =
+    startActivityForResult(Intent(requireContext(), Class.forName(clsStr)), requestCode, options)
 
 // ============================================================
 // ====== Open Settings ==========

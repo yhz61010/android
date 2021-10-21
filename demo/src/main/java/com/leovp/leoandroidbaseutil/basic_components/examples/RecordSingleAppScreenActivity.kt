2b15ee96295f7982e6940e67cb6a97d19e93da57
Michael Leo <yhzemail61010@aliyun.com>
2021-10-21 11:11:52 +0800

    Further improve screen record feature and its demo.

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/RecordSingleAppScreenActivity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/RecordSingleAppScreenActivity.kt
@@ -1,8 +1,10 @@
 package com.leovp.leoandroidbaseutil.basic_components.examples
 
 import android.media.MediaCodec
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import com.leovp.androidbase.exts.android.densityDpi
 import com.leovp.androidbase.exts.android.getAvailableResolution
@@ -16,6 +18,7 @@ import com.leovp.log_sdk.base.ITAG
 import com.leovp.min_base_sdk.toHexString
 import com.leovp.screencapture.screenrecord.ScreenCapture
 import com.leovp.screencapture.screenrecord.base.ScreenDataListener
+import com.leovp.screencapture.screenrecord.base.strategies.ScreenRecordMediaCodecStrategy
 import com.leovp.screencapture.screenrecord.base.strategies.Screenshot2H26xStrategy
 import java.io.BufferedOutputStream
 import java.io.File
@@ -24,29 +27,42 @@ import java.io.FileOutputStream
 
 class RecordSingleAppScreenActivity : BaseDemonstrationActivity() {
 
+    companion object {
+        val VIDEO_ENCODE_TYPE = ScreenRecordMediaCodecStrategy.EncodeType.H265
+    }
+
     private lateinit var binding: ActivityScreenshotRecordH264Binding
 
-    private lateinit var videoH264OsForDebug: BufferedOutputStream
+    private lateinit var videoH26xOsForDebug: BufferedOutputStream
 
     private val screenDataListener = object : ScreenDataListener {
         override fun onDataUpdate(buffer: Any, flags: Int, presentationTimeUs: Long) {
             val data = buffer as ByteArray
             when (flags) {
-                MediaCodec.BUFFER_FLAG_CODEC_CONFIG -> LogContext.log.i(ITAG, "Get h264 data[${data.size}]=${data.toHexString()} presentationTimeUs=$presentationTimeUs")
-                MediaCodec.BUFFER_FLAG_KEY_FRAME -> LogContext.log.i(ITAG, "Get h264 data Key-Frame[${data.size}] presentationTimeUs=$presentationTimeUs")
-                else -> LogContext.log.i(ITAG, "Get h264 data[${data.size}] presentationTimeUs=$presentationTimeUs")
+                MediaCodec.BUFFER_FLAG_CODEC_CONFIG -> LogContext.log.i(
+                    ITAG,
+                    "Get $VIDEO_ENCODE_TYPE data[${data.size}]=${data.toHexString()} presentationTimeUs=$presentationTimeUs"
+                )
+                MediaCodec.BUFFER_FLAG_KEY_FRAME -> LogContext.log.i(ITAG, "Get $VIDEO_ENCODE_TYPE data Key-Frame[${data.size}] presentationTimeUs=$presentationTimeUs")
+                else -> LogContext.log.i(ITAG, "Get $VIDEO_ENCODE_TYPE data[${data.size}] presentationTimeUs=$presentationTimeUs")
             }
-            videoH264OsForDebug.write(data)
+            videoH26xOsForDebug.write(data)
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityScreenshotRecordH264Binding.inflate(layoutInflater).apply { setContentView(root) }
 
         val file = FileUtil.getBaseDirString(this, "output")
-        val dstFile = File(file, "screen.h264")
-        videoH264OsForDebug = BufferedOutputStream(FileOutputStream(dstFile))
+        val dstFile = File(
+            file, "screen" + when (VIDEO_ENCODE_TYPE) {
+                ScreenRecordMediaCodecStrategy.EncodeType.H264 -> ".h264"
+                ScreenRecordMediaCodecStrategy.EncodeType.H265 -> ".h265"
+            }
+        )
+        videoH26xOsForDebug = BufferedOutputStream(FileOutputStream(dstFile))
         LogContext.log.i("dstFile=${dstFile.absolutePath}")
 
         val screenInfo = getAvailableResolution()
@@ -65,7 +81,9 @@ class RecordSingleAppScreenActivity : BaseDemonstrationActivity() {
             null,
             ScreenCapture.BY_IMAGE_2_H26x,
             screenDataListener
-        ).setFps(setting.fps)
+        )
+            .setEncodeType(VIDEO_ENCODE_TYPE)
+            .setFps(setting.fps)
             .setKeyFrameRate(20)
             .setQuality(80)
             .setSampleSize(1)
@@ -73,20 +91,21 @@ class RecordSingleAppScreenActivity : BaseDemonstrationActivity() {
 
         binding.toggleBtn.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
+                // @RequiresApi(Build.VERSION_CODES.O)
                 (screenProcessor as Screenshot2H26xStrategy).startRecord(this)
             } else {
-                videoH264OsForDebug.flush()
-                videoH264OsForDebug.close()
+                videoH26xOsForDebug.flush()
+                videoH26xOsForDebug.close()
                 screenProcessor.onRelease()
             }
         }
     }
 
-    fun onShowToastClick(view: View) {
+    fun onShowToastClick(@Suppress("UNUSED_PARAMETER") view: View) {
         toast("Custom Toast")
     }
 
-    fun onShowDialogClick(view: View) {
+    fun onShowDialogClick(@Suppress("UNUSED_PARAMETER") view: View) {
         AlertDialog.Builder(this)
             .setTitle("Title")
             .setMessage("This is a dialog")

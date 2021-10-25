5f40559245be3371a0c9e8109af75edbca209637
Michael Leo <yhzemail61010@aliyun.com>
2021-10-25 13:55:22 +0800

    Fix bug for BetterActivityResult.kt

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/AppSettingsActivity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/AppSettingsActivity.kt
@@ -1,13 +1,13 @@
 package com.leovp.leoandroidbaseutil.basic_components
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.view.View
-import com.leovp.androidbase.exts.android.startAppDetailSetting
-import com.leovp.androidbase.exts.android.startAppStorageSettings
-import com.leovp.androidbase.exts.android.startManageDrawOverlaysPermission
-import com.leovp.androidbase.exts.android.startUnitySettingPage
+import androidx.annotation.RequiresApi
+import com.leovp.androidbase.exts.android.packageUri
+import com.leovp.androidbase.exts.android.toast
 import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
 import com.leovp.leoandroidbaseutil.databinding.ActivityAppSettingsBinding
 
@@ -20,21 +20,32 @@ class AppSettingsActivity : BaseDemonstrationActivity() {
     }
 
     fun onOpenStorageClick(@Suppress("UNUSED_PARAMETER") view: View) {
-        runCatching { startAppStorageSettings() }
+        runCatching {
+            simpleActivityLauncher.launch(Intent(Settings.ACTION_INTERNAL_STORAGE_SETTINGS, this.packageUri))
+        }
     }
 
     fun onOpenAppDetailClick(@Suppress("UNUSED_PARAMETER") view: View) {
-        runCatching { startAppDetailSetting() }
+        runCatching {
+            simpleActivityLauncher.launch(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, this.packageUri)) { result ->
+                toast("onOpenAppDetailClick result=${result.resultCode}")
+            }
+        }
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     fun onOverlayPermissionClick(@Suppress("UNUSED_PARAMETER") view: View) {
-        runCatching { startManageDrawOverlaysPermission() }
+        runCatching {
+            simpleActivityLauncher.launch(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, this.packageUri)) { result ->
+                toast("onOverlayPermissionClick result=${result.resultCode}")
+            }
+        }
     }
 
     fun onUnitySettingClick(@Suppress("UNUSED_PARAMETER") view: View) {
         runCatching {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                startUnitySettingPage(Settings.ACTION_MANAGE_WRITE_SETTINGS)
+                simpleActivityLauncher.launch(Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, this.packageUri))
             }
         }
     }

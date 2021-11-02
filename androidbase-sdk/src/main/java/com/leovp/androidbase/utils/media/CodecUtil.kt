99c6e084fdf182a4a35cbf6db5ff50df744577dc
Michael Leo <yhzemail61010@aliyun.com>
2021-11-02 17:38:04 +0800

    minor changes

--- androidbase-sdk/src/main/java/com/leovp/androidbase/utils/media/CodecUtil.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/utils/media/CodecUtil.kt
@@ -10,29 +10,26 @@ import android.media.MediaCodecList
  * Date: 20-5-20 上午9:48
  */
 object CodecUtil {
-    @Suppress("unused")
     fun getCodecListByMimeType(mimeType: String, encoder: Boolean = true): List<MediaCodecInfo> = MediaCodecList(MediaCodecList.REGULAR_CODECS) // MediaCodecList.ALL_CODECS
         .codecInfos.filter { it.isEncoder == encoder }.filter { it.supportedTypes.indexOfFirst { type -> type.equals(mimeType, true) } > -1 }
 
-    @Suppress("unused")
     fun hasCodecByName(mimeType: String, codecName: String, encoder: Boolean = true): Boolean =
         getCodecListByMimeType(mimeType, encoder).indexOfFirst { it.name.equals(codecName, true) } > -1
 
-    @Suppress("unused")
     fun getAllSupportedCodecList(): Array<MediaCodecInfo> = MediaCodecList(MediaCodecList.REGULAR_CODECS).codecInfos // MediaCodecList.ALL_CODECS
 
     /**
      * The result is the color format defined in MediaCodecInfo.CodecCapabilities.COLOR_Formatxxx
      */
     fun getSupportedColorFormat(codec: MediaCodec, mime: String): IntArray = getSupportedColorFormat(codec.codecInfo.getCapabilitiesForType(mime))
-    fun getSupportedColorFormat(caps: CodecCapabilities): IntArray = caps.colorFormats
     fun getSupportedColorFormatForEncoder(mime: String): IntArray = getSupportedColorFormat(MediaCodec.createEncoderByType(mime), mime)
     fun getSupportedColorFormatForDecoder(mime: String): IntArray = getSupportedColorFormat(MediaCodec.createDecoderByType(mime), mime)
+    private fun getSupportedColorFormat(caps: CodecCapabilities): IntArray = caps.colorFormats
 
     fun getSupportedProfileLevels(codec: MediaCodec, mime: String): Array<MediaCodecInfo.CodecProfileLevel> =
         getSupportedProfileLevels(codec.codecInfo.getCapabilitiesForType(mime))
 
-    fun getSupportedProfileLevels(caps: CodecCapabilities): Array<MediaCodecInfo.CodecProfileLevel> = caps.profileLevels
     fun getSupportedProfileLevelsForEncoder(mime: String): Array<MediaCodecInfo.CodecProfileLevel> = getSupportedProfileLevels(MediaCodec.createEncoderByType(mime), mime)
     fun getSupportedProfileLevelsForDecoder(mime: String): Array<MediaCodecInfo.CodecProfileLevel> = getSupportedProfileLevels(MediaCodec.createDecoderByType(mime), mime)
+    private fun getSupportedProfileLevels(caps: CodecCapabilities): Array<MediaCodecInfo.CodecProfileLevel> = caps.profileLevels
 }

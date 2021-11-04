ba042d182b04706b0eeadaba6fdeef474a1d87c7
Michael Leo <yhzemail61010@aliyun.com>
2021-11-04 13:27:37 +0800

    minor changes

--- x264/src/main/java/com/leovp/x264/X264Encoder.kt
+++ x264/src/main/java/com/leovp/x264/X264Encoder.kt
@@ -1,19 +1,20 @@
-package com.leovp.ffmpeg.audio.x264
+package com.leovp.x264
 
 /**
  * Author: Michael Leo
  * Date: 21-3-18 下午4:21
  */
 class X264Encoder {
-    external fun initEncoder(params: X264Params?): X264InitResult?
+    external fun initEncoder(params: X264Params): X264InitResult
     external fun releaseEncoder()
-    external fun encodeFrame(frame: ByteArray?, colorFormat: Int, pts: Long): X264EncodeResult?
+    external fun encodeFrame(frame: ByteArray, colorFormat: Int, pts: Long): X264EncodeResult
     external fun getVersion(): String
     private val ctx: Long = 0
 
     companion object {
         init {
-            System.loadLibrary("x264a")
+            System.loadLibrary("libx264-encoder")
+            System.loadLibrary("libx264")
         }
     }
 }

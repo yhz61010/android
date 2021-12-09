9378a993ca42270c13ab4a6d92a30ad2778c31cb
Michael Leo <yhzemail61010@aliyun.com>
2021-12-09 14:31:26 +0800

    1. Get screen dimension in real time. 2. Improve the method of hiding navigation bar on Android 11+.

--- androidbase-sdk/src/main/java/com/leovp/androidbase/exts/android/SystemUI.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/exts/android/SystemUI.kt
@@ -7,7 +7,9 @@ import android.os.Build
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 /**
  * Author: Michael Leo
@@ -15,56 +17,128 @@ import androidx.core.view.WindowInsetsCompat
  */
 
 /**
- * This method must be called before setContentView.
- *
- * Example:
+ * Usage:
  * ```kotlin
- * // Call this method in `onCreate` before `setContentView`
- * requestFullScreen(this)
+ * // Call [requestFullScreenBeforeSetContentView] method in `onCreate` before `setContentView`
+ * override fun onCreate(savedInstanceState: Bundle?) {
+ *     requestFullScreenBeforeSetContentView()
+ *     super.onCreate(savedInstanceState)
+ *     setContentView(R.layout.activity_main)
+ * }
  *
- * // Generally, call this method in `onResume` to let navigation bar always hide
- * hideNavigationBar(this)
+ * override fun onResume() {
+ *     super.onResume()
+ *     // Call [requestFullScreenAfterVisible] method after activity is visual.
+ *     requestFullScreenAfterVisible()
+ *     // Generally, call this method in `onResume` to let navigation bar always hide.
+ *     hideNavigationBar(this)
+ * }
  * ```
  */
-fun Activity.requestFullScreen() {
+fun Activity.requestFullScreenBeforeSetContentView() {
     this.requestWindowFeature(Window.FEATURE_NO_TITLE)
+}
+
+/**
+ * Usage:
+ * ```kotlin
+ * // Call [requestFullScreenBeforeSetContentView] method in `onCreate` before `setContentView`
+ * override fun onCreate(savedInstanceState: Bundle?) {
+ *     requestFullScreenBeforeSetContentView()
+ *     super.onCreate(savedInstanceState)
+ *     setContentView(R.layout.activity_main)
+ * }
+ *
+ * override fun onResume() {
+ *     super.onResume()
+ *     // Call [requestFullScreenAfterVisible] method after activity is visual.
+ *     requestFullScreenAfterVisible()
+ *     // Generally, call this method in `onResume` to let navigation bar always hide.
+ *     hideNavigationBar(this)
+ * }
+ * ```
+ */
+fun Activity.requestFullScreenAfterVisible() {
+    window.requestFullScreenAfterVisible()
+}
+
+/**
+ * Usage:
+ * ```kotlin
+ * // Call [requestFullScreenBeforeSetContentView] method in `onCreate` before `setContentView`
+ * override fun onCreate(savedInstanceState: Bundle?) {
+ *     requestFullScreenBeforeSetContentView()
+ *     super.onCreate(savedInstanceState)
+ *     setContentView(R.layout.activity_main)
+ * }
+ *
+ * override fun onResume() {
+ *     super.onResume()
+ *     // Call [requestFullScreenAfterVisible] method after activity is visual.
+ *     window.requestFullScreenAfterVisible()
+ *     // Generally, call this method in `onResume` to let navigation bar always hide.
+ *     hideNavigationBar(this)
+ * }
+ * ```
+ */
+fun Window.requestFullScreenAfterVisible() {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        this.window.insetsController?.hide(WindowInsetsCompat.Type.statusBars())
+        this.insetsController?.hide(WindowInsetsCompat.Type.statusBars())
     } else {
         @Suppress("DEPRECATION")
-        this.window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        this.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
     // or
 //        act.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
 
 /**
- * This method must be called before setContentView.
+ * Generally, call this method in `onResume` to let navigation bar always hide.
  *
  * Example:
  * ```kotlin
- * requestFullScreen(this)
- * hideNavigationBar(this)
- * setContentView(R.layout.activity_splash)
+ *  override fun onResume() {
+ *     super.onResume()
+ *     // Generally, call this method in `onResume` to let navigation bar always hide.
+ *     hideNavigationBar(this)
+ * }
  * ```
  */
 @SuppressLint("ObsoleteSdkInt")
-fun Activity.hideNavigationBar() {
-    this.window.hideNavigationBar()
+fun Activity.hideNavigationBar(rootView: View) {
+    this.window.hideNavigationBar(rootView)
 }
 
+/**
+ * Generally, call this method in `onResume` to let navigation bar always hide.
+ *
+ * Example:
+ * ```kotlin
+ *  override fun onResume() {
+ *     super.onResume()
+ *     // Generally, call this method in `onResume` to let navigation bar always hide.
+ *     window.hideNavigationBar(this)
+ * }
+ * ```
+ */
 @SuppressLint("ObsoleteSdkInt")
-fun Window.hideNavigationBar() {
+fun Window.hideNavigationBar(rootView: View) {
     // Translucent virtual NavigationBar
 //    this.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        this.setDecorFitsSystemWindows(false)
+        // https://stackoverflow.com/a/64828028
+        WindowCompat.setDecorFitsSystemWindows(this, false)
+        WindowInsetsControllerCompat(this, rootView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.statusBars())
+            controller.hide(WindowInsetsCompat.Type.navigationBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
     } else {
-        @Suppress("DEPRECATION")
         // https://blog.csdn.net/c15522627353/article/details/52452490
         // https://blog.csdn.net/lyabc123456/article/details/88683425
         // Always hide virtual navigation
+        @Suppress("DEPRECATION")
         this.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION

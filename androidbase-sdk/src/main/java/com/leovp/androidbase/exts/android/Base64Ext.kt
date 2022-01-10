2b34cea97dd63e4b3ec4fca25d6300bc52e638ce
Michael Leo <yhzemail61010@aliyun.com>
2022-01-10 10:36:41 +0800

    Refactor some utils.

--- androidbase-sdk/src/main/java/com/leovp/androidbase/exts/android/Base64Ext.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/exts/android/Base64Ext.kt
@@ -27,7 +27,7 @@ import java.nio.charset.Charset
 fun String.toBase64(flag: Int = Base64.DEFAULT): String = Base64.encodeToString(this.toByteArray(), flag)
 
 /**
- * Convert base64 ByteArray to String
+ * Decode base64 String to String
  *
  * @param flag
  * Base64.DEFAULT    -> Default values for encoder/decoder flags.
@@ -40,6 +40,17 @@ fun String.toBase64(flag: Int = Base64.DEFAULT): String = Base64.encodeToString(
 @JvmOverloads
 fun String.fromBase64(flag: Int = Base64.DEFAULT): String = Base64.decode(this, flag).toString(Charset.forName("US-ASCII"))
 
+/**
+ * Decode base64 String to ByteArray
+ *
+ * @param flag
+ * Base64.DEFAULT    -> Default values for encoder/decoder flags.
+ * Base64.CRLF       -> Encoder flag bit to indicate lines should be terminated with a CRLF pair instead of just an LF.  Has no effect if {@code NO_WRAP} is specified as well.
+ * Base64.NO_PADDING -> Encoder flag bit to omit the padding '=' characters at the end of the output (if any).
+ * Base64.NO_WRAP    -> Encoder flag bit to omit all line terminators (i.e., the output will be on one long line).
+ * Base64.URL_SAFE   -> Encoder/decoder flag bit to indicate using the "URL and filename safe" variant of Base64
+ * (see RFC 3548 section 4) where `-` and `_` are used in place of `+` and '/'.
+ */
 fun String.fromBase64ToByteArray(flag: Int = Base64.DEFAULT): ByteArray = Base64.decode(this, flag)
 
 /** Convert String to URL and Filename safe type base64 String */
@@ -55,7 +66,7 @@ val String.toMimeBase64: String @RequiresApi(API.O) get() = java.util.Base64.get
 val String.fromMimeBase64: String @RequiresApi(API.O) get() = java.util.Base64.getMimeDecoder().decode(this).toString()
 
 /**
- * Convert base64 ByteArray to ByteArray
+ * Decode base64 ByteArray to ByteArray
  *
  * @param flag
  * Base64.DEFAULT    -> Default values for encoder/decoder flags.
@@ -81,28 +92,48 @@ fun ByteArray.fromBase64(flag: Int = Base64.DEFAULT): ByteArray = Base64.decode(
  */
 @JvmOverloads
 fun ByteArray.toBase64ByteArray(flag: Int = Base64.DEFAULT): ByteArray = Base64.encode(this, flag)
+
+/**
+ * Convert ByteArray to base64 String
+ *
+ * @param flag
+ * Base64.DEFAULT    -> Default values for encoder/decoder flags.
+ * Base64.CRLF       -> Encoder flag bit to indicate lines should be terminated with a CRLF pair instead of just an LF.  Has no effect if {@code NO_WRAP} is specified as well.
+ * Base64.NO_PADDING -> Encoder flag bit to omit the padding '=' characters at the end of the output (if any).
+ * Base64.NO_WRAP    -> Encoder flag bit to omit all line terminators (i.e., the output will be on one long line).
+ * Base64.URL_SAFE   -> Encoder/decoder flag bit to indicate using the "URL and filename safe" variant of Base64
+ * (see RFC 3548 section 4) where `-` and `_` are used in place of `+` and '/'.
+ */
+@JvmOverloads
 fun ByteArray.toBase64(flag: Int = Base64.DEFAULT): String = Base64.encodeToString(this, flag)
 
 /** Convert ByteArray to base64 ByteArray */
 val ByteArray.toBase64: ByteArray @RequiresApi(API.O) get() = java.util.Base64.getEncoder().encode(this)
 
-/** Convert base64 ByteArray to ByteArray */
+/** Decode base64 ByteArray to ByteArray */
 val ByteArray.fromBase64: ByteArray @RequiresApi(API.O) get() = java.util.Base64.getDecoder().decode(this)
 
 /** Convert ByteArray to URL and Filename safe type base64 ByteArray */
 val ByteArray.toUrlBase64ByteArray: ByteArray @RequiresApi(API.O) get() = java.util.Base64.getUrlEncoder().encode(this)
+
+/** Convert ByteArray to URL and Filename safe type base64 String */
 val ByteArray.toUrlBase64: String @RequiresApi(API.O) get() = java.util.Base64.getUrlEncoder().encodeToString(this)
 
-/** Convert URL and Filename safe type base64 ByteArray to ByteArray */
+/** Decode URL and Filename safe type base64 ByteArray to ByteArray */
 val ByteArray.fromUrlBase64: ByteArray @RequiresApi(API.O) get() = java.util.Base64.getUrlDecoder().decode(this)
 
 /** Convert ByteArray to Mime type base64 ByteArray */
 val ByteArray.toMimeBase64ByteArray: ByteArray @RequiresApi(API.O) get() = java.util.Base64.getMimeEncoder().encode(this)
+
+/** Convert ByteArray to Mime type base64 String */
 val ByteArray.toMimeBase64: String @RequiresApi(API.O) get() = java.util.Base64.getMimeEncoder().encodeToString(this)
 
-/** Convert Mime type base64 ByteArray to ByteArray */
+/** Decode Mime type base64 ByteArray to ByteArray */
 val ByteArray.fromMimeBase64: ByteArray @RequiresApi(API.O) get() = java.util.Base64.getMimeDecoder().decode(this)
 
+/**
+ * Convert Drawable to base64 String.
+ */
 fun Drawable.toBase64(imgType: Bitmap.CompressFormat = Bitmap.CompressFormat.PNG): String? {
     val bmp = this.getBitmap()
     return if (bmp == null) null
@@ -113,6 +144,9 @@ fun Drawable.toBase64(imgType: Bitmap.CompressFormat = Bitmap.CompressFormat.PNG
     }
 }
 
+/**
+ * Decode drawable base64 String to Drawable.
+ */
 fun String.fromBase64ToDrawable(resources: Resources, flag: Int = Base64.DEFAULT): Drawable? {
     return runCatching {
         val drawableBytes = this.fromBase64ToByteArray(flag)
---
+++ demo-dex/src/main/java/com/leovp/demo_dex/network/NetworkMonitor.java
@@ -0,0 +1,163 @@
+package com.leovp.demo_dex.network;
+
+import static android.net.NetworkCapabilities.TRANSPORT_BLUETOOTH;
+import static android.net.NetworkCapabilities.TRANSPORT_CELLULAR;
+import static android.net.NetworkCapabilities.TRANSPORT_ETHERNET;
+import static android.net.NetworkCapabilities.TRANSPORT_VPN;
+import static android.net.NetworkCapabilities.TRANSPORT_WIFI;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.IConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.net.NetworkRequest;
+import android.os.Build;
+import android.os.Looper;
+import android.util.Log;
+
+import com.leovp.demo_dex.utils.DexHelper;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
+public class NetworkMonitor {
+    private Method getServiceMethod;
+    private Object wifiManagerService;
+    private Object connectivityManagerService;
+    private ConnectivityManager connectivityManager;
+    private Context context;
+    private HashMap<String, String> networks;
+
+    public NetworkMonitor() {
+        Looper.prepare();
+        try {
+            networks = new HashMap<>();
+            getServiceMethod = Class.forName("android.os.ServiceManager").getDeclaredMethod("getService", String.class);
+            context = DexHelper.getInstance().getContext();
+
+            wifiManagerService = DexHelper.getInstance().getService("wifi", "android.net.wifi.IWifiManager");
+            connectivityManagerService = DexHelper.getInstance().getService("connectivity", "android.net.IConnectivityManager");
+
+            Class c = Class.forName("android.net.IConnectivityManager");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                Constructor constructor = ConnectivityManager.class.getDeclaredConstructor(Context.class, c);
+                constructor.setAccessible(true);
+                connectivityManager = (ConnectivityManager) constructor.newInstance(context, connectivityManagerService);
+            } else {
+                Constructor constructor = ConnectivityManager.class.getDeclaredConstructor(c);
+                constructor.setAccessible(true);
+                connectivityManager = (ConnectivityManager) constructor.newInstance(connectivityManagerService);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Init services error>>>>>>>>>>");
+            e.printStackTrace();
+        }
+    }
+
+    public void registerNetworkCallback() {
+        try {
+            NetworkRequest request = new NetworkRequest.Builder().build();
+            final CustomConnectivityManager customConnectivityManager = new CustomConnectivityManager((IConnectivityManager) connectivityManagerService);
+            customConnectivityManager.registerNetworkCallback(request, new CustomConnectivityManager.NetworkCallback() {
+                @Override
+                public void onAvailable(Network network) {
+                    super.onAvailable(network);
+                    Log.e("Leo", "=> onAvailable <=");
+                    networkAvail(network);
+                }
+
+                @Override
+                public void onLost(Network network) {
+                    super.onLost(network);
+                    Log.e("Leo", "=> onLost <=");
+                    networkLost(network);
+                }
+
+                @Override
+                public void onCapabilitiesChanged(Network network, NetworkCapabilities networkCapabilities) {
+                    super.onCapabilitiesChanged(network, networkCapabilities);
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Looper.loop();
+    }
+
+    private void networkLost(Network network) {
+        Log.e("Leo", ">>> Lost <<<");
+        String networkType = "unknown";
+        if (networks.containsKey(network.toString())) {
+            networkType = networks.get(network.toString());
+            networks.remove(network.toString());
+        }
+        Log.e("Leo", "networkType=" + networkType);
+    }
+
+    private void networkAvail(Network network) {
+        Log.e("Leo", ">>> networkAvail <<< ");
+//        WifiInfo wifiInfo = DexHelper.getInstance().getWifiInfo();
+//        String networkType = getType(network);
+    }
+
+    private String getType(Network network) {
+        try {
+            NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+
+            Method getTransportTypes = NetworkCapabilities.class.getDeclaredMethod("getTransportTypes", new Class[]{});
+            getTransportTypes.setAccessible(true);
+            Object object = getTransportTypes.invoke(networkCapabilities);
+            int types = ((int[]) object)[0];
+            String transport = "";
+            switch (types) {
+                case TRANSPORT_CELLULAR:
+                    transport = "CELLULAR";
+                    break;
+                case TRANSPORT_WIFI:
+                    transport = "WIFI";
+                    break;
+                case TRANSPORT_BLUETOOTH:
+                    transport = "BLUETOOTH";
+                    break;
+                case TRANSPORT_ETHERNET:
+                    transport = "ETHERNET";
+                    break;
+                case TRANSPORT_VPN:
+                    transport = "VPN";
+                    break;
+            }
+            if (!networks.containsKey(network.toString()))
+                networks.put(network.toString(), transport);
+            return transport;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String getSSID(String srcSSID, Network network) {
+        try {
+            NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+            Method method = NetworkCapabilities.class.getMethod("getSSID");
+            method.setAccessible(true);
+            String ssid = (String) method.invoke(networkCapabilities);
+            if (ssid == null)
+                return srcSSID;
+
+            return ssid.substring(1, ssid.length() - 1);
+        } catch (Exception e) {
+            NetworkInfo wifiInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            String wifiName = wifiInfo.getExtraInfo();
+            if (wifiName != null && wifiName.length() > 1)
+                srcSSID = wifiName.substring(1, wifiName.length() - 1);
+        }
+
+        return srcSSID;
+    }
+}

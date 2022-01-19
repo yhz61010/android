f02c7376c4b7f0028c65897a68ecdc2d15ef3584
Michael Leo <yhzemail61010@aliyun.com>
2022-01-19 10:05:07 +0800

    Add more dex demo - screenshot demo. See README.md in [demo-dex] module.

---
+++ demo-dex/src/main/java/com/leovp/demo_dex/screenshot/ImageFormat.java
@@ -0,0 +1,32 @@
+package com.leovp.demo_dex.screenshot;
+
+import android.text.TextUtils;
+
+import androidx.annotation.NonNull;
+
+public enum ImageFormat {
+    UNKNOWN(""),
+    PNG("png"),
+    JPEG("jpeg"),
+    WEBP("webp");
+    private final String value;
+
+    ImageFormat(String value) {
+        this.value = value;
+    }
+
+    @NonNull
+    public static ImageFormat resolveFormat(String format) {
+        if (TextUtils.isEmpty(format)) {
+            return UNKNOWN;
+        }
+
+        for (ImageFormat fmt : values()) {
+            if (fmt.value.equalsIgnoreCase(format)) {
+                return fmt;
+            }
+        }
+
+        return UNKNOWN;
+    }
+}

c26d8cf8f237838cbab8e9c12184550fe6ca59df
Michael Leo <yhzemail61010@aliyun.com>
2022-02-07 10:57:45 +0800

    Totally refactor [CircleProgressbar] module.

--- circle-progressbar-sdk/src/main/java/com/leovp/circle_progressbar/state/FinishState.kt
+++ circle-progressbar-sdk/src/main/java/com/leovp/circle_progressbar/state/FinishState.kt
@@ -2,8 +2,11 @@ package com.leovp.circle_progressbar.state
 
 import android.content.Context
 import android.content.res.TypedArray
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.View
+import androidx.annotation.ColorInt
+import com.leovp.circle_progressbar.CircleProgressbar
 import com.leovp.circle_progressbar.R
 import com.leovp.circle_progressbar.base.State
 
@@ -12,17 +15,19 @@ import com.leovp.circle_progressbar.base.State
  * Date: 2022/1/28 16:33
  */
 class FinishState(view: View) : State(view) {
-    override fun setAttributes(context: Context, attrs: AttributeSet?, attr: TypedArray?) {
+    override fun state(): Int = CircleProgressbar.STATE_FINISHED
+
+    override fun setAttributes(context: Context, attrs: AttributeSet?, attr: TypedArray?, @ColorInt defColor: Int, defDrawable: Drawable?) {
         if (attrs != null && attr != null) {
             val iconResId = attr.getResourceId(R.styleable.CircleProgressbar_finishIconDrawable, R.drawable.ic_default_finish)
             _icon = context.getDrawable(iconResId)!!
+            iconTint = attr.getColor(R.styleable.CircleProgressbar_finishIconTintColor, DEF_ICON_TINT)
             width = attr.getDimensionPixelSize(R.styleable.CircleProgressbar_finishIconWidth, getIcon().minimumWidth)
             height = attr.getDimensionPixelSize(R.styleable.CircleProgressbar_finishIconHeight, getIcon().minimumHeight)
 
             val backgroundResId = attr.getResourceId(R.styleable.CircleProgressbar_finishBackgroundDrawable, -1)
-            if (backgroundResId != -1) backgroundDrawable = context.getDrawable(backgroundResId)
-            backgroundColor = attr.getColor(R.styleable.CircleProgressbar_finishBackgroundColor, DEF_BG_COLOR)
-            iconTint = attr.getColor(R.styleable.CircleProgressbar_finishIconTintColor, DEF_ICON_TINT)
+            backgroundDrawable = if (backgroundResId != -1) context.getDrawable(backgroundResId) else defDrawable
+            backgroundColor = attr.getColor(R.styleable.CircleProgressbar_finishBackgroundColor, defColor)
         } else {
             _icon = context.getDrawable(R.drawable.ic_default_finish)!!
             width = getIcon().minimumWidth

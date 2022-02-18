c76ec1909149ebb713eb4785c6a37b3a938d2997
Michael Leo <yhzemail61010@aliyun.com>
2022-02-18 13:42:35 +0800

    Separate sub modules about ffmpeg.

---
+++ h264-hevc-decoder-sdk/src/main/java/com/leovp/ffmpeg/video/H264HevcDecoder.kt
@@ -0,0 +1,32 @@
+package com.leovp.ffmpeg.video
+
+import androidx.annotation.Keep
+
+/**
+ * Author: Michael Leo
+ * Date: 2021/6/11 09:57
+ */
+class H264HevcDecoder(vpsBytes: ByteArray?, spsBytes: ByteArray, ppsBytes: ByteArray) {
+    companion object {
+        init {
+            System.loadLibrary("h264-hevc-decoder")
+            System.loadLibrary("avcodec")
+            System.loadLibrary("avutil")
+            System.loadLibrary("avformat")
+        }
+    }
+
+    init {
+        init(vpsBytes, spsBytes, ppsBytes)
+    }
+
+    private external fun init(vpsBytes: ByteArray?, spsBytes: ByteArray, ppsBytes: ByteArray): Int
+    external fun release()
+
+    //    external fun decode(rawBytes: ByteArray): ByteArray
+    external fun decode(rawBytes: ByteArray): DecodedVideoFrame
+    external fun getVersion(): String
+
+    @Keep
+    class DecodedVideoFrame(val yuvBytes: ByteArray, val format: Int, val width: Int, val height: Int)
+}

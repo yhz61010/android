5a120b2e58899679150ed9936e0a987ad0f03103
Michael Leo <yhzemail61010@aliyun.com>
2022-02-18 13:42:35 +0800

    Separate sub modules about ffmpeg.

--- ffmpeg-sdk/src/main/jni/Android.mk
+++ ffmpeg-sdk/src/main/jni/Android.mk
@@ -27,10 +27,10 @@ include $(PREBUILT_SHARED_LIBRARY)
 #LOCAL_SRC_FILES := $(MY_PREBUILT)/lib/libavfilter.so
 #include $(PREBUILT_SHARED_LIBRARY)
 #
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := libavformat
-#LOCAL_SRC_FILES := $(MY_PREBUILT)/lib/libavformat.so
-#include $(PREBUILT_SHARED_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libavformat
+LOCAL_SRC_FILES := $(MY_PREBUILT)/lib/libavformat.so
+include $(PREBUILT_SHARED_LIBRARY)
 #
 #include $(CLEAR_VARS)
 #LOCAL_MODULE := libswscale
@@ -40,15 +40,33 @@ include $(PREBUILT_SHARED_LIBRARY)
 # ==================================
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := adpcm-ima-qt-decoder
-LOCAL_SRC_FILES := $(LOCAL_PATH)/adpcm_ima_qt_decoder/adpcm_ima_qt_decoder.cpp \
-                   $(LOCAL_PATH)/adpcm_ima_qt_decoder/native_adpcm_ima_qt_decoder.cpp
+LOCAL_MODULE := h264-hevc-decoder
+LOCAL_SRC_FILES := $(LOCAL_PATH)/h264_hevc_decoder/h264_hevc_decoder_all_in_one_file.cpp
 LOCAL_CFLAGS    :=
 LOCAL_LDLIBS    := -llog -ljnigraphics -lz -landroid -lm -pthread -L$(SYSROOT)/usr/lib
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
-                    $(LOCAL_C_INCLUDES) \
-                    $(MY_PREBUILT)/include \
-                    $(LOCAL_PATH)/adpcm_ima_qt_decoder
+$(LOCAL_C_INCLUDES) \
+$(MY_PREBUILT)/include \
+$(LOCAL_PATH)/h264_hevc_decoder
+# The following libraries will be generated in src/main/lib folder
+#LOCAL_SHARED_LIBRARIES := libavdevice libavcodec libavfilter libavformat libavutil libswresample libswscale
+LOCAL_SHARED_LIBRARIES := libavcodec libavutil libavformat
+LOCAL_DISABLE_FORMAT_STRING_CHECKS := true
+LOCAL_DISABLE_FATAL_LINKER_WARNINGS := true
+include $(BUILD_SHARED_LIBRARY)
+
+# ==================================
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := adpcm-ima-qt-decoder
+LOCAL_SRC_FILES := $(LOCAL_PATH)/adpcm_ima_qt_decoder/adpcm_ima_qt_decoder.cpp \
+$(LOCAL_PATH)/adpcm_ima_qt_decoder/native_adpcm_ima_qt_decoder.cpp
+LOCAL_CFLAGS    :=
+LOCAL_LDLIBS    := -llog -ljnigraphics -lz -landroid -lm -pthread -L$(SYSROOT)/usr/lib
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+$(LOCAL_C_INCLUDES) \
+$(MY_PREBUILT)/include \
+$(LOCAL_PATH)/adpcm_ima_qt_decoder
 # The following libraries will be generated in src/main/lib folder
 #LOCAL_SHARED_LIBRARIES := libavdevice libavcodec libavfilter libavformat libavutil libswresample libswscale
 LOCAL_SHARED_LIBRARIES := libavcodec libavutil

226b7d125394e3dbd20f4fd26cca6da73be630cc
Michael Leo <yhzemail61010@aliyun.com>
2022-03-01 16:34:11 +0800

    minor changes

--- androidbase-sdk/src/main/java/com/leovp/androidbase/opengl/util/ShaderUtil.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/opengl/util/ShaderUtil.kt
@@ -44,10 +44,14 @@ fun loadShader(ctx: Context, type: Int): Int {
  *
  * https://www.jianshu.com/p/a772bfc2276b
  * https://blog.csdn.net/mengks1987/article/details/104186060
+ * https://mp.weixin.qq.com/s?__biz=MzI0NzI0NDY2OQ==&mid=2652749998&idx=1&sn=73cb65f64e6c7ff6b823919d9c016b13&chksm=f25b92bcc52c1baa2c482431f111f98da33832d8fb2b1385e6fc197847b690de137601e23f68&token=1476949111&lang=zh_CN&scene=21#wechat_redirect
  *
- * attribute：一般用于各个顶点各不相同的量。如顶点位置、纹理坐标、法向量、颜色等等。
- * uniform  ：一般用于对于物体中所有顶点或者所有的片段都相同的量。比如光源位置、统一变换矩阵、颜色等。
- * varying  ：表示易变量，一般用于顶点着色器传递到片段着色器的量。
+ * attribute：只能用在顶点着色器中，一般用于表示顶点数据。
+ *            一般用于各个顶点各不相同的量。如顶点位置、纹理坐标、法向量、颜色等等。
+ * uniform  ：可用于顶点和片段着色器，一般用于对于物体中所有顶点或者所有的片段都相同的量。比如光源位置、统一变换矩阵、颜色等。
+ * varying  ：可用于顶点和片段着色器，一般用于在着色器之间做数据传递。
+ *            通常，varying 在顶点着色器中进行计算，片段着色器使用 varying 计算后的值。
+ *            表示易变量，一般用于顶点着色器传递到片段着色器的量。
  * vec4     ：4个分量的向量：x、y、z、w
  */
 //internal const val vertexShaderCode = """

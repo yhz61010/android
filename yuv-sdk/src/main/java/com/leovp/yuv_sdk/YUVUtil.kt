a5bb4b7282b7a6cdbd491b8fe84d4ec061ce7044
Michael Leo <yhzemail61010@aliyun.com>
2022-03-11 17:51:59 +0800

    Improve [yuv-sdk] module.

--- yuv-sdk/src/main/java/com/leovp/yuv_sdk/YUVUtil.kt
+++ yuv-sdk/src/main/java/com/leovp/yuv_sdk/YUVUtil.kt
@@ -15,12 +15,31 @@ object YUVUtil {
     /**
      * Mirror(height only) first then do rotate
      *
-     * @param type 1: No rotate
+     * @param format The [srcYuvByteArray] format.
+     * ```
+     *               1: I420
+     *               2: NV21
+     *               3: NV12
+     *               4: YUY2
+     * ```
      *
+     * @param degree The yuv data should be rotated by degree.
+     * ```
+     *                 0: No rotation.
+     *                 90: Rotate 90 degrees clockwise.
+     *                180: Rotate 180 degrees.
+     *                270: Rotate 270 degrees clockwise.
+     * ```
      */
-    external fun convertToI420(yuvByteArray: ByteArray, format: Int, width: Int, height: Int, type: Int): ByteArray
-    external fun convertToI420NegativeStride(yuvByteArray: ByteArray, width: Int, height: Int, type: Int): ByteArray
+    external fun convertToI420(srcYuvByteArray: ByteArray, format: Int, width: Int, height: Int, verticallyFlip: Boolean, degree: Int): ByteArray
+    external fun convertToI420NegativeStride(srcYuvByteArray: ByteArray, width: Int, height: Int, type: Int): ByteArray
 
+    /**
+     * @param degree    0: No rotation.
+     *                 90: Rotate 90 degrees clockwise.
+     *                180: Rotate 180 degrees.
+     *                270: Rotate 270 degrees clockwise.
+     */
     external fun rotateI420(i420ByteArray: ByteArray, width: Int, height: Int, degree: Int): ByteArray
     external fun mirrorI420(i420ByteArray: ByteArray, width: Int, height: Int): ByteArray
 }

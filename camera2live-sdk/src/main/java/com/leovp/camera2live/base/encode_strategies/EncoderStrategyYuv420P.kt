1ff97ecdc8f541e281b20cbaf6e9533bc828d723
Michael Leo <yhzemail61010@aliyun.com>
2022-03-11 14:57:38 +0800

    Improve [yuv-sdk] module.

--- camera2live-sdk/src/main/java/com/leovp/camera2live/base/encode_strategies/EncoderStrategyYuv420P.kt
+++ camera2live-sdk/src/main/java/com/leovp/camera2live/base/encode_strategies/EncoderStrategyYuv420P.kt
@@ -11,19 +11,15 @@ import com.leovp.yuv_sdk.YUVUtil
  * Date: 20-4-1 上午11:12
  */
 class EncoderStrategyYuv420P : IDataProcessStrategy {
-    private val yuvUtil = YUVUtil()
     override fun doProcess(image: Image, lensFacing: Int, cameraSensorOrientation: Int): ByteArray {
         val width = image.width
         val height = image.height
         // Get I420/YU12(YUV420P) data YYYYYYYYUUVV
         val yuvData = YuvUtil.getYuvDataFromImage(image, YuvUtil.COLOR_FORMAT_I420)
-//        val yuvData = YuvUtil.getBytesFromImage(image)
         return if (lensFacing == CameraMetadata.LENS_FACING_BACK) {
             // LENS_FACING_BACK
-            yuvUtil.mirrorI420(yuvData, width, height)
-//            YuvUtil.yuvRotate90(yuvUtil.mirrorI420(yuvData, width, height), width, height)
-
-//            yuvUtil.convertToI420(yuvData, width, height, 1)
+            // YuvUtil.yuvRotate90(yuvData, width, height)
+            YUVUtil.rotateI420(yuvData, width, height, 90)
         } else {
             // LENS_FACING_FRONT
             YuvUtil.yuvRotate90(YuvUtil.yuvFlipHorizontal(yuvData, width, height), width, height)

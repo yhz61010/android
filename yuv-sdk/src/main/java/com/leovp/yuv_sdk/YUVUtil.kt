c9255b6a11308b147e876d709f4e46cd93cd24d1
Michael Leo <yhzemail61010@aliyun.com>
2022-03-14 10:04:24 +0800

    Improve [yuv-sdk] module.

--- yuv-sdk/src/main/java/com/leovp/yuv_sdk/YUVUtil.kt
+++ yuv-sdk/src/main/java/com/leovp/yuv_sdk/YUVUtil.kt
@@ -15,6 +15,9 @@ object YUVUtil {
     /**
      * Mirror(height only) first then do rotate
      *
+     * @param width The original video width
+     * @param height The original video height
+     *
      * @param format The [srcYuvByteArray] format.
      * ```
      *               1: I420
@@ -34,15 +37,27 @@ object YUVUtil {
     external fun convertToI420(srcYuvByteArray: ByteArray, format: Int, width: Int, height: Int, verticallyFlip: Boolean, degree: Int): ByteArray?
 
     /**
+     * @param width The original video width before rotation
+     * @param height The original video height before rotation
      * @param degree    0: No rotation.
      *                 90: Rotate 90 degrees clockwise.
      *                180: Rotate 180 degrees.
      *                270: Rotate 270 degrees clockwise.
      */
     external fun rotateI420(i420ByteArray: ByteArray, width: Int, height: Int, degree: Int): ByteArray
+
+    /**
+     * @param width The original video width
+     * @param height The original video height
+     */
     external fun mirrorI420(i420ByteArray: ByteArray, width: Int, height: Int): ByteArray
 
     /**
+     * @param srcWidth The original video width
+     * @param srcHeight The original video height
+     * @param dstWidth The width after scaled
+     * @param dstHeight The height after scaled
+     *
      * @param mode
      *              kFilterNone = 0,      // Point sample; Fastest.
      *              kFilterLinear = 1,    // Filter horizontally only.

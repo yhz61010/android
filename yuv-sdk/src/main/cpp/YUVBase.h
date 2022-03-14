c2d5254ac85d924c640375794a7141cf1c960e67
Michael Leo <yhzemail61010@aliyun.com>
2022-03-14 14:15:03 +0800

    Improve [yuv-sdk] module.

---
+++ yuv-sdk/src/main/cpp/YUVBase.h
@@ -0,0 +1,33 @@
+#ifndef LEOANDROIDBASEUTIL_YUVBASE_H
+#define LEOANDROIDBASEUTIL_YUVBASE_H
+
+#include <jni.h>
+#include <string>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "libyuv.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+void mirrorI420(const uint8_t *src_i420_data, jint width, jint height, uint8_t *dst_i420_data);
+
+void rotateI420(const uint8_t *src_i420_data, jint width, jint height, uint8_t *dst_i420_data, jint degree);
+
+void scaleI420(const uint8_t *src_i420_data, jint width, jint height, uint8_t *dst_i420_data, jint dst_width, jint dst_height, jint mode);
+
+void cropI420(jbyte *src_i420_data, jint src_length, jint width, jint height, jbyte *dst_i420_data, jint dst_width, jint dst_height, jint left, jint top);
+
+void nv21ToI420(jbyte *src_nv21_data, jint width, jint height, jbyte *src_i420_data);
+
+void i420ToNv21(jbyte *src_i420_data, jint width, jint height, jbyte *src_nv21_data);
+
+void nv12ToI420(jbyte *Src_data, jint src_width, jint src_height, jbyte *Dst_data);
+
+void i420ToNv12(jbyte *src_i420_data, jint width, jint height, jbyte *src_nv12_data);
+
+#endif //LEOANDROIDBASEUTIL_YUVBASE_H

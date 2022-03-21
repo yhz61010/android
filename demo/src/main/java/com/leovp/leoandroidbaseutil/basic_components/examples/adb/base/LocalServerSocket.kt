741c2e8c616ceb2f5b8c6f92de1d5d39a6723151
Michael Leo <yhzemail61010@aliyun.com>
2022-03-21 10:36:07 +0800

    Update [Adb Communication] demo.

---
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/adb/base/LocalServerSocket.kt
@@ -0,0 +1,36 @@
+package com.leovp.leoandroidbaseutil.basic_components.examples.adb.base
+
+import android.net.LocalServerSocket
+import android.net.LocalSocket
+import com.leovp.log_sdk.LogContext
+import java.io.InputStream
+
+/**
+ * ```
+ * adb forward tcp:8888 localabstract:local_name
+ * ```
+ * Note that: the `local_name` must be the same value in `LocalServerSocket` constructor.
+ *
+ * Author: Michael Leo
+ *
+ * Date: 2022/3/19 11:36
+ */
+class LocalServerSocket {
+    fun startServer(name: String) {
+        LogContext.log.i("startServer($name)")
+        val serverSocket = LocalServerSocket(name)
+        LogContext.log.i("LocalServerSocket created!")
+        // Blocking
+        val client: LocalSocket = serverSocket.accept()
+        LogContext.log.i("Client connected!")
+        while (true) {
+            if (!client.isConnected) {
+                LogContext.log.i("isConnected=false")
+                return
+            }
+            val inputStream: InputStream = client.inputStream
+            val result: String = inputStream.reader().readText()
+            LogContext.log.i("ServerSocket rcv=$result")
+        }
+    }
+}

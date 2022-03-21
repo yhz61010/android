1241b29c3ed053d46794c572d0a366de0a3c437d
Michael Leo <yhzemail61010@aliyun.com>
2022-03-21 10:36:07 +0800

    Update [Adb Communication] demo.

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/adb/AdbCommunication.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/adb/AdbCommunication.kt
@@ -3,7 +3,8 @@ package com.leovp.leoandroidbaseutil.basic_components.examples.adb
 import android.os.Bundle
 import com.leovp.leoandroidbaseutil.R
 import com.leovp.leoandroidbaseutil.base.BaseDemonstrationActivity
-import com.leovp.leoandroidbaseutil.basic_components.examples.adb.base.LocalServer
+import com.leovp.leoandroidbaseutil.basic_components.examples.adb.base.LocalServerSocket
+import com.leovp.log_sdk.LogContext
 import kotlin.concurrent.thread
 
 /**
@@ -15,8 +16,12 @@ class AdbCommunication : BaseDemonstrationActivity() {
         setContentView(R.layout.activity_adb_communication)
 
         thread {
-            val server = LocalServer()
-            server.startServer("local-server")
+            LogContext.log.i("LocalServer starting...")
+            val server = LocalServerSocket()
+            //            val server = LocalServer()
+            LogContext.log.i("LocalServer created.")
+            server.startServer("local_name")
+            LogContext.log.i("LocalServer started!")
         }
     }
 }

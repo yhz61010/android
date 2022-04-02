97393d0daadc941f95ecb51fb7f9a1ddd9e85899
Michael Leo <yhzemail61010@aliyun.com>
2022-04-02 14:11:31 +0800

    minor changes

--- opengl-sdk/src/main/java/com/leovp/opengl_sdk/util/CmnExt.kt
+++ opengl-sdk/src/main/java/com/leovp/opengl_sdk/util/CmnExt.kt
@@ -2,18 +2,7 @@ package com.leovp.opengl_sdk.util
 
 import android.content.Context
 import androidx.annotation.RawRes
-import java.io.File
 import java.nio.charset.StandardCharsets
-
-/**
- * Author: Michael Leo
- * Date: 2022/4/2 11:12
- */
-internal fun Context.readAssetsFileAsString(subdirectory: String?, filename: String): String {
-    return resources.assets.open(if (subdirectory.isNullOrBlank()) filename else "$subdirectory${File.separatorChar}$filename").use {
-        it.readBytes().toString(StandardCharsets.UTF_8)
-    }
-}
 
 internal fun Context.readAssetsFileAsString(@RawRes rawId: Int): String {
     return resources.openRawResource(rawId).use { it.readBytes().toString(StandardCharsets.UTF_8) }

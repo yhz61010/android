c95c8dff5ec17522aafb7148881695cc8e991408
Michael Leo <yhzemail61010@aliyun.com>
2022-04-07 15:39:11 +0800

    Add more OpenGL ES 2.0 demo.

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/opengl/renderers/L5_IndexRenderer.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/opengl/renderers/L5_IndexRenderer.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.opengl.GLES20
 import com.leovp.opengl_sdk.BaseRenderer
 import com.leovp.opengl_sdk.util.ProjectionMatrixHelper
-import com.leovp.opengl_sdk.util.createFloatBuffers
+import com.leovp.opengl_sdk.util.createFloatBuffer
 import com.leovp.opengl_sdk.util.createShortBuffer
 import java.nio.FloatBuffer
 import java.nio.ShortBuffer
@@ -71,7 +71,7 @@ class L5_IndexRenderer(@Suppress("unused") private val ctx: Context) : BaseRende
      *
      * 分配一个块 Native 内存，用于与 GL 通讯传递。(我们通常用的数据存在于 Dalvik 的内存中，1.无法访问硬件；2.会被垃圾回收)
      */
-    private val vertexData: FloatBuffer = createFloatBuffers(POINT_DATA)
+    private val vertexData: FloatBuffer = createFloatBuffer(POINT_DATA)
 
     /**
      * 顶点索引数据缓冲区：ShortBuff，占 2 位的Byte

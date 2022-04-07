9bf9ed51410ec61c8290f37b5aae8b0df8c4bbd7
Michael Leo <yhzemail61010@aliyun.com>
2022-04-07 14:20:48 +0800

    Add more OpenGL ES 2.0 demo.

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/opengl/renderers/L4_2_BetterGradientRenderer.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/opengl/renderers/L4_2_BetterGradientRenderer.kt
@@ -12,8 +12,8 @@ import javax.microedition.khronos.opengles.GL10
 /**
  * 渐变色
  */
-class L4_1_GradientRenderer(@Suppress("unused") private val ctx: Context) : BaseRenderer() {
-    override fun getTagName(): String = "L4_1_GradientRenderer"
+class L4_2_BetterGradientRenderer(@Suppress("unused") private val ctx: Context) : BaseRenderer() {
+    override fun getTagName(): String = "L4_2_BetterGradientRenderer"
 
     private companion object {
         /** 顶点着色器：之后定义的每个都会传 1 次给顶点着色器 */
@@ -47,22 +47,19 @@ class L4_1_GradientRenderer(@Suppress("unused") private val ctx: Context) : Base
 
         /**
          * 顶点数据数组
-         * 点的 x,y 坐标（x，y 各占 1 个分量，也就是说每个点占用 2 个分量）。
-         * 该数组表示 4 个顶点数据，也就是 4 个点的坐标。
+         * 一个顶点有 5 个向量数据：x、y、r、g、b
          */
         private val POINT_DATA = floatArrayOf(
-            -0.5f, -0.5f,
-            0.5f, -0.5f,
-            -0.5f, 0.5f,
-            0.5f, 0.5f
+            -0.5f, -0.5f, 1f, 0.5f, 0.5f,
+            0.5f, -0.5f, 1f, 0f, 1f,
+            -0.5f, 0.5f, 0f, 1f, 1f,
+            0.5f, 0.5f, 1f, 1f, 0f
         )
 
-        /** 一个顶点有 3 个向量数据：r、g、b */
-        private val COLOR_DATA = floatArrayOf(
-            1f, 0.5f, 0.5f,
-            1f, 0f, 1f,
-            0f, 1f, 1f,
-            1f, 1f, 0f)
+        /**
+         * 数据数组中每个顶点起始数据的间距：数组中每个顶点相关属性占的 Byte 值
+         */
+        private val STRIDE = (TWO_DIMENSIONS_POSITION_COMPONENT_COUNT + RGB_COLOR_COMPONENT_COUNT) * Float.SIZE_BYTES
     }
 
     /**
@@ -70,7 +67,6 @@ class L4_1_GradientRenderer(@Suppress("unused") private val ctx: Context) : Base
      * 分配一个块 Native 内存，用于与 GL 通讯传递。(我们通常用的数据存在于 Dalvik 的内存中，1.无法访问硬件；2.会被垃圾回收)
      */
     private val vertexData: FloatBuffer = createFloatBuffers(POINT_DATA)
-    private val colorData: FloatBuffer = createFloatBuffers(COLOR_DATA)
 
     private lateinit var projectionMatrixHelper: ProjectionMatrixHelper
 
@@ -83,7 +79,6 @@ class L4_1_GradientRenderer(@Suppress("unused") private val ctx: Context) : Base
         val aColorLocation = getAttrib("a_Color")
         projectionMatrixHelper = ProjectionMatrixHelper(programObjId, "u_Matrix")
 
-
         // 关联顶点坐标属性和缓存数据
         // 1. 位置索引；
         // 2. 每个顶点属性需要关联的分量个数(必须为1、2、3或者4。初始值为4。)；
@@ -91,10 +86,14 @@ class L4_1_GradientRenderer(@Suppress("unused") private val ctx: Context) : Base
         // 4. 指定当被访问时，固定点数据值是否应该被归一化(GL_TRUE)或者直接转换为固定点值(GL_FALSE)(只有使用整数数据时)
         // 5. 指定连续顶点属性之间的偏移量。如果为0，那么顶点属性会被理解为：它们是紧密排列在一起的。初始值为0。
         // 6. 数据缓冲区
-        GLES20.glVertexAttribPointer(aPositionLocation, TWO_DIMENSIONS_POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT, false, 0, vertexData)
+        GLES20.glVertexAttribPointer(aPositionLocation, TWO_DIMENSIONS_POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT, false, STRIDE, vertexData)
         GLES20.glEnableVertexAttribArray(aPositionLocation)
 
-        GLES20.glVertexAttribPointer(aColorLocation, RGB_COLOR_COMPONENT_COUNT, GLES20.GL_FLOAT, false, 0, colorData)
+        // 将数组的初始读取位置右移 2 位，所以数组读取的顺序是 r1, g1, b1, x2, y2, r2, g2, b2...
+        vertexData.position(TWO_DIMENSIONS_POSITION_COMPONENT_COUNT)
+        // RGB_COLOR_COMPONENT_COUNT：从数组中每次读取 3 个向量
+        // STRIDE：每次读取间隔是 (2 个位置 + 3 个颜色值) * Float 占的 Byte 位
+        GLES20.glVertexAttribPointer(aColorLocation, RGB_COLOR_COMPONENT_COUNT, GLES20.GL_FLOAT, false, STRIDE, vertexData)
         GLES20.glEnableVertexAttribArray(aColorLocation)
     }
 
@@ -109,7 +108,7 @@ class L4_1_GradientRenderer(@Suppress("unused") private val ctx: Context) : Base
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
         // 几何图形相关定义：http://wiki.jikexueyuan.com/project/opengl-es-guide/basic-geometry-definition.html
         // GL_TRIANGLE_STRIP: 相邻3个点构成一个三角形,不包括首位两个点。例如：ABC、BCD、CDE、DEF
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, POINT_DATA.size / TWO_DIMENSIONS_POSITION_COMPONENT_COUNT)
-        GLES20.glDrawArrays(GLES20.GL_POINTS, 0, POINT_DATA.size / TWO_DIMENSIONS_POSITION_COMPONENT_COUNT)
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, POINT_DATA.size / (TWO_DIMENSIONS_POSITION_COMPONENT_COUNT + RGB_COLOR_COMPONENT_COUNT))
+        GLES20.glDrawArrays(GLES20.GL_POINTS, 0, POINT_DATA.size / (TWO_DIMENSIONS_POSITION_COMPONENT_COUNT + RGB_COLOR_COMPONENT_COUNT))
     }
 }

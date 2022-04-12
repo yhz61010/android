30a261861a77c1ce816e370d397aeaf0cc865e42
Michael Leo <yhzemail61010@aliyun.com>
2022-04-12 11:00:22 +0800

    Add more OpenGL ES 2.0 demo.

--- demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/opengl/OpenGLES20Activity.kt
+++ demo/src/main/java/com/leovp/leoandroidbaseutil/basic_components/examples/opengl/OpenGLES20Activity.kt
@@ -38,6 +38,7 @@ class OpenGLES20Activity : BaseDemonstrationActivity(), AdapterView.OnItemClickL
         Item("L6_2_2_纹理渲染 - 多个纹理单元", L6_2_2_TextureRenderer::class.java),
         Item("L6_2_3_纹理渲染 - 蒙版遮罩", L6_2_3_TextureRenderer::class.java),
         Item("L7_1_FrameBuffer 离屏渲染", L7_1_FBORenderer::class.java),
+        Item("L7_2_FrameBuffer 离屏渲染 - RenderBuffer", L7_2_FBORenderer::class.java),
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {

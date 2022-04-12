3b977cc8e529acaf3da7a850aea02f2164971226
Michael Leo <yhzemail61010@aliyun.com>
2022-04-12 14:08:08 +0800

    Add more OpenGL ES 2.0 demo.

---
+++ opengl-sdk/src/main/java/com/leovp/opengl_sdk/filter/GrayFilter.kt
@@ -0,0 +1,23 @@
+package com.leovp.opengl_sdk.filter
+
+import android.content.Context
+
+/**
+ * 灰色滤镜
+ */
+class GrayFilter(context: Context) : BaseFilter(context, fragmentShader = FRAGMENT_SHADER) {
+    override fun getTagName() = "GrayFilter"
+
+    companion object {
+        const val FRAGMENT_SHADER = """
+                precision mediump float;
+                varying vec2 v_TexCoord;
+                uniform sampler2D u_TextureUnit;
+                void main() {
+                    vec4 src = texture2D(u_TextureUnit, v_TexCoord);
+                    float gray = (src.r + src.g + src.b) / 3.0;
+                    gl_FragColor =vec4(gray, gray, gray, 1.0);
+                }
+                """
+    }
+}

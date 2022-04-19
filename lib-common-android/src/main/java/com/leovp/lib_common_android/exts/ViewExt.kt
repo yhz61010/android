d7bb222e7caee825631c8b64ad3c5de22e454614
Michael Leo <yhzemail61010@aliyun.com>
2022-04-19 09:14:51 +0800

    minor changes

--- lib-common-android/src/main/java/com/leovp/lib_common_android/exts/ViewExt.kt
+++ lib-common-android/src/main/java/com/leovp/lib_common_android/exts/ViewExt.kt
@@ -17,14 +17,22 @@ import androidx.core.view.WindowInsetsControllerCompat
  * Date: 2020/9/29 上午11:17
  */
 
-/** Combination of all flags required to put activity into immersive mode */
-//const val FLAGS_FULLSCREEN =
-//    View.SYSTEM_UI_FLAG_LOW_PROFILE or
-//            View.SYSTEM_UI_FLAG_FULLSCREEN or
-//            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-//            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-//            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-//            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+/**
+ * Attention:
+ * Use this below Build.VERSION_CODES.R(API < 30 AKA Android 11)
+ *
+ * Combination of all flags required to put activity into immersive mode
+ */
+@Suppress("DEPRECATION")
+const val FLAGS_FULLSCREEN =
+        View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                // More flags
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 
 /** Milliseconds used for UI animations */
 const val ANIMATION_FAST_MILLIS = 50L
@@ -109,13 +117,9 @@ fun AlertDialog.showImmersive() {
         // https://blog.csdn.net/c15522627353/article/details/52452490
         // https://blog.csdn.net/lyabc123456/article/details/88683425
         // Always hide virtual navigation
+
         @Suppress("DEPRECATION")
-        win.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        win.decorView.systemUiVisibility = FLAGS_FULLSCREEN
     }
 
     // Show the dialog while still in immersive mode

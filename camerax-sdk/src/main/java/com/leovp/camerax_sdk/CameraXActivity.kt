19bca2df8c22f0c242f4e79afc3ac86e94af8203
Michael Leo <yhzemail61010@aliyun.com>
2022-04-20 10:34:43 +0800

    [CameraX] module: Replace [LocalBroadcastManager] with [LiveData].

--- camerax-sdk/src/main/java/com/leovp/camerax_sdk/CameraXActivity.kt
+++ camerax-sdk/src/main/java/com/leovp/camerax_sdk/CameraXActivity.kt
@@ -1,11 +1,10 @@
 package com.leovp.camerax_sdk
 
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.KeyEvent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import androidx.navigation.fragment.NavHostFragment
 import com.leovp.camerax_sdk.databinding.ActivityCameraxMainBinding
 import com.leovp.camerax_sdk.fragments.CameraFragment
 import com.leovp.lib_common_android.exts.hideNavigationBar
@@ -33,9 +32,11 @@ class CameraXActivity : AppCompatActivity() {
     /** When key down event is triggered, relay it via local broadcast so fragments can handle it */
     override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
         return when (keyCode) {
+            KeyEvent.KEYCODE_VOLUME_UP,
             KeyEvent.KEYCODE_VOLUME_DOWN -> {
-                val intent = Intent(CameraFragment.KEY_EVENT_ACTION).apply { putExtra(CameraFragment.KEY_EVENT_EXTRA, keyCode) }
-                LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+                val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_camerax) as NavHostFragment
+                val cameraFragment: CameraFragment = navHostFragment.childFragmentManager.primaryNavigationFragment as CameraFragment
+                cameraFragment.functionKey.value = keyCode
                 true
             }
             else                         -> super.onKeyDown(keyCode, event)

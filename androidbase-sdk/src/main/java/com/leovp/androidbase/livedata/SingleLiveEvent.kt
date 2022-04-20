920a8e053955dc2819bd4d26a97659233202f926
Michael Leo <yhzemail61010@aliyun.com>
2022-04-20 09:50:33 +0800

    minor changes

--- androidbase-sdk/src/main/java/com/leovp/androidbase/livedata/SingleLiveEvent.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/livedata/SingleLiveEvent.kt
@@ -1,10 +1,10 @@
 package com.leovp.androidbase.livedata
 
-import android.util.Log
 import androidx.annotation.MainThread
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import com.leovp.log_sdk.LogContext
 import java.util.concurrent.atomic.AtomicBoolean
 
 /**
@@ -25,15 +25,15 @@ class SingleLiveEvent<T> : MutableLiveData<T>() {
     @MainThread
     override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
         if (hasActiveObservers()) {
-            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
+            LogContext.log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
         }
 
         // Observe the internal MutableLiveData
-        super.observe(owner, { t ->
+        super.observe(owner) { t ->
             if (pending.compareAndSet(true, false)) {
                 observer.onChanged(t)
             }
-        })
+        }
     }
 
     @MainThread

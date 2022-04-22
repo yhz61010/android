6aeed5ed2f5ef03d455126ce0667e35f84b339ed
Michael Leo <yhzemail61010@aliyun.com>
2022-04-22 10:24:01 +0800

    Enhance CameraX

--- camerax-sdk/src/main/java/com/leovp/camerax_sdk/fragments/GalleryFragment.kt
+++ camerax-sdk/src/main/java/com/leovp/camerax_sdk/fragments/GalleryFragment.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.leovp.camerax_sdk.fragments
 
 import android.content.Intent
@@ -143,7 +127,7 @@ class GalleryFragment internal constructor() : Fragment() {
                     .setTitle(getString(R.string.delete_title))
                     .setMessage(getString(R.string.delete_dialog))
                     .setIcon(android.R.drawable.ic_dialog_alert)
-                    .setPositiveButton(android.R.string.yes) { _, _ ->
+                    .setPositiveButton(android.R.string.ok) { _, _ ->
 
                         // Delete current photo
                         mediaFile.delete()

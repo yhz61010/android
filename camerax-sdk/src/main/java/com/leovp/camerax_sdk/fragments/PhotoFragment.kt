b344aa9b58f11bf8ff377d756049731f440a8307
Michael Leo <yhzemail61010@aliyun.com>
2022-04-22 10:24:01 +0800

    Enhance CameraX

--- camerax-sdk/src/main/java/com/leovp/camerax_sdk/fragments/PhotoFragment.kt
+++ camerax-sdk/src/main/java/com/leovp/camerax_sdk/fragments/PhotoFragment.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.leovp.camerax_sdk.fragments
 
 import android.os.Bundle
@@ -25,7 +9,6 @@ import androidx.fragment.app.Fragment
 import com.leovp.camerax_sdk.R
 import com.leovp.lib_image.getBitmap
 import java.io.File
-
 
 /** Fragment used for each individual page showing a photo inside of [GalleryFragment] */
 class PhotoFragment internal constructor() : Fragment() {

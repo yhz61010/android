ee2e394c9b6b505829c89c2017d80c2148a33216
Michael Leo <yhzemail61010@aliyun.com>
2022-04-25 15:33:45 +0800

    Refactor CameraFragment.

---
+++ camerax-sdk/src/main/java/com/leovp/camerax_sdk/bean/CaptureImage.kt
@@ -0,0 +1,27 @@
+package com.leovp.camerax_sdk.bean
+
+/**
+ * Author: Michael Leo
+ * Date: 2022/4/25 14:33
+ */
+data class CaptureImage(val imgBytes: ByteArray, val width: Int, val height: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as CaptureImage
+
+        if (!imgBytes.contentEquals(other.imgBytes)) return false
+        if (width != other.width) return false
+        if (height != other.height) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = imgBytes.contentHashCode()
+        result = 31 * result + width
+        result = 31 * result + height
+        return result
+    }
+}

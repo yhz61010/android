0537537c066acae6cdba670bb030485fb7ccdde7
Michael Leo <yhzemail61010@aliyun.com>
2022-05-25 14:22:48 +0800

    Implement more features for CameraX.

--- androidbase-sdk/src/main/java/com/leovp/androidbase/utils/media/YuvUtil.kt
+++ androidbase-sdk/src/main/java/com/leovp/androidbase/utils/media/YuvUtil.kt
@@ -24,6 +24,60 @@ object YuvUtil {
         return false
     }
 
+    fun Image.generateNV21Data(): ByteArray {
+        val crop = cropRect
+        val format = format
+        val width = crop.width()
+        val height = crop.height()
+        val planes = planes
+        val data = ByteArray(width * height * ImageFormat.getBitsPerPixel(format) / 8)
+        val rowData = ByteArray(planes[0].rowStride)
+        var channelOffset = 0
+        var outputStride = 1
+        for (i in planes.indices) {
+            when (i) {
+                0 -> {
+                    channelOffset = 0
+                    outputStride = 1
+                }
+                1 -> {
+                    channelOffset = width * height + 1
+                    outputStride = 2
+                }
+                2 -> {
+                    channelOffset = width * height
+                    outputStride = 2
+                }
+            }
+            val buffer = planes[i].buffer
+            val rowStride = planes[i].rowStride
+            val pixelStride = planes[i].pixelStride
+            val shift = if (i == 0) 0 else 1
+            val w = width shr shift
+            val h = height shr shift
+            buffer.position(rowStride * (crop.top shr shift) + pixelStride * (crop.left shr shift))
+            for (row in 0 until h) {
+                val length: Int
+                if (pixelStride == 1 && outputStride == 1) {
+                    length = w
+                    buffer.get(data, channelOffset, length)
+                    channelOffset += length
+                } else {
+                    length = (w - 1) * pixelStride + 1
+                    buffer.get(rowData, 0, length)
+                    for (col in 0 until w) {
+                        data[channelOffset] = rowData[col * pixelStride]
+                        channelOffset += outputStride
+                    }
+                }
+                if (row < h - 1) {
+                    buffer.position(buffer.position() + rowStride - length)
+                }
+            }
+        }
+        return data
+    }
+
     /**
      * @return Return data in
      *             `I420(YU12)` format `YYYYYYYY UUVV` or
@@ -156,7 +210,8 @@ object YuvUtil {
     // The input imageBytes is in YYYYYYYYUVUV(NV12)
     // Return data in YYYYYYYYUVUV(NV12)
     fun rotateYUV420Degree90(imageBytes: ByteArray, imageWidth: Int, imageHeight: Int): ByteArray {
-        val yuv = ByteArray(imageWidth * imageHeight * ImageFormat.getBitsPerPixel(ImageFormat.YUV_420_888) shr 3) // Rotate the Y luma
+        val yuv =
+                ByteArray(imageWidth * imageHeight * ImageFormat.getBitsPerPixel(ImageFormat.YUV_420_888) shr 3) // Rotate the Y luma
         var i = 0
         for (x in 0 until imageWidth) {
             for (y in imageHeight - 1 downTo 0) {

0af7535382c2dd0001b49b45e75c0360caa1a1c8
Michael Leo <yhzemail61010@aliyun.com>
2022-05-26 10:30:28 +0800

    minor changes

---
+++ camerax-sdk/src/main/java/com/leovp/camerax_sdk/enums/RecordUiState.kt
@@ -0,0 +1,13 @@
+package com.leovp.camerax_sdk.enums
+
+/**
+ * Camera UI  states and inputs
+ *
+ * Author: Michael Leo
+ */
+enum class RecordUiState {
+    IDLE,      // Not recording, all UI controls are active.
+    RECORDING, // Camera is recording, only display Pause/Resume & Stop button.
+    FINALIZED, // Recording just completes, disable all RECORDING UI controls.
+    //        RECOVERY   // For future use.
+}

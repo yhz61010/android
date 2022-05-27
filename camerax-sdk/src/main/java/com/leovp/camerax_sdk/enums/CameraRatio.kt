bafedde38592c479571e65f9eeb3f33404f374f4
Michael Leo <yhzemail61010@aliyun.com>
2022-05-27 09:06:36 +0800

    Improve CameraX UI interaction.

--- camerax-sdk/src/main/java/com/leovp/camerax_sdk/enums/CameraRatio.kt
+++ camerax-sdk/src/main/java/com/leovp/camerax_sdk/enums/CameraRatio.kt
@@ -1,8 +1,8 @@
 package com.leovp.camerax_sdk.enums
 
 enum class CameraRatio(val ratioString: String) {
-    R4v3("3:4"),
-    R16v9("9:16"),
-    R1v1("1:1"),
-    RFull("1:1")
+    R4v3("V,3:4"),
+    R16v9("V,9:16"),
+    R1v1("V,1:1"),
+    RFull("V,1:1")
 }

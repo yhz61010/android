c1a592d17c3f88b82d05ee012f1f6e5fc1904f2a
Michael Leo <yhzemail61010@aliyun.com>
2022-06-02 17:26:45 +0800

    minor changes

--- demo/src/main/java/com/leovp/demo/jetpack_components/examples/camerax/CameraXActivityDemo.kt
+++ demo/src/main/java/com/leovp/demo/jetpack_components/examples/camerax/CameraXActivityDemo.kt
@@ -76,6 +76,44 @@ class CameraXDemoActivity : CameraXActivity() {
         }
     }
 
+    /**
+     * Orientation: The orientation of the camera relative to the scene, when the image was captured.
+     *              The start point of stored data is,
+     *              '1' means upper left,
+     *              '3' lower right,
+     *              '6' upper right,
+     *              '8' lower left,
+     *              '9' undefined.
+     *
+     * https://www.media.mit.edu/pia/Research/deepview/exif.html
+     * http://sylvana.net/jpegcrop/exif_orientation.html
+     *
+     * ```
+     * Value    0th Row   0th Column  Rotation(clockwise)
+     *   1	     top        left      0°
+     *   2	     top       right      flip horizontal
+     *   3	  bottom       right      180°
+     *   4	  bottom        left      flip vertical
+     *   5	    left         top      transpose(90°+flip horizontal)
+     *   6	   right         top      90°
+     *   7	   right      bottom      transverse(90°+flip vertical)
+     *   8	    left      bottom      270°
+     * ```
+     *
+     * ```
+     *     1        2       3      4         5            6           7          8
+     *
+     *   888888  888888      88  88      8888888888  88                  88  8888888888
+     *   88          88      88  88      88  88      88  88          88  88      88  88
+     *   8888      8888    8888  8888    88          8888888888  8888888888          88
+     *   88          88      88  88
+     *   88          88  888888  888888
+     * ```
+     *
+     * ![orient_flag](http://lib.leovp.com/resources/jpeg/orient_flag.jpg)
+     *
+     * https://blog.csdn.net/ouyangtianhan/article/details/29825885
+     */
     private fun saveExif(filePath: String, width: Int? = null, height: Int? = null, savedImage: CaptureImage) {
         ExifInterface(filePath).apply {
             setAttribute(ExifInterface.TAG_CAMERA_OWNER_NAME, "Leo Camera")

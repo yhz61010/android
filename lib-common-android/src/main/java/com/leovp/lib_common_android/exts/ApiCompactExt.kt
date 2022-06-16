5507e3cffbb332362a81ca9a5a78637b201debae
Michael Leo <yhzemail61010@aliyun.com>
2022-06-16 20:37:12 +0800

    1. Add more utils. 2. Update version to 5.4.3

--- lib-common-android/src/main/java/com/leovp/lib_common_android/exts/ApiCompactExt.kt
+++ lib-common-android/src/main/java/com/leovp/lib_common_android/exts/ApiCompactExt.kt
@@ -8,8 +8,10 @@ import android.app.Service
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageInfo
 import android.content.pm.PackageItemInfo
 import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
 import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
@@ -84,43 +86,88 @@ inline fun <reified T : Parcelable> Intent.getParcelableExtraOrNull(key: String)
  * - ServiceInfo for `Service` context
  */
 inline fun <reified T : Context, reified O : PackageItemInfo> getCompatContextInfo(ctx: T,
-    value: Int,
+    flags: Int,
     clazz: Class<*>? = null): O {
     when (ctx) {
         is Activity    -> return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             ctx.packageManager.getActivityInfo(ctx.componentName,
-                PackageManager.ComponentInfoFlags.of(value.toLong()))
+                PackageManager.ComponentInfoFlags.of(flags.toLong()))
         } else {
-            @Suppress("DEPRECATION") ctx.getPackageManager()
-                .getActivityInfo(ctx.componentName, value)
+            @Suppress("DEPRECATION")
+            ctx.packageManager.getActivityInfo(ctx.componentName, flags)
         } as O // ActivityInfo
         is Application -> return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            ctx.getPackageManager()
-                .getApplicationInfo(ctx.getPackageName(),
-                    PackageManager.ApplicationInfoFlags.of(value.toLong()))
+            ctx.packageManager.getApplicationInfo(ctx.getPackageName(),
+                PackageManager.ApplicationInfoFlags.of(flags.toLong()))
         } else {
-            @Suppress("DEPRECATION") ctx.getPackageManager()
-                .getApplicationInfo(ctx.getPackageName(), value)
+            @Suppress("DEPRECATION")
+            ctx.packageManager.getApplicationInfo(ctx.getPackageName(), flags)
         } as O // ApplicationInfo
         is Service     -> {
             val cn = ComponentName(ctx, clazz!!)
             return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                ctx.getPackageManager()
-                    .getServiceInfo(cn, PackageManager.ComponentInfoFlags.of(value.toLong()))
+                ctx.packageManager.getServiceInfo(cn,
+                    PackageManager.ComponentInfoFlags.of(flags.toLong()))
             } else {
                 @Suppress("DEPRECATION")
-                ctx.getPackageManager().getServiceInfo(cn, value)
+                ctx.packageManager.getServiceInfo(cn, flags)
             } as O // ServiceInfo
         }
         else           -> { // BroadcastReceiver
             val cn = ComponentName(ctx, clazz!!)
             return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                 ctx.packageManager.getReceiverInfo(cn,
-                    PackageManager.ComponentInfoFlags.of(value.toLong()))
+                    PackageManager.ComponentInfoFlags.of(flags.toLong()))
             } else {
                 @Suppress("DEPRECATION")
-                ctx.packageManager.getReceiverInfo(cn, value)
+                ctx.packageManager.getReceiverInfo(cn, flags)
             } as O // ActivityInfo
         }
+    }
+}
+
+// ==============================
+
+fun Context.getCompactPackageArchiveInfo(archiveFilePath: String, flags: Int): PackageInfo? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        this.packageManager.getPackageArchiveInfo(archiveFilePath,
+            PackageManager.PackageInfoFlags.of(flags.toLong()))
+    } else {
+        @Suppress("DEPRECATION")
+        this.packageManager.getPackageArchiveInfo(archiveFilePath, flags)
+    }
+}
+
+fun Context.queryCompactIntentActivities(intent: Intent, flags: Int): List<ResolveInfo> {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        this.packageManager.queryIntentActivities(intent,
+            PackageManager.ResolveInfoFlags.of(flags.toLong()))
+    } else {
+        @Suppress("DEPRECATION")
+        this.packageManager.queryIntentActivities(intent, flags)
+    }
+}
+
+fun Context.getCompactPackageInfo(packageName: String, flags: Int): PackageInfo {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        this.packageManager.getPackageInfo(packageName,
+            PackageManager.PackageInfoFlags.of(flags.toLong()))
+    } else {
+        @Suppress("DEPRECATION")
+        this.packageManager.getPackageInfo(packageName, flags)
+    }
+}
+
+// ==============================
+
+fun Service.stopCompactForeground(removeNotification: Boolean) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        if (removeNotification)
+            stopForeground(Service.STOP_FOREGROUND_REMOVE)
+        else
+            stopForeground(Service.STOP_FOREGROUND_DETACH)
+    } else {
+        @Suppress("DEPRECATION")
+        stopForeground(removeNotification)
     }
 }

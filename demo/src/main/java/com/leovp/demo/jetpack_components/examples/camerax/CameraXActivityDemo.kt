32206a32a99fe489d9ca105d7c54bd85ce28b4f9
Michael Leo <yhzemail61010@aliyun.com>
2022-06-21 13:31:31 +0800

    Improve features for CameraX.

--- demo/src/main/java/com/leovp/demo/jetpack_components/examples/camerax/CameraXActivityDemo.kt
+++ demo/src/main/java/com/leovp/demo/jetpack_components/examples/camerax/CameraXActivityDemo.kt
@@ -1,11 +1,10 @@
 package com.leovp.demo.jetpack_components.examples.camerax
 
-import android.graphics.BitmapFactory
 import com.leovp.camerax_sdk.CameraXActivity
 import com.leovp.camerax_sdk.bean.CaptureImage
 import com.leovp.camerax_sdk.listeners.CaptureImageListener
-import com.leovp.camerax_sdk.utils.ExifUtil
 import com.leovp.lib_common_android.exts.getBaseDirString
+import com.leovp.lib_image.toBitmap
 import com.leovp.lib_image.writeToFile
 import com.leovp.log_sdk.LogContext
 import com.leovp.log_sdk.base.ITAG
@@ -38,13 +37,13 @@ import java.io.File
  * Date: 2022/4/25 14:50
  */
 class CameraXDemoActivity : CameraXActivity() {
-    override fun allowToOutputCaptureFile() = true
+    override fun allowToOutputCaptureFile() = false
 
     /** You can implement `CaptureImageListener` or `SimpleCaptureImageListener` */
     override var captureImageListener: CaptureImageListener? = object : CaptureImageListener {
         override fun onSavedImageUri(savedImage: CaptureImage.ImageUri) {
             LogContext.log.w(ITAG,
-                "onSavedImageUri mirror=${savedImage.mirror} rotationDegrees=${savedImage.rotationDegrees} uri=${savedImage.fileUri} path=${savedImage.fileUri.path}")
+                "onSavedImageUri uri=${savedImage.fileUri} path=${savedImage.fileUri.path}")
 
             // To verify the original bitmap orientation.
             //            val filePath: String = savedImage.fileUri.path!!
@@ -56,11 +55,12 @@ class CameraXDemoActivity : CameraXActivity() {
 
         override fun onSavedImageBytes(savedImage: CaptureImage.ImageBytes) {
             LogContext.log.w(ITAG, "onSavedImageBytes=$savedImage")
-            val outFile = File(getBaseDirString("Leo"), "" + System.currentTimeMillis() + ".jpg")
-            LogContext.log.w(ITAG, "outFile=${outFile.absolutePath}")
-            BitmapFactory.decodeByteArray(savedImage.imgBytes, 0, savedImage.imgBytes.size).run {
-                writeToFile(outFile)
+
+            savedImage.imgBytes.toBitmap(savedImage.width, savedImage.height)?.apply {
+                val oriOutFile = File(getBaseDirString("Leo"), "ori.jpg")
+                writeToFile(oriOutFile)
                 recycle()
+                LogContext.log.w(ITAG, "oriOutFile=${oriOutFile.absolutePath}")
             }
 
             //            val outUri: Uri? =
@@ -70,7 +70,7 @@ class CameraXDemoActivity : CameraXActivity() {
             //                setOrientation(uri, savedImage.rotationDegrees, this@CameraXDemoActivity)
             //            }
 
-            ExifUtil.saveExif(outFile.absolutePath, savedImage.width, savedImage.height, savedImage)
+            //            ExifUtil.saveExif(outFile.absolutePath, savedImage.width, savedImage.height, savedImage)
         }
     }
 

0edda3cb7458454ff612eb5cd569338b52e78648
Michael Leo <yhzemail61010@aliyun.com>
2022-07-06 14:34:19 +0800

    minor changes

--- lib-common-android/src/main/java/com/leovp/lib_common_android/exts/DeviceExt.kt
+++ lib-common-android/src/main/java/com/leovp/lib_common_android/exts/DeviceExt.kt
@@ -273,13 +273,28 @@ fun Context.getDimenInPixel(name: String): Int {
 }
 
 fun isProbablyAnEmulator(): Boolean {
-    return (Build.FINGERPRINT.startsWith("google/sdk_gphone_") && Build.FINGERPRINT.endsWith(":user/release-keys") && Build.MANUFACTURER == "Google" && Build.PRODUCT.startsWith(
-        "sdk_gphone_") && Build.BRAND == "google" && Build.MODEL.startsWith("sdk_gphone_")) // Android SDK emulator
-            || Build.FINGERPRINT.startsWith("generic") || Build.FINGERPRINT.startsWith("unknown") || Build.MODEL.contains(
-        "google_sdk") || Build.MODEL.contains("Emulator") || Build.MODEL.contains("Android SDK built for x86") || "QC_Reference_Phone" == Build.BOARD  // bluestacks
-            || Build.MANUFACTURER.contains("Genymotion") || Build.HOST.startsWith("Build") // MSI App Player
-            || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic") || Build.PRODUCT == "google_sdk" || DeviceProp.getSystemProperty(
-        "ro.kernel.qemu") == "1" // another Android SDK emulator check
+    return (Build.FINGERPRINT.startsWith("google/sdk_gphone_")
+            && Build.FINGERPRINT.endsWith(":user/release-keys")
+            && Build.MANUFACTURER == "Google"
+            && Build.PRODUCT.startsWith("sdk_gphone_")
+            && Build.BRAND == "google"
+            && Build.MODEL.startsWith("sdk_gphone_"))
+
+            // Android SDK emulator
+            || Build.FINGERPRINT.startsWith("generic")
+            || Build.FINGERPRINT.startsWith("unknown")
+            || Build.MODEL.contains("google_sdk")
+            || Build.MODEL.contains("Emulator")
+            || Build.MODEL.contains("Android SDK built for x86")
+            || "QC_Reference_Phone" == Build.BOARD
+
+            // bluestacks
+            || Build.MANUFACTURER.contains("Genymotion")
+            || Build.HOST.startsWith("Build") // MSI App Player
+            || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+            || Build.PRODUCT == "google_sdk"
+            // another Android SDK emulator check
+            || DeviceProp.getSystemProperty("ro.kernel.qemu") == "1"
 }
 
 fun Context.isTablet(): Boolean {

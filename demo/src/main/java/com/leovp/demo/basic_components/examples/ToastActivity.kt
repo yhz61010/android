0c368c6b6d316bba77bb5d28b288b965a5015d32
Michael Leo <yhzemail61010@aliyun.com>
2022-07-22 17:04:29 +0800

    Improve [floatview-sdk] module.

--- demo/src/main/java/com/leovp/demo/basic_components/examples/ToastActivity.kt
+++ demo/src/main/java/com/leovp/demo/basic_components/examples/ToastActivity.kt
@@ -1,15 +1,30 @@
 package com.leovp.demo.basic_components.examples
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
+import com.leovp.demo.R
 import com.leovp.demo.base.BaseDemonstrationActivity
 import com.leovp.demo.databinding.ActivityToastBinding
 import com.leovp.lib_common_android.exts.cancelToast
+import com.leovp.lib_common_android.exts.initForegroundComponentForToast
 import com.leovp.lib_common_android.exts.toast
+import com.leovp.lib_common_android.exts.toastIcon
 import com.leovp.log_sdk.base.ITAG
 
 class ToastActivity : BaseDemonstrationActivity<ActivityToastBinding>() {
     override fun getTagName(): String = ITAG
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initForegroundComponentForToast(application)
+        toastIcon = R.mipmap.ic_launcher_round
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            toast("Custom toast in background test.")
+        }, 2000)
+    }
 
     override fun getViewBinding(savedInstanceState: Bundle?): ActivityToastBinding {
         return ActivityToastBinding.inflate(layoutInflater)

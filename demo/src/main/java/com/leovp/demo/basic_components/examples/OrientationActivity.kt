a5277ff2ed9e0d976b235d3e88540ad2ef0e4a72
Michael Leo <yhzemail61010@aliyun.com>
2022-07-27 14:37:52 +0800

    minor changes

--- demo/src/main/java/com/leovp/demo/basic_components/examples/OrientationActivity.kt
+++ demo/src/main/java/com/leovp/demo/basic_components/examples/OrientationActivity.kt
@@ -3,13 +3,16 @@ package com.leovp.demo.basic_components.examples
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.OrientationEventListener
 import com.leovp.demo.base.BaseDemonstrationActivity
 import com.leovp.demo.databinding.ActivityOrientationBinding
 import com.leovp.lib_common_android.exts.*
 import com.leovp.log_sdk.LogContext
 import com.leovp.log_sdk.base.ITAG
+
 
 class OrientationActivity : BaseDemonstrationActivity<ActivityOrientationBinding>() {
     override fun getTagName(): String = ITAG
@@ -33,10 +36,23 @@ class OrientationActivity : BaseDemonstrationActivity<ActivityOrientationBinding
         super.onDestroy()
     }
 
+    /** @return The [Configuration.ORIENTATION_SQUARE], [Configuration.ORIENTATION_PORTRAIT], [Configuration.ORIENTATION_LANDSCAPE] constants based on the current phone screen pixel relations.
+     */
+    private fun getScreenOrientation(): Int {
+        val dm: DisplayMetrics = resources.displayMetrics // Screen rotation effected
+        LogContext.log.w(ITAG,
+            "dm.widthPixels=${dm.widthPixels} dm.heightPixels=${dm.heightPixels}")
+        return if (dm.widthPixels > dm.heightPixels)
+            Configuration.ORIENTATION_LANDSCAPE
+        else Configuration.ORIENTATION_PORTRAIT
+    }
+
     inner class ScreenOrientationListener(private val ctx: Context) : OrientationEventListener(ctx) {
         @SuppressLint("SetTextI18n")
         override fun onOrientationChanged(orientation: Int) {
-            LogContext.log.d("orientation=$orientation")
+            getScreenOrientation()
+            val confOrientation = resources.configuration.orientation
+            LogContext.log.d("orientation=$orientation confOrientation=$confOrientation")
             binding.tvOrientationDegree.text = orientation.toString()
             binding.tvScreenWidth.text = ctx.screenWidth.toString()
             val newOrientation = getScreenOrientation(orientation)

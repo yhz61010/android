5893256d32f88cac0d1607b1062d4a36355bdba9
Michael Leo <yhzemail61010@aliyun.com>
2022-07-28 13:24:37 +0800

    1. Add [lib-reflection] module. 2. Improve [float-view] sdk.

---
+++ lib-reflection/src/main/java/com/leovp/lib_reflection/wrappers/PowerManager.java
@@ -0,0 +1,36 @@
+package com.leovp.lib_reflection.wrappers;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.os.IInterface;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public final class PowerManager {
+    private final IInterface manager;
+    private Method isScreenOnMethod;
+
+    public PowerManager(IInterface manager) {
+        this.manager = manager;
+    }
+
+    private Method getIsScreenOnMethod() throws NoSuchMethodException {
+        if (isScreenOnMethod == null) {
+            @SuppressLint("ObsoleteSdkInt") // we may lower minSdkVersion in the future
+            String methodName = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH ? "isInteractive" : "isScreenOn";
+            isScreenOnMethod = manager.getClass().getMethod(methodName);
+        }
+        return isScreenOnMethod;
+    }
+
+    public boolean isScreenOn() {
+        try {
+            Method method = getIsScreenOnMethod();
+            return (boolean) method.invoke(manager);
+        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}

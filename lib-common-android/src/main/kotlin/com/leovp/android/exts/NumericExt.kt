67f258dff2ac9278df25e097cfd109d9fa20a437
Michael Leo <yhzemail61010@aliyun.com>
2022-08-24 15:51:14 +0800

    minor changes

--- lib-common-android/src/main/kotlin/com/leovp/android/exts/NumericExt.kt
+++ lib-common-android/src/main/kotlin/com/leovp/android/exts/NumericExt.kt
@@ -6,16 +6,6 @@ import android.util.Size
  * Author: Michael Leo
  * Date: 2022/5/30 10:29
  */
-/**
- * Compute the greatest common divisor of two values.
- * @param a one value
- * @param b another value
- * @return the greatest common divisor of a and b
- */
-fun gcd(a: Int, b: Int): Int {
-    require(a > -1 && b > -1) { "Both $a and $b must be greater than or equal to 0." }
-    return if (b == 0) a else gcd(b, a % b)
-}
 
 fun getRatio(size: Size, delimiters: String = ":", swapResult: Boolean = false): String {
     return com.leovp.kotlin.exts.getRatio(size.width, size.height, delimiters, swapResult)

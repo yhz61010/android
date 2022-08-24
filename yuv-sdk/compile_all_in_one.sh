5f3ede8339d94326b61b7ae4c8817e8ff31d32a8
Michael Leo <yhzemail61010@aliyun.com>
2022-08-24 10:49:02 +0800

    Update `yuv-sdk` README.md and add handy compile shell script.

--- yuv-sdk/compile_all_in_one.sh
+++ yuv-sdk/compile_all_in_one.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-# Clean `include` folder in `yuv-sdk` project.
-rm -f src/main/cpp/include
+# Clean `include` and `libs` folder in `yuv-sdk` project.
+echo "Clean [include] and [libs] folder in [yuv-sdk] project."
+rm -rf src/main/cpp/include
+rm -rf libs/
 
 cd ..
 # Clean `libyuv` build folders
+echo "Clean [libyuv] build folders"
 rm -rf libyuv/libs libyuv/obj
 
 cd libyuv/jni
 # Compile `libyuv`
+echo "Compile [libyuv]"
 ndk-build
 
 # Copy `include` folder from `libyuv/jni` to `yuv-sdk/src/main/cpp`.
+echo "Copy [include] folder"
 cp -R include/ ../../yuv-sdk/src/main/cpp/include
 
 # Copy `so` files from `libyuv/libs` to `yuv-sdk/libs`.
-cp ../libs/armeabi-v7a ../../yuv-sdk/libs/armeabi-v7a
-cp ../libs/arm64-v8a   ../../yuv-sdk/libs/arm64-v8a
+echo "Copy [libs] folder"
+mkdir -p ../../yuv-sdk/libs/armeabi-v7a
+mkdir -p ../../yuv-sdk/libs/arm64-v8a
+cp -R ../libs/armeabi-v7a ../../yuv-sdk/libs/
+cp -R ../libs/arm64-v8a   ../../yuv-sdk/libs/
+
+echo "All done."

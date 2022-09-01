d4026c6ce8269d619d825ee2fc38dd979b4f891d
Michael Leo <yhzemail61010@aliyun.com>
2022-09-01 17:23:33 +0800

    Enable x86 and x86_64 for libjpeg.

--- libjpeg-turbo-main/00_build_jpeg_all.sh
+++ libjpeg-turbo-main/00_build_jpeg_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # for arch in armeabi-v7a arm64-v8a x86 x86_64
-for arch in armeabi-v7a arm64-v8a
+for arch in armeabi-v7a arm64-v8a x86 x86_64
 do
     bash 00_build_jpeg.sh $arch
 done

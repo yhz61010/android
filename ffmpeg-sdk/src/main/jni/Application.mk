575fe21b89dc36d1e42c1f8bbf40d6c342d45203
Michael Leo <yhzemail61010@aliyun.com>
2022-09-01 16:31:18 +0800

    Enable x86 and x86_64 for ffmpeg.

--- ffmpeg-sdk/src/main/jni/Application.mk
+++ ffmpeg-sdk/src/main/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_STL := c++_shared
-# x86 armeabi-v7a arm64-v8a
-APP_ABI := armeabi-v7a arm64-v8a
+# armeabi-v7a arm64-v8a x86 x86_64 or all
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 APP_PLATFORM := android-21
 APP_OPTIM := release
 #APP_CPPFLAGS += -fexceptions

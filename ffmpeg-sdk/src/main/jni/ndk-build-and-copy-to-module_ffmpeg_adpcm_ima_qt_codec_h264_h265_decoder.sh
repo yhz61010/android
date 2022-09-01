2dee6461a709dbac22d684b3843e1206311653cc
Michael Leo <yhzemail61010@aliyun.com>
2022-09-01 16:40:24 +0800

    Enable x86 and x86_64 for ffmpeg. [adpcm-ima-qt-codec-h264-hevc-decoder-sdk]

--- ffmpeg-sdk/src/main/jni/ndk-build-and-copy-to-module_ffmpeg_adpcm_ima_qt_codec_h264_h265_decoder.sh
+++ ffmpeg-sdk/src/main/jni/ndk-build-and-copy-to-module_ffmpeg_adpcm_ima_qt_codec_h264_h265_decoder.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # Delete existed so files in [adpcm-ima-qt-codec-h264-hevc-decoder-sdk] module.
-rm -f ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/*.so
-rm -f ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/*.so
+rm -rf   ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
+rm -rf   ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
+rm -rf   ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/x86/
+rm -rf   ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/x86_64/
+
+mkdir -p ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
+mkdir -p ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
+mkdir -p ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/x86/
+mkdir -p ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/x86_64/
 
 # NDK build
 ndk-build clean
@@ -11,3 +18,5 @@ ndk-build
 # Copy new so files to [adpcm-ima-qt-codec-h264-hevc-decoder-sdk] module.
 cp ../libs/armeabi-v7a/*.so ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
 cp ../libs/arm64-v8a/*.so   ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
+cp ../libs/x86/*.so         ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/x86/
+cp ../libs/x86_64/*.so      ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/x86_64/

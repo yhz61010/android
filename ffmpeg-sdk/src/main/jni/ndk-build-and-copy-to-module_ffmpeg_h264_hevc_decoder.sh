0af7f48f75c46079343695dabbcdc981ebbb6906
Michael Leo <yhzemail61010@aliyun.com>
2022-09-01 17:00:56 +0800

    Enable x86 and x86_64 for ffmpeg. [h264-hevc-decoder-sdk]

--- ffmpeg-sdk/src/main/jni/ndk-build-and-copy-to-module_ffmpeg_h264_hevc_decoder.sh
+++ ffmpeg-sdk/src/main/jni/ndk-build-and-copy-to-module_ffmpeg_h264_hevc_decoder.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # Delete existed so files in [h264-hevc-decoder-sdk] module.
-rm -f ../../../../h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/*.so
-rm -f ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/*.so
+rm -rf   ../../../../h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
+rm -rf   ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
+rm -rf   ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
+rm -rf   ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
+
+mkdir -p ../../../../h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
+mkdir -p ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
+mkdir -p ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
+mkdir -p ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
 
 # NDK build
 ndk-build clean
@@ -18,3 +25,13 @@ cp ../libs/arm64-v8a/libh264-hevc-decoder.so   ../../../../h264-hevc-decoder-sdk
 cp ../libs/arm64-v8a/libavcodec.so             ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
 cp ../libs/arm64-v8a/libavutil.so              ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
 cp ../libs/arm64-v8a/libc++_shared.so          ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
+
+cp ../libs/x86/libh264-hevc-decoder.so   ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
+cp ../libs/x86/libavcodec.so             ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
+cp ../libs/x86/libavutil.so              ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
+cp ../libs/x86/libc++_shared.so          ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
+
+cp ../libs/x86_64/libh264-hevc-decoder.so   ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
+cp ../libs/x86_64/libavcodec.so             ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
+cp ../libs/x86_64/libavutil.so              ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
+cp ../libs/x86_64/libc++_shared.so          ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/

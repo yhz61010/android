11e3306b03d735ab98932d51b833144b11198c29
Michael Leo <yhzemail61010@aliyun.com>
2022-09-01 16:31:18 +0800

    Enable x86 and x86_64 for ffmpeg.

--- ffmpeg-sdk/src/main/jni/ndk-build-and-copy-to-module_ffmpeg_adpcm_ima_qt_codec.sh
+++ ffmpeg-sdk/src/main/jni/ndk-build-and-copy-to-module_ffmpeg_adpcm_ima_qt_codec.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # Delete existed so files in [adpcm-ima-qt-codec-sdk] module.
-rm -f ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/armeabi-v7a/*.so
-rm -f ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/arm64-v8a/*.so
+rm -rf ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/armeabi-v7a/
+rm -rf ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/arm64-v8a/
+rm -rf ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/x86/
+rm -rf ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/x86_64/
+
+mkdir -p ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/armeabi-v7a/
+mkdir -p ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/arm64-v8a/
+mkdir -p ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/x86/
+mkdir -p ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/x86_64/
 
 # NDK build
 ndk-build clean
@@ -20,3 +27,15 @@ cp ../libs/arm64-v8a/libadpcm-ima-qt-encoder.so   ../../../../adpcm-ima-qt-codec
 cp ../libs/arm64-v8a/libavcodec.so                ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/arm64-v8a/
 cp ../libs/arm64-v8a/libavutil.so                 ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/arm64-v8a/
 cp ../libs/arm64-v8a/libc++_shared.so             ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/arm64-v8a/
+
+cp ../libs/x86/libadpcm-ima-qt-decoder.so         ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/x86/
+cp ../libs/x86/libadpcm-ima-qt-encoder.so         ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/x86/
+cp ../libs/x86/libavcodec.so                      ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/x86/
+cp ../libs/x86/libavutil.so                       ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/x86/
+cp ../libs/x86/libc++_shared.so                   ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/x86/
+
+cp ../libs/x86_64/libadpcm-ima-qt-decoder.so      ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/x86_64/
+cp ../libs/x86_64/libadpcm-ima-qt-encoder.so      ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/x86_64/
+cp ../libs/x86_64/libavcodec.so                   ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/x86_64/
+cp ../libs/x86_64/libavutil.so                    ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/x86_64/
+cp ../libs/x86_64/libc++_shared.so                ../../../../adpcm-ima-qt-codec-sdk/src/main/libs/x86_64/

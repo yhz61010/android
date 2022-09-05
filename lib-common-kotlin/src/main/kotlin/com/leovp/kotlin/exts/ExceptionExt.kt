834e3f77a58bc4162c46d63ed4ce0dc73ada4a0e
Michael Leo <yhzemail61010@aliyun.com>
2022-09-05 11:26:23 +0800

    Add multiCatch method.

--- lib-common-kotlin/src/main/kotlin/com/leovp/kotlin/exts/ExceptionExt.kt
+++ lib-common-kotlin/src/main/kotlin/com/leovp/kotlin/exts/ExceptionExt.kt
@@ -1,4 +1,8 @@
+@file:Suppress("unused")
+
 package com.leovp.kotlin.exts
+
+import kotlin.reflect.KClass
 
 fun fail(message: String): Nothing {
     throw IllegalArgumentException(message)
@@ -6,4 +10,12 @@ fun fail(message: String): Nothing {
 
 fun exception(message: String): Nothing {
     throw Exception(message)
+}
+
+inline fun multiCatch(runBlock: () -> Unit, vararg exceptions: KClass<out Throwable>, catchBlock: (Throwable) -> Unit) {
+    try {
+        runBlock()
+    } catch (e: Throwable) {
+        if (e::class in exceptions) catchBlock(e) else throw e
+    }
 }

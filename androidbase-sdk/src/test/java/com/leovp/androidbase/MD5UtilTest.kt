f65602f74fd95f9958e13e757335313484160839
Michael Leo <yhzemail61010@aliyun.com>
2022-09-27 14:57:02 +0800

    Fix unit tests in modules.

--- androidbase-sdk/src/test/java/com/leovp/androidbase/MD5UtilTest.kt
+++ androidbase-sdk/src/test/java/com/leovp/androidbase/MD5UtilTest.kt
@@ -3,8 +3,8 @@ package com.leovp.androidbase
 import android.util.Log
 import com.leovp.androidbase.exts.kotlin.toMd5
 import com.leovp.androidbase.utils.cipher.MD5Util
-import org.junit.Assert
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import org.junit.runner.RunWith
 import org.powermock.core.classloader.annotations.PrepareForTest
 import org.powermock.modules.junit4.PowerMockRunner
@@ -19,7 +19,7 @@ class MD5UtilTest {
 
     @Test
     fun md5String() {
-        Assert.assertEquals("E10ADC3949BA59ABBE56E057F20F883E", MD5Util.encrypt("123456"))
-        Assert.assertEquals("E10ADC3949BA59ABBE56E057F20F883E", "123456".toMd5(true))
+        assertEquals("E10ADC3949BA59ABBE56E057F20F883E", MD5Util.encrypt("123456"))
+        assertEquals("E10ADC3949BA59ABBE56E057F20F883E", "123456".toMd5(true))
     }
 }

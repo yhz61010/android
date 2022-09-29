de493ef07b4a330ed916ab5a23c35a89b65644a5
Michael Leo <yhzemail61010@aliyun.com>
2022-09-29 10:25:10 +0800

    1. Remove useless common methods. 2. Improve ReflectManagerTest.kt

--- lib-common-kotlin/src/main/kotlin/com/leovp/kotlin/exts/ExceptionExt.kt
+++ lib-common-kotlin/src/main/kotlin/com/leovp/kotlin/exts/ExceptionExt.kt
@@ -4,13 +4,13 @@ package com.leovp.kotlin.exts
 
 import kotlin.reflect.KClass
 
-fun fail(message: String): Nothing {
-    throw IllegalArgumentException(message)
-}
-
-fun exception(message: String): Nothing {
-    throw Exception(message)
-}
+// fun fail(message: String): Nothing {
+//     throw IllegalArgumentException(message)
+// }
+//
+// fun exception(message: String): Nothing {
+//     throw Exception(message)
+// }
 
 inline fun multiCatch(
     runBlock: () -> Unit,

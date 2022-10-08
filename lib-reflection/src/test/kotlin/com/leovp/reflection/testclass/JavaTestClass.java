c02a07af46e40348f65d90de881bf36354dc74d4
Michael Leo <yhzemail61010@aliyun.com>
2022-10-08 11:06:26 +0800

    Add unit tests for ReflectManager.kt

--- lib-reflection/src/test/kotlin/com/leovp/reflection/testclass/JavaTestClass.java
+++ lib-reflection/src/test/kotlin/com/leovp/reflection/testclass/JavaTestClass.java
@@ -7,6 +7,29 @@ import androidx.annotation.NonNull;
  * Date: 2022/10/8 10:38
  */
 public class JavaTestClass {
+    public static class NoArgClass {
+
+        private Integer getFixedCode() {
+            return 10086;
+        }
+
+        private int secret;
+
+        public int getSecret() {
+            return secret;
+        }
+
+        public void setSecret(int secret) {
+            this.secret = secret;
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            return "Got a NoArgClass.";
+        }
+    }
+
     public static class JavaPerson {
         private String name;
         private char sex;
@@ -23,9 +46,37 @@ public class JavaTestClass {
             }
         }
 
+        JavaPerson(String name, char sex) {
+            this(name, sex, -1);
+        }
+
         JavaPerson(String name, char sex, int age) {
             this.name = name;
             this.sex = sex;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public char getSex() {
+            return sex;
+        }
+
+        public void setSex(char sex) {
+            this.sex = sex;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
             this.age = age;
         }
 

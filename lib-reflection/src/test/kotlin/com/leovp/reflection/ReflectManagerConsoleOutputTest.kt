056d869ed989d567f2022656773188c961cf97d9
Michael Leo <yhzemail61010@aliyun.com>
2022-10-13 11:01:11 +0800

    Add more unit tests for ReflectManager.kt

--- lib-reflection/src/test/kotlin/com/leovp/reflection/ReflectManagerConsoleOutputTest.kt
+++ lib-reflection/src/test/kotlin/com/leovp/reflection/ReflectManagerConsoleOutputTest.kt
@@ -1,16 +1,10 @@
-@file:Suppress("unused", "MemberVisibilityCanBePrivate")
-
 package com.leovp.reflection
 
 import com.leovp.reflection.testclass.Creature
 import com.leovp.reflection.testclass.DEPT_ID_DEV
-import com.leovp.reflection.testclass.DataClassOneArg
 import com.leovp.reflection.testclass.Employee
 import com.leovp.reflection.testclass.HR
-import com.leovp.reflection.testclass.JavaTestClass
-import com.leovp.reflection.testclass.JavaTestClass.NoArgClass
 import com.leovp.reflection.testclass.Person
-import com.leovp.reflection.testclass.PrivateClass
 import kotlin.reflect.KClass
 import kotlin.reflect.full.companionObject
 import kotlin.reflect.full.companionObjectInstance
@@ -34,23 +28,16 @@ import kotlin.reflect.jvm.isAccessible
 import kotlin.test.assertEquals
 import kotlin.test.assertIs
 import kotlin.test.assertNotNull
-import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestMethodOrder
 
 /**
- * https://www.baeldung.com/kotlin/kclass-new-instance
- * https://dwz.win/azW6
- *
  * Author: Michael Leo
- * Date: 2022/9/26 16:52
+ * Date: 2022/10/13 09:59
  */
-// https://www.baeldung.com/junit-5-test-order
-@TestMethodOrder(MethodOrderer.MethodName::class)
-class ReflectManagerTest {
+class ReflectManagerConsoleOutputTest {
 
     @Test
-    fun newInstance() {
+    fun consoleOutput() {
         // https://stackoverflow.com/a/64742576/1685062
         println("=====>Unit: ${Unit::class.javaPrimitiveType}") // null
         println("=====>Unit: ${Unit::class.javaObjectType}") // class kotlin.Unit
@@ -62,304 +49,7 @@ class ReflectManagerTest {
         // https://stackoverflow.com/a/64742576/1685062
         println("=====>Nothing: ${Nothing::class.javaPrimitiveType}") // void
         println("=====>Nothing: ${Nothing::class.javaObjectType}") // class java.lang.Void
-
-        // ----------
-
-        val rfltPrivateCls1: PrivateClass = ReflectManager.reflect(PrivateClass::class).newInstance().get()
-        assertEquals("Get a PrivateClass with paramA=-1 paramB=NA.", rfltPrivateCls1.toString())
-
-        val rfltPrivateCls2 = ReflectManager.reflect(PrivateClass::class).newInstance(1, "HM").get<PrivateClass>()
-        assertEquals("Get a PrivateClass with paramA=1 paramB=HM.", rfltPrivateCls2.toString())
-
-        // ----------
-
-        val rfltCreature1: Creature = ReflectManager.reflect(Creature::class).newInstance().get()
-        assertEquals("Get a new creature.", rfltCreature1.toString())
-
-        val rfltCreature2 = ReflectManager.reflect(Creature::class.java).newInstance().get<Creature>()
-        assertEquals("Get a new creature.", rfltCreature2.toString())
-
-        val rfltCreature3: Any = ReflectManager
-            .reflect("com.leovp.reflection.testclass.Creature")
-            .newInstance()
-            .get()
-        assertEquals("Get a new creature.", rfltCreature3.toString())
-
-        val rfltCreature4: Creature = ReflectManager.reflect(rfltCreature1).newInstance().get()
-        assertEquals("Get a new creature.", rfltCreature4.toString())
-
-        // ----------
-
-        val rfltPerson1: Person = ReflectManager.reflect(Person::class).newInstance("Man1", 'M', 38).get()
-        assertEquals("Man1[M] is 38 years old.", rfltPerson1.toString())
-
-        val rfltPerson2 = ReflectManager.reflect(Person::class.java).newInstance("Man2", 'M', 28).get<Person>()
-        assertEquals("Man2[M] is 28 years old.", rfltPerson2.toString())
-
-        val rfltPerson3: Any = ReflectManager
-            .reflect("com.leovp.reflection.testclass.Person")
-            .newInstance("Woman1", 'F', 20)
-            .get()
-        assertEquals("Woman1[F] is 20 years old.", rfltPerson3.toString())
-
-        val rfltPerson4: Person = ReflectManager.reflect(rfltPerson1).newInstance("Woman2", 'F', 19).get()
-        assertEquals("Woman2[F] is 19 years old.", rfltPerson4.toString())
-
-        val dataClass1: DataClassOneArg = ReflectManager.reflect(DataClassOneArg::class).newInstance("DC1").get()
-        assertEquals("DataClassOneArg(arg1=DC1)", dataClass1.toString())
-
-        val dataClass2: Any = ReflectManager
-            .reflect("com.leovp.reflection.testclass.DataClassTwoArg")
-            .newInstance("DC2", 20021008)
-            .get()
-        println("dataClass2=${dataClass2::class.java}")
-        assertEquals("DataClassTwoArg(arg1=DC2, num=20021008)", dataClass2.toString())
     }
-
-    @Test
-    fun property() {
-        // ====================
-        // ===== get
-        // ====================
-        val privateClass = PrivateClass.of(10, "Hello")
-        val paramA: Int = ReflectManager.reflect(privateClass).property("paramA").get()
-        assertEquals(10, paramA)
-        val paramB = ReflectManager.reflect(privateClass).property("paramB").get<String>()
-        assertEquals("Hello", paramB)
-
-        val person: Person = ReflectManager.reflect(Person::class).newInstance("Michael", 'M', 22).get()
-        val name: String = ReflectManager.reflect(person).property("name").get()
-        assertEquals("Michael", name)
-        val sex: Char = ReflectManager.reflect(person).property("sex").get()
-        assertEquals('M', sex)
-        val age: Int = ReflectManager
-            .reflect(Person::class)
-            .newInstance("World", 'F', 18)
-            .property("age")
-            .get()
-        assertEquals(18, age)
-
-        // ====================
-        // ===== set
-        // ====================
-        ReflectManager.reflect(privateClass).property("paramA", 666)
-        // Change `val` value.
-        assertEquals(666, ReflectManager.reflect(privateClass).property("paramA").get())
-
-        ReflectManager.reflect(person).property("age", 24)
-        assertEquals(24, person.age)
-
-        val reflectedPerson: Person = ReflectManager.reflect(Person::class).newInstance("Jim", 'M', 23).get()
-        val employee: Employee = ReflectManager
-            .reflect(Employee::class)
-            .newInstance("e2003241067", reflectedPerson)
-            .property("salary", 3500)
-            .property("deptId", DEPT_ID_DEV)
-            .property("comment", "No comment.")
-            .get()
-        assertEquals(
-            "[Leo Group] Employee(Jim[M] is 23 years old.) with ID e2003241067 works in 1000 departure. Salary: 3500.",
-            employee.toString()
-        )
-        assertEquals("No comment.", employee.comment)
-        ReflectManager.reflect(employee).property("comment", null)
-        assertEquals(null, employee.comment)
-
-        val company: String = ReflectManager.reflect(employee).property("company").get()
-        assertEquals("Leo Group", company)
-    }
-
-    @Test
-    fun function() {
-        val person = Person("Michael", 'M', 24)
-        val sayResult: String = ReflectManager.reflect(person).method("say", "Hello World.").get()
-        assertEquals("Michael says: Hello World.", sayResult)
-
-        val actionMethodResult: String = ReflectManager
-            .reflect(person)
-            .method("action", "Count Number", 888)
-            .get()
-        assertEquals("Michael do [Count Number] with exceptResult: 888.", actionMethodResult)
-
-        ReflectManager.reflect(person).method("increaseAge")
-        assertEquals(25, person.age)
-
-        val stopWorkingResult: String = ReflectManager
-            .reflect(Employee::class)
-            .newInstance("e00000001", DEPT_ID_DEV, person)
-            .method("stopWorking", 20221008100549).get()
-        assertEquals(
-            "Michael do [Michael[e00000001] stops working at 20221008100549.] with exceptResult: -10.",
-            stopWorkingResult
-        )
-
-        val privateMethod: String = ReflectManager
-            .reflect(Person::class)
-            .newInstance("Tom", 'M', 26)
-            .method("secretMethod", "<SECRET>").get()
-        assertEquals("Tom does [secret method](<SECRET>).", privateMethod)
-
-        val showClothesResult: String = ReflectManager
-            .reflect(Person::class)
-            .newInstance("Michael", 'M', 22)
-            .method("showClothes", true).get()
-        assertEquals("Someone shows all(true) clothes.", showClothesResult)
-
-        ReflectManager
-            .reflect(Person::class)
-            .newInstance("John", 'M', 24)
-            .method("sayHi")
-
-        val objInstanceShowClothesResult: String = ReflectManager
-            .reflect(Person::class)
-            .method("showClothes", false).get()
-        assertEquals("Someone shows all(false) clothes.", objInstanceShowClothesResult)
-
-        val employeeInstanceSayHi: String = ReflectManager
-            .reflect(Employee::class)
-            .method("sayHi").get()
-        assertEquals("Employee said: Hi.", employeeInstanceSayHi)
-
-        val employeeInstanceGlobalSay: String = ReflectManager
-            .reflect(Employee::class)
-            .method("globalSay", "New World").get()
-        assertEquals("Employee said to global: New World", employeeInstanceGlobalSay)
-    }
-
-    // ========================================
-    // ========================================
-    // ========================================
-
-    @Test
-    fun javaNewInstance() {
-        val javaPerson1: JavaTestClass.JavaPerson = ReflectManager
-            .reflect(JavaTestClass.JavaPerson::class)
-            .newInstance("Java", 'M', 24)
-            .get()
-        assertEquals("""{name: "Java", sex: "M", age: 24}""", javaPerson1.toString())
-
-        val javaPerson2: JavaTestClass.JavaPerson = ReflectManager
-            .reflect(JavaTestClass.JavaPerson::class)
-            .newInstance("Girl", 'F')
-            .get()
-        assertEquals("""{name: "Girl", sex: "F", age: -1}""", javaPerson2.toString())
-
-        val noArgClass: NoArgClass = ReflectManager.reflect(NoArgClass::class).newInstance().get()
-        assertEquals("Got a NoArgClass.", noArgClass.toString())
-    }
-
-    @Test
-    fun javaField() {
-        val javaPerson1: JavaTestClass.JavaPerson = ReflectManager
-            .reflect(JavaTestClass.JavaPerson::class)
-            .newInstance("Kotlin", 'M', 23)
-            .get()
-        assertEquals("""{name: "Kotlin", sex: "M", age: 23}""", javaPerson1.toString())
-
-        val javaPerson1Name: String = ReflectManager.reflect(javaPerson1).property("name").get()
-        assertEquals("Kotlin", javaPerson1Name)
-
-        val javaPerson1OpenField: String = ReflectManager.reflect(javaPerson1).property("openField").get()
-        assertEquals("Open Field", javaPerson1OpenField)
-
-        val rfltPerson1: JavaTestClass.JavaPerson = ReflectManager
-            .reflect(JavaTestClass.JavaPerson::class)
-            .newInstance("Reflected Person", 'F', 18)
-            .get()
-        val javaPerson1PublicStatic: String = ReflectManager.reflect(rfltPerson1).property("PUBLIC_NAME").get()
-        assertEquals("Public Name", javaPerson1PublicStatic)
-
-        val javaPerson1PrivateStatic: String = ReflectManager.reflect(rfltPerson1).property("NO_NAME").get()
-        assertEquals("No Name", javaPerson1PrivateStatic)
-
-        val rfltPersonName: String = ReflectManager.reflect(rfltPerson1).property("name").get()
-        assertEquals("Reflected Person", rfltPersonName)
-
-        val javaPerson1PublicFinal: String = ReflectManager.reflect(rfltPerson1).property("PUBLIC_FINAL").get()
-        assertEquals("Public Final", javaPerson1PublicFinal)
-
-        val javaPerson1PrivateFinal: String = ReflectManager.reflect(rfltPerson1).property("PRIVATE_FINAL").get()
-        assertEquals("Private Final", javaPerson1PrivateFinal)
-
-        println("1 PUBLIC_STATIC_FINAL_INT=${JavaTestClass.JavaPerson.PUBLIC_STATIC_FINAL_INT}")
-        ReflectManager.reflect(JavaTestClass.JavaPerson::class).property("PUBLIC_STATIC_FINAL_INT", 10086)
-        println("2 PUBLIC_STATIC_FINAL_INT=${JavaTestClass.JavaPerson.PUBLIC_STATIC_FINAL_INT}")
-        assertEquals(10086, JavaTestClass.JavaPerson.PUBLIC_STATIC_FINAL_INT)
-
-        val rfltPerson2: JavaTestClass.JavaPerson = ReflectManager
-            .reflect(JavaTestClass.JavaPerson::class)
-            .newInstance("Reflected Person", 'F', 18)
-            .get()
-
-        ReflectManager.reflect(rfltPerson2).property("PUBLIC_FINAL", "Modified public final")
-        assertEquals("Modified public final", rfltPerson2.PUBLIC_FINAL)
-
-        ReflectManager.reflect(rfltPerson2).property("PRIVATE_FINAL", "Modified private final")
-        val privateFinal: String = ReflectManager.reflect(rfltPerson2).property("PRIVATE_FINAL").get()
-        assertEquals("Modified private final", privateFinal)
-    }
-
-    @Test
-    fun javaFunction() {
-        val javaPerson1: JavaTestClass.JavaPerson = ReflectManager
-            .reflect(JavaTestClass.JavaPerson::class)
-            .newInstance("Java", 'M', 24)
-            .get()
-
-        val getSexInString1: String = ReflectManager
-            .reflect(javaPerson1)
-            .newInstance("JavaGirl", 'F', 18)
-            .method("getSexInString")
-            .get()
-        assertEquals("Female", getSexInString1)
-
-        val getSexInString2: String = ReflectManager
-            .reflect(JavaTestClass.JavaPerson::class)
-            .newInstance("Java", 'M', 24)
-            .method("getSexInString")
-            .get()
-        assertEquals("Male", getSexInString2)
-
-        val noArgClass: NoArgClass = ReflectManager.reflect(NoArgClass::class).newInstance().get()
-        noArgClass.secret = 10010
-        assertEquals(10010, noArgClass.secret)
-
-        val fixedCode = ReflectManager.reflect(NoArgClass::class).newInstance().method("getFixedCode").get<Int>()
-        assertEquals(10086, fixedCode)
-
-        val javaPerson2: JavaTestClass.JavaPerson = ReflectManager
-            .reflect(JavaTestClass.JavaPerson::class)
-            .newInstance("Man", 'M', 23)
-            .method("setSex", 'F')
-            .get()
-        assertEquals('F', javaPerson2.sex)
-        assertEquals("""{name: "Man", sex: "F", age: 23}""", javaPerson2.toString())
-
-        val newPrivateVal: Int = ReflectManager
-            .reflect(JavaTestClass.JavaPerson::class)
-            .newInstance("Man", 'M')
-            .method("changeOnlyPrivate", 6789)
-            .get()
-        assertEquals(6789, newPrivateVal)
-
-        ReflectManager.reflect(NoArgClass::class.java).method("print", "Hello World.")
-
-        val javaStaticMethodResult: String = ReflectManager
-            .reflect(NoArgClass::class.java)
-            .method("say", "I'm sorry.")
-            .get()
-        assertEquals("NoArgClass say: I'm sorry.", javaStaticMethodResult)
-
-        val javaPrivateStaticMethodResult: String = ReflectManager
-            .reflect(NoArgClass::class.java)
-            .method("privateMessage", "Phone me later.")
-            .get()
-        assertEquals("PM: Phone me later.", javaPrivateStaticMethodResult)
-    }
-
-    // ========================================
-    // ========================================
-    // ========================================
 
     @Test
     fun newInstanceByKotlinReflect() {

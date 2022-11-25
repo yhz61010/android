051dbfbccef99aee08f4a1848fd79c461bbcc184
Michael Leo <yhzemail61010@aliyun.com>
2022-11-25 14:42:39 +0800

    Various minor changes.

--- camerax-sdk/src/main/kotlin/com/leovp/camerax/fragments/base/BaseCameraXFragment.kt
+++ camerax-sdk/src/main/kotlin/com/leovp/camerax/fragments/base/BaseCameraXFragment.kt
@@ -671,10 +671,10 @@ abstract class BaseCameraXFragment<B : ViewBinding> : Fragment() {
             LogContext.log.i(
                 logTag,
                 "FACE RETOUCH: ${
-                    extensionsManager.isExtensionAvailable(
-                        lensFacing,
-                        ExtensionMode.FACE_RETOUCH
-                    )
+                extensionsManager.isExtensionAvailable(
+                    lensFacing,
+                    ExtensionMode.FACE_RETOUCH
+                )
                 }"
             )
             LogContext.log.i(
@@ -887,35 +887,35 @@ cameraSensorOrientation=${characteristics.cameraSensorOrientation()}
           hardwareLevel=$hardwareLevel[${characteristics.hardwareLevelName()}]
 
  Supported color format for  AVC=${
-                    getSupportedColorFormatForEncoder(MediaFormat.MIMETYPE_VIDEO_AVC).sorted()
-                        .joinToString(",")
+                getSupportedColorFormatForEncoder(MediaFormat.MIMETYPE_VIDEO_AVC).sorted()
+                    .joinToString(",")
                 }
  Supported color format for HEVC=${
-                    getSupportedColorFormatForEncoder(MediaFormat.MIMETYPE_VIDEO_HEVC).sorted()
-                        .joinToString(",")
+                getSupportedColorFormatForEncoder(MediaFormat.MIMETYPE_VIDEO_HEVC).sorted()
+                    .joinToString(",")
                 }
 
 Supported profile/level for  AVC=${
-                    getSupportedProfileLevelsForEncoder(MediaFormat.MIMETYPE_VIDEO_AVC).joinToString(
-                        ","
-                    ) { "${it.profile}/${it.level}" }
+                getSupportedProfileLevelsForEncoder(MediaFormat.MIMETYPE_VIDEO_AVC).joinToString(
+                    ","
+                ) { "${it.profile}/${it.level}" }
                 }
 Supported profile/level for HEVC=${
-                    getSupportedProfileLevelsForEncoder(MediaFormat.MIMETYPE_VIDEO_AVC).joinToString(
-                        ","
-                    ) { "${it.profile}/${it.level}" }
+                getSupportedProfileLevelsForEncoder(MediaFormat.MIMETYPE_VIDEO_AVC).joinToString(
+                    ","
+                ) { "${it.profile}/${it.level}" }
                 }
 
      highSpeedVideoFpsRanges=${highSpeedVideoFpsRanges?.contentToString()}
          highSpeedVideoSizes=${
-                    highSpeedVideoSizes?.joinToString(",") { "${it.width}x${it.height}(${getRatio(it.width, it.height)})" }
+                highSpeedVideoSizes?.joinToString(",") { "${it.width}x${it.height}(${getRatio(it.width, it.height)})" }
                 }
 
         Supported FPS Ranges=${cameraSupportedFpsRanges.contentToString()}
               Supported Size=${
-                    allCameraSupportSize?.joinToString(",") {
-                        "${it.width}x${it.height}(${getCameraSizeTotalPixels(it)}-${getRatio(it.width, it.height)})"
-                    }
+                allCameraSupportSize?.joinToString(",") {
+                    "${it.width}x${it.height}(${getCameraSizeTotalPixels(it)}-${getRatio(it.width, it.height)})"
+                }
                 }
                 """.trimIndent()
                 LogContext.log.i(logTag, cameraParametersString)

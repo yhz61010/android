9f3e047ae00e8ce82063994c987be34d6264cbee
Michael Leo <yhzemail61010@aliyun.com>
2022-11-25 14:25:35 +0800

    Various minor changes.

--- lib-common-android/src/main/kotlin/com/leovp/android/exts/NumericExt.kt
+++ lib-common-android/src/main/kotlin/com/leovp/android/exts/NumericExt.kt
@@ -7,10 +7,10 @@ import android.util.Size
  * Date: 2022/5/30 10:29
  */
 
-fun getRatio(size: Size, delimiters: String = ":", swapResult: Boolean = false): String {
+fun getRatio(size: Size, delimiters: String = ":", swapResult: Boolean = false): String? {
     return com.leovp.kotlin.exts.getRatio(size.width, size.height, delimiters, swapResult)
 }
 
-fun getRatio(size: SmartSize, delimiters: String = ":", swapResult: Boolean = false): String {
+fun getRatio(size: SmartSize, delimiters: String = ":", swapResult: Boolean = false): String? {
     return com.leovp.kotlin.exts.getRatio(size.long, size.short, delimiters, swapResult)
 }

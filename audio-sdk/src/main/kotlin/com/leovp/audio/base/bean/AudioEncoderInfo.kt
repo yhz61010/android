d9562b3e99c4ee352dde100f2dec40c4a80a793d
Michael Leo <yhzemail61010@aliyun.com>
2023-04-25 16:26:38 +0800

    1. Upgrade agp to latest version 8.0.0 2. Add opus demo.

--- audio-sdk/src/main/kotlin/com/leovp/audio/base/bean/AudioEncoderInfo.kt
+++ audio-sdk/src/main/kotlin/com/leovp/audio/base/bean/AudioEncoderInfo.kt
@@ -13,8 +13,6 @@ import androidx.annotation.Keep
  * channelConfig:
  *              AudioFormat.CHANNEL_IN_MONO(16=0x10)
  *              AudioFormat.CHANNEL_IN_STEREO(12=0xc)
- *              AudioFormat.CHANNEL_OUT_STEREO(12=0xc)
- *              AudioFormat.CHANNEL_OUT_MONO (0x4)
  * channelCount: 1
  * audioFormat(bit depth):
  *              AudioFormat.ENCODING_PCM_16BIT(0x2)

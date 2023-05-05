439744c899275a35b845185692fa2a0a7d66eebd
Michael Leo <yhzemail61010@aliyun.com>
2023-05-05 15:14:05 +0800

    Refactor [audio-sdk]

--- audio-sdk/src/main/kotlin/com/leovp/audio/mediacodec/iter/IAudioMediaCodec.kt
+++ audio-sdk/src/main/kotlin/com/leovp/audio/mediacodec/iter/IAudioMediaCodec.kt
@@ -1,6 +1,7 @@
 package com.leovp.audio.mediacodec.iter
 
 import android.media.MediaCodec
+import android.media.MediaCodecInfo
 import android.media.MediaFormat
 import java.nio.ByteBuffer
 
@@ -13,10 +14,17 @@ interface IAudioMediaCodec {
         const val OPUS_AOPUSHDR = 0x524448_5355504F41L // "AOPUSHDR" in ASCII (little-endian)
         const val OPUS_AOPUSDLY = 0x594c44_5355504F41L // "AOPUSDLY" in ASCII (little-endian)
         const val OPUS_AOPUSPRL = 0x4c5250_5355504F41L // "AOPUSPRL" in ASCII (little-endian)
+
+        const val AAC_PROFILE_LC = MediaCodecInfo.CodecProfileLevel.AACObjectLC
     }
 
     // ==========
-    fun onInputData(inBuf: ByteBuffer)
+    /**
+     * Fill [inBuf] with valid data.
+     *
+     * @return The length of bytes of valid input data.
+     */
+    fun onInputData(inBuf: ByteBuffer): Int
 
     fun onOutputData(outBuf: ByteBuffer, info: MediaCodec.BufferInfo, isConfig: Boolean, isKeyFrame: Boolean)
 

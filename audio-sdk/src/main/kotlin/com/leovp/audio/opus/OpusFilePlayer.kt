3e6dca23025b0a9bee1b6de3bc3afa576d4814ad
Michael Leo <yhzemail61010@aliyun.com>
2023-05-09 09:44:19 +0800

    Refactor [audio-sdk]

--- audio-sdk/src/main/kotlin/com/leovp/audio/opus/OpusFilePlayer.kt
+++ audio-sdk/src/main/kotlin/com/leovp/audio/opus/OpusFilePlayer.kt
@@ -16,10 +16,12 @@ import java.io.File
 import java.io.IOException
 import java.io.RandomAccessFile
 import java.nio.ByteOrder
+import java.util.concurrent.ArrayBlockingQueue
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.launch
 
@@ -40,13 +42,97 @@ class OpusFilePlayer(
         const val startCode = "|leo|"
     }
 
+    private val queue = ArrayBlockingQueue<ByteArray>(64)
+
     private val startCodeSize = startCode.length
     private val ioScope = CoroutineScope(Dispatchers.IO + CoroutineName("opus-file-player"))
 
     private val audioTrackPlayer: AudioTrackPlayer = AudioTrackPlayer(ctx, audioDecoderInfo, usage = usage, contentType = contentType)
     private var decoder: OpusDecoder? = null
 
+    private var cb: (() -> Unit)? = null
+
     private lateinit var rf: RandomAccessFile
+
+    fun playOpus(opusFile: File, endCallback: () -> Unit) {
+        cb = endCallback
+        audioTrackPlayer.play()
+        rf = RandomAccessFile(opusFile, "r")
+        LogContext.log.w(TAG, "File length=${rf.length()}")
+        val opusCsd = getCsd()!!
+        LogContext.log.w(TAG, "csd0=${opusCsd.csd0.toHexString()}")
+        LogContext.log.w(TAG, "csd1=${opusCsd.csd1.toHexString()}")
+        LogContext.log.w(TAG, "csd2=${opusCsd.csd2.toHexString()}")
+        decoder = OpusDecoder(
+            audioDecoderInfo.sampleRate, audioDecoderInfo.channelCount,
+            opusCsd.csd0, opusCsd.csd1, opusCsd.csd2,
+            object : IDecodeCallback {
+                override fun onDecoded(pcmData: ByteArray) {
+                    queue.put(pcmData)
+                    LogContext.log.i(TAG, "onDecoded -> queue[${queue.size}] pcm[${pcmData.size}]")
+                }
+            }
+        ).apply { start() }
+        ioScope.launch {
+            var startCodeBeginPos = findStartCode(startCodeSize.toLong())
+            var frame: Long = 0
+            val frameSize: Int = audioDecoderInfo.sampleRate / 1000 * 20 // 20ms
+            val baseDelay = 16L
+            var calcDelay = baseDelay
+            while (true) {
+                ensureActive()
+                var startCodeEndPos: Long
+                try {
+                    startCodeEndPos = findStartCode(startCodeBeginPos + startCodeSize)
+                    if (startCodeEndPos < 0) {
+                        LogContext.log.w(TAG, "Can't find start code.")
+                        break
+                    }
+                    // LogContext.log.w(tag, "startCodeBeginPos=$startCodeBeginPos  startCodeEndPos=$startCodeEndPos")
+                    val audioFrameData = ByteArray((startCodeEndPos - startCodeBeginPos - startCodeSize).toInt())
+                    rf.seek(startCodeBeginPos + startCodeSize)
+                    rf.readFully(audioFrameData)
+                    // LogContext.log.d(tag, "audioFrameData[${audioFrameData.size}]=${audioFrameData.toHexString()}")
+                    decoder?.decode(audioFrameData)
+                    val delayMs = if (calcDelay > 20) 20 else calcDelay
+                    if (delayMs < 20) {
+                        calcDelay = baseDelay + (++frame / frameSize)
+                    }
+                    LogContext.log.e(TAG, "-----> Delay=$delayMs")
+                    delay(delayMs)
+                } catch (e: EOFException) {
+                    LogContext.log.e(TAG, "EOFException", e)
+                    break
+                } catch (ioe: IOException) {
+                    LogContext.log.e(TAG, "IOException", ioe)
+                    break
+                } catch (nase: NegativeArraySizeException) {
+                    LogContext.log.e(TAG, "NegativeArraySizeException", nase)
+                    break
+                }
+                startCodeBeginPos = startCodeEndPos
+            }
+        }
+
+        ioScope.launch {
+            while (true) {
+                ensureActive()
+                val pcmData: ByteArray = queue.take()
+                // LogContext.log.w(TAG, "play -> queue[${queue.size}] pcm=${pcmData.size}")
+                audioTrackPlayer.write(pcmData)
+            }
+        }
+    }
+
+    fun stop() {
+        if (audioTrackPlayer.state == AudioTrack.STATE_UNINITIALIZED) {
+            return
+        }
+        audioTrackPlayer.release()
+        decoder?.release()
+        ioScope.cancel()
+        queue.clear()
+    }
 
     private fun getCsd(): OpusCsd? {
         val csdEndPos = findStartCode(5)
@@ -95,66 +181,5 @@ class OpusFilePlayer(
             }
         }
         return -1
-    }
-
-    private var cb: (() -> Unit)? = null
-
-    fun playOpus(opusFile: File, endCallback: () -> Unit) {
-        cb = endCallback
-        audioTrackPlayer.play()
-        rf = RandomAccessFile(opusFile, "r")
-        LogContext.log.w(TAG, "File length=${rf.length()}")
-        val opusCsd = getCsd()!!
-        LogContext.log.w(TAG, "csd0=${opusCsd.csd0.toHexString()}")
-        LogContext.log.w(TAG, "csd1=${opusCsd.csd1.toHexString()}")
-        LogContext.log.w(TAG, "csd2=${opusCsd.csd2.toHexString()}")
-        decoder = OpusDecoder(
-            audioDecoderInfo.sampleRate, audioDecoderInfo.channelCount,
-            opusCsd.csd0, opusCsd.csd1, opusCsd.csd2,
-            object : IDecodeCallback {
-                override fun onDecoded(pcmData: ByteArray) {
-                    audioTrackPlayer.write(pcmData)
-                }
-            }
-        ).apply { start() }
-        ioScope.launch {
-            var startCodeBeginPos = findStartCode(startCodeSize.toLong())
-            while (true) {
-                ensureActive()
-                var startCodeEndPos: Long
-                try {
-                    startCodeEndPos = findStartCode(startCodeBeginPos + startCodeSize)
-                    if (startCodeEndPos < 0) {
-                        LogContext.log.w(TAG, "Can't find start code.")
-                        break
-                    }
-                    // LogContext.log.w(tag, "startCodeBeginPos=$startCodeBeginPos  startCodeEndPos=$startCodeEndPos")
-                    val audioFrameData = ByteArray((startCodeEndPos - startCodeBeginPos - startCodeSize).toInt())
-                    rf.seek(startCodeBeginPos + startCodeSize)
-                    rf.readFully(audioFrameData)
-                    // LogContext.log.d(tag, "audioFrameData[${audioFrameData.size}]=${audioFrameData.toHexString()}")
-                    decoder?.decode(audioFrameData)
-                } catch (e: EOFException) {
-                    LogContext.log.e(TAG, "EOFException", e)
-                    break
-                } catch (ioe: IOException) {
-                    LogContext.log.e(TAG, "EOFException", ioe)
-                    break
-                } catch (nase: NegativeArraySizeException) {
-                    LogContext.log.e(TAG, "NegativeArraySizeException", nase)
-                    break
-                }
-                startCodeBeginPos = startCodeEndPos
-            }
-        }
-    }
-
-    fun stop() {
-        if (audioTrackPlayer.state == AudioTrack.STATE_UNINITIALIZED) {
-            return
-        }
-        audioTrackPlayer.release()
-        decoder?.release()
-        ioScope.cancel()
     }
 }

8f505a9de4e461a37a5c69f43d96e7faf4b5e5d4
Michael Leo <yhzemail61010@aliyun.com>
2023-05-11 15:25:27 +0800

    Upgrade NDK to latest version. (25.1.8937393)

--- ffmpeg-sdk/src/main/ffmpeg_build/config.sh
+++ ffmpeg-sdk/src/main/ffmpeg_build/config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 FFMPEG_FOLDER=ffmpeg-6.0
-NDK_PATH=~/Library/Android/sdk/ndk/21.4.7075529
+NDK_PATH=~/Library/Android/sdk/ndk/25.1.8937393
 # linux-x86_64
 HOST_TAG=darwin-x86_64
-MIN_SDK_VER=21
+MIN_SDK_VER=23
 
 # ==================================
 

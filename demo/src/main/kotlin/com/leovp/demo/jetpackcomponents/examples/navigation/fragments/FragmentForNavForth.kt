501de57b42c816966bc7f3c59040221d6ac525bb
Michael Leo <yhzemail61010@aliyun.com>
2023-07-11 11:17:32 +0800

    Improve StatusBarExt.kt

--- demo/src/main/kotlin/com/leovp/demo/jetpackcomponents/examples/navigation/fragments/FragmentForNavForth.kt
+++ demo/src/main/kotlin/com/leovp/demo/jetpackcomponents/examples/navigation/fragments/FragmentForNavForth.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.leovp.androidbase.framework.BaseFragment
+import com.leovp.demo.R
 import com.leovp.demo.databinding.FragmentForNavForthBinding
 import com.leovp.log.base.ITAG
 
-class FragmentForNavForth : BaseFragment<FragmentForNavForthBinding>() {
+class FragmentForNavForth : BaseFragment<FragmentForNavForthBinding>(R.layout.fragment_for_nav_forth) {
     override fun getTagName(): String = ITAG
 
     override fun getViewBinding(

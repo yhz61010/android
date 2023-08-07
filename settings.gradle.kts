ff61499e716bf0ce4cb969f36b4bf653a0777a61
yuan.hongzhi <yuan.hongzhi@linecorp.com>
2023-08-07 11:16:59 +0800

    Minor changes.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,7 +31,9 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    @Suppress("UnstableApiUsage")
     repositories {
         google()
         mavenCentral()

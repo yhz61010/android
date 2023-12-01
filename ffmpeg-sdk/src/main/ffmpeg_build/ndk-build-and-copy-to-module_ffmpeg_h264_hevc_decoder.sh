41918ad23c588cf781052ffb4fd6c91d65efbd2f
yuan.hongzhi <yuan.hongzhi@linecorp.com>
2023-12-01 16:06:45 +0800

    Allow decode video data to rgb by ffmpeg.

--- ffmpeg-sdk/src/main/ffmpeg_build/ndk-build-and-copy-to-module_ffmpeg_h264_hevc_decoder.sh
+++ ffmpeg-sdk/src/main/ffmpeg_build/ndk-build-and-copy-to-module_ffmpeg_h264_hevc_decoder.sh
@@ -19,19 +19,23 @@ ndk-build
 cp ../libs/armeabi-v7a/libh264-hevc-decoder.so ../../../../h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
 cp ../libs/armeabi-v7a/libavcodec.so           ../../../../h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
 cp ../libs/armeabi-v7a/libavutil.so            ../../../../h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
+cp ../libs/armeabi-v7a/libswscale.so           ../../../../h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
 cp ../libs/armeabi-v7a/libc++_shared.so        ../../../../h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
 
 cp ../libs/arm64-v8a/libh264-hevc-decoder.so   ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
 cp ../libs/arm64-v8a/libavcodec.so             ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
 cp ../libs/arm64-v8a/libavutil.so              ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
+cp ../libs/arm64-v8a/libswscale.so             ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
 cp ../libs/arm64-v8a/libc++_shared.so          ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
 
 cp ../libs/x86/libh264-hevc-decoder.so   ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
 cp ../libs/x86/libavcodec.so             ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
 cp ../libs/x86/libavutil.so              ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
+cp ../libs/x86/libswscale.so             ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
 cp ../libs/x86/libc++_shared.so          ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
 
 cp ../libs/x86_64/libh264-hevc-decoder.so   ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
 cp ../libs/x86_64/libavcodec.so             ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
 cp ../libs/x86_64/libavutil.so              ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
+cp ../libs/x86_64/libswscale.so             ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
 cp ../libs/x86_64/libc++_shared.so          ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/

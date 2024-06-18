d28c15f9c7a078531dbf04831f7e3542746e4622
Michael Leo <yhzemail61010@aliyun.com>
2024-06-18 11:16:27 +0800

    1. Update version to 5.12.38 2. Restore libc++_shared.so files to corresponding projects. 3. Update demo version to 1.9 (19)

--- ffmpeg-sdk/src/main/ffmpeg_build/ndk-build-and-copy-to-module_ffmpeg_adpcm_ima_qt_codec_h264_h265_decoder.sh
+++ ffmpeg-sdk/src/main/ffmpeg_build/ndk-build-and-copy-to-module_ffmpeg_adpcm_ima_qt_codec_h264_h265_decoder.sh
@@ -16,7 +16,11 @@ ndk-build clean
 ndk-build
 
 # Copy new so files to [adpcm-ima-qt-codec-h264-hevc-decoder-sdk] module.
-rsync -avh --exclude='libc++_shared.so' ../libs/armeabi-v7a/*.so ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
-rsync -avh --exclude='libc++_shared.so' ../libs/arm64-v8a/*.so   ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
-rsync -avh --exclude='libc++_shared.so' ../libs/x86/*.so         ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/x86/
-rsync -avh --exclude='libc++_shared.so' ../libs/x86_64/*.so      ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/x86_64/
+#rsync -avh --exclude='libc++_shared.so' ../libs/armeabi-v7a/*.so ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
+#rsync -avh --exclude='libc++_shared.so' ../libs/arm64-v8a/*.so   ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
+#rsync -avh --exclude='libc++_shared.so' ../libs/x86/*.so         ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/x86/
+#rsync -avh --exclude='libc++_shared.so' ../libs/x86_64/*.so      ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/x86_64/
+rsync -avh ../libs/armeabi-v7a/*.so ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
+rsync -avh ../libs/arm64-v8a/*.so   ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
+rsync -avh ../libs/x86/*.so         ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/x86/
+rsync -avh ../libs/x86_64/*.so      ../../../../adpcm-ima-qt-codec-h264-hevc-decoder-sdk/src/main/libs/x86_64/

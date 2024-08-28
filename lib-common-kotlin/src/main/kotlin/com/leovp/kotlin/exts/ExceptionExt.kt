3321bba5ae9da8342c4fdc2e17381e2936f07b28
Michael Leo <yhzemail61010@aliyun.com>
2024-08-28 14:57:17 +0800

    1. Update version to 5.12.40 2. Improve ExceptionExt.kt 3. Update demo version to 2.1(21)

--- lib-common-kotlin/src/main/kotlin/com/leovp/kotlin/exts/ExceptionExt.kt
+++ lib-common-kotlin/src/main/kotlin/com/leovp/kotlin/exts/ExceptionExt.kt
@@ -12,14 +12,39 @@ import kotlin.reflect.KClass
 //     throw Exception(message)
 // }
 
+/**
+ * Usage:
+ * ```
+ * multiCatch(
+ *     runBlock = {
+ *     },
+ *     exceptions = arrayOf(
+ *         SerializationException::class,
+ *         IllegalArgumentException::class
+ *     ),
+ *     // nullable argument
+ *     catchBlock = {
+ *     },
+ *     // nullable argument
+ *     uncaughtBlock = {
+ *     }
+ * )
+ * ```
+ */
 inline fun multiCatch(
     runBlock: () -> Unit,
     vararg exceptions: KClass<out Throwable>,
-    catchBlock: (Throwable) -> Unit
+    noinline catchBlock: ((Throwable) -> Unit)? = null,
+    noinline uncaughtBlock: ((Throwable) -> Unit)? = null,
 ) {
     try {
         runBlock()
     } catch (e: Throwable) {
-        if (e::class in exceptions) catchBlock(e) else throw e
+        if (exceptions.any { it.isInstance(e) }) {
+            catchBlock?.invoke(e)
+        } else {
+            // Re-throw if not in the specified exceptions
+            uncaughtBlock?.invoke(e) ?: throw e
+        }
     }
 }

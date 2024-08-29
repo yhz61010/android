0d98a01875d78275eb793c6cdd9266f401d1de87
Michael Leo <yhzemail61010@aliyun.com>
2024-08-29 15:47:41 +0800

    Add more utils.

--- lib-common-kotlin/src/main/kotlin/com/leovp/kotlin/exts/CalendarExt.kt
+++ lib-common-kotlin/src/main/kotlin/com/leovp/kotlin/exts/CalendarExt.kt
@@ -2,15 +2,22 @@
 
 package com.leovp.kotlin.exts
 
+import java.util.Locale
 import java.util.concurrent.TimeUnit
 
 /**
-  * Author: Michael Leo
-  * Date: 2023/12/5 10:35
-  */
+ * Author: Michael Leo
+ * Date: 2023/12/5 10:35
+ */
 fun Long.formatTimestamp(): String {
     val hours = TimeUnit.MILLISECONDS.toHours(this)
     val minutes = TimeUnit.MILLISECONDS.toMinutes(this) - TimeUnit.HOURS.toMinutes(hours)
     val seconds = TimeUnit.MILLISECONDS.toSeconds(this) - TimeUnit.MINUTES.toSeconds(minutes) - TimeUnit.HOURS.toSeconds(hours)
-    return String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    return String.format(Locale.ENGLISH, "%02d:%02d:%02d", hours, minutes, seconds)
+}
+
+fun Long.formatTimestampShort(): String {
+    val minutes = TimeUnit.MILLISECONDS.toMinutes(this)
+    val seconds = TimeUnit.MILLISECONDS.toSeconds(this) - TimeUnit.MINUTES.toSeconds(minutes)
+    return String.format(Locale.ENGLISH, "%02d:%02d", minutes, seconds)
 }

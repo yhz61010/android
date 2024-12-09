a8fd025fa0db3748b2fe30a73c876ed3f02a3fa4
Michael Leo <yhzemail61010@aliyun.com>
2024-12-09 11:01:25 +0800

    1. Update version to 5.12.48 2. Upgrade all dependencies to the latest version.

---
+++ change_git_user.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git config --global --replace-all user.name "Michael Leo"
+git config --global --replace-all user.email "yhzemail61010@aliyun.com"
+
+git config --local --replace-all user.name "Michael Leo"
+git config --local --replace-all user.email "yhzemail61010@aliyun.com"

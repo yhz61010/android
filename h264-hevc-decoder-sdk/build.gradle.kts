be1fa585a1a771f34040124b744b07b1ac5b43d6
Michael Leo <yhzemail61010@aliyun.com>
2025-05-27 17:15:09 +0800

    1. Upgrade version to 5.12.60 2. Refactor "publishing" for build.gradle.kts

--- h264-hevc-decoder-sdk/build.gradle.kts
+++ h264-hevc-decoder-sdk/build.gradle.kts
@@ -45,9 +45,7 @@ android {
     publishing {
         // Publishes "release" build variant with "release" component created by
         // Android Gradle plugin
-        singleVariant("release") {
-            withSourcesJar()
-        }
+        singleVariant("release")
     }
 }
 

c50412dc42ed4842b96f4fe0beedf5a0b0979042
Michael Leo <yhzemail61010@aliyun.com>
2025-05-27 15:21:24 +0800

    1. Upgrade version to 5.12.57 2. Refactor "publishing" for build.gradle.kts

--- adpcm-ima-qt-codec-sdk/build.gradle.kts
+++ adpcm-ima-qt-codec-sdk/build.gradle.kts
@@ -41,11 +41,13 @@ android {
         }
     }
 
-// publishing {
-//         // Publishes "release" build variant with "release" component created by
-//         // Android Gradle plugin
-//         singleVariant("release")
-//     }
+    publishing {
+        // Publishes "release" build variant with "release" component created by
+        // Android Gradle plugin
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {

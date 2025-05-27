4e7ca72d9188fc24f1bc5ebb7853a3c93fd1b810
Michael Leo <yhzemail61010@aliyun.com>
2025-05-27 13:11:07 +0800

    1. Upgrade version to 5.12.54 2. Refactor "publishing" for build.gradle.kts

--- screencapture-sdk/build.gradle.kts
+++ screencapture-sdk/build.gradle.kts
@@ -39,17 +39,18 @@ val sourceJar by tasks.registering(Jar::class) {
 //     archiveClassifier.set("sources")
 // }
 
-afterEvaluate {
-    publishing {
-        publications {
-            // Creates a Maven publication called "release".
-            create<MavenPublication>("release") {
-                from(components["release"])
-                groupId = "${rootProject.group}"
-                artifactId = "screencapture"
-                version = libs.versions.leo.version.get()
+publishing {
+    publications {
+        val customGroup: String by rootProject.extra
+        // Creates a Maven publication called "release".
+        create<MavenPublication>("release") {
+            groupId = customGroup
+            artifactId = "screencapture"
+            version = libs.versions.leo.version.get()
 
-                artifact(sourceJar.get())
+            artifact(sourceJar.get())
+            afterEvaluate {
+                from(components["release"])
             }
         }
     }

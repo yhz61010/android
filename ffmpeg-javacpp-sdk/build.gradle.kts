353bcadedced856b0ad710f7ccf9c89ff59a5666
Michael Leo <yhzemail61010@aliyun.com>
2025-05-27 15:37:23 +0800

    1. Upgrade version to 5.12.57 2. Refactor "publishing" for build.gradle.kts

--- ffmpeg-javacpp-sdk/build.gradle.kts
+++ ffmpeg-javacpp-sdk/build.gradle.kts
@@ -99,10 +99,10 @@ val sourceJar by tasks.registering(Jar::class) {
 
 publishing {
     publications {
-        val customGroup: String by rootProject.extra
+        val mavenGroupId: String by rootProject.extra
         // Creates a Maven publication called "release".
         create<MavenPublication>("release") {
-            groupId = customGroup
+            groupId = mavenGroupId
             artifactId = "ffmpeg-javacpp"
             version = libs.versions.leo.version.get()
 

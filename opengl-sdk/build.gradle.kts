c9443bf31a1c989c815169ce7bc0e41b9d2d433f
Michael Leo <yhzemail61010@aliyun.com>
2025-05-27 17:15:09 +0800

    1. Upgrade version to 5.12.60 2. Refactor "publishing" for build.gradle.kts

--- opengl-sdk/build.gradle.kts
+++ opengl-sdk/build.gradle.kts
@@ -12,9 +12,7 @@ android {
     publishing {
         // Publishes "release" build variant with "release" component created by
         // Android Gradle plugin
-        singleVariant("release") {
-            withSourcesJar()
-        }
+        singleVariant("release")
     }
 }
 

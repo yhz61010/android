3c9875e2e29286e309770d85ecf0281dd7c917a0
Michael Leo <yhzemail61010@aliyun.com>
2025-06-03 09:05:09 +0800

    1. Upgrade version to 5.12.63 2. Refactor "publishing" for build.gradle.kts

--- camera2live/build.gradle.kts
+++ camera2live/build.gradle.kts
@@ -7,7 +7,13 @@ plugins {
 }
 
 android {
-    namespace = "com.leovp.opengl"
+    namespace = "com.leovp.camera2live"
+
+    // https://medium.com/androiddevelopers/5-ways-to-prepare-your-app-build-for-android-studio-flamingo-release-da34616bb946
+    buildFeatures {
+        // Generate BuildConfig.java file
+        buildConfig = true
+    }
 
     publishing {
         // Publishes "release" build variant with "release" component created by
@@ -17,11 +23,22 @@ android {
 }
 
 dependencies {
-    api(libs.androidx.annotation)
+    implementation(fileTree(mapOf("dir" to "libs", "includes" to listOf("*.jar"))))
 
-    implementation(projects.logSdk)
-    implementation(projects.libCommonAndroid)
-    implementation(projects.libCommonKotlin)
+    api(libs.androidx.appcompat)
+
+    api(projects.androidbase)
+    api(projects.yuv)
+    api(projects.libExif)
+
+    // ===== The following dependencies are needed in [androidbase-sdk] module =====
+    //    api project(':log-sdk')
+    //    api project(':lib-json')
+    //    api project(':lib-bytes')
+    //    api project(':lib-image')
+    //    api project(':lib-common-android')
+    //    api project(':lib-common-kotlin')
+    // ==============================
 }
 
 /** When use it: sourceJar.get() */
@@ -44,7 +61,7 @@ afterEvaluate {
             // name: Module name
             create<MavenPublication>("release") {
                 groupId = mavenGroupId
-                artifactId = "opengl"
+                artifactId = "camera2live"
                 version = libs.versions.leo.version.get()
 
                 artifact(sourceJar.get())

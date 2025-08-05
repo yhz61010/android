953920f2c52eab6abcc6245a5fb0222c7deed496
Michael Leo <yhzemail61010@aliyun.com>
2025-08-05 09:58:59 +0800

    1. Upgrade Gradle to 8.14.3 2. Upgrade all dependencies to the latest version. 3. Replace XXPermissions `Permission.*` with `PermissionLists.*`. 4. Add new module: `lib-mvvm`.

--- camerax/src/main/kotlin/com/leovp/camerax/fragments/PermissionsFragment.kt
+++ camerax/src/main/kotlin/com/leovp/camerax/fragments/PermissionsFragment.kt
@@ -10,6 +10,8 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.Navigation
 import com.hjq.permissions.OnPermissionCallback
 import com.hjq.permissions.XXPermissions
+import com.hjq.permissions.permission.PermissionLists
+import com.hjq.permissions.permission.base.IPermission
 import com.leovp.camerax.R
 import kotlinx.coroutines.launch
 
@@ -20,24 +22,25 @@ import kotlinx.coroutines.launch
 class PermissionsFragment : Fragment() {
 
     companion object {
-        val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA)
+//        val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA)
+        val PERMISSIONS_REQUIRED = PermissionLists.getCameraPermission()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (XXPermissions.isGranted(requireContext(), PERMISSIONS_REQUIRED)) {
+        if (XXPermissions.isGrantedPermission(requireContext(), PERMISSIONS_REQUIRED)) {
             navigateToCamera()
         } else {
             XXPermissions.with(this)
                 .permission(PERMISSIONS_REQUIRED)
                 .request(object : OnPermissionCallback {
-                    override fun onGranted(granted: MutableList<String>, all: Boolean) {
+                    override fun onGranted(granted: MutableList<IPermission>, all: Boolean) {
                         // Take the user to the success fragment when permission is granted
                         Toast.makeText(context, "Permission request granted", Toast.LENGTH_LONG).show()
                         navigateToCamera()
                     }
 
-                    override fun onDenied(denied: MutableList<String>, never: Boolean) {
+                    override fun onDenied(denied: MutableList<IPermission>, never: Boolean) {
                         Toast.makeText(context, "Permission request denied", Toast.LENGTH_LONG).show()
                     }
                 })

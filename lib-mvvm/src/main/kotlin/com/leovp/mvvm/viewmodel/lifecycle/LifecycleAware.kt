e30a7771092acfc28734f3297b4cbd039db5d773
Michael Leo <yhzemail61010@aliyun.com>
2025-08-21 13:43:21 +0800

    1. Update version to 5.12.84 2. Remove some specific files in lib-mvvm module.

--- lib-mvvm/src/main/kotlin/com/leovp/mvvm/viewmodel/lifecycle/LifecycleAware.kt
+++ lib-mvvm/src/main/kotlin/com/leovp/mvvm/viewmodel/lifecycle/LifecycleAware.kt
@@ -1,5 +1,8 @@
 package com.leovp.mvvm.viewmodel.lifecycle
 
+import com.leovp.log.base.LogOutType
+import com.leovp.log.base.w
+
 /**
  * Author: Michael Leo
  * Date: 2025/4/7 10:09
@@ -10,4 +13,55 @@ interface LifecycleAware {
     fun onStart() {}
     fun onStop() {}
     fun onDestroy() {}
+}
+
+open class SimpleLifecycleAware : LifecycleAware {
+    companion object {
+        private const val TAG = "BaseLifecycle"
+    }
+
+    override fun onResume() {
+        super.onResume()
+        w {
+            tag = TAG
+            message = "onResume()"
+            outputType = LogOutType.FRAMEWORK
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        w {
+            tag = TAG
+            message = "onPause()"
+            outputType = LogOutType.FRAMEWORK
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        w {
+            tag = TAG
+            message = "onStart()"
+            outputType = LogOutType.FRAMEWORK
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        w {
+            tag = TAG
+            message = "onStop()"
+            outputType = LogOutType.FRAMEWORK
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        w {
+            tag = TAG
+            message = "onDestroy()"
+            outputType = LogOutType.FRAMEWORK
+        }
+    }
 }

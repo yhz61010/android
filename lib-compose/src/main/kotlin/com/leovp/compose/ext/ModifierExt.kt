ec151f913f362740fcf0f68f079f96489a8eb501
Michael Leo <yhzemail61010@aliyun.com>
2025-09-03 09:16:25 +0800

    1. Update version to 5.12.89 2. Add more extension utils.

--- lib-compose/src/main/kotlin/com/leovp/compose/ext/ModifierExt.kt
+++ lib-compose/src/main/kotlin/com/leovp/compose/ext/ModifierExt.kt
@@ -4,7 +4,11 @@ package com.leovp.compose.ext
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawBehind
@@ -53,3 +57,27 @@ fun Modifier.bottomBorder(color: Color, strokeWidth: Dp = 1.dp) = composed(
         }
     }
 )
+
+/**
+ * Usage:
+ * ```
+ * Box(
+ *     modifier = Modifier.debounceClickable(debounceTime = 1000L) { },
+ * ) { }
+ * ```
+ */
+@Composable
+fun Modifier.debounceClickable(
+    debounceTime: Long = 1000L,
+    onClick: () -> Unit,
+): Modifier {
+    var lastClickTime by remember { mutableLongStateOf(0L) }
+
+    return this.clickable {
+        val currentTime = System.currentTimeMillis()
+        if (currentTime - lastClickTime > debounceTime) {
+            lastClickTime = currentTime
+            onClick()
+        }
+    }
+}

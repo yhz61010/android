034ee2b71a3e96fbf3943f1cf46c33469fbd604a
Michael Leo <yhzemail61010@aliyun.com>
2025-09-05 16:09:52 +0800

    1. Update version to 5.12.93 2. Replace deprecated api.

--- androidbase/src/main/kotlin/com/leovp/androidbase/utils/system/KeepAlive.kt
+++ androidbase/src/main/kotlin/com/leovp/androidbase/utils/system/KeepAlive.kt
@@ -6,14 +6,20 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.media.MediaPlayer
 import android.os.PowerManager
 import android.os.SystemClock
 import androidx.annotation.RawRes
 import androidx.core.content.ContextCompat
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.leovp.log.LogContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 /**
  * You must assign `app` which means the application context first.
@@ -34,25 +40,19 @@ import com.leovp.log.LogContext
  */
 class KeepAlive(
     private val app: Application,
-    @RawRes private val undeadAudioResId: Int,
+    @param:RawRes private val undeadAudioResId: Int,
     private val keepAliveTimeInMin: Float = 25f,
     val callback: () -> Unit,
 ) {
     companion object {
         private const val TAG = "KA"
-        private const val INTENT_KEEP_ALIVE_RECEIVER = "intent.com.leovp.receiver.keep.alive"
 //        private const val SAMPLE_RATE = 44100
 //        private const val CHANNEL_CONFIG = AudioFormat.CHANNEL_OUT_MONO
 //        private const val AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT
     }
 
     private var mediaPlayer: MediaPlayer? = null
-    private val keepAliveArgumentReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            LogContext.log.d(TAG, "KeepAliveArgumentReceiver()")
-            callback()
-        }
-    }
+    private var job: Job? = null
 
     fun start() {
         release()
@@ -65,9 +65,6 @@ class KeepAlive(
                 start()
             }
         }.onFailure { it.printStackTrace() }
-
-        LocalBroadcastManager.getInstance(app)
-            .registerReceiver(keepAliveArgumentReceiver, IntentFilter(INTENT_KEEP_ALIVE_RECEIVER))
 
         val aliveTimeInMs: Long = (keepAliveTimeInMin * 60 * 1000).toLong()
         val alarmManager: AlarmManager = ContextCompat.getSystemService(app, AlarmManager::class.java)!!
@@ -85,94 +82,38 @@ class KeepAlive(
             // }
         )
         alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pendingIntent)
+
+        job = CoroutineScope(Dispatchers.Main).launch {
+            KeepAliveBus.events.collect {
+                LogContext.log.d(TAG, "KeepAlive event received.")
+                callback()
+            }
+        }
     }
 
     fun release() {
         LogContext.log.w(TAG, "Release keepAlive()")
-        runCatching {
-            LocalBroadcastManager.getInstance(app).unregisterReceiver(keepAliveArgumentReceiver)
-        }.onFailure { it.printStackTrace() }
         runCatching { mediaPlayer?.run { release() } }.onFailure { it.printStackTrace() }
+        job?.cancel()
+        job = null
     }
 
     internal class KeepAliveReceiver : BroadcastReceiver() {
+        @OptIn(DelicateCoroutinesApi::class)
         override fun onReceive(context: Context, intent: Intent) {
             LogContext.log.d(TAG, "KeepAliveReceiver")
-            LocalBroadcastManager.getInstance(context).sendBroadcast(Intent(INTENT_KEEP_ALIVE_RECEIVER))
+            GlobalScope.launch {
+                KeepAliveBus.sendAliveEvent()
+            }
         }
     }
+}
 
-//    private val ioScope = CoroutineScope(Dispatchers.IO)
-//    private lateinit var inputStream: InputStream
-//    private lateinit var musicBytes: ByteArray
-//
-//    private lateinit var audioAttributes: AudioAttributes
-//    private var audioTrack: AudioTrack? = null
-//    private lateinit var audioFormat: AudioFormat
-//    private var bufferSize = 0
-//    private var sessionId = 0
-//
-//    fun keepAlive() {
-//        LogContext.log.w(TAG, "Start keepAlive()")
-//        ioScope.launch {
-//            runCatching {
-//                inputStream = context.resources.openRawResource(R.raw.magic)
-//                musicBytes = inputStream.readBytes()
-//                init()
-//                while (true) {
-//                    ensureActive()
-//                    if (LogContext.log.DEBUG_MODE) {
-//                        LogContext.log.d(TAG, "Keep alive play()")
-//                    }
-//                    play()
-//                }
-//            }.onFailure { it.printStackTrace() }
-//        }
-//    }
-//
-//    fun release() {
-//        LogContext.log.w(TAG, "Release keepAlive()")
-//        ioScope.launch { stop() }
-//        ioScope.cancel()
-//    }
-//
-//    private fun init() {
-//        val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-//        bufferSize = AudioTrack.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT)
-//        sessionId = audioManager.generateAudioSessionId()
-//        audioAttributes = AudioAttributes.Builder().apply {
-//            setUsage(AudioAttributes.USAGE_MEDIA)              // AudioAttributes.USAGE_MEDIA         AudioAttributes.USAGE_VOICE_COMMUNICATION
-//            setContentType(AudioAttributes.CONTENT_TYPE_MUSIC) // AudioAttributes.CONTENT_TYPE_MUSIC   AudioAttributes.CONTENT_TYPE_SPEECH
-//            setLegacyStreamType(AudioManager.STREAM_MUSIC)
-//        }.build()
-//        audioFormat = AudioFormat.Builder().setSampleRate(SAMPLE_RATE)
-//            .setEncoding(AUDIO_FORMAT)
-//            .setChannelMask(CHANNEL_CONFIG)
-//            .build()
-//        audioTrack = AudioTrack(audioAttributes, audioFormat, bufferSize, AudioTrack.MODE_STREAM, sessionId)
-//        audioTrack?.setVolume(0.001F)
-//        audioTrack?.play()
-//    }
-//
-//    private fun play() {
-//        runCatching { audioTrack?.write(musicBytes, 0, musicBytes.size) }.onFailure { it.printStackTrace() }
-//    }
-//
-//    private fun pause() {
-//        runCatching {
-//            audioTrack?.pause()
-//            audioTrack?.flush()
-//        }.onFailure { it.printStackTrace() }
-//    }
-//
-//    private fun stop() {
-//        pause()
-//        runCatching {
-//            audioTrack?.stop()
-//        }.onFailure { it.printStackTrace() }
-//        runCatching {
-//            audioTrack?.release()
-//        }.onFailure { it.printStackTrace() }
-//        audioTrack = null
-//    }
+object KeepAliveBus {
+    private val _events = MutableSharedFlow<Unit>()
+    val events = _events.asSharedFlow()
+
+    suspend fun sendAliveEvent() {
+        _events.emit(Unit)
+    }
 }

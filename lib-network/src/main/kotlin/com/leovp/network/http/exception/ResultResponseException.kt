8f9704e9f0209396f938f489f6a3395524a67795
Michael Leo <yhzemail61010@aliyun.com>
2025-11-26 10:26:06 +0800

    1. Update version to 5.13.1 2. Improve lib-network -> network -> exception

--- lib-network/src/main/kotlin/com/leovp/network/http/exception/ResultResponseException.kt
+++ lib-network/src/main/kotlin/com/leovp/network/http/exception/ResultResponseException.kt
@@ -1,14 +1,13 @@
 package com.leovp.network.http.exception
 
-import okhttp3.Response
-
 /**
  * Author: Michael Leo
  * Date: 2025/8/14 13:26
  */
 open class ResultResponseException(
+    val statusCode: Int? = null,
     message: String? = null,
     cause: Throwable? = null,
-    val response: Response,
+    val responseBodyString: String? = null,
     tag: Any? = null,
 ) : ResultException(message = message, cause = cause, tag = tag)

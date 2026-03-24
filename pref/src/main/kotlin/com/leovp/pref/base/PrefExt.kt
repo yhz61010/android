ac89d250d377f70b345a62770ce25c5048f5d679
Michael Leo <yhzemail61010@aliyun.com>
2026-03-24 13:35:37 +0800

    1. Refactor `AbsPref` and `PrefExt` to support non-nullable `String` types in generic `prefPut` and `prefGet` functions. 2. Rename `prefPutStr` to `prefPutNullableStr` and update related documentation. 3. Remove restriction preventing `String` types from being used in generic `get` methods within `AbsPref`. 4. Update unit tests to accommodate changes in null handling and object serialization.

--- pref/src/main/kotlin/com/leovp/pref/base/PrefExt.kt
+++ pref/src/main/kotlin/com/leovp/pref/base/PrefExt.kt
@@ -9,40 +9,37 @@ import com.leovp.pref.PrefContext.pref
  * Date: 2025/4/11 09:03
  */
 
-fun prefPutStr(key: String, value: String?) = pref.put(key, value)
-
 /**
- * Put value which type is following list:
+ * Put none nullable value which type is following list:
  * - Int
  * - Long
  * - Boolean
  * - Float
+ * - String
  * - Object except Set
  */
 inline fun <reified T : Any> prefPut(key: String, value: T) = pref.put(key, value)
 
+fun prefPutNullableStr(key: String, value: String?) = pref.put(key, value)
+
 fun prefPutSet(key: String, value: Set<String>?) = pref.putSet(key, value)
 
-// ----------
+// ------------------------------
 
 /**
- * Get non-nullable string value which type is String.
+ * Get none nullable value which type is following list:
+ * - Int
+ * - Long
+ * - Boolean
+ * - Float
+ * - String
  */
-fun prefGetStr(key: String, default: String): String = pref.getString(key, default) ?: default
+inline fun <reified T : Any> prefGet(key: String, default: T): T = pref.get(key, default)
 
 /**
  * Get nullable string value which type is String.
  */
 fun prefGetNullableStr(key: String, default: String? = null): String? = pref.getString(key, default)
-
-/**
- * Get value which type is following list:
- * - Int
- * - Long
- * - Boolean
- * - Float
- */
-inline fun <reified T> prefGet(key: String, default: T): T = pref.get(key, default)
 
 fun prefGetSet(key: String, default: Set<String>? = null): Set<String>? = pref.getStringSet(key, default)
 

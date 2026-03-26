a1f56a623f98ea3ab895224b642cb0ae53b637e2
Michael Leo <yhzemail61010@aliyun.com>
2026-03-26 13:11:36 +0800

    1. Update version to 5.13.27 2. Set maximum page size to 16KB in `Android.mk` and `CMakeLists.txt` for Android compatibility. 3. Rename destination paths in NDK build scripts to remove `-sdk` suffix. 4. Update prebuilt shared libraries for various architectures. 5. Minor documentation improvements in `README.md`.

--- ffmpeg-sdk/src/main/ffmpeg_build/ndk-build-and-copy-to-module_ffmpeg_h264_hevc_decoder.sh
+++ ffmpeg-sdk/src/main/ffmpeg_build/ndk-build-and-copy-to-module_ffmpeg_h264_hevc_decoder.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-# Delete existed so files in [h264-hevc-decoder-sdk] module.
-rm -rf   ../../../../h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
-rm -rf   ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
-rm -rf   ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
-rm -rf   ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
+# Delete existed so files in [h264-hevc-decoder] module.
+rm -rf   ../../../../h264-hevc-decoder/src/main/libs/armeabi-v7a/
+rm -rf   ../../../../h264-hevc-decoder/src/main/libs/arm64-v8a/
+rm -rf   ../../../../h264-hevc-decoder/src/main/libs/x86/
+rm -rf   ../../../../h264-hevc-decoder/src/main/libs/x86_64/
 
-mkdir -p ../../../../h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
-mkdir -p ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
-mkdir -p ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
-mkdir -p ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
+mkdir -p ../../../../h264-hevc-decoder/src/main/libs/armeabi-v7a/
+mkdir -p ../../../../h264-hevc-decoder/src/main/libs/arm64-v8a/
+mkdir -p ../../../../h264-hevc-decoder/src/main/libs/x86/
+mkdir -p ../../../../h264-hevc-decoder/src/main/libs/x86_64/
 
 # NDK build
 ndk-build clean
 ndk-build
 
-# Copy new so files to [adpcm-ima-qt-codec-h264-hevc-decoder-sdk] module.
-cp ../libs/armeabi-v7a/libh264-hevc-decoder.so ../../../../h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
-cp ../libs/armeabi-v7a/libavcodec.so           ../../../../h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
-cp ../libs/armeabi-v7a/libavutil.so            ../../../../h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
-cp ../libs/armeabi-v7a/libswscale.so           ../../../../h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
-cp ../libs/armeabi-v7a/libc++_shared.so        ../../../../h264-hevc-decoder-sdk/src/main/libs/armeabi-v7a/
+# Copy new so files to [h264-hevc-decoder] module.
+cp ../libs/armeabi-v7a/libh264-hevc-decoder.so ../../../../h264-hevc-decoder/src/main/libs/armeabi-v7a/
+cp ../libs/armeabi-v7a/libavcodec.so           ../../../../h264-hevc-decoder/src/main/libs/armeabi-v7a/
+cp ../libs/armeabi-v7a/libavutil.so            ../../../../h264-hevc-decoder/src/main/libs/armeabi-v7a/
+cp ../libs/armeabi-v7a/libswscale.so           ../../../../h264-hevc-decoder/src/main/libs/armeabi-v7a/
+cp ../libs/armeabi-v7a/libc++_shared.so        ../../../../h264-hevc-decoder/src/main/libs/armeabi-v7a/
 
-cp ../libs/arm64-v8a/libh264-hevc-decoder.so   ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
-cp ../libs/arm64-v8a/libavcodec.so             ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
-cp ../libs/arm64-v8a/libavutil.so              ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
-cp ../libs/arm64-v8a/libswscale.so             ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
-cp ../libs/arm64-v8a/libc++_shared.so          ../../../../h264-hevc-decoder-sdk/src/main/libs/arm64-v8a/
+cp ../libs/arm64-v8a/libh264-hevc-decoder.so   ../../../../h264-hevc-decoder/src/main/libs/arm64-v8a/
+cp ../libs/arm64-v8a/libavcodec.so             ../../../../h264-hevc-decoder/src/main/libs/arm64-v8a/
+cp ../libs/arm64-v8a/libavutil.so              ../../../../h264-hevc-decoder/src/main/libs/arm64-v8a/
+cp ../libs/arm64-v8a/libswscale.so             ../../../../h264-hevc-decoder/src/main/libs/arm64-v8a/
+cp ../libs/arm64-v8a/libc++_shared.so          ../../../../h264-hevc-decoder/src/main/libs/arm64-v8a/
 
-cp ../libs/x86/libh264-hevc-decoder.so          ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
-cp ../libs/x86/libavcodec.so                    ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
-cp ../libs/x86/libavutil.so                     ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
-cp ../libs/x86/libswscale.so                    ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
-cp ../libs/x86/libc++_shared.so                 ../../../../h264-hevc-decoder-sdk/src/main/libs/x86/
+cp ../libs/x86/libh264-hevc-decoder.so          ../../../../h264-hevc-decoder/src/main/libs/x86/
+cp ../libs/x86/libavcodec.so                    ../../../../h264-hevc-decoder/src/main/libs/x86/
+cp ../libs/x86/libavutil.so                     ../../../../h264-hevc-decoder/src/main/libs/x86/
+cp ../libs/x86/libswscale.so                    ../../../../h264-hevc-decoder/src/main/libs/x86/
+cp ../libs/x86/libc++_shared.so                 ../../../../h264-hevc-decoder/src/main/libs/x86/
 
-cp ../libs/x86_64/libh264-hevc-decoder.so   ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
-cp ../libs/x86_64/libavcodec.so             ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
-cp ../libs/x86_64/libavutil.so              ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
-cp ../libs/x86_64/libswscale.so             ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
-cp ../libs/x86_64/libc++_shared.so          ../../../../h264-hevc-decoder-sdk/src/main/libs/x86_64/
+cp ../libs/x86_64/libh264-hevc-decoder.so   ../../../../h264-hevc-decoder/src/main/libs/x86_64/
+cp ../libs/x86_64/libavcodec.so             ../../../../h264-hevc-decoder/src/main/libs/x86_64/
+cp ../libs/x86_64/libavutil.so              ../../../../h264-hevc-decoder/src/main/libs/x86_64/
+cp ../libs/x86_64/libswscale.so             ../../../../h264-hevc-decoder/src/main/libs/x86_64/
+cp ../libs/x86_64/libc++_shared.so          ../../../../h264-hevc-decoder/src/main/libs/x86_64/
--- x264/src/main/jni/Android.mk
+++ x264/src/main/jni/Android.mk
@@ -5,6 +5,10 @@ MY_PREBUILT := $(LOCAL_PATH)/prebuilt/$(TARGET_ARCH_ABI)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libx264
 LOCAL_SRC_FILES := $(MY_PREBUILT)/lib/$(LOCAL_MODULE).so
+# 告诉 linker 将最大页面大小设置为 16KB（16384 字节）
+# 这确保了生成的 .so 文件的 LOAD section 是 16KB 对齐的，符合 Android 的要求
+# -Wl,--max-page-size=16384 这个参数在 NDK r25+ 中不被直接支持
+LOCAL_LDFLAGS += -Wl,-z,max-page-size=16384
 include $(PREBUILT_SHARED_LIBRARY)
 #include $(PREBUILT_STATIC_LIBRARY)
 
